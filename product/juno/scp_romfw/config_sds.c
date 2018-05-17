e90f46f24eec08e403853d03d09750308af789eb
Pedro Custodio <pedro.krewinkelcustodio@arm.com>
2018-05-17 14:41:05 +0100

    juno/romfw: Add SDS support
    Change-Id: Id0dbb755ab67dce1190166887ada30fff4a9dba5
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/juno/scp_romfw/config_sds.c
@@ -0,0 +1,170 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <assert.h>
+#include <stdint.h>
+#include <fwk_assert.h>
+#include <fwk_element.h>
+#include <fwk_macros.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+#include <mod_sds.h>
+#include <juno_id.h>
+#include <juno_mmap.h>
+#include <juno_scc.h>
+#include <juno_sds.h>
+#include <juno_ssc.h>
+#include <scp_config.h>
+#include <software_mmap.h>
+#include <system_mmap.h>
+
+static const uint32_t version_packed = FWK_BUILD_VERSION;
+static struct juno_sds_platid platid;
+static uint32_t reset_syndrome;
+static uint32_t primary_cpu;
+
+static const struct fwk_element element_table[] = {
+    {
+        .name = "CPU Info",
+        .data = &(struct mod_sds_structure_desc) {
+            .id = JUNO_SDS_CPU_INFO,
+            .size = JUNO_SDS_CPU_INFO_SIZE,
+            .payload = &primary_cpu,
+            .finalize = true,
+        },
+    },
+
+    {
+        .name = "ROM Version",
+        .data = &(struct mod_sds_structure_desc) {
+            .id = JUNO_SDS_ROM_VERSION,
+            .size = JUNO_SDS_ROM_VERSION_SIZE,
+            .payload = &version_packed,
+            .finalize = true,
+        },
+    },
+
+    {
+        .name = "Platform ID",
+        .data = &(struct mod_sds_structure_desc) {
+            .id = JUNO_SDS_PLATFORM_ID,
+            .size = JUNO_SDS_PLATFORM_ID_SIZE,
+            .payload = &platid,
+            .finalize = true,
+        },
+    },
+
+    {
+        .name = "Reset Syndrome",
+        .data = &(struct mod_sds_structure_desc) {
+            .id = JUNO_SDS_RESET_SYNDROME,
+            .size = JUNO_SDS_RESET_SYNDROME_SIZE,
+            .payload = &reset_syndrome,
+            .finalize = true,
+        },
+    },
+
+    {
+        .name = "Bootloader",
+        .data = &(struct mod_sds_structure_desc) {
+            .id = JUNO_SDS_BOOTLOADER,
+            .size = JUNO_SDS_BOOTLOADER_SIZE,
+            .finalize = true,
+        },
+    },
+
+    {
+        .name = "Features",
+        .data = &(struct mod_sds_structure_desc) {
+            .id = JUNO_SDS_FEATURE_AVAILABILITY,
+            .size = JUNO_SDS_FEATURE_AVAILABILITY_SIZE,
+            .finalize = true,
+        },
+    },
+
+    #ifdef BUILD_MODE_DEBUG
+    {
+        .name = "Boot Counters",
+        .data = &(struct mod_sds_structure_desc) {
+            .id = JUNO_SDS_CPU_BOOTCTR,
+            .size = JUNO_SDS_CPU_BOOTCTR_SIZE,
+            .finalize = true,
+        },
+    },
+
+    {
+        .name = "CPU Flags",
+        .data = &(struct mod_sds_structure_desc) {
+            .id = JUNO_SDS_CPU_FLAGS,
+            .size = JUNO_SDS_CPU_FLAGS_SIZE,
+            .finalize = true,
+        },
+    },
+    #endif
+
+    { 0 }, /* Termination description */
+};
+
+static const struct fwk_element *get_element_table(fwk_id_t module_id)
+{
+    int status;
+    enum juno_idx_platform platform_id = JUNO_IDX_PLATFORM_COUNT;
+
+    static_assert(BUILD_VERSION_MAJOR < UINT8_MAX, "Invalid version size");
+    static_assert(BUILD_VERSION_MINOR < UINT8_MAX, "Invalid version size");
+    static_assert(BUILD_VERSION_PATCH < UINT16_MAX, "Invalid version size");
+
+    platid.platform_identifier = SSC->SSC_VERSION;
+
+    status = juno_id_get_platform(&platform_id);
+    if (!fwk_expect(status == FWK_SUCCESS))
+        return NULL;
+    platid.platform_type_identifier = (uint32_t)platform_id;
+
+    /*
+     * Make the reset syndrome available to the AP.
+     *
+     * On some platforms this is simply a case of copying from the
+     * RESET_SYNDROME register but on Juno this is complicated by a hardware
+     * defect which prevents the WDOGRESET_SCP and WDOGRESET_SYS bits [2:1] from
+     * being set correctly when a watchdog reset occurs. To work around this
+     * defect, the MCC on Juno is able to report whether the reset was caused by
+     * a watchdog event using the RESET field in the GPR1.
+     *
+     * If the reset was caused by any of the watchdogs, the SCP firmware will
+     * set the relevant bit (using the RESET_SYNDROME register layout) when
+     * sharing the RESET_SYNDROME value with Trusted Firmware.
+     */
+
+    /* Start with the original reset syndrome */
+    reset_syndrome = SCP_CONFIG->RESET_SYNDROME;
+
+    /*
+     * Adjust values in case MCC is reporting the reset was caused by one of the
+     * watchdogs.
+     */
+    if ((SCC->GPR1 & SCC_GPR1_RESET) == SCC_GPR1_RESET_WDOG) {
+        reset_syndrome &= ~SCP_CONFIG_RESET_SYNDROME_PORESET;
+        reset_syndrome |= SCP_CONFIG_RESET_SYNDROME_WDOGRESET;
+    }
+
+    if (SCC->GPR1 & SCC_GPR1_BOOT_MAP_ENABLE) {
+        primary_cpu =
+            (SCC->GPR1 & SCC_GPR1_PRIMARY_CPU) >> SCC_GPR1_PRIMARY_CPU_POS;
+    }
+
+    return element_table;
+}
+
+struct fwk_module_config config_sds = {
+    .get_element_table = get_element_table,
+    .data = &(struct mod_sds_config) {
+        .region_base_address = SDS_SHARED_MEM_BASE,
+        .region_size = SDS_SHARED_MEM_SIZE,
+        .clock_id = FWK_ID_NONE_INIT,
+    },
+};
