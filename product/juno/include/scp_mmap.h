a0ec1b606369e08e9ee1eeb6964f8218c550d19a
Pedro Custodio <pedro.krewinkelcustodio@arm.com>
2018-05-17 11:23:59 +0100

    juno: Add memory maps
     - Product memory map
     - SoC-level memory map
     - Software-defined memory map
     - SCP linker script memory map
    
    Change-Id: I8a17daf9044c03c3b0fed41d58091770d604ae25
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/juno/include/scp_mmap.h
@@ -0,0 +1,22 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SCP_MMAP_H
+#define SCP_MMAP_H
+
+#define SCP_ROM_SIZE  (64 * 1024)
+#define SCP_RAM_SIZE  (128 * 1024)
+
+#if SCP_ROM_BYPASS
+#define SCP_ROM_BASE  0xABE40000
+#else
+#define SCP_ROM_BASE  0x00000000
+#endif
+
+#define SCP_RAM_BASE  0x10000000
+
+#endif /* SCP_MMAP_H */
