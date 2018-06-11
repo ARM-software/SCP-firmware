0c67833de137212f16b59f74a0e53c88ea31c17e
Pedro Custodio <pedro.krewinkelcustodio@arm.com>
2018-06-11 15:48:16 +0100

    juno/ramfw: Add SDS config
    Change-Id: Ib5d22f078df4af8020f728fd7be491b6428257eb
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/juno/scp_ramfw/config_sds.c
@@ -0,0 +1,57 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdint.h>
+#include <fwk_assert.h>
+#include <fwk_macros.h>
+#include <fwk_module.h>
+#include <mod_sds.h>
+#include <juno_sds.h>
+#include <software_mmap.h>
+
+static const uint32_t feature_flags = JUNO_SDS_FEATURE_FIRMWARE_MASK;
+static const uint32_t version_packed = FWK_BUILD_VERSION;
+
+static const struct fwk_element element_table[] = {
+    {
+        .name = "RAM Version",
+        .data = &(struct mod_sds_structure_desc) {
+            .id = JUNO_SDS_RAM_VERSION,
+            .size = JUNO_SDS_RAM_VERSION_SIZE,
+            .payload = &version_packed,
+            .finalize = true,
+        },
+    },
+    {
+        .name = "Feature Availability",
+        .data = &(struct mod_sds_structure_desc) {
+            .id = JUNO_SDS_FEATURE_AVAILABILITY,
+            .size = sizeof(feature_flags),
+            .payload = &feature_flags,
+            .finalize = true,
+        },
+    },
+    { 0 },
+};
+
+static const struct fwk_element *get_element_table(fwk_id_t module_id)
+{
+    static_assert(BUILD_VERSION_MAJOR < UINT8_MAX, "Invalid version size");
+    static_assert(BUILD_VERSION_MINOR < UINT8_MAX, "Invalid version size");
+    static_assert(BUILD_VERSION_PATCH < UINT16_MAX, "Invalid version size");
+
+    return element_table;
+}
+
+struct fwk_module_config config_sds = {
+    .get_element_table = get_element_table,
+    .data = &(struct mod_sds_config) {
+        .region_base_address = SDS_SHARED_MEM_BASE,
+        .region_size = SDS_SHARED_MEM_SIZE,
+        .clock_id = FWK_ID_NONE_INIT,
+    },
+};
