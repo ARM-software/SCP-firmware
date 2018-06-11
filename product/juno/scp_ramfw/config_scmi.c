0c0c13aa04a3b70c20ae53aa8eee0ed70f04e592
Pedro Custodio <pedro.krewinkelcustodio@arm.com>
2018-06-11 15:50:22 +0100

    juno/ramfw: Add SCMI config
    Change-Id: I640a2e04c1f3482e6d28b870aad54dc44a6d1a71
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/juno/scp_ramfw/config_scmi.c
@@ -0,0 +1,95 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_macros.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+#include <mod_scmi.h>
+#include <mod_smt.h>
+#include <juno_scmi.h>
+#include <internal/scmi.h>
+
+static const struct fwk_element element_table[] = {
+    [JUNO_SCMI_SERVICE_IDX_PSCI] = {
+        .name = "PSCI",
+        .data = &(struct mod_scmi_service_config) {
+            .transport_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_SMT,
+                JUNO_SCMI_SERVICE_IDX_PSCI),
+            .transport_api_id = FWK_ID_API_INIT(
+                FWK_MODULE_IDX_SMT,
+                MOD_SMT_API_IDX_SCMI_TRANSPORT),
+            .transport_notification_init_id =
+                FWK_ID_NOTIFICATION_INIT(FWK_MODULE_IDX_SMT,
+                    MOD_SMT_NOTIFICATION_IDX_INITIALIZED),
+            .scmi_agent_id = JUNO_SCMI_AGENT_IDX_PSCI,
+        },
+    },
+
+    [JUNO_SCMI_SERVICE_IDX_OSPM_0] = {
+        .name = "OSPM 0",
+        .data = &(struct mod_scmi_service_config) {
+            .transport_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_SMT,
+                JUNO_SCMI_SERVICE_IDX_OSPM_0),
+            .transport_api_id = FWK_ID_API_INIT(
+                FWK_MODULE_IDX_SMT,
+                MOD_SMT_API_IDX_SCMI_TRANSPORT),
+            .transport_notification_init_id =
+                FWK_ID_NOTIFICATION_INIT(FWK_MODULE_IDX_SMT,
+                    MOD_SMT_NOTIFICATION_IDX_INITIALIZED),
+            .scmi_agent_id = JUNO_SCMI_AGENT_IDX_OSPM,
+        },
+    },
+
+    [JUNO_SCMI_SERVICE_IDX_OSPM_1] = {
+        .name = "OSPM 1",
+        .data = &(struct mod_scmi_service_config) {
+            .transport_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_SMT,
+                JUNO_SCMI_SERVICE_IDX_OSPM_1),
+            .transport_api_id = FWK_ID_API_INIT(
+                FWK_MODULE_IDX_SMT,
+                MOD_SMT_API_IDX_SCMI_TRANSPORT),
+            .transport_notification_init_id =
+                FWK_ID_NOTIFICATION_INIT(FWK_MODULE_IDX_SMT,
+                    MOD_SMT_NOTIFICATION_IDX_INITIALIZED),
+            .scmi_agent_id = JUNO_SCMI_AGENT_IDX_OSPM,
+        },
+    },
+
+    [JUNO_SCMI_SERVICE_IDX_COUNT] = { 0 },
+};
+
+static const struct fwk_element *get_element_table(fwk_id_t module_id)
+{
+    return element_table;
+}
+
+static const struct mod_scmi_agent agent_table[] = {
+    [JUNO_SCMI_AGENT_IDX_OSPM] = {
+        .type = SCMI_AGENT_TYPE_OSPM,
+        .name = "OSPM",
+    },
+    [JUNO_SCMI_AGENT_IDX_PSCI] = {
+        .type = SCMI_AGENT_TYPE_PSCI,
+        .name = "PSCI",
+    },
+};
+
+struct fwk_module_config config_scmi = {
+    .get_element_table = get_element_table,
+    .data = &(struct mod_scmi_config) {
+        .protocol_count_max = 2,
+        .agent_count = FWK_ARRAY_SIZE(agent_table) - 1,
+        .agent_table = agent_table,
+        .vendor_identifier = "arm",
+        .sub_vendor_identifier = "arm",
+    },
+};
