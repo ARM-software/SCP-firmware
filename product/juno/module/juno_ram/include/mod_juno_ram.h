0c370fe0c91d1fd0a91d15f06f76f6f944cd4503
Pedro Custodio <pedro.krewinkelcustodio@arm.com>
2018-06-11 14:49:07 +0100

    juno_ram: Add module
    This module configures the following features and peripherals:
    - NIC400 QoS
    - PCSMs
    - PCIe
    - SMC
    
    Change-Id: I1b5f1ff9658dce9c37ac956647071a910ae7d9bc
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/juno/module/juno_ram/include/mod_juno_ram.h
@@ -0,0 +1,35 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MOD_JUNO_RAM_H
+#define MOD_JUNO_RAM_H
+
+#include <fwk_id.h>
+
+/*!
+ * \ingroup GroupJunoModule
+ * \defgroup GroupJunoRAM Juno RAM
+ * \{
+ */
+
+/*!
+ * \brief Module configuration.
+ */
+struct mod_juno_ram_config {
+    /*!
+     * \brief Identifier of the timer.
+     *
+     * \details Used for time-out when configuring the module's peripherals.
+     */
+    fwk_id_t timer_id;
+};
+
+/*!
+ * \}
+ */
+
+#endif /* MOD_JUNO_RAM_H */
