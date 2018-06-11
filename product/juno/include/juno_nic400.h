2e41ee2d1f0ed119ef5016ae603bb8697a710118
Pedro Custodio <pedro.krewinkelcustodio@arm.com>
2018-06-11 14:26:57 +0100

    juno: Add Juno-specific NIC400 definitions
    Change-Id: I41cdab3816d0f9d1dffddd90a7ab7308b4025a95
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/juno/include/juno_nic400.h
@@ -0,0 +1,25 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Juno-specific NIC-400 Network Interconnect register definitions.
+ */
+
+#ifndef JUNO_NIC400_H
+#define JUNO_NIC400_H
+
+#include <fwk_macros.h>
+#include <juno_mmap.h>
+
+struct nic400_reg {
+    FWK_RW  uint32_t  REMAP;
+};
+
+#define NIC400 ((struct nic400_reg *) NIC400_BASE)
+
+#define NIC400_REMAP_TLX_EN UINT32_C(0x00000080)
+
+#endif /* JUNO_NIC400_H */
