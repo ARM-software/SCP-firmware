d581673dccfd621996523a30ece4b8d5aed434e3
Pedro Custodio <pedro.krewinkelcustodio@arm.com>
2018-06-14 17:30:57 +0100

    juno: Add Juno-specific PCIe definitions
    Change-Id: I2c69b75fb5efe7c39a111ff7b10be1edbec1202e
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/juno/include/juno_pcie.h
@@ -0,0 +1,33 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Definitions specific to the Juno PCIe implementation
+ */
+
+#ifndef JUNO_PCIE_H
+#define JUNO_PCIE_H
+
+#define PCI_BRIDGE_REVISION_ID  1
+#define PCI_BRIDGE_CLASS        UINT32_C(0x00060400)
+
+/* Juno-specific memory translation regions */
+#define PCI_ECAM_BASE                  UINT32_C(0x40000000)
+#define PCI_ECAM_SIZE                  UINT32_C(0x10000000)
+#define PCI_IO_BASE                    UINT32_C(0x5F800000)
+#define PCI_IO_SIZE                    UINT32_C(0x00800000)
+#define PCI_MEM32_BASE                 UINT32_C(0x50000000)
+#define PCI_MEM32_SIZE                 UINT32_C(0x08000000)
+#define PCI_MEM64_BASE                 UINT64_C(0x4000000000)
+#define PCI_MEM64_SIZE                 UINT64_C(0x100000000)
+#define GICV2M_MSI_BASE                UINT32_C(0x2C1C0000)
+#define GICV2M_MSI_SIZE                UINT32_C(0x00040000)
+#define JUNO_SYSTEM_MEMORY_BASE        UINT32_C(0x80000000)
+#define JUNO_SYSTEM_MEMORY_SIZE        UINT32_C(0x7F000000)
+#define JUNO_EXTRA_SYSTEM_MEMORY_BASE  UINT64_C(0x0880000000)
+#define JUNO_EXTRA_SYSTEM_MEMORY_SIZE  UINT64_C(0x180000000)
+
+#endif /* JUNO_PCIE_H */
