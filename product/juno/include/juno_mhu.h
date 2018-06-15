b9c2272aa317e47484dfcabe3ecee410ca982264
Pedro Custodio <pedro.krewinkelcustodio@arm.com>
2018-06-15 17:04:14 +0100

    juno: Add SCMI definitions
    Change-Id: Ief08c8c882aefeae71a47d824ae6f89704cbdbd2
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/juno/include/juno_mhu.h
@@ -0,0 +1,21 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Definitions for the MHU module configuration.
+ */
+
+#ifndef JUNO_MHU_H
+#define JUNO_MHU_H
+
+enum juno_mhu_device_idx {
+    JUNO_MHU_DEVICE_IDX_S,
+    JUNO_MHU_DEVICE_IDX_NS_H,
+    JUNO_MHU_DEVICE_IDX_NS_L,
+    JUNO_MHU_DEVICE_IDX_COUNT,
+};
+
+#endif /* JUNO_MHU_H */
---
+++ product/juno/include/juno_scmi.h
@@ -0,0 +1,28 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Definitions for SCMI and SMT module configurations.
+ */
+
+#ifndef JUNO_SCMI_H
+#define JUNO_SCMI_H
+
+enum juno_scmi_agent_idx {
+    /* 0 is reserved for the platform */
+    JUNO_SCMI_AGENT_IDX_OSPM = 1,
+    JUNO_SCMI_AGENT_IDX_PSCI,
+    JUNO_SCMI_AGENT_IDX_COUNT,
+};
+
+enum juno_scmi_service_idx {
+    JUNO_SCMI_SERVICE_IDX_PSCI,
+    JUNO_SCMI_SERVICE_IDX_OSPM_0,
+    JUNO_SCMI_SERVICE_IDX_OSPM_1,
+    JUNO_SCMI_SERVICE_IDX_COUNT,
+};
+
+#endif /* JUNO_SCMI_H */
