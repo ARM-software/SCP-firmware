73f656f28050137f1dea5229b5f9f7ef09d66198
Pedro Custodio <pedro.krewinkelcustodio@arm.com>
2018-06-15 17:00:40 +0100

    juno: Add clock rate definitions
    Change-Id: I0cb548f8db8579013e7ff1cbe35633e7eda308da
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/juno/include/system_clock.h
@@ -0,0 +1,22 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     System clock rate definitions.
+ */
+
+#ifndef SYSTEM_CLOCK_H
+#define SYSTEM_CLOCK_H
+
+#include <fwk_macros.h>
+
+#define CLOCK_RATE_REFCLK   (50UL * FWK_MHZ)
+#define CLOCK_RATE_SYSCLK   (1600UL * FWK_MHZ)
+
+/* Default for Juno SoC */
+#define SYSINCLK            (1600UL * FWK_MHZ)
+
+#endif /* SYSTEM_CLOCK_H */
