a74f82ea739459c17691d715008f480e63923372
Pedro Custodio <pedro.krewinkelcustodio@arm.com>
2018-06-15 17:04:01 +0100

    juno/ramfw: Add MHU config
    Change-Id: I100f936dff458e9acc13fd67d688462e07352cdc
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/juno/scp_ramfw/config_mhu.c
@@ -0,0 +1,55 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_module.h>
+#include <mod_mhu.h>
+#include <juno_irq.h>
+#include <juno_mhu.h>
+#include <system_mmap.h>
+
+static const struct fwk_element element_table[] = {
+    [JUNO_MHU_DEVICE_IDX_S] = {
+        .name = "MHU Secure",
+        .sub_element_count = 1,
+        .data = &(struct mod_mhu_device_config) {
+            .irq = MHU_SECURE_IRQ,
+            .in = MHU_CPU_INTR_S_BASE,
+            .out = MHU_SCP_INTR_S_BASE,
+        },
+    },
+    [JUNO_MHU_DEVICE_IDX_NS_H] = {
+        .name = "MHU Non-Secure High Priority",
+        .sub_element_count = 1,
+        .data = &(struct mod_mhu_device_config) {
+            .irq = MHU_HIGH_PRIO_IRQ,
+            .in = MHU_CPU_INTR_H_BASE,
+            .out = MHU_SCP_INTR_H_BASE,
+        },
+    },
+    [JUNO_MHU_DEVICE_IDX_NS_L] = {
+        .name = "MHU Non-Secure Low Priority",
+        .sub_element_count = 1,
+        .data = &(struct mod_mhu_device_config) {
+            .irq = MHU_LOW_PRIO_IRQ,
+            .in = MHU_CPU_INTR_L_BASE,
+            .out = MHU_SCP_INTR_L_BASE,
+        },
+    },
+    [JUNO_MHU_DEVICE_IDX_COUNT] = { 0 },
+};
+
+static const struct fwk_element *get_element_table(fwk_id_t module_id)
+{
+    return element_table;
+}
+
+struct fwk_module_config config_mhu = {
+    .get_element_table = get_element_table,
+    .data = NULL,
+};
