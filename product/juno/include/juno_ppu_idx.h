6d345cb6cc7d40a65d9aebbd70f4226220313116
Pedro Custodio <pedro.krewinkelcustodio@arm.com>
2018-06-15 16:57:55 +0100

    juno: Add PPU index definitions
    Change-Id: I984c3ac3310c1a07debe0cdb8c09385ffdfe54f8
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/juno/include/juno_ppu_idx.h
@@ -0,0 +1,32 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Juno PPU element indices.
+ */
+
+#ifndef JUNO_PPU_IDX_H
+#define JUNO_PPU_IDX_H
+
+enum juno_ppu_idx {
+    JUNO_PPU_DEV_IDX_BIG_CPU0,
+    JUNO_PPU_DEV_IDX_BIG_CPU1,
+    JUNO_PPU_DEV_IDX_BIG_SSTOP,
+
+    JUNO_PPU_DEV_IDX_LITTLE_CPU0,
+    JUNO_PPU_DEV_IDX_LITTLE_CPU1,
+    JUNO_PPU_DEV_IDX_LITTLE_CPU2,
+    JUNO_PPU_DEV_IDX_LITTLE_CPU3,
+    JUNO_PPU_DEV_IDX_LITTLE_SSTOP,
+
+    JUNO_PPU_DEV_IDX_GPUTOP,
+    JUNO_PPU_DEV_IDX_SYSTOP,
+    JUNO_PPU_DEV_IDX_DBGSYS,
+
+    JUNO_PPU_DEV_IDX_COUNT,
+};
+
+#endif /* JUNO_PPU_IDX_H */
