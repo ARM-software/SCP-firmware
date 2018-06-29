eb662f3db2c19633c0c8ae374413712b6d2142fd
Chris Kay <chris.kay@arm.com>
2018-06-29 12:25:19 +0100

    arch: Simplify handling of the stack and the heap
    The stack/heap separation has been removed, allowing us to treat the
    stack and the heap as a single block of memory. This allows us to grow
    them towards each other rather than giving them both a fixed, non-
    overlapping size.
    
    Change-Id: I537535eeb21c947dc657daaacb75ebf2d9eeb7c5
    Signed-off-by: Chris Kay <chris.kay@arm.com>

--- arch/src/arm_mm.c
+++ arch/src/arm_mm.c
@@ -13,46 +13,16 @@
 #include <fwk_assert.h>
 #include <fwk_errno.h>
 #include <fwk_macros.h>
-#include <section.h>
+
+extern unsigned int __stackheap_start__;
+extern unsigned int __stackheap_size__;
 
 int arm_mm_init(struct fwk_arch_mm_data *data)
 {
-    const unsigned int ALIGNMENT = sizeof(uint64_t);
+    fwk_assert(data != NULL);
 
-    uintptr_t start;
-    uintptr_t end;
-
-    if (data == NULL)
-        return FWK_E_PARAM;
-
-    if (ARCH_SECTION_HEAP_SIZE == 0)
-        return FWK_E_NOMEM;
-
-    /* Check if the main stack section overlaps the .bss section */
-    if (ARCH_SECTION_STACK_START <= ARCH_SECTION_BSS_END) {
-        assert(false);
-        return FWK_E_PANIC;
-    }
-
-    /* Ensure the start address did not overflow following 64-bit alignment */
-    start = FWK_ALIGN_NEXT(ARCH_SECTION_HEAP_START, ALIGNMENT);
-    if (start < ARCH_SECTION_HEAP_START)
-        return FWK_E_NOMEM;
-
-    /* Ensure the end address did not underflow following 64-bit alignment */
-    end = FWK_ALIGN_PREVIOUS(ARCH_SECTION_HEAP_END, ALIGNMENT);
-    if (end > ARCH_SECTION_HEAP_END)
-        return FWK_E_NOMEM;
-
-    /*
-     * Ensure there is space left after performing 64-bit alignment on the start
-     * and end addresses.
-     */
-    if (end <= start)
-        return FWK_E_NOMEM;
-
-    data->start = start;
-    data->size = end - start;
+    data->start = (uintptr_t)(&__stackheap_start__);
+    data->size = (size_t)(&__stackheap_size__);
 
     return FWK_SUCCESS;
 }
