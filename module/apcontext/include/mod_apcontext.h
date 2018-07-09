16e5a3646cd803f86867bc9ab217ec993e889f3b
Filipe Rinaldi <Filipe.Rinaldi@arm.com>
2018-07-09 16:54:52 +0100

    apcontext: New module to zero the AP context area
    Add a new module responsible for zeroing the AP context area as
    required by Arm Trusted Firmware.
    
    Co-authored-by: Elieva Pignat <Elieva.Pignat@arm.com>
    
    Change-Id: Id4e440621d485f844edeead49f3a35ba8569e479
    Signed-off-by: Filipe Rinaldi <Filipe.Rinaldi@arm.com>
    Signed-off-by: Chris Kay <chris.kay@arm.com>

---
+++ module/apcontext/include/mod_apcontext.h
@@ -0,0 +1,43 @@
+
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MOD_APCONTEXT_H
+#define MOD_APCONTEXT_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+/*!
+ * \ingroup GroupModules Modules
+ * \defgroup GroupModuleAPContext AP Context
+ *
+ * \brief Application Processor (AP) context module.
+ *
+ * \details This module implements the AP context zero-initialization.
+ * \{
+ */
+
+/*!
+ * \brief AP context configuration data
+ */
+struct mod_apcontext_config {
+    /*! Base address of the AP context */
+    uintptr_t base;
+
+    /*! Size of the AP context */
+    size_t size;
+
+    /*! Identifier of the clock this module depends on */
+    fwk_id_t clock_id;
+};
+
+/*!
+ * \}
+ */
+
+#endif /* MOD_APCONTEXT_H */
