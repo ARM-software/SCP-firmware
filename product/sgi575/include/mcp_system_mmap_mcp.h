c20be21d950b3d8e40cef4a6b177cf522e0490e3
Amit Daniel Kachhap <amit.kachhap@arm.com>
2018-07-20 14:58:16 +0530

    sgi575: Add System memory map for MCP
    Change-Id: I953bcbfa7e0ec5a5478f5377a2f273c4ca5fb11d
    Signed-off-by: Amit Daniel Kachhap <amit.kachhap@arm.com>

---
+++ product/sgi575/include/mcp_system_mmap_mcp.h
@@ -0,0 +1,17 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MCP_SYSTEM_MMAP_MCP_H
+#define MCP_SYSTEM_MMAP_MCP_H
+
+#include <mcp_sgi575_mmap_mcp.h>
+
+#define MCP_ROM_SIZE                 (64 * 1024)
+#define MCP_RAM0_SIZE                (64 * 1024)
+#define MCP_RAM1_SIZE                (64 * 1024)
+
+#endif /* MCP_SYSTEM_MMAP_MCP_H */
