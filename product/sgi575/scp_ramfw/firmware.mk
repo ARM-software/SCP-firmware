5ceb9491f127a4d8dde9101c0dcef69f27eff46a
Amit Daniel Kachhap <amit.kachhap@arm.com>
2018-07-20 17:50:27 +0530

    sgi575/scp_ramfw: Add timer and gtimer support
    Change-Id: If994d8ea2fb2199756526beed14cca00e78ae601
    Signed-off-by: Amit Daniel Kachhap <amit.kachhap@arm.com>

--- product/sgi575/scp_ramfw/firmware.mk
+++ product/sgi575/scp_ramfw/firmware.mk
@@ -9,17 +9,19 @@ BS_FIRMWARE_CPU := cortex-m7
 BS_FIRMWARE_HAS_MULTITHREADING := yes
 BS_FIRMWARE_HAS_NOTIFICATION := yes
 BS_FIRMWARE_MODULE_HEADERS_ONLY := \
-    power_domain \
-    timer
+    power_domain
 
 BS_FIRMWARE_MODULES := \
     pl011 \
     log \
-    clock
+    clock \
+    gtimer \
+    timer
 
 BS_FIRMWARE_SOURCES := \
     rtx_config.c \
     config_log.c \
-    config_clock.c
+    config_clock.c \
+    config_timer.c
 
 include $(BS_DIR)/firmware.mk
