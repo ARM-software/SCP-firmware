fccd8baf94c69b5ccdf734040d1a9e5243890c0d
Amit Daniel Kachhap <amit.kachhap@arm.com>
2018-07-20 15:22:33 +0530

    sgi575: Add MHU definitions for SCP
    Change-Id: If4cbc1e91bec2328dcbd5a261ea656ca646ead65
    Signed-off-by: Amit Daniel Kachhap <amit.kachhap@arm.com>

---
+++ product/sgi575/include/scp_sgi575_mhu.h
@@ -0,0 +1,20 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     MHU module device indexes.
+ */
+
+#ifndef SCP_SGI575_MHU_H
+#define SCP_SGI575_MHU_H
+
+enum scp_sgi575_mhu_device_idx {
+    SCP_SGI575_MHU_DEVICE_IDX_SCP_AP_S,
+    SCP_SGI575_MHU_DEVICE_IDX_SCP_AP_NS,
+    SCP_SGI575_MHU_DEVICE_IDX_COUNT
+};
+
+#endif /* SCP_SGI575_MHU_H */
