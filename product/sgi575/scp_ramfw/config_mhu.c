125bab78711f1cf2c7ae0a454dbd03d3d63b51d4
Amit Daniel Kachhap <amit.kachhap@arm.com>
2018-07-20 15:28:42 +0530

    sgi575/scp_ramfw: Add MHU config
    Change-Id: I11fab9db93e6a21063bb95dfe9db1285696a53c5
    Signed-off-by: Amit Daniel Kachhap <amit.kachhap@arm.com>

---
+++ product/sgi575/scp_ramfw/config_mhu.c
@@ -0,0 +1,46 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_module.h>
+#include <mod_mhu.h>
+#include <scp_sgi575_irq.h>
+#include <scp_sgi575_mhu.h>
+#include <scp_sgi575_mmap.h>
+
+static const struct fwk_element mhu_element_table[] = {
+    [SCP_SGI575_MHU_DEVICE_IDX_SCP_AP_S] = {
+        .name = "MHU_SCP_AP_S",
+        .sub_element_count = 1,
+        .data = &((struct mod_mhu_device_config) {
+            .irq = MHU_AP_SEC_IRQ,
+            .in = SCP_MHU_AP_SCP_S(0),
+            .out = SCP_MHU_SCP_AP_S(0),
+        })
+    },
+    [SCP_SGI575_MHU_DEVICE_IDX_SCP_AP_NS] = {
+        .name = "MHU_SCP_AP_NS",
+        .sub_element_count = 1,
+        .data = &((struct mod_mhu_device_config) {
+            .irq = MHU_AP_NONSEC_IRQ,
+            .in = SCP_MHU_AP_SCP_NS(0),
+            .out = SCP_MHU_SCP_AP_NS(0),
+        })
+    },
+    [SCP_SGI575_MHU_DEVICE_IDX_COUNT] = { 0 },
+};
+
+
+static const struct fwk_element *mhu_get_element_table(fwk_id_t module_id)
+{
+    return mhu_element_table;
+}
+
+const struct fwk_module_config config_mhu = {
+    .get_element_table = mhu_get_element_table,
+};
