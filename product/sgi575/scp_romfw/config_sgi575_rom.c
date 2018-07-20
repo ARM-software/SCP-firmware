70230a660fa0ccbb014b537350b55532b9c95bc0
Amit Daniel Kachhap <amit.kachhap@arm.com>
2018-07-20 15:09:57 +0530

    sgi575/scp_romfw: Add base support for scp rom image
    Change-Id: Ie5e46f774f44f0708c62aa2852f50e79db7b703e
    Signed-off-by: Amit Daniel Kachhap <amit.kachhap@arm.com>

---
+++ product/sgi575/scp_romfw/config_sgi575_rom.c
@@ -0,0 +1,19 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_module.h>
+#include <mod_sgi575_rom.h>
+#include <scp_software_mmap.h>
+#include <scp_sgi575_mmap_scp.h>
+
+const struct fwk_module_config config_sgi575_rom = {
+    .data = &((struct sgi575_rom_config) {
+        .ramfw_base = SCP_RAM0_BASE,
+        .nor_base = SCP_NOR_BASE,
+        .load_ram_size = SCP_IMAGE_SIZE,
+    })
+};
---
+++ product/sgi575/scp_romfw/firmware.mk
@@ -0,0 +1,26 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+BS_FIRMWARE_CPU := cortex-m7
+BS_FIRMWARE_HAS_MULTITHREADING := no
+BS_FIRMWARE_HAS_NOTIFICATION := yes
+BS_FIRMWARE_MODULE_HEADERS_ONLY := \
+    power_domain \
+    timer
+
+BS_FIRMWARE_MODULES := \
+    pl011 \
+    clock \
+    log \
+    sgi575_rom
+
+BS_FIRMWARE_SOURCES := \
+    config_log.c \
+    config_clock.c \
+    config_sgi575_rom.c
+
+include $(BS_DIR)/firmware.mk
