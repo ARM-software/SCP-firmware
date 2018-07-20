ae6487b1a021182abf07cadb9b1f4bcd9a0baa46
Amit Daniel Kachhap <amit.kachhap@arm.com>
2018-07-20 15:32:10 +0530

    sgi575/scp_ramfw: Add SMT config
    Change-Id: I5a2f9199bc2962b2d85de3666dd3c7229c207b41
    Signed-off-by: Amit Daniel Kachhap <amit.kachhap@arm.com>

---
+++ product/sgi575/scp_ramfw/config_smt.c
@@ -0,0 +1,53 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdint.h>
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+#include <mod_smt.h>
+#include <scp_software_mmap.h>
+#include <scp_sgi575_mhu.h>
+#include <scp_sgi575_scmi.h>
+
+static const struct fwk_element smt_element_table[] = {
+    [SCP_SGI575_SCMI_SERVICE_IDX_PSCI] = {
+        .name = "PSCI",
+        .data = &((struct mod_smt_channel_config) {
+            .type = MOD_SMT_CHANNEL_TYPE_SLAVE,
+            .policies = MOD_SMT_POLICY_INIT_MAILBOX | MOD_SMT_POLICY_SECURE,
+            .mailbox_address = (uintptr_t)SCP_SCMI_PAYLOAD_S_A2P_BASE,
+            .mailbox_size = SCP_SCMI_PAYLOAD_SIZE,
+            .driver_id = FWK_ID_SUB_ELEMENT_INIT(FWK_MODULE_IDX_MHU,
+                SCP_SGI575_MHU_DEVICE_IDX_SCP_AP_S, 0),
+            .driver_api_id = FWK_ID_API_INIT(FWK_MODULE_IDX_MHU, 0),
+        })
+    },
+    [SCP_SGI575_SCMI_SERVICE_IDX_OSPM] = {
+        .name = "OSPM",
+        .data = &((struct mod_smt_channel_config) {
+            .type = MOD_SMT_CHANNEL_TYPE_SLAVE,
+            .policies = MOD_SMT_POLICY_INIT_MAILBOX,
+            .mailbox_address = (uintptr_t)SCP_SCMI_PAYLOAD_NS_A2P_BASE,
+            .mailbox_size = SCP_SCMI_PAYLOAD_SIZE,
+            .driver_id = FWK_ID_SUB_ELEMENT_INIT(FWK_MODULE_IDX_MHU,
+                 SCP_SGI575_MHU_DEVICE_IDX_SCP_AP_NS, 0),
+            .driver_api_id = FWK_ID_API_INIT(FWK_MODULE_IDX_MHU, 0),
+        })
+    },
+    [SCP_SGI575_SCMI_SERVICE_IDX_COUNT] = { 0 },
+};
+
+static const struct fwk_element *smt_get_element_table(fwk_id_t module_id)
+{
+    return smt_element_table;
+}
+
+const struct fwk_module_config config_smt = {
+    .get_element_table = smt_get_element_table,
+};
