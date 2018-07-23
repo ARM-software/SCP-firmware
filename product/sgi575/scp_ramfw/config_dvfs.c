4ee0fb7142d79df115265f8a12c1d93138e42102
Amit Daniel Kachhap <amit.kachhap@arm.com>
2018-07-23 14:13:34 +0530

    sgi575/scp_ramfw: Add support for DVFS and SCMI Perf
    Change-Id: Ie3a3e0c72f9afc30b36f5e4732e5dd5a451d7305
    Signed-off-by: Amit Daniel Kachhap <amit.kachhap@arm.com>

---
+++ product/sgi575/scp_ramfw/config_dvfs.c
@@ -0,0 +1,74 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_element.h>
+#include <fwk_macros.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+#include <mod_dvfs.h>
+#include <config_clock.h>
+
+static struct mod_dvfs_opp opps[] = {
+        {
+            .frequency = 1313 * FWK_MHZ,
+            .voltage = 100,
+        },
+        {
+            .frequency = 1531 * FWK_MHZ,
+            .voltage = 200,
+        },
+        {
+            .frequency = 1750 * FWK_MHZ,
+            .voltage = 300,
+        },
+        {
+            .frequency = 2100 * FWK_MHZ,
+            .voltage = 400,
+        },
+        {
+            .frequency = 2600 * FWK_MHZ,
+            .voltage = 500,
+        },
+        { 0 }
+};
+
+static const struct mod_dvfs_domain_config cpu_group0 = {
+    .psu_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_PSU, 0),
+    .clock_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_CLOCK, CLOCK_IDX_CPU_GROUP0),
+    .latency = 1200,
+    .sustained_idx = 2,
+    .opps = opps,
+};
+
+static const struct mod_dvfs_domain_config cpu_group1 = {
+    .psu_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_PSU, 1),
+    .clock_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_CLOCK, CLOCK_IDX_CPU_GROUP1),
+    .latency = 1200,
+    .sustained_idx = 2,
+    .opps = opps,
+};
+
+static const struct fwk_element element_table[] = {
+    [0] = {
+        .name = "GROUP0",
+        .data = &cpu_group0,
+    },
+    [1] = {
+        .name = "GROUP1",
+        .data = &cpu_group1,
+    },
+    { 0 }
+};
+
+static const struct fwk_element *dvfs_get_element_table(fwk_id_t module_id)
+{
+    return element_table;
+}
+
+const struct fwk_module_config config_dvfs = {
+    .get_element_table = dvfs_get_element_table,
+};
