361b8a98236bbcf0371c7ba721e0b8f08f84a1ad
Pedro Custodio <Pedro.Krewinkelcustodio@arm.com>
2018-08-14 16:29:24 +0100

    module: PCID support
    Change-Id: I32066b1df94a76320923ea46c9307a4133a9acdf
    Signed-off-by: Elieva Pignat <Elieva.Pignat@arm.com>
    Signed-off-by: Chris Kay <chris.kay@arm.com>

---
+++ module/pcid/src/mod_pcid.c
@@ -0,0 +1,35 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018-2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdbool.h>
+#include <string.h>
+#include <fwk_assert.h>
+#include <fwk_module.h>
+#include <mod_pcid.h>
+
+bool mod_pcid_check_registers(const struct mod_pcid_registers *registers,
+                              const struct mod_pcid_registers *expected)
+{
+    assert(registers != NULL);
+    assert(expected != NULL);
+
+    return !memcmp(registers, expected, sizeof(*registers));
+}
+
+static int pcid_init(fwk_id_t module_id,
+                     unsigned int element_count,
+                     const void *data)
+{
+    return FWK_SUCCESS;
+}
+
+const struct fwk_module_config config_pcid = { 0 };
+const struct fwk_module module_pcid = {
+    .name = "PCID",
+    .type = FWK_MODULE_TYPE_SERVICE,
+    .init = pcid_init,
+};
