662fb5a3af724dd19ea678f7ec898090b790feae
Manoj Kumar <manoj.kumar3@arm.com>
2018-10-26 10:27:12 +0530

    n1sdp: add mhu/scmi/sds header files for n1sdp SCP
    Change-Id: Ide10244c5021c3abc6970c58aac6d247224f5271
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/n1sdp/include/n1sdp_scp_mhu.h
@@ -0,0 +1,21 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     MHU module device indexes.
+ */
+
+#ifndef N1SDP_SCP_MHU_H
+#define N1SDP_SCP_MHU_H
+
+enum scp_n1sdp_mhu_device_idx {
+    N1SDP_MHU_DEVICE_IDX_S_CLUS0,
+    N1SDP_MHU_DEVICE_IDX_NS_CLUS0,
+    N1SDP_MHU_DEVICE_IDX_S_MCP,
+    N1SDP_MHU_DEVICE_IDX_COUNT,
+};
+
+#endif /* N1SDP_SCP_MHU_H */
---
+++ product/n1sdp/include/n1sdp_scp_scmi.h
@@ -0,0 +1,28 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Definitions for SCMI and SMT module configurations.
+ */
+
+#ifndef N1SDP_SCP_SCMI_H
+#define N1SDP_SCP_SCMI_H
+
+/* SCMI agent identifiers */
+enum scp_n1sdp_scmi_agent_id {
+    /* 0 is reserved for the platform */
+    SCP_SCMI_AGENT_ID_OSPM = 1,
+    SCP_SCMI_AGENT_ID_PSCI,
+};
+
+/* SCMI service indexes */
+enum scp_n1sdp_scmi_service_idx {
+    SCP_N1SDP_SCMI_SERVICE_IDX_PSCI,
+    SCP_N1SDP_SCMI_SERVICE_IDX_OSPM,
+    SCP_N1SDP_SCMI_SERVICE_IDX_COUNT,
+};
+
+#endif /* N1SDP_SCP_SCMI_H */
