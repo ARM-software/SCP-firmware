b787d6de1d87b5dbf4da3aa6723d4a3b9bb4cdad
Manoj Kumar <manoj.kumar3@arm.com>
2018-10-26 11:59:32 +0530

    n1sdp: add memory map include files for n1sdp MCP
    This patch adds the following header files:
    
    n1sdp_mcp_mmap.h -> N1SDP MCP's peripheral memory map information.
    n1sdp_mcp_software_mmap.h -> Definitions of software mapping of
                                 hardware memories.
    n1sdp_mcp_system_mmap.h -> Definitions of external & internal
                               memories connected to MCP.
    
    Change-Id: I1283885b4b7676a18f9c20b0b586309b27db8d60
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/n1sdp/include/n1sdp_mcp_software_mmap.h
@@ -0,0 +1,29 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Software defined memory map shared between SCP and MCP cores.
+ */
+
+#ifndef N1SDP_MCP_SOFTWARE_MMAP_H
+#define N1SDP_MCP_SOFTWARE_MMAP_H
+
+#include <n1sdp_mcp_mmap.h>
+
+/* SCMI payload size */
+#define MCP_SCMI_PAYLOAD_SIZE      (128)
+
+/* SCMI non-secure payload areas */
+#define SCMI_PAYLOAD_SCP_TO_MCP_NS (MCP_SCP_SHARED_NONSECURE_RAM)
+#define SCMI_PAYLOAD_MCP_TO_SCP_NS (MCP_SCP_SHARED_NONSECURE_RAM + \
+                                                MCP_SCMI_PAYLOAD_SIZE)
+
+/* SCMI secure payload areas */
+#define SCMI_PAYLOAD_SCP_TO_MCP_S  (MCP_SCP_SHARED_SECURE_RAM)
+#define SCMI_PAYLOAD_MCP_TO_SCP_S  (MCP_SCP_SHARED_SECURE_RAM + \
+                                                MCP_SCMI_PAYLOAD_SIZE)
+
+#endif /* N1SDP_MCP_SOFTWARE_MMAP_H */
