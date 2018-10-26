4e286558bba72bab6e0c08504606d3f602977b6b
Manoj Kumar <manoj.kumar3@arm.com>
2018-10-26 11:44:54 +0530

    n1sdp: add product.mk file to build scp_ramfw and include n1sdp in ci
    Change-Id: I455cf2cd2dbd50e6aa1a68750b04e0d3b7bec828
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/n1sdp/product.mk
@@ -0,0 +1,9 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+BS_PRODUCT_NAME := n1sdp
+BS_FIRMWARE_LIST := scp_ramfw
