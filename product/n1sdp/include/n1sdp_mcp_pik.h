dc906baf1e48cef4875f1a46f23b74c738751fc5
Manoj Kumar <manoj.kumar3@arm.com>
2018-10-26 12:02:17 +0530

    n1sdp: add pik header files for n1sdp MCP
    Change-Id: I4dd4208c02aa549c7809eca90f2366a9ec0a2374
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/n1sdp/include/n1sdp_mcp_pik.h
@@ -0,0 +1,16 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef N1SDP_MCP_PIK_H
+#define N1SDP_MCP_PIK_H
+
+#include <n1sdp_mcp_mmap.h>
+#include <n1sdp_pik_mcp.h>
+
+#define PIK_MCP  ((struct pik_mcp_reg *) MCP_PIK_BASE)
+
+#endif /* N1SDP_MCP_PIK_H */
