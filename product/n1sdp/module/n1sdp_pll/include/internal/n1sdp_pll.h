195d5ab06af7325609d849051f2fa19b1c33f682
Manoj Kumar <manoj.kumar3@arm.com>
2018-10-26 11:05:41 +0530

    n1sdp/n1sdp_pll: add new module n1sdp_pll
    This patch adds a new module for N1SDP PLL hardware under product/n1sdp/module
    directory.
    
    N1SDP platform has a different PLL compared to the PLL used in other platforms
    like MSYS/ISYS (uses system_pll module). So a separate module is added.
    
    Change-Id: I5189d6069c1c4df105dc7f221d73fd01a02a5b90
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/n1sdp/module/n1sdp_pll/include/internal/n1sdp_pll.h
@@ -0,0 +1,42 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *      N1SDP PLL register definitions
+ */
+
+#ifndef N1SDP_PLL_H
+#define N1SDP_PLL_H
+
+#include <fwk_macros.h>
+
+/* PLL Control Register 0 bit positions. */
+#define PLL_HARD_BYPASS_BIT_POS      0
+#define PLL_PD_TIMER_BYPASS_BIT_POS  1
+#define PLL_LOCK_SEL_BIT_POS         2
+#define PLL_DSMEN_BIT_POS            4
+#define PLL_DACEN_BIT_POS            5
+#define PLL_BYPASS_POS               6
+#define PLL_FBDIV_BIT_POS            8
+#define PLL_REFDIV_POS               20
+#define PLL_PLLEN_POS                31
+
+/* PLL Control Register 1 bit positions. */
+#define PLL_FRAC_POS                 0
+#define PLL_POSTDIV1_POS             24
+#define PLL_POSTDIV2_POS             28
+#define PLL_LOCK_STATUS_POS          31
+
+/*! The minimum frequency that the PLL hardware can output. */
+#define MOD_N1SDP_PLL_RATE_MIN       (50UL * FWK_MHZ)
+
+/*! The maximum frequency that the PLL hardware can output. */
+#define MOD_N1SDP_PLL_RATE_MAX       (3200UL * FWK_MHZ)
+
+/*! Step size for the PLL. */
+#define MOD_N1SDP_PLL_STEP_SIZE      UINT64_C(1000)
+
+#endif /* N1SDP_PLL_H */
