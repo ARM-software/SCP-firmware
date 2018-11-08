d3797573ce301ef05de8b758877655cbd52eeac3
Ronald Cron <ronald.cron@arm.com>
2018-11-08 16:14:08 +0100

    juno: Add SoC clock driver
    Change-Id: I9edac591a458f0bdeb3a69c752381b7abf522fc2
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/juno/module/juno_soc_clock/include/mod_juno_soc_clock.h
@@ -0,0 +1,62 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Clock driver for the clocks of the Juno SoC
+ */
+
+#ifndef MOD_JUNO_SOC_CLOCK_H
+#define MOD_JUNO_SOC_CLOCK_H
+
+#include <stdint.h>
+
+/*!
+ * \addtogroup GroupJunoModule Juno Product Modules
+ * @{
+ */
+
+/*!
+ * \defgroup GroupJunoSocClock Juno SoC Clock Driver
+ *
+ * \details A driver for clocks of the Juno SoC.
+ *
+ * @{
+ */
+
+/*!
+ * \brief Sub-types of Juno SOC clock.
+ */
+enum mod_juno_soc_clock_type {
+    /*! A clock with a fixed source. Only its divider can be changed. */
+    MOD_JUNO_SOC_CLOCK_TYPE_SINGLE_SOURCE,
+
+    /*! A clock with multiple, selectable sources and at least one divider */
+    MOD_JUNO_SOC_CLOCK_TYPE_MULTI_SOURCE,
+};
+
+/*!
+ * \brief Juno SoC clock configuration.
+ */
+struct mod_juno_soc_clock_dev_config {
+    /*! The type of the clock */
+    enum mod_juno_soc_clock_type type;
+
+    /*! Pointer to the clock's control register */
+    volatile uint32_t * const control_reg;
+
+    /*! Clock's rate in Hertz */
+    uint32_t rate;
+};
+
+/*!
+ * @}
+ */
+
+/*!
+ * @}
+ */
+
+#endif /* MOD_JUNO_SOC_CLOCK_H */
