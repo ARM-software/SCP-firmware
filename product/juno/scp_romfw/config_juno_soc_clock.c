b4b87c02976c458d7864f6efadf24ade4f6ab4fb
Ronald Cron <ronald.cron@arm.com>
2018-11-08 16:21:26 +0100

    juno/romfw: Add juno_soc_clock support
    Change-Id: I1556f4f58def9259ae580c2744dcccff2d3c76be
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/juno/scp_romfw/config_juno_soc_clock.c
@@ -0,0 +1,13 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Configuration of the Juno SoC clock module
+ */
+
+#include <fwk_module.h>
+
+const struct fwk_module_config config_juno_soc_clock = { 0 };
