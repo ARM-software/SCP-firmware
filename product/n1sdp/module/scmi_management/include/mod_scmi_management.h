86085d4302b0b72cd953748a30aafbf170b80c7e
Manoj Kumar <manoj.kumar3@arm.com>
2018-11-13 12:28:37 +0530

    n1sdp/scmi_management: add protocol module scmi_management
    This protocol module, to be used in scp, handles scmi commands from mcp.
    
    Change-Id: I61ea4941ad20d3d4038e19daab5cfd1dd49862ee
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/n1sdp/module/scmi_management/include/mod_scmi_management.h
@@ -0,0 +1,77 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *      SCMI Management Protocol Support.
+ */
+
+#ifndef MOD_SCMI_MANAGEMENT_H
+#define MOD_SCMI_MANAGEMENT_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+/*!
+ * \addtogroup GroupN1SDPModule N1SDP Product Modules
+ * @{
+ */
+
+/*!
+ * \defgroup GroupN1SDPSCMI_MANAGEMENT SCMI Management Protocol
+ * @{
+ */
+
+/*!
+ * \brief Macro defining the SCMI Management Protocol's ID.
+ */
+#define SCMI_PROTOCOL_ID_MANAGEMENT      UINT32_C(0x89)
+
+/*!
+ * \brief Macro defining the SCMI Management Protocol's version.
+ */
+#define SCMI_PROTOCOL_VERSION_MANAGEMENT UINT32_C(0x10000)
+
+/*!
+ * \brief Identifiers of the SCMI Management Protocol commands.
+ */
+enum scmi_management_command_id {
+    /*! Command ID for getting clock status */
+    SCMI_MANAGEMENT_CLOCK_STATUS_GET = 0x3,
+    /*! Commmand ID for chip ID information */
+    SCMI_MANAGEMENT_CHIPID_INFO_GET  = 0x4,
+};
+
+/*!
+ * \brief Protocol handler for getting clock status from SCP.
+ */
+struct scmi_management_clock_status_get_p2a {
+    /*! SCMI status */
+    int32_t status;
+    /*! Clock status */
+    uint32_t clock_status;
+};
+
+/*!
+ * \brief Protocol handler for chip ID information.
+ */
+struct scmi_management_chipid_info_get_p2a {
+    /*! SCMI status */
+    int32_t status;
+    /*! Multi chip mode identifier */
+    uint32_t multi_chip_mode;
+    /*! Chip ID of the chip if multi_chip_mode is set */
+    uint32_t chipid;
+};
+
+/*!
+ * @}
+ */
+
+/*!
+ * @}
+ */
+
+#endif /* MOD_SCMI_MANAGEMENT_H */
