9358949871a9dd92147da25972085abc14ef9b30
Manoj Kumar <manoj.kumar3@arm.com>
2018-11-13 13:31:52 +0530

    n1sdp: add n1sdp_mhu/n1sdp_smt/agent config files for mcp ramfw
    Change-Id: Id1e28c23e4c547b5e726bb8758ba4fb6ee048aa7
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/n1sdp/mcp_ramfw/config_smt.c
@@ -0,0 +1,40 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdint.h>
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+#include <mod_smt.h>
+#include <n1sdp_mcp_mhu.h>
+#include <n1sdp_mcp_software_mmap.h>
+
+static const struct fwk_element smt_element_table[] = {
+    [0] = {
+        .name = "MANAGEMENT-S",
+        .data = &((struct mod_smt_channel_config) {
+            .type = MOD_SMT_CHANNEL_TYPE_MASTER,
+            .policies = MOD_SMT_POLICY_INIT_MAILBOX | MOD_SMT_POLICY_SECURE,
+            .mailbox_address = (uintptr_t)SCMI_PAYLOAD_SCP_TO_MCP_S,
+            .mailbox_size = MCP_SCMI_PAYLOAD_SIZE,
+            .driver_id = FWK_ID_SUB_ELEMENT_INIT(FWK_MODULE_IDX_N1SDP_MHU,
+                N1SDP_MHU_DEVICE_IDX_S_SCP, 0),
+            .driver_api_id = FWK_ID_API_INIT(FWK_MODULE_IDX_N1SDP_MHU, 0),
+        })
+    },
+    [1] = { 0 },
+};
+
+static const struct fwk_element *smt_get_element_table(fwk_id_t module_id)
+{
+    return smt_element_table;
+}
+
+const struct fwk_module_config config_n1sdp_smt = {
+    .get_element_table = smt_get_element_table,
+};
