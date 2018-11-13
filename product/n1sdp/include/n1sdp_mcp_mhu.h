0b426456563c558b1432ed43c6cf8ddbd51f4604
Manoj Kumar <manoj.kumar3@arm.com>
2018-11-13 11:48:15 +0530

    n1sdp: add mhu & scmi header files for n1sdp MCP
    Change-Id: I839fcb2a6d58797aa9f5239c0c092e0d08846dc4
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/n1sdp/include/n1sdp_mcp_mhu.h
@@ -0,0 +1,16 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef N1SDP_MCP_MHU_H
+#define N1SDP_MCP_MHU_H
+
+enum mcp_n1sdp_mhu_device_idx {
+    N1SDP_MHU_DEVICE_IDX_S_SCP,
+    N1SDP_MHU_DEVICE_IDX_COUNT,
+};
+
+#endif /* N1SDP_MCP_MHU_H */
---
+++ product/n1sdp/include/n1sdp_mcp_scmi.h
@@ -0,0 +1,17 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef N1SDP_MCP_SCMI_H
+#define N1SDP_MCP_SCMI_H
+
+/* SCMI agent identifiers */
+enum mcp_n1sdp_scmi_agent_idx {
+    MCP_N1SDP_SCMI_AGENT_IDX_MANAGEMENT,
+    MCP_N1SDP_SCMI_AGENT_IDX_COUNT,
+};
+
+#endif /* N1SDP_MCP_SCMI_H */
