3aeb7ca72969ae9d70505fe3b2b471bb2569d619
Manoj Kumar <manoj.kumar3@arm.com>
2018-11-13 13:31:52 +0530

    n1sdp: add n1sdp_mhu/n1sdp_smt/agent config files for mcp ramfw
    Change-Id: Id1e28c23e4c547b5e726bb8758ba4fb6ee048aa7
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/n1sdp/mcp_ramfw/config_scmi_agent.c
@@ -0,0 +1,41 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdbool.h>
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_macros.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+#include <mod_scmi_agent.h>
+#include <mod_smt.h>
+#include <n1sdp_mcp_scmi.h>
+
+static const struct fwk_element agent_table[] = {
+    [MCP_N1SDP_SCMI_AGENT_IDX_MANAGEMENT] = {
+        .name = "MCP-AGENT",
+        .data = &((struct mod_scmi_agent_config) {
+            .transport_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_N1SDP_SMT,
+                MCP_N1SDP_SCMI_AGENT_IDX_MANAGEMENT),
+            .transport_api_id = FWK_ID_API_INIT(
+                FWK_MODULE_IDX_N1SDP_SMT,
+                MOD_SMT_API_IDX_SCMI_AGENT_TRANSPORT),
+        }),
+    },
+    [MCP_N1SDP_SCMI_AGENT_IDX_COUNT] = { 0 }
+};
+
+static const struct fwk_element *get_agent_table(fwk_id_t module_id)
+{
+    return agent_table;
+}
+
+const struct fwk_module_config config_scmi_agent = {
+    .get_element_table = get_agent_table,
+    .data = NULL,
+};
