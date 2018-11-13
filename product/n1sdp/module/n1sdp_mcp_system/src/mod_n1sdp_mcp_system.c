46ecbb1df678f50ffb88868f6b4b7edcf0a9bb79
Manoj Kumar <manoj.kumar3@arm.com>
2018-11-13 13:28:02 +0530

    n1sdp/n1sdp_mcp_system: add module n1sdp_mcp_system
    This module is the main system module that runs in mcp after all other
    modules. This is similar to n1sdp_system module in scp.
    
    Change-Id: I1270b9dcc2f7223aae6486ef38e11ce025e4e9d9
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/n1sdp/module/n1sdp_mcp_system/src/mod_n1sdp_mcp_system.c
@@ -0,0 +1,159 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     N1SDP MCP System Support.
+ */
+
+#include <stdint.h>
+#include <fwk_id.h>
+#include <fwk_macros.h>
+#include <fwk_mm.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+#include <fwk_thread.h>
+#include <mod_clock.h>
+#include <mod_n1sdp_mcp_system.h>
+#include <mod_log.h>
+#include <mod_pik_clock.h>
+#include <mod_power_domain.h>
+#include <mod_scmi_agent.h>
+#include <n1sdp_mcp_pik.h>
+#include <config_clock.h>
+
+/* Module context */
+struct n1sdp_mcp_system_ctx {
+
+    /* Log API pointer */
+    const struct mod_log_api *log_api;
+
+    /* SCMI agent API pointer */
+    const struct mod_scmi_agent_api *scmi_api;
+
+    /* PIK clock API - MCP core clock */
+    const struct mod_clock_drv_api *pik_coreclk_api;
+
+    /* PIK clock API - MCP AXI clock */
+    const struct mod_clock_drv_api *pik_axiclk_api;
+};
+
+static struct n1sdp_mcp_system_ctx n1sdp_mcp_system_ctx;
+const struct fwk_module_config config_n1sdp_mcp_system = { 0 };
+
+/*
+ * Functions fulfilling the framework's module interface
+ */
+
+static int n1sdp_mcp_system_init(fwk_id_t module_id, unsigned int unused,
+    const void *unused2)
+{
+    return FWK_SUCCESS;
+}
+
+static int n1sdp_mcp_system_bind(fwk_id_t id, unsigned int round)
+{
+    int status;
+
+    if (round == 0) {
+        status = fwk_module_bind(FWK_ID_MODULE(FWK_MODULE_IDX_LOG),
+                                 FWK_ID_API(FWK_MODULE_IDX_LOG, 0),
+                                 &n1sdp_mcp_system_ctx.log_api);
+        if (status != FWK_SUCCESS)
+            return status;
+
+        status = fwk_module_bind(FWK_ID_MODULE(FWK_MODULE_IDX_SCMI_AGENT),
+                                 FWK_ID_API(FWK_MODULE_IDX_SCMI_AGENT, 0),
+                                 &n1sdp_mcp_system_ctx.scmi_api);
+        if (status != FWK_SUCCESS)
+            return status;
+
+        status = fwk_module_bind(FWK_ID_ELEMENT(FWK_MODULE_IDX_PIK_CLOCK,
+                                                CLOCK_PIK_IDX_MCP_CORECLK),
+                                 FWK_ID_API(FWK_MODULE_IDX_PIK_CLOCK,
+                                            MOD_PIK_CLOCK_API_TYPE_CLOCK),
+                                 &n1sdp_mcp_system_ctx.pik_coreclk_api);
+        if (status != FWK_SUCCESS)
+            return status;
+
+        return fwk_module_bind(FWK_ID_ELEMENT(FWK_MODULE_IDX_PIK_CLOCK,
+                                              CLOCK_PIK_IDX_MCP_AXICLK),
+                               FWK_ID_API(FWK_MODULE_IDX_PIK_CLOCK,
+                                          MOD_PIK_CLOCK_API_TYPE_CLOCK),
+                               &n1sdp_mcp_system_ctx.pik_axiclk_api);
+    }
+
+    return FWK_SUCCESS;
+}
+
+static int n1sdp_mcp_system_start(fwk_id_t id)
+{
+    struct fwk_event event = {
+        .source_id = id,
+        .target_id = id,
+        .id = FWK_ID_EVENT(FWK_MODULE_IDX_N1SDP_MCP_SYSTEM,
+                           MOD_MCP_SYSTEM_EVENT_RUN),
+    };
+
+    return fwk_thread_put_event(&event);
+}
+
+static int n1sdp_mcp_system_process_event(const struct fwk_event *event,
+                                         struct fwk_event *resp)
+{
+    int status;
+    uint32_t clock_status = 0;
+    uint8_t mc_mode = 0;
+    uint8_t chipid = 0;
+
+    status = n1sdp_mcp_system_ctx.scmi_api->get_clock_status(
+                 FWK_ID_ELEMENT(FWK_MODULE_IDX_SCMI_AGENT, 0), &clock_status);
+    if (status != FWK_SUCCESS)
+        return status;
+
+    n1sdp_mcp_system_ctx.log_api->log(MOD_LOG_GROUP_DEBUG,
+        "[MCP SYSTEM] SCP clock status: 0x%x\n",
+        clock_status);
+
+    status = n1sdp_mcp_system_ctx.pik_coreclk_api->process_power_transition(
+                 FWK_ID_ELEMENT(FWK_MODULE_IDX_PIK_CLOCK,
+                                CLOCK_PIK_IDX_MCP_CORECLK),
+                 MOD_PD_STATE_ON);
+    if (status != FWK_SUCCESS)
+        return FWK_SUCCESS;
+
+    status = n1sdp_mcp_system_ctx.pik_coreclk_api->process_power_transition(
+                 FWK_ID_ELEMENT(FWK_MODULE_IDX_PIK_CLOCK,
+                                CLOCK_PIK_IDX_MCP_AXICLK),
+                 MOD_PD_STATE_ON);
+    if (status != FWK_SUCCESS)
+        return FWK_SUCCESS;
+
+    n1sdp_mcp_system_ctx.log_api->log(MOD_LOG_GROUP_DEBUG,
+        "[MCP SYSTEM] MCP PIK clocks configured\n");
+
+    status = n1sdp_mcp_system_ctx.scmi_api->get_chipid_info(
+                 FWK_ID_ELEMENT(FWK_MODULE_IDX_SCMI_AGENT, 0),
+                 &mc_mode, &chipid);
+    if (status != FWK_SUCCESS)
+        return status;
+
+    n1sdp_mcp_system_ctx.log_api->log(MOD_LOG_GROUP_DEBUG,
+        "[MCP SYSTEM] MC Mode: 0x%x CHIPID: 0x%x\n",
+        mc_mode, chipid);
+
+    return status;
+}
+
+const struct fwk_module module_n1sdp_mcp_system = {
+    .name = "N1SDP_MCP_SYSTEM",
+    .type = FWK_MODULE_TYPE_DRIVER,
+    .api_count = 0,
+    .event_count = MOD_MCP_SYSTEM_EVENT_COUNT,
+    .init = n1sdp_mcp_system_init,
+    .bind = n1sdp_mcp_system_bind,
+    .process_event = n1sdp_mcp_system_process_event,
+    .start = n1sdp_mcp_system_start,
+};
