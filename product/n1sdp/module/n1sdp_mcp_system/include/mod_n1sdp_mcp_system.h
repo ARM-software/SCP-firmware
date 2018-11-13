2a4ecd2216e46529464bd81502638f2da0e9e27b
Manoj Kumar <manoj.kumar3@arm.com>
2018-11-13 13:28:02 +0530

    n1sdp/n1sdp_mcp_system: add module n1sdp_mcp_system
    This module is the main system module that runs in mcp after all other
    modules. This is similar to n1sdp_system module in scp.
    
    Change-Id: I1270b9dcc2f7223aae6486ef38e11ce025e4e9d9
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/n1sdp/module/n1sdp_mcp_system/include/mod_n1sdp_mcp_system.h
@@ -0,0 +1,42 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     N1SDP MCP System Support
+ */
+
+#ifndef MOD_N1SDP_MCP_SYSTEM_H
+#define MOD_N1SDP_MCP_SYSTEM_H
+
+/*!
+ * \addtogroup GroupN1SDPModule N1SDP Product Modules
+ * @{
+ */
+
+/*!
+ * \defgroup GroupN1SDPMCPSystem N1SDP MCP System Support
+ * @{
+ */
+
+/*!
+ * \brief Events used by MCP system module.
+ */
+enum mcp_system_event {
+    /*! MCP system run event */
+    MOD_MCP_SYSTEM_EVENT_RUN,
+    /*! MCP system event count */
+    MOD_MCP_SYSTEM_EVENT_COUNT,
+};
+
+/*!
+ * @}
+ */
+
+/*!
+ * @}
+ */
+
+#endif /* MOD_N1SDP_MCP_SYSTEM_H */
