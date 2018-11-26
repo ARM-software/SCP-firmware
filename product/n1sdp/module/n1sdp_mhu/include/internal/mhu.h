03932702a2e309e41474664cb8088f74718f178d
Manoj Kumar <manoj.kumar3@arm.com>
2018-11-26 19:16:01 +0530

    n1sdp/n1sdp_mhu: add module n1sdp_mhu for MHU handling
    This patch adds module n1sdp_mhu which is taken from core mhu module
    but modified to work for N1SDP platform until SCMI agent support is
    added.
    
    Change-Id: Ibf2d64c1d3f2853da06b05a55488cdd2e74fbd08
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/n1sdp/module/n1sdp_mhu/include/internal/mhu.h
@@ -0,0 +1,29 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2015-2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MHU_H
+#define MHU_H
+
+#include <stdint.h>
+#include <fwk_macros.h>
+#include <mod_mhu.h>
+
+/*!
+ * \brief MHU Register Definitions
+ */
+struct mhu_reg {
+    /*! Status register */
+    FWK_R  uint32_t STAT;
+           uint32_t RESERVED0;
+    /*! Set register (sets the value of STAT) */
+    FWK_W  uint32_t SET;
+           uint32_t RESERVED1;
+    /*! Clear register (clears STAT) */
+    FWK_W  uint32_t CLEAR;
+};
+
+#endif /* MHU_H */
