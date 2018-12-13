f66ebc8e96a6a359467fdd97b6ba05313ee2874a
Deepak Pandey <Deepak.Pandey@arm.com>
2018-12-13 10:54:14 +0530

    n1sdp/scp_rom: add n1sdp_rom module.
    This code is responsible for the first level of the
    capsule firmware parsing to fetch the ramfw and load
    to the respective location and jump to it.
    
    Change-Id: Id1dbc3fc80784370a5f0726bb63de61f63e5ca7c
    Signed-off-by: Deepak Pandey <Deepak.Pandey@arm.com>

---
+++ product/n1sdp/module/n1sdp_rom/include/mod_n1sdp_rom.h
@@ -0,0 +1,42 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MOD_N1SDP_ROM_H
+#define MOD_N1SDP_ROM_H
+
+#include <stdint.h>
+
+/*!
+ * \addtogroup GroupN1SDPModule N1SDP Product Modules
+ * @{
+ */
+
+/*!
+ * \defgroup GroupN1SDPRom N1SDP SCP ROM Support
+ * @{
+ */
+
+/*!
+ * \brief Module configuration data.
+ */
+struct n1sdp_rom_config {
+    /*! Base address of the RAM to which SCP BL2 will be copied to */
+    const uintptr_t ramfw_base;
+
+    /*! Type of RAM Firmware to load */
+    const uint8_t image_type;
+};
+
+/*!
+ * @}
+ */
+
+/*!
+ * @}
+ */
+
+#endif /* MOD_N1SDP_ROM_H */
