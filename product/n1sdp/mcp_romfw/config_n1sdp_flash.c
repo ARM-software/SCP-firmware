3db5fb006f0944e16e9d66cfc21be7a74e40cdd7
Deepak Pandey <Deepak.Pandey@arm.com>
2018-12-13 11:57:25 +0530

    n1sdp/mcp_rom: add config file for n1sdp_flash module
    Change-Id: I7602a4c3531dd2588acd13d2d84f57443ecf49d7
    Signed-off-by: Deepak Pandey <Deepak.Pandey@arm.com>

---
+++ product/n1sdp/mcp_romfw/config_n1sdp_flash.c
@@ -0,0 +1,18 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_module.h>
+#include <mod_n1sdp_flash.h>
+#include <n1sdp_mcp_system_mmap.h>
+
+const struct fwk_module_config config_n1sdp_flash = {
+    .data = &((struct mod_n1sdp_flash_config) {
+        .flash_base_address = MCP_QSPI_FLASH_BASE_ADDR,
+        .flash_base_address_alt = MCP_QSPI_FLASH_BASE_ADDR_ALT,
+        .toc_offset = 0x0,
+    })
+};
