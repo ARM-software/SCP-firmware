3829ea11f18d44d5b305c6b44fffa179fe15fb55
Nicola Mazzucato <nicola.mazzucato@arm.com>
2018-12-18 09:21:43 +0000

    juno/module: Add PVT driver
    This patch introduces the PVT sensors driver. These sensors
    provide temperature and voltage values.
    
    Change-Id: I73f7e8e9846665d636cce61c3ab5b7907544447c
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>
    Signed-off-by: Raphael Gault <raphael.gault@arm.com>

---
+++ product/juno/include/pvt_sensor_calibration.h
@@ -0,0 +1,59 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef PVT_SENSOR_CALIBRATION_H
+#define PVT_SENSOR_CALIBRATION_H
+
+#include <stdint.h>
+#include <juno_mmap.h>
+
+#define SOC_PART_ID_LEN     7
+
+/*
+ * PVT Sensor Calibration register definitions.
+ */
+struct juno_pvt_calibration_reg {
+    FWK_R   char        PART_ID_PREFIX;
+    FWK_R   char        PART_ID[SOC_PART_ID_LEN];
+
+    /* Temperature type sensors: PVTs */
+    FWK_R   uint16_t    G1_S0_810MV_45C;
+    FWK_R   uint16_t    G1_S0_810MV_85C;
+    FWK_R   uint16_t    G1_S0_900MV_45C;
+    FWK_R   uint16_t    G1_S0_900MV_85C;
+    FWK_R   uint16_t    G2_S0_810MV_45C;
+    FWK_R   uint16_t    G2_S0_810MV_85C;
+    FWK_R   uint16_t    G2_S0_900MV_45C;
+    FWK_R   uint16_t    G2_S0_900MV_85C;
+    FWK_R   uint16_t    G2_S1_810MV_45C;
+    FWK_R   uint16_t    G2_S1_810MV_85C;
+    FWK_R   uint16_t    G2_S1_900MV_45C;
+    FWK_R   uint16_t    G2_S1_900MV_85C;
+    FWK_R   uint16_t    G3_S0_810MV_45C;
+    FWK_R   uint16_t    G3_S0_810MV_85C;
+    FWK_R   uint16_t    G3_S0_900MV_45C;
+    FWK_R   uint16_t    G3_S0_900MV_85C;
+    FWK_R   uint16_t    G4_S0_810MV_45C;
+    FWK_R   uint16_t    G4_S0_810MV_85C;
+    FWK_R   uint16_t    G4_S0_900MV_45C;
+    FWK_R   uint16_t    G4_S0_900MV_85C;
+
+    /* Voltage type sensors: Ring oscillators */
+    FWK_R   uint16_t    G0_S1_810MV_85C;
+    FWK_R   uint16_t    G0_S1_900MV_85C;
+    FWK_R   uint16_t    G0_S9_810MV_85C;
+    FWK_R   uint16_t    G0_S9_900MV_85C;
+    FWK_R   uint16_t    G3_S1_810MV_85C;
+    FWK_R   uint16_t    G3_S1_900MV_85C;
+    FWK_R   uint16_t    G4_S1_810MV_85C;
+    FWK_R   uint16_t    G4_S1_900MV_85C;
+};
+
+#define JUNO_PVT_CALIBRATION ((struct juno_pvt_calibration_reg *) \
+                                 SENSOR_CALIBRATION_BASE)
+
+#endif /* PVT_SENSOR_CALIBRATION_H */
---
+++ product/juno/module/juno_pvt/include/mod_juno_pvt.h
@@ -0,0 +1,115 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Juno PVT Driver header file
+ */
+
+#ifndef MOD_JUNO_PVT_H
+#define MOD_JUNO_PVT_H
+
+/*!
+ * \ingroup GroupJunoModule
+ * \defgroup GroupJunoPVT Juno PVT
+ * \{
+ * \brief The Juno SoC provides Power, Voltage, and Temperature (PVT) sensors
+ *      that can be used to power down the chip when it exceeds the maximum
+ *      operating temperature. These sensors may be located next to the cores,
+ *      gpu, or distributed on the SoC, and are grouped together within the same
+ *      location.
+ *      The PVT sensors are calibrated during board manufacture.
+ *
+ * \note Only one sensor per group can be read at a time.
+ */
+
+/*!
+ * \brief Sensor type indices.
+ */
+enum juno_sensor_pvt_type {
+    /*! Temperature type PVT sensor */
+    JUNO_PVT_TYPE_TEMP,
+
+    /*! Voltage type PVT sensor */
+    JUNO_PVT_TYPE_VOLT,
+
+    /*! Type count of PVT sensors */
+    JUNO_PVT_TYPE_COUNT
+};
+
+/*!
+ * \brief Sensor Group identifiers.
+ */
+enum juno_group_pvt_id {
+    /*! BIG CPU group */
+    JUNO_PVT_GROUP_BIG = 0,
+
+    /*! LITTLE CPU group */
+    JUNO_PVT_GROUP_LITTLE,
+
+    /*! GPU group */
+    JUNO_PVT_GROUP_GPU,
+
+    /*! SoC group */
+    JUNO_PVT_GROUP_SOC,
+
+    /*! Standard Cell group */
+    JUNO_PVT_GROUP_STDCELL,
+
+    /*! Number of groups */
+    JUNO_PVT_GROUP_COUNT
+};
+
+/*!
+ * \brief Sensor Group Descriptor.
+ */
+struct juno_group_desc {
+    /*! Configuration register */
+    struct juno_pvt_reg *regs;
+
+    /*! Interrupt number */
+    unsigned int irq;
+
+    /*! Sensor count for the group */
+    uint8_t sensor_count;
+
+    /*! Identifier of the Power Domain associated with the sensor group */
+    fwk_id_t pd_id;
+};
+
+/*!
+ * \brief Sensor Descriptor.
+ */
+struct mod_juno_pvt_dev_config {
+    /*! The group the sensor belongs to */
+    const struct juno_group_desc *group;
+
+    /*! The index of the sensor within its group */
+    uint8_t index;
+
+    /*! Sensor type */
+    enum juno_sensor_pvt_type type;
+
+    /*! Sensor information */
+    struct mod_sensor_info *info;
+
+    /*! Calibration register, first point */
+    uint16_t *cal_reg_a;
+
+    /*! Calibration register, second point */
+    uint16_t *cal_reg_b;
+
+    /*! Calibration offset, first point */
+    uint16_t offset_cal_reg_a;
+
+    /*! Calibration offset, second point */
+    uint16_t offset_cal_reg_b;
+};
+
+/*!
+ * \}
+ */
+
+#endif /* MOD_JUNO_PVT_H */
