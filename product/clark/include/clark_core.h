5a78203fa4bf4deafe8e9d1010320ceff213325f
Nicola Mazzucato <nicola.mazzucato@arm.com>
2019-01-11 14:40:35 +0000

    clark: Fix GCC 7 -Wformat-truncation error
    GCC 7 introduces the -Wformat-truncation warning and associated
    heuristics. This warning identifies potentially unexpected string
    truncation issues by statically range-checking the vararg inputs to
    snprintf().
    
    For us, GCC seems to be unable to reliably identify the bounds of the
    inputs (cluster_idx, core_idx) even though we have static guarantees in
    place as those guarantees are made in a separate compilation unit from
    the one in which the error occurs.
    
    This patch allows the compiler to inline the cluster/core count
    functions to ensure it can properly ascertain the proper bounds on the
    inputs to snprintf().
    
    Change-Id: Ie332561573d33a38063863fa7a4423c0c82543c4
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

--- product/clark/include/clark_core.h
+++ product/clark/include/clark_core.h
@@ -8,10 +8,30 @@
 #ifndef CLARK_CORE_H
 #define CLARK_CORE_H
 
+#include <fwk_assert.h>
+
 #define CLARK_CORE_PER_CLUSTER_MAX 8
 
-unsigned int clark_core_get_core_per_cluster_count(unsigned int cluster);
-unsigned int clark_core_get_core_count(void);
-unsigned int clark_core_get_cluster_count(void);
+/* Clark only has one configuration, hence the constant values */
+#define CORES_PER_CLUSTER   4
+#define NUMBER_OF_CLUSTERS  2
+
+static inline unsigned int clark_core_get_cluster_count(void)
+{
+    return NUMBER_OF_CLUSTERS;
+}
+
+static inline unsigned int clark_core_get_core_per_cluster_count(
+    unsigned int cluster)
+{
+    fwk_assert(cluster < clark_core_get_cluster_count());
+
+    return CORES_PER_CLUSTER;
+}
+
+static inline unsigned int clark_core_get_core_count(void)
+{
+    return NUMBER_OF_CLUSTERS * CORES_PER_CLUSTER;
+}
 
 #endif /* CLARK_CORE_H */
