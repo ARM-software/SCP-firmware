e939a0c54c22b065583773535a038cb40022c6e5
Manoj Kumar <manoj.kumar3@arm.com>
2019-01-18 11:11:50 +0530

    n1sdp: add fixes for pcie driver
    This patch fixes:
     - Type 0 & 1 configuration for both PCIe & CCIX RCs
     - Enable Type 1 MEM & IO bars in root ports
     - Log negotiated speed and lane count
    
    Change-Id: Id3a9996ff79e9db30aac19756df7df48a2273478
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

--- product/n1sdp/module/n1sdp_pcie/src/mod_n1sdp_pcie.c
+++ product/n1sdp/module/n1sdp_pcie/src/mod_n1sdp_pcie.c
@@ -9,6 +9,7 @@
 #include <fwk_assert.h>
 #include <fwk_errno.h>
 #include <fwk_id.h>
+#include <fwk_math.h>
 #include <fwk_mm.h>
 #include <fwk_module.h>
 #include <fwk_module_idx.h>
@@ -92,14 +93,39 @@ struct n1sdp_pcie_ctx pcie_ctx;
 static int n1sdp_pcie_setup(struct n1sdp_pcie_dev_ctx *dev_ctx)
 {
     uint32_t ecam_base_addr;
+    uint8_t neg_config;
+    struct pcie_wait_condition_data wait_data;
     int status;
 
     /* Enable the CCIX/PCIe controller */
+    wait_data.ctrl_apb = NULL;
     if (dev_ctx->config->ccix_capable) {
         SCC->CCIX_PM_CTRL = SCC_CCIX_PM_CTRL_PWR_REQ_POS;
+        wait_data.stage = PCIE_INIT_STAGE_CCIX_POWER_ON;
+        status = pcie_ctx.timer_api->wait(FWK_ID_ELEMENT(FWK_MODULE_IDX_TIMER,
+                                                         0),
+                                          PCIE_POWER_ON_TIMEOUT,
+                                          pcie_wait_condition,
+                                          &wait_data);
+        if (status != FWK_SUCCESS) {
+            pcie_ctx.log_api->log(MOD_LOG_GROUP_INFO,
+                "[PCIe] Controller power-on failed!\n");
+            return status;
+        }
         SCC->SYS_MAN_RESET &= ~(1 << SCC_SYS_MAN_RESET_CCIX_POS);
     } else {
         SCC->PCIE_PM_CTRL = SCC_PCIE_PM_CTRL_PWR_REQ_POS;
+        wait_data.stage = PCIE_INIT_STAGE_PCIE_POWER_ON;
+        status = pcie_ctx.timer_api->wait(FWK_ID_ELEMENT(FWK_MODULE_IDX_TIMER,
+                                                         0),
+                                          PCIE_POWER_ON_TIMEOUT,
+                                          pcie_wait_condition,
+                                          &wait_data);
+        if (status != FWK_SUCCESS) {
+            pcie_ctx.log_api->log(MOD_LOG_GROUP_INFO,
+                "[PCIe] Controller power-on failed!\n");
+            return status;
+        }
         SCC->SYS_MAN_RESET &= ~(1 << SCC_SYS_MAN_RESET_PCIE_POS);
     }
 
@@ -138,10 +164,25 @@ static int n1sdp_pcie_setup(struct n1sdp_pcie_dev_ctx *dev_ctx)
     }
     pcie_ctx.log_api->log(MOD_LOG_GROUP_INFO, "Done\n");
 
+    neg_config = (dev_ctx->ctrl_apb->RP_CONFIG_OUT &
+        RP_CONFIG_OUT_NEGOTIATED_SPD_MASK) >> RP_CONFIG_OUT_NEGOTIATED_SPD_POS;
+    pcie_ctx.log_api->log(MOD_LOG_GROUP_INFO,
+        "[PCIe] Negotiated speed: GEN%d\n", neg_config + 1);
+
+    neg_config = (dev_ctx->ctrl_apb->RP_CONFIG_OUT &
+        RP_CONFIG_OUT_NEGOTIATED_LINK_WIDTH_MASK) >>
+        RP_CONFIG_OUT_NEGOTIATED_LINK_WIDTH_POS;
+    pcie_ctx.log_api->log(MOD_LOG_GROUP_INFO,
+        "[PCIe] Negotiated link width: x%d\n", fwk_math_pow2(neg_config));
+
     /* Root Complex setup */
     pcie_ctx.log_api->log(MOD_LOG_GROUP_INFO,
                           "[PCIe] Setup Type0 configuration...");
-    ecam_base_addr = dev_ctx->config->axi_slave_base32 +
+    if (dev_ctx->config->ccix_capable)
+        ecam_base_addr = dev_ctx->config->axi_slave_base32 +
+                         CCIX_AXI_ECAM_TYPE0_OFFSET;
+    else
+        ecam_base_addr = dev_ctx->config->axi_slave_base32 +
                          PCIE_AXI_ECAM_TYPE0_OFFSET;
     status = axi_outbound_region_setup(dev_ctx->rc_axi_config_apb,
                  (ecam_base_addr - SCP_AP_AXI_OFFSET),
@@ -155,7 +196,11 @@ static int n1sdp_pcie_setup(struct n1sdp_pcie_dev_ctx *dev_ctx)
 
     pcie_ctx.log_api->log(MOD_LOG_GROUP_INFO,
         "[PCIe] Setup Type1 configuration...");
-    ecam_base_addr = dev_ctx->config->axi_slave_base32 +
+    if (dev_ctx->config->ccix_capable)
+        ecam_base_addr = dev_ctx->config->axi_slave_base32 +
+                         CCIX_AXI_ECAM_TYPE1_OFFSET;
+    else
+        ecam_base_addr = dev_ctx->config->axi_slave_base32 +
                          PCIE_AXI_ECAM_TYPE1_OFFSET;
     status = axi_outbound_region_setup(dev_ctx->rc_axi_config_apb,
                  (ecam_base_addr - SCP_AP_AXI_OFFSET),
@@ -236,6 +281,14 @@ static int n1sdp_pcie_setup(struct n1sdp_pcie_dev_ctx *dev_ctx)
         return status;
     }
     pcie_ctx.log_api->log(MOD_LOG_GROUP_INFO, "Done\n");
+
+    pcie_ctx.log_api->log(MOD_LOG_GROUP_INFO,
+        "[PCIe] Enable Type 1 I/O configuration\n");
+    *(uint32_t *)(dev_ctx->lm_apb + PCIE_LM_RC_BAR_CONFIG_REG) =
+        (TYPE1_PREF_MEM_BAR_ENABLE_MASK |
+         TYPE1_PREF_MEM_BAR_SIZE_64BIT_MASK |
+         TYPE1_PREF_IO_BAR_ENABLE_MASK |
+         TYPE1_PREF_IO_BAR_SIZE_32BIT_MASK);
 
     return FWK_SUCCESS;
 }
