ea8bf6883432e069fcb5092e146607289bcebde6
Manoj Kumar <manoj.kumar3@arm.com>
2019-01-21 18:40:54 +0530

    n1sdp/n1sdp_ddr_phy: add DDR PHY module for N1SDP platform
    Change-Id: I52092002f282daf0848d7c118c2620002923dd9f
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/n1sdp/module/n1sdp_ddr_phy/include/mod_n1sdp_ddr_phy.h
@@ -0,0 +1,44 @@
+ /*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     N1SDP DDR-PHY driver
+ */
+#ifndef MOD_N1SDP_DDR_PHY_H
+#define MOD_N1SDP_DDR_PHY_H
+
+#include <stdint.h>
+
+/*!
+ * \addtogroup GroupN1SDPModule N1SDP Product Modules
+ * @{
+ */
+
+/*!
+ * \defgroup GroupModuleN1SDPDDRPhy N1SDP DDR PHY Driver
+ *
+ * \brief Driver support for N1SDP DDR PHY instances.
+ *
+ * \{
+ */
+
+/*!
+ * \brief Element configuration.
+ */
+struct mod_n1sdp_ddr_phy_element_config {
+    /*! Base address of a device configuration register. */
+    uintptr_t ddr;
+};
+
+/*!
+ * @}
+ */
+
+/*!
+ * @}
+ */
+
+#endif /* MOD_N1SDP_DDR_PHY_H */
