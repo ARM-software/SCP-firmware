61d7b46d405566454935fb9bcfa34400dd585782
Manoj Kumar <manoj.kumar3@arm.com>
2019-01-21 18:42:43 +0530

    n1sdp: add config files for DMC620 & DDR PHY and include in build
    Change-Id: Ia796a27999ffbabaeccde75acf03d668039455e8
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/n1sdp/scp_ramfw/config_n1sdp_ddr_phy.c
@@ -0,0 +1,38 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <fwk_element.h>
+#include <fwk_module.h>
+#include <internal/n1sdp_ddr_phy.h>
+#include <mod_n1sdp_ddr_phy.h>
+#include <n1sdp_scp_mmap.h>
+#include <config_clock.h>
+
+/* Table of N1SDP DDR PHY element descriptions. */
+static struct fwk_element n1sdp_ddr_phy_element_table[] = {
+    [0] = { .name = "DDR_PHY-0",
+            .data = &((struct mod_n1sdp_ddr_phy_element_config) {
+                .ddr = SCP_DDR_PHY0,
+        }),
+    },
+    [1] = { .name = "DDR_PHY-1",
+            .data = &((struct mod_n1sdp_ddr_phy_element_config) {
+                .ddr = SCP_DDR_PHY1,
+        }),
+    },
+    [2] = { 0 }, /* Termination description. */
+};
+
+static const struct fwk_element *n1sdp_ddr_phy_get_element_table
+    (fwk_id_t module_id)
+{
+    return n1sdp_ddr_phy_element_table;
+}
+
+/* Configuration of the N1SDP DDR PHY module. */
+const struct fwk_module_config config_n1sdp_ddr_phy = {
+    .get_element_table = n1sdp_ddr_phy_get_element_table,
+};
---
+++ product/n1sdp/scp_ramfw/config_n1sdp_dmc620.c
@@ -0,0 +1,52 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_element.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+#include <mod_n1sdp_dmc620.h>
+#include <n1sdp_scp_mmap.h>
+#include <config_clock.h>
+
+/* Table of DMC620 elements descriptions. */
+static struct fwk_element dmc620_element_table[] = {
+    [0] = { .name = "DMC620-0",
+            .data = &((struct mod_dmc620_element_config) {
+                .dmc = SCP_DMC0,
+                .ddr_id = FWK_ID_ELEMENT_INIT(
+                    FWK_MODULE_IDX_N1SDP_DDR_PHY, 0),
+                .clock_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_CLOCK,
+                    CLOCK_IDX_INTERCONNECT),
+        }),
+    },
+    [1] = { .name = "DMC620-1",
+            .data = &((struct mod_dmc620_element_config) {
+                .dmc = SCP_DMC1,
+                .ddr_id = FWK_ID_ELEMENT_INIT(
+                    FWK_MODULE_IDX_N1SDP_DDR_PHY, 1),
+                .clock_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_CLOCK,
+                    CLOCK_IDX_INTERCONNECT),
+        }),
+    },
+    [2] = { 0 }, /* Termination description. */
+};
+
+static const struct fwk_element *dmc620_get_element_table(fwk_id_t module_id)
+{
+    return dmc620_element_table;
+}
+
+/* Configuration of the DMC620 module. */
+const struct fwk_module_config config_n1sdp_dmc620 = {
+    .get_element_table = dmc620_get_element_table,
+    .data = &((struct mod_dmc620_module_config) {
+            .ddr_module_id = FWK_ID_MODULE_INIT(
+                FWK_MODULE_IDX_N1SDP_DDR_PHY),
+            .ddr_api_id = FWK_ID_API_INIT(FWK_MODULE_IDX_N1SDP_DDR_PHY,
+                0),
+        }),
+};
