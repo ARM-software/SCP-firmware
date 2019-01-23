e6cd479308b8799c437d2d79b46a6c554ea625b2
Nicola Mazzucato <nicola.mazzucato@arm.com>
2019-01-23 14:04:21 +0000

    clock: Add full support for asynchronous drivers
    This patch adds support for handling non-blocking
    drivers that defer the requested operation as well
    as the responses to be sent to the requesters.
    A caller to this HAL must handle the response event
    if pending status is returned from the request.
    
    Change-Id: Ida0cbce6387183f11c5c6eed0317985f57372da8
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ module/clock/src/clock.h
@@ -0,0 +1,31 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef CLOCK_H
+#define CLOCK_H
+
+#include <fwk_id.h>
+#include <mod_clock.h>
+
+/*
+ * Clock event indexes.
+ */
+enum clock_event_idx {
+    CLOCK_EVENT_IDX_REQUEST = MOD_CLOCK_EVENT_IDX_REQUEST,
+    CLOCK_EVENT_IDX_RESPONSE,
+    CLOCK_EVENT_IDX_COUNT
+};
+
+/*
+ * Event identifiers.
+ */
+
+/* Identifier of the response event */
+static const fwk_id_t mod_clock_event_id_response =
+    FWK_ID_EVENT_INIT(FWK_MODULE_IDX_CLOCK, CLOCK_EVENT_IDX_RESPONSE);
+
+#endif /* CLOCK_H */
