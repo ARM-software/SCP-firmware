614d2b0dc7d6c9067917014d8512cfdedfcdb8f8
Nicola Mazzucato <nicola.mazzucato@arm.com>
2019-02-06 10:15:35 +0000

    sgm775: Use DDR product-specific modules
    Change-Id: I2c44367f333ccc1349136b38d32e4d27a7e0df14
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/sgm775/scp_ramfw/config_sgm775_ddr_phy500.c
@@ -0,0 +1,48 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2015-2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_element.h>
+#include <fwk_module.h>
+#include <mod_sgm775_ddr_phy500.h>
+#include <system_mmap.h>
+
+/* Table of DDR PHY500 element descriptions. */
+static struct fwk_element sgm775_ddr_phy500_element_table[] = {
+    [0] = { .name = "DDR_PHY500-0",
+            .data = &((struct mod_sgm775_ddr_phy500_element_config) {
+                .ddr = DDR_PHY0,
+        }),
+    },
+    [1] = { .name = "DDR_PHY500-1",
+            .data = &((struct mod_sgm775_ddr_phy500_element_config) {
+                .ddr = DDR_PHY1,
+        }),
+    },
+    [2] = { .name = "DDR_PHY500-2",
+            .data = &((struct mod_sgm775_ddr_phy500_element_config) {
+                .ddr = DDR_PHY2,
+        }),
+    },
+    [3] = { .name = "DDR_PHY500-3",
+            .data = &((struct mod_sgm775_ddr_phy500_element_config) {
+                .ddr = DDR_PHY3,
+        }),
+    },
+    [4] = { 0 }, /* Termination description. */
+};
+
+static const struct fwk_element *sgm775_ddr_phy500_get_element_table
+    (fwk_id_t module_id)
+{
+    return sgm775_ddr_phy500_element_table;
+}
+
+/* Configuration of the DDR PHY500 module. */
+struct fwk_module_config config_sgm775_ddr_phy500 = {
+    .get_element_table = sgm775_ddr_phy500_get_element_table,
+    .data = NULL,
+};
