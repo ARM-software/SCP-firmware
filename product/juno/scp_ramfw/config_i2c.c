331c9881cc17da9bff69083fa2818a777ada4193
Elieva Pignat <Elieva.Pignat@arm.com>
2019-02-13 14:07:20 +0000

    juno: add i2c support
    This patch adds the I2C HAL module and the I2C controller configuration
    for juno and adds them to the build.
    
    Change-Id: I031c52d5303b19d223736015eceb656ea9b789db
    Signed-off-by: Elieva Pignat <Elieva.Pignat@arm.com>

---
+++ product/juno/scp_ramfw/config_i2c.c
@@ -0,0 +1,58 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+#include <mod_dw_apb_i2c.h>
+#include <mod_i2c.h>
+#include <juno_irq.h>
+#include <juno_mmap.h>
+
+static const struct fwk_element dw_apb_i2c_element_table[] = {
+    [0] = {
+        .name = "DW APB I2C",
+        .data = &(struct mod_dw_apb_i2c_dev_config) {
+            .timer_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_TIMER, 0),
+            .i2c_irq = I2C_IRQ,
+            .reg = (uintptr_t)I2C_BASE,
+        }
+    },
+    [1] = {0},
+};
+
+static const struct fwk_element *dw_apb_i2c_get_element_table(
+    fwk_id_t module_id)
+{
+    return dw_apb_i2c_element_table;
+}
+
+struct fwk_module_config config_dw_apb_i2c = {
+    .get_element_table = dw_apb_i2c_get_element_table,
+};
+
+static const struct fwk_element i2c_element_table[] = {
+    [0] = {
+        .name = "I2C",
+        .data = &(struct mod_i2c_dev_config) {
+            .driver_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_DW_APB_I2C, 0),
+            .api_id = FWK_ID_API_INIT(FWK_MODULE_IDX_DW_APB_I2C,
+                MOD_DW_APB_I2C_API_IDX_DRIVER),
+        },
+    },
+    [1] = {0},
+};
+
+static const struct fwk_element *i2c_get_element_table(fwk_id_t module_id)
+{
+    return i2c_element_table;
+}
+
+struct fwk_module_config config_i2c = {
+    .get_element_table = i2c_get_element_table,
+};
