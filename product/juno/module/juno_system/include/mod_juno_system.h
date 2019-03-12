7a87e50506f75838896d37b0409382d47998bfcc
Nicola Mazzucato <nicola.mazzucato@arm.com>
2019-03-12 15:54:21 +0000

    juno/module: Add juno_system module
    Change-Id: I2a9404ae652b32479236516ccd1e7f0d8b43e747
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/juno/module/juno_system/include/mod_juno_system.h
@@ -0,0 +1,21 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MOD_JUNO_SYSTEM_H
+#define MOD_JUNO_SYSTEM_H
+
+/*!
+ * \ingroup GroupJunoModule
+ * \defgroup GroupJunoSystem Juno System
+ * \{
+ */
+
+/*!
+ * \}
+ */
+
+#endif /* MOD_JUNO_SYSTEM_H */
