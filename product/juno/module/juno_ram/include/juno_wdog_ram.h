0843cc101485f09ce053012a8b1071af7b4340ac
Nicola Mazzucato <nicola.mazzucato@arm.com>
2019-03-21 10:49:39 +0000

    juno_ram: Add watchdog
    Change-Id: Iac150e0e08e02dc28b583dd7c3c0c87ee9debb48
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/juno/module/juno_ram/include/juno_wdog_ram.h
@@ -0,0 +1,13 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef JUNO_WDOG_RAM_H
+#define JUNO_WDOG_RAM_H
+
+void juno_wdog_ram_disable(void);
+
+#endif /* JUNO_WDOG_RAM_H */
