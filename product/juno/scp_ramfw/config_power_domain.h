c1eaa383441507957c762d849afcd0a081c91b6c
Nicola Mazzucato <nicola.mazzucato@arm.com>
2019-05-09 15:37:29 +0100

    juno/ramfw: Add power_domain config
    Change-Id: Id3f4e5ad8793ea581ad39d666a2f60944ab513c3
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/juno/scp_ramfw/config_power_domain.h
@@ -0,0 +1,38 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef CONFIG_POWER_DOMAIN_H
+#define CONFIG_POWER_DOMAIN_H
+
+/*
+ * Power domain element indexes defined in increasing order of power domain
+ * level.
+ */
+
+enum power_domain_idx {
+    /* Level 0 */
+    POWER_DOMAIN_IDX_BIG_CPU0,
+    POWER_DOMAIN_IDX_BIG_CPU1,
+    POWER_DOMAIN_IDX_LITTLE_CPU0,
+    POWER_DOMAIN_IDX_LITTLE_CPU1,
+    POWER_DOMAIN_IDX_LITTLE_CPU2,
+    POWER_DOMAIN_IDX_LITTLE_CPU3,
+
+    /* Level 1 */
+    POWER_DOMAIN_IDX_BIG_SSTOP,
+    POWER_DOMAIN_IDX_LITTLE_SSTOP,
+    POWER_DOMAIN_IDX_DBGSYS,
+    POWER_DOMAIN_IDX_GPUTOP,
+
+    /* Level 2 */
+    POWER_DOMAIN_IDX_SYSTOP,
+
+    /* Number of defined elements */
+    POWER_DOMAIN_IDX_COUNT,
+};
+
+#endif /* CONFIG_POWER_DOMAIN_H */
