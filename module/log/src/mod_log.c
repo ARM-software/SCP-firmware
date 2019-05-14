b09e6a57314b0143532a8e61b54f0e467162a5d5
Elieva Pignat <Elieva.Pignat@arm.com>
2019-05-14 10:52:58 +0100

    log: add support to print uint64_t
    This patch adds support to print uint64_t variable using
    the log module with the %lu argument specifier.
    
    Change-Id: Icf4d89c8f8511f8dc8931a351daf38c66d4deae1
    Signed-off-by: Elieva Pignat <Elieva.Pignat@arm.com>

--- module/log/src/mod_log.c
+++ module/log/src/mod_log.c
@@ -199,9 +199,9 @@ next_symbol:
 
             case 'u':
                 if (bit64)
-                    return FWK_E_DATA;
-
-                unum = va_arg(*args, uint32_t);
+                    unum = va_arg(*args, uint64_t);
+                else
+                    unum = va_arg(*args, uint32_t);
 
                 status = print_uint64(unum, 10, fill);
                 if (status != FWK_SUCCESS)
