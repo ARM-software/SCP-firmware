82cc94b1b718fc917acfec50030183b3bbbfe0ae
Elieva Pignat <Elieva.Pignat@arm.com>
2019-06-12 13:45:02 +0100

    juno: Add SoC clock support for RAM
    This patch adds the SoC clock support for Juno board and
    the management of the cluster clocks.
    
    Change-Id: Ice954f4628d0c2535586f469efac84d42957e711
    Co-authored-by: Ronald Cron <Ronald.Cron@arm.com>
    Signed-off-by: Elieva Pignat <Elieva.Pignat@arm.com>
    Signed-off-by: Jim Quigley <jim.quigley@arm.com>
    Signed-off-by: Raphael Gault <raphael.gault@arm.com>

---
+++ product/juno/module/juno_soc_clock_ram/include/mod_juno_soc_clock_ram.h
@@ -0,0 +1,119 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MOD_JUNO_SOC_CLOCK_RAM_H
+#define MOD_JUNO_SOC_CLOCK_RAM_H
+
+#include <stdint.h>
+#include <fwk_id.h>
+#include <fwk_macros.h>
+#include <fwk_module_idx.h>
+
+/*!
+ * \addtogroup GroupModules Modules
+ * @{
+ */
+
+/*!
+ * \defgroup GroupJunoSocClockRam Juno Soc Clock Ram Driver
+ *
+ * \details A driver for Juno Soc clock devices for GPU and
+ *          big/little core clusters.
+ *
+ * @{
+ */
+
+/*! Juno SoC clock API indexes */
+enum mod_juno_soc_clock_ram_api_idx {
+    MOD_JUNO_SOC_CLOCK_RAM_API_IDX_DRIVER,
+    MOD_JUNO_SOC_CLOCK_RAM_API_IDX_COUNT,
+};
+
+static const fwk_id_t mod_juno_soc_clock_ram_api_id_driver =
+    FWK_ID_API_INIT(FWK_MODULE_IDX_JUNO_SOC_CLOCK_RAM,
+        MOD_JUNO_SOC_CLOCK_RAM_API_IDX_DRIVER);
+
+/*!
+ * \brief Juno SoC clock module configuration.
+ */
+struct mod_juno_soc_clock_ram_config {
+    /*!
+     *  Identifier of the timer to use for timeout checking.
+     */
+    fwk_id_t timer_id;
+
+    /*!
+     * Identifier of the debug power domain.
+     */
+    fwk_id_t debug_pd_id;
+
+    /*!
+     * Identifier of the systop power domain.
+     */
+    fwk_id_t systop_pd_id;
+};
+
+/*!
+ * \brief PLL setting definition.
+ */
+struct juno_soc_clock_ram_pll_setting {
+    /*! PLL feedback divider */
+    uint16_t nf;
+
+    /*! PLL reference clock divider */
+    uint8_t nr;
+
+    /*! PLL output divider */
+    uint8_t od;
+};
+
+/*!
+ * \brief Rate lookup table entry.
+ */
+struct juno_soc_clock_ram_rate {
+    /*! Rate of the clock in Hertz. */
+    uint32_t rate;
+
+    /*! The clock source used to attain the rate. */
+    uint32_t source;
+
+    /*! The clock divider used to attain the rate. */
+    unsigned int divider;
+
+    /*!
+     *  Dedicated PLL setting to be used to attain the rate if the PLL is the
+     *  source of the clock.
+     */
+    struct juno_soc_clock_ram_pll_setting pll_setting;
+};
+
+/*!
+ * \brief Juno SoC clock device configuration.
+ */
+struct mod_juno_soc_clock_ram_dev_config {
+    /*! Pointer to the registers of the dedicated PLL. */
+    struct pll_reg *pll;
+
+    /*! Pointer to the clock control register. */
+    FWK_RW uint32_t *clock_control;
+
+    /*! Pointer to the clock's rate lookup table. */
+    const struct juno_soc_clock_ram_rate *rate_table;
+
+    /*! The number of rates in the rate lookup table. */
+    unsigned int rate_count;
+};
+
+/*!
+ * @}
+ */
+
+/*!
+ * @}
+ */
+
+#endif /* MOD_JUNO_SOC_CLOCK_RAM_H */
---
+++ product/juno/module/juno_soc_clock_ram/src/juno_soc_clock_ram_pll.h
@@ -0,0 +1,25 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef JUNO_SOC_CLOCK_RAM_PLL_H
+#define JUNO_SOC_CLOCK_RAM_PLL_H
+
+#include <stdint.h>
+#include <mod_juno_soc_clock_ram.h>
+#include <juno_scc.h>
+
+void juno_soc_clock_ram_pll_init(void);
+
+int juno_soc_clock_ram_pll_set(
+    struct pll_reg *pll,
+    const struct juno_soc_clock_ram_pll_setting *pll_setting);
+
+int juno_soc_clock_ram_pll_get(struct pll_reg *pll,
+                               uint32_t refclk_hz,
+                               uint32_t *freq_hz);
+
+#endif /* JUNO_SOC_CLOCK_RAM_PLL_H */
