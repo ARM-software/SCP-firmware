6d6fcd7962b56f3c0ebff6fc617585772502d3fd
Nicola Mazzucato <nicola.mazzucato@arm.com>
2019-06-24 13:39:02 +0100

    juno/ramfw: Add ADC sensor configuration
    This patch adds the configuration of the
    ADC sensor elements for the Juno board.
    
    Change-Id: I97038224cbab6dd5af6fcffd0d31237bcee4e2f6
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/juno/scp_ramfw/config_juno_adc.c
@@ -0,0 +1,65 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_element.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+#include <mod_juno_adc.h>
+#include <mod_sensor.h>
+
+static const struct fwk_element adc_juno_element_table[] = {
+    [ADC_TYPE_CURRENT] = {
+        .name = "ADC Current",
+        .sub_element_count = ADC_DEV_TYPE_COUNT,
+        .data = &((struct mod_juno_adc_dev_config) {
+            .info = &((struct mod_sensor_info) {
+                .type = MOD_SENSOR_TYPE_AMPS,
+                .unit_multiplier = -3,
+            }),
+        }),
+    },
+    [ADC_TYPE_VOLT] = {
+        .name = "ADC Voltage",
+        .sub_element_count = ADC_DEV_TYPE_COUNT,
+        .data = &((struct mod_juno_adc_dev_config) {
+            .info = &((struct mod_sensor_info) {
+                .type = MOD_SENSOR_TYPE_VOLTS,
+                .unit_multiplier = -3,
+            }),
+        }),
+    },
+    [ADC_TYPE_POWER] = {
+        .name = "ADC Power",
+        .sub_element_count = ADC_DEV_TYPE_COUNT,
+        .data = &((struct mod_juno_adc_dev_config) {
+            .info = &((struct mod_sensor_info) {
+                .type = MOD_SENSOR_TYPE_WATTS,
+                .unit_multiplier = -6,
+            }),
+        }),
+    },
+    [ADC_TYPE_ENERGY] = {
+        .name = "ADC Energy",
+        .sub_element_count = ADC_DEV_TYPE_COUNT,
+        .data = &((struct mod_juno_adc_dev_config) {
+            .info = &((struct mod_sensor_info) {
+                .type = MOD_SENSOR_TYPE_JOULES,
+                .unit_multiplier = -6,
+            }),
+        }),
+    },
+    [ADC_TYPE_COUNT] = { 0 },
+};
+
+static const struct fwk_element *get_adc_juno_element_table(fwk_id_t id)
+{
+    return adc_juno_element_table;
+}
+
+struct fwk_module_config config_juno_adc = {
+    .get_element_table = get_adc_juno_element_table,
+};
