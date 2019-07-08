e9b605049e0b3b08083d363b291cedcd9557e3df
Elieva Pignat <Elieva.Pignat@arm.com>
2019-07-08 09:34:57 +0100

    juno_xrp7724: Add temperature sensor support
    This patch adds the support for the temperature sensor of the
    XRP7724 component of the Juno board.
    
    It provides an implementation for the sensor driver API and
    uses the I2C HAL API to make sensor requests to the XRP7724
    component. It adds the temperature sensor to the configuration
    of the XRP7724 module on Juno.
    
    Change-Id: Ic6d1e47328b118eb648ba2c4869cde9f1b3866fc
    Signed-off-by: Elieva Pignat <Elieva.Pignat@arm.com>
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

--- product/juno/scp_ramfw/config_juno_xrp7724.c
+++ product/juno/scp_ramfw/config_juno_xrp7724.c
@@ -11,12 +11,10 @@
 #include <fwk_module_idx.h>
 #include <mod_i2c.h>
 #include <mod_juno_xrp7724.h>
+#include <mod_sensor.h>
 #include <mod_timer.h>
-
-enum mod_juno_xrp7724_element_idx {
-    MOD_JUNO_XRP7724_ELEMENT_IDX_GPIO,
-    MOD_JUNO_XRP7724_ELEMENT_IDX_COUNT
-};
+#include <config_sensor.h>
+#include <config_juno_xrp7724.h>
 
 enum mod_juno_xrp7724_gpio_idx  {
     MOD_JUNO_XRP7724_GPIO_IDX_ASSERT,
@@ -24,7 +22,28 @@ enum mod_juno_xrp7724_gpio_idx  {
     MOD_JUNO_XRP7724_GPIO_IDX_COUNT,
 };
 
+/*
+ * Temperature sensor information
+ */
+static struct mod_sensor_info juno_xrp7724_info_temperature = {
+    .type = MOD_SENSOR_TYPE_DEGREES_C,
+    .update_interval = 0,
+    .update_interval_multiplier = 0,
+    .unit_multiplier = -3,
+};
+
 static const struct fwk_element juno_xrp7724_element_table[] = {
+    [MOD_JUNO_XRP7724_ELEMENT_IDX_SENSOR] = {
+        .name = "TEMPERATURE SENSOR",
+        .data = &(const struct mod_juno_xrp7724_dev_config) {
+            .driver_response_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_SENSOR,
+                MOD_JUNO_SENSOR_XRP7724_PMIC_TEMP_IDX),
+            .driver_response_api_id = FWK_ID_API_INIT(FWK_MODULE_IDX_SENSOR,
+                MOD_SENSOR_API_IDX_DRIVER_RESPONSE),
+            .sensor_info = &juno_xrp7724_info_temperature,
+            .type = MOD_JUNO_XRP7724_ELEMENT_TYPE_SENSOR,
+        },
+    },
     [MOD_JUNO_XRP7724_ELEMENT_IDX_GPIO] = {
         .name = "GPIOs",
         .sub_element_count = MOD_JUNO_XRP7724_GPIO_IDX_COUNT,
---
+++ product/juno/scp_ramfw/config_juno_xrp7724.h
@@ -0,0 +1,18 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef CONFIG_JUNO_XRP7724_H
+#define CONFIG_JUNO_XRP7724_H
+
+/* Element indices for Juno XRP7724 */
+enum mod_juno_xrp7724_element_idx {
+    MOD_JUNO_XRP7724_ELEMENT_IDX_SENSOR,
+    MOD_JUNO_XRP7724_ELEMENT_IDX_GPIO,
+    MOD_JUNO_XRP7724_ELEMENT_IDX_COUNT
+};
+
+#endif /* CONFIG_JUNO_XRP7724_H */
