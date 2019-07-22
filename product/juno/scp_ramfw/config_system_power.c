1c7f8380473e7e43c464679584bfca13b5aedf31
Nicola Mazzucato <nicola.mazzucato@arm.com>
2019-07-22 13:55:25 +0100

    juno/system_power: Remove GPU from the list of external PPUs
    This patch removes a previous workaround not required
    anymore.
    
    Change-Id: Ie1c6ebfea6c06163868049c3cc36990cee69e83f
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

--- product/juno/scp_ramfw/config_system_power.c
+++ product/juno/scp_ramfw/config_system_power.c
@@ -15,19 +15,6 @@
 #include <mod_juno_ppu.h>
 #include <mod_system_power.h>
 
-/*
- * These PPUs in this list are there as a temporary workaround, until
- * Linux supports handling these domains on its own.
- */
-static const struct mod_system_power_ext_ppu_config ext_ppus[] = {
-    {
-        .ppu_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_JUNO_PPU,
-            JUNO_PPU_DEV_IDX_GPUTOP),
-        .api_id = FWK_ID_API_INIT(FWK_MODULE_IDX_JUNO_PPU,
-            MOD_JUNO_PPU_API_IDX_PD),
-    },
-};
-
 static const uint8_t system_power_to_sys_ppu_state[] = {
     [MOD_PD_STATE_ON]                     = (uint8_t)MOD_PD_STATE_ON,
     [MOD_SYSTEM_POWER_POWER_STATE_SLEEP0] =
@@ -59,8 +46,6 @@ static const struct fwk_element *system_power_get_element_table(
 const struct fwk_module_config config_system_power = {
     .data = &((struct mod_system_power_config) {
         .soc_wakeup_irq = EXT_WAKEUP_IRQ,
-        .ext_ppus = ext_ppus,
-        .ext_ppus_count = FWK_ARRAY_SIZE(ext_ppus),
         .driver_id = FWK_ID_MODULE_INIT(FWK_MODULE_IDX_JUNO_SYSTEM),
         .driver_api_id = FWK_ID_API_INIT(FWK_MODULE_IDX_JUNO_SYSTEM, 0),
         .initial_system_power_state = MOD_PD_STATE_ON,
