69f8589754f8179d4ea3c8898efa4f96738c8772
Elieva Pignat <Elieva.Pignat@arm.com>
2019-07-26 12:06:01 +0100

    juno/juno_clock.h: Add indices for the different clocks
    This patch adds enumeration of the indices for the different
    clocks available on the Juno platform.
    
    Change-Id: Iea0f7272d688ae2141f3ea62d817db058bc78b32
    Signed-off-by: Elieva Pignat <Elieva.Pignat@arm.com>
    Signed-off-by: Jim Quigley <jim.quigley@arm.com>
    Signed-off-by: Raphael Gault <raphael.gault@arm.com>
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

--- product/juno/include/juno_clock.h
+++ product/juno/include/juno_clock.h
@@ -20,6 +20,42 @@
  */
 #define JUNO_CLOCK_HDLCD_LOOKUP_HIGH_PXCLK_ENABLE_COUNT 746
 
+/* Juno clock indices */
+enum juno_clock_idx {
+    JUNO_CLOCK_IDX_I2SCLK,
+    JUNO_CLOCK_IDX_HDLCDREFCLK,
+    JUNO_CLOCK_IDX_HDLCDPXL,
+    JUNO_CLOCK_IDX_HDLCD0,
+    JUNO_CLOCK_IDX_HDLCD1,
+    JUNO_CLOCK_IDX_BIGCLK,
+    JUNO_CLOCK_IDX_LITTLECLK,
+    JUNO_CLOCK_IDX_GPUCLK,
+    JUNO_CLOCK_IDX_COUNT
+};
+
+/* Juno CDCEL937 clock indices */
+enum juno_clock_cdcel937_idx {
+    JUNO_CLOCK_CDCEL937_IDX_I2SCLK,
+    JUNO_CLOCK_CDCEL937_IDX_HDLCDREFCLK,
+    JUNO_CLOCK_CDCEL937_IDX_HDLCDPXL,
+    JUNO_CLOCK_CDCEL937_IDX_COUNT
+};
+
+/* Juno HDLCD clock indices */
+enum juno_clock_hdlcd_idx {
+    JUNO_CLOCK_HDLCD_IDX_HDLCD0,
+    JUNO_CLOCK_HDLCD_IDX_HDLCD1,
+    JUNO_CLOCK_HDLCD_IDX_COUNT
+};
+
+/* Juno SOC clock indices for the RAM firmware */
+enum juno_clock_soc_ram_idx {
+    JUNO_CLOCK_SOC_RAM_IDX_BIGCLK,
+    JUNO_CLOCK_SOC_RAM_IDX_LITTLECLK,
+    JUNO_CLOCK_SOC_RAM_IDX_GPUCLK,
+    JUNO_CLOCK_SOC_RAM_IDX_COUNT
+};
+
 struct juno_clock_preset {
     /*
      * \brief Denominator value
