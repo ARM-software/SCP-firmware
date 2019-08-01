980c6740d8a03560decb8e92d99473a38e5362c7
Elieva Pignat <Elieva.Pignat@arm.com>
2019-08-01 16:38:59 +0100

    juno: Add PSU channels support for juno XRP7724
    This patch adds support for the PSU driver of the
    XRP7724 component of the juno board.
    
    It provides an implementation for the PSU driver API and
    uses the I2C HAL API to make PSU request to the XRP7724
    component.
    
    Change-Id: I2c0b910307de92e49f23336ca16606c1cf12fbcb
    Signed-off-by: Elieva Pignat <Elieva.Pignat@arm.com>
    Signed-off-by: Jim Quigley <jim.quigley@arm.com>

--- product/juno/scp_ramfw/config_psu.c
+++ product/juno/scp_ramfw/config_psu.c
@@ -5,6 +5,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <fwk_assert.h>
 #include <fwk_element.h>
 #include <fwk_module.h>
 #include <fwk_module_idx.h>
@@ -12,6 +13,48 @@
 #include <mod_mock_psu.h>
 #include <mod_psu.h>
 #include <config_juno_xrp7724.h>
+#include <config_psu.h>
+#include <juno_id.h>
+
+static struct fwk_element psu_dev_desc_table[] = {
+    [MOD_PSU_ELEMENT_IDX_VSYS] = {
+        .name = "VSYS",
+        .data = &(const struct mod_psu_element_cfg) {
+            .driver_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_JUNO_XRP7724,
+                MOD_JUNO_XRP7724_ELEMENT_IDX_PSU_VSYS),
+            .driver_api_id = FWK_ID_API_INIT(FWK_MODULE_IDX_JUNO_XRP7724,
+                MOD_JUNO_XRP7724_API_IDX_PSU),
+        },
+    },
+    [MOD_PSU_ELEMENT_IDX_VBIG] = {
+        .name = "VBIG",
+        .data = &(const struct mod_psu_element_cfg) {
+            .driver_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_JUNO_XRP7724,
+                MOD_JUNO_XRP7724_ELEMENT_IDX_PSU_VBIG),
+            .driver_api_id = FWK_ID_API_INIT(FWK_MODULE_IDX_JUNO_XRP7724,
+                MOD_JUNO_XRP7724_API_IDX_PSU),
+        },
+    },
+    [MOD_PSU_ELEMENT_IDX_VLITTLE] = {
+        .name = "VLITLLE",
+        .data = &(const struct mod_psu_element_cfg) {
+            .driver_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_JUNO_XRP7724,
+                MOD_JUNO_XRP7724_ELEMENT_IDX_PSU_VLITTLE),
+            .driver_api_id = FWK_ID_API_INIT(FWK_MODULE_IDX_JUNO_XRP7724,
+                MOD_JUNO_XRP7724_API_IDX_PSU),
+        },
+    },
+    [MOD_PSU_ELEMENT_IDX_VGPU] = {
+        .name = "VGPU",
+        .data = &(const struct mod_psu_element_cfg) {
+            .driver_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_JUNO_XRP7724,
+                MOD_JUNO_XRP7724_ELEMENT_IDX_PSU_VGPU),
+            .driver_api_id = FWK_ID_API_INIT(FWK_MODULE_IDX_JUNO_XRP7724,
+                MOD_JUNO_XRP7724_API_IDX_PSU),
+        },
+    },
+    [MOD_PSU_ELEMENT_IDX_COUNT] = { 0 },
+};
 
 /* When running on a model, use the mock_psu */
 static struct fwk_element psu_element_table_fvp[] = {
@@ -56,10 +99,19 @@ static struct fwk_element psu_element_table_fvp[] = {
 
 static const struct fwk_element *psu_get_dev_desc_table(fwk_id_t module_id)
 {
-    return psu_element_table_fvp;
+    int status;
+    enum juno_idx_platform platform_id = JUNO_IDX_PLATFORM_COUNT;
+
+    status = juno_id_get_platform(&platform_id);
+    if (!fwk_expect(status == FWK_SUCCESS))
+        return NULL;
+
+    if (platform_id == JUNO_IDX_PLATFORM_FVP)
+        return psu_element_table_fvp;
+    else
+        return psu_dev_desc_table;
 }
 
 struct fwk_module_config config_psu = {
     .get_element_table = psu_get_dev_desc_table,
-    .data = NULL,
 };
