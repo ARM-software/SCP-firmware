e0e66917c9c2b316331efd7d10ccf1c092cde11d
Chris Kay <chris.kay@arm.com>
2019-08-09 14:19:16 +0100

    juno/scp_romfw_bypass: Ensure system PLLs are locked within MCC timeout
    The current approach to resetting the PLLs, which is to use a pre-main
    constructor function, is not infallible - if we statically allocate
    enough data, eventually the time it takes for the C runtime to
    initialize exceeds the amount of time that the motherboard
    microcontroller gives us to ensure the system PLLs are locked.
    
    The workaround for this is to instead wrap the core's reset handler.
    This way we ensure that the PLLs have been reset before we do any time-
    consuming work.
    
    Change-Id: Ie74df40c8de2ea353ec048c447c85cc7d013b94b
    Signed-off-by: Chris Kay <chris.kay@arm.com>

---
+++ product/juno/scp_romfw_bypass/juno_pll_workaround.c
@@ -0,0 +1,39 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *      Workaround for the PLL lock error emitted by the motherboard, seen if
+ *      the SCP fails to lock the system PLLs within a certain (very short)
+ *      timeframe.
+ */
+
+#include <fwk_noreturn.h>
+#include <fwk_module.h>
+#include <juno_scc.h>
+
+#ifdef __ARMCC_VERSION
+#   define __wrap_arm_exception_reset $Sub$$arm_exception_reset
+#   define __real_arm_exception_reset $Super$$arm_exception_reset
+#endif
+
+/*
+ * These PLLs must be released from reset very shortly after the SCP is released
+ * from reset, otherwise the motherboard microcontroller will kill the SCP and
+ * error out. We do this at the earliest possible point in time in order to
+ * ensure nothing delays it from happening.
+ */
+noreturn void __wrap_arm_exception_reset(void)
+{
+    extern noreturn void __real_arm_exception_reset(void);
+
+    unsigned int pll_idx;
+
+    /* Release All system PLLs from reset */
+    for (pll_idx = 0; pll_idx < PLL_IDX_COUNT; pll_idx++)
+        SCC->PLL[pll_idx].REG0 &= ~PLL_REG0_PLL_RESET;
+
+    __real_arm_exception_reset();
+}
