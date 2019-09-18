2966a70a3866dd68f7802e036ab3fff5200a9f63
Jim Quigley <jim.quigley@arm.com>
2019-09-18 11:43:33 +0100

    juno: Add SCMI Performance Protocol to juno build
    Add SCMI Performance Protocol to juno platform build.
    
    Change-Id: I0cf3926d85cee89fb192da4786404f69db773dd6
    Signed-off-by: Jim Quigley <jim.quigley@arm.com>

---
+++ product/juno/scp_ramfw/config_scmi_perf.c
@@ -0,0 +1,47 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdint.h>
+#include <fwk_element.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+#include <config_dvfs.h>
+#include <juno_scmi.h>
+#include <mod_scmi.h>
+#include <mod_scmi_perf.h>
+
+static const struct mod_scmi_perf_domain_config domains[] = {
+    [DVFS_ELEMENT_IDX_LITTLE] = {
+        .permissions = &(const uint32_t[]) {
+            [JUNO_SCMI_AGENT_IDX_OSPM] = MOD_SCMI_PERF_PERMS_SET_LEVEL  |
+                                   MOD_SCMI_PERF_PERMS_SET_LIMITS,
+            [JUNO_SCMI_AGENT_IDX_PSCI] = 0 /* No Access */,
+        }
+    },
+    [DVFS_ELEMENT_IDX_BIG] = {
+        .permissions = &(const uint32_t[]) {
+            [JUNO_SCMI_AGENT_IDX_OSPM] = MOD_SCMI_PERF_PERMS_SET_LEVEL  |
+                                   MOD_SCMI_PERF_PERMS_SET_LIMITS,
+            [JUNO_SCMI_AGENT_IDX_PSCI] = 0 /* No Access */,
+        }
+    },
+    [DVFS_ELEMENT_IDX_GPU] = {
+        .permissions = &(const uint32_t[]) {
+            [JUNO_SCMI_AGENT_IDX_OSPM] = MOD_SCMI_PERF_PERMS_SET_LEVEL  |
+                                   MOD_SCMI_PERF_PERMS_SET_LIMITS,
+            [JUNO_SCMI_AGENT_IDX_PSCI] = 0 /* No Access */,
+        }
+    },
+};
+
+
+struct fwk_module_config config_scmi_perf = {
+    .get_element_table = NULL,
+    .data = &((struct mod_scmi_perf_config) {
+        .domains = &domains,
+    }),
+};
