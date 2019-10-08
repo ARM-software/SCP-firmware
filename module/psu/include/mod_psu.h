b67a9d46dc7bcb157066771242e8c3426226b71f
Chris Kay <chris.kay@arm.com>
2019-10-08 10:37:03 +0100

    psu: Add support for asynchronous drivers
    This commit adds the extra handling required to support drivers that may
    pend certain requests, as opposed to acting on them immediately.
    
    This extra support is intended for drivers that might otherwise busy-
    wait for extended periods, such as when they do not immediately know the
    result of an operation they have begun.
    
    Change-Id: I39aa9e4129d833044bd0e87b324c701d59a6908f
    Signed-off-by: Chris Kay <chris.kay@arm.com>

--- module/psu/include/mod_psu.h
+++ module/psu/include/mod_psu.h
@@ -21,14 +21,101 @@
  * \details The `psu` module represents an abstract interface through which
  *      power supply units can be used. It provides functions for manipulating
  *      the voltage as well as enabling/disabling the supply.
+ *
+ *      Users of the HAL interact with the individual power supply units through
+ *      the [device API](::mod_psu_device_api). This is the primary interface
+ *      that this module exposes and implements.
+ *
+ *      Separate to this is the [driver API](::mod_psu_driver_api), through
+ *      which the HAL will communicate with various driver implementations.
+ *      Driver implementations that need it may also utilise the [driver
+ *      response API](::mod_psu_driver_response_api), which allows power supply
+ *      units to pend requests and respond on-demand.
+ *
  * \{
  */
+
+/*!
+ * \brief API indices.
+ */
+enum mod_psu_api_idx {
+    /*!
+     * \brief Device API.
+     *
+     * \note This API identifier implements the ::mod_psu_device_api interface.
+     *
+     * \warning Binding to this API must occur through an element of this
+     *      module.
+     */
+    MOD_PSU_API_IDX_DEVICE,
+
+    /*!
+     * \brief Driver response API index.
+     *
+     * \note This API identifier implements the ::mod_psu_driver_response_api
+     *      interface.
+     *
+     * \warning Binding to this API must occur through an element of this
+     *      module.
+     */
+    MOD_PSU_API_IDX_DRIVER_RESPONSE,
+
+    /*!
+     * \brief Number of defined APIs.
+     */
+    MOD_PSU_API_IDX_COUNT
+};
+
+/*!
+ * \brief Event indices.
+ */
+enum mod_psu_event_idx {
+    /*!
+     * \brief Response event to a ::mod_psu_device_api::get_enabled call.
+     *
+     * \note This event identifier uses the ::mod_psu_response structure as its
+     *      event parameters. The ::mod_psu_response::enabled field is active.
+     */
+    MOD_PSU_EVENT_IDX_GET_ENABLED,
+
+    /*!
+     * \brief Response event to a ::mod_psu_device_api::set_enabled call.
+     *
+     * \note This event identifier uses the ::mod_psu_response structure as its
+     *      event parameters.
+     */
+    MOD_PSU_EVENT_IDX_SET_ENABLED,
+
+    /*!
+     * \brief Response event to a ::mod_psu_device_api::get_voltage call.
+     *
+     * \note This event identifier uses the ::mod_psu_response structure as its
+     *      event parameters. The ::mod_psu_response::voltage field is active.
+     */
+    MOD_PSU_EVENT_IDX_GET_VOLTAGE,
+
+    /*!
+     * \brief Response event to a ::mod_psu_device_api::set_voltage call.
+     *
+     * \note This event identifier uses the ::mod_psu_response structure as its
+     *      event parameters.
+     */
+    MOD_PSU_EVENT_IDX_SET_VOLTAGE,
+
+    /*!
+     * \brief Number of defined events.
+     */
+    MOD_PSU_EVENT_IDX_COUNT,
+};
 
 /*!
  * \brief Device API.
  *
  * \details The PSU device API represents the abstract interface used to control
  *      individual power supplies.
+ *
+ *      This is the primary interface through which users of the HAL will
+ *      interact with individual power supply units.
  */
 struct mod_psu_device_api {
     /*!
@@ -93,6 +180,92 @@ struct mod_psu_device_api {
 };
 
 /*!
+ * \brief Device API identifier.
+ *
+ * \note This identifier corresponds to the ::MOD_PSU_API_IDX_DEVICE API index.
+ */
+static const fwk_id_t mod_psu_api_id_device =
+    FWK_ID_API_INIT(FWK_MODULE_IDX_PSU, MOD_PSU_API_IDX_DEVICE);
+
+/*!
+ * \brief Response parameters.
+ *
+ * \details This structure is passed as the parameters to a response event
+ *      dispatched by the `psu` module.
+ */
+struct mod_psu_response {
+    /*!
+     * \brief Status code representing the result of the operation.
+     */
+    int status;
+
+    /*!
+     * \brief Response fields.
+     *
+     * \details These fields are used if the module is responding with extra
+     *      information. The activated field, if there is one, is based on the
+     *      event identifier of the response.
+     */
+    union {
+        /*!
+         * \brief `true` if the device is enabled, otherwise `false`.
+         *
+         * \warning Used only in response to a ::mod_psu_device_api::get_enabled
+         *      call.
+         */
+        bool enabled;
+
+        /*!
+         * \brief Voltage in millivolts (mV).
+         *
+         * \warning Used only in response to a ::mod_psu_device_api::get_voltage
+         *      call.
+         */
+        uint64_t voltage;
+    };
+};
+
+/*!
+ * \brief Identifier for a ::mod_psu_device_api::get_enabled call response
+ *      event.
+ *
+ * \note This identifier corresponds to the
+ *      ::MOD_PSU_EVENT_IDX_GET_ENABLED event index.
+ */
+static const fwk_id_t mod_psu_event_id_get_enabled = FWK_ID_EVENT_INIT(
+    FWK_MODULE_IDX_PSU, MOD_PSU_EVENT_IDX_GET_ENABLED);
+
+/*!
+ * \brief Identifier for a ::mod_psu_device_api::set_enabled call response
+ *      event.
+ *
+ * \note This identifier corresponds to the
+ *      ::MOD_PSU_EVENT_IDX_SET_ENABLED event index.
+ */
+static const fwk_id_t mod_psu_event_id_set_enabled = FWK_ID_EVENT_INIT(
+    FWK_MODULE_IDX_PSU, MOD_PSU_EVENT_IDX_SET_ENABLED);
+
+/*!
+ * \brief Identifier for a ::mod_psu_device_api::get_voltage call response
+ *      event.
+ *
+ * \note This identifier corresponds to the
+ *      ::MOD_PSU_EVENT_IDX_GET_VOLTAGE event index.
+ */
+static const fwk_id_t mod_psu_event_id_get_voltage = FWK_ID_EVENT_INIT(
+    FWK_MODULE_IDX_PSU, MOD_PSU_EVENT_IDX_GET_VOLTAGE);
+
+/*!
+ * \brief Identifier for a ::mod_psu_device_api::set_voltage call response
+ *      event.
+ *
+ * \note This identifier corresponds to the
+ *      ::MOD_PSU_EVENT_IDX_SET_VOLTAGE event index.
+ */
+static const fwk_id_t mod_psu_event_id_set_voltage = FWK_ID_EVENT_INIT(
+    FWK_MODULE_IDX_PSU, MOD_PSU_EVENT_IDX_SET_VOLTAGE);
+
+/*!
  * \brief Driver API.
  *
  * \details The PSU driver API represents the abstract interface that power
@@ -108,6 +281,7 @@ struct mod_psu_driver_api {
      *      disabled.
      *
      * \retval ::FWK_E_HANDLER The operation failed.
+     * \retval ::FWK_PENDING The result of the operation is pending.
      * \retval ::FWK_SUCCESS The operation succeeded.
      *
      * \return Status code representing the result of the operation.
@@ -121,6 +295,7 @@ struct mod_psu_driver_api {
      * \param[in] enable `true` to enable the device, or `false` to disable it.
      *
      * \retval ::FWK_E_HANDLER The operation failed.
+     * \retval ::FWK_PENDING The result of the operation is pending.
      * \retval ::FWK_SUCCESS The operation succeeded.
      *
      * \return Status code representing the result of the operation.
@@ -134,6 +309,7 @@ struct mod_psu_driver_api {
      * \param[out] voltage Voltage in millivolts (mV).
      *
      * \retval ::FWK_E_HANDLER The operation failed.
+     * \retval ::FWK_PENDING The result of the operation is pending.
      * \retval ::FWK_SUCCESS The operation succeeded.
      *
      * \return Status code representing the result of the operation.
@@ -147,12 +323,79 @@ struct mod_psu_driver_api {
      * \param[in] voltage New voltage in millivolts (mV).
      *
      * \retval ::FWK_E_HANDLER The operation failed.
+     * \retval ::FWK_PENDING The result of the operation is pending.
      * \retval ::FWK_SUCCESS The operation succeeded.
      *
      * \return Status code representing the result of the operation.
      */
     int (*set_voltage)(fwk_id_t id, uint64_t voltage);
 };
+
+/*!
+ * \brief Driver response.
+ *
+ * \details This structure is passed to the `psu` module through the [driver
+ *      response API](::mod_psu_driver_response_api::respond).
+ */
+struct mod_psu_driver_response {
+    /*!
+     * \brief Status code representing the result of the operation.
+     */
+    int status;
+
+    /*!
+     * \brief Response fields.
+     *
+     * \details These fields are used if the driver is responding with extra
+     *      information. The activated field, if there is one, is based on the
+     *      event type of the response.
+     */
+    union {
+        /*!
+         * \brief `true` if the device is enabled, otherwise `false`.
+         *
+         * \warning Used only in response to a
+         *      ::mod_psu_driver_api::get_enabled call.
+         */
+        bool enabled;
+
+        /*!
+         * \brief Voltage in millivolts (mV).
+         *
+         * \warning Used only in response to a
+         *      ::mod_psu_driver_api::get_voltage call.
+         */
+        uint64_t voltage;
+    };
+};
+
+/*!
+ * \brief Driver response API.
+ *
+ * \details The driver response API is the API used by drivers to communicate
+ *      the result of a previously-pended operation.
+ */
+struct mod_psu_driver_response_api {
+    /*!
+     * \brief Respond to a previous driver request.
+     *
+     * \param element_id Identifier of the power supply element which submitted
+     *      the request to the driver.
+     * \param response Driver operation response.
+     */
+    void (*respond)(
+        fwk_id_t element_id,
+        struct mod_psu_driver_response response);
+};
+
+/*!
+ * \brief Driver response API identifier.
+ *
+ * \note This identifier corresponds to the ::MOD_PSU_API_IDX_DRIVER_RESPONSE
+ *      API index.
+ */
+static const fwk_id_t mod_psu_api_id_driver_response =
+    FWK_ID_API_INIT(FWK_MODULE_IDX_PSU, MOD_PSU_API_IDX_DRIVER_RESPONSE);
 
 /*!
  * \brief Device configuration.
@@ -177,37 +420,6 @@ struct mod_psu_element_cfg {
      */
     fwk_id_t driver_api_id;
 };
-
-/*!
- * \brief API indices.
- */
-enum mod_psu_api_idx {
-    /*!
-     * \brief Device API index.
-     *
-     * \details This API is the abstract power supply interface exposed and
-     *      implemented by this module.
-     *
-     * \note This API identifier implements the ::mod_psu_device_api interface.
-     *
-     * \warning Binding to this API must occur through an element of this
-     *      module.
-     */
-    MOD_PSU_API_IDX_DEVICE,
-
-    /*!
-     * \brief Number of defined APIs.
-     */
-    MOD_PSU_API_IDX_COUNT
-};
-
-/*!
- * \brief Device API identifier.
- *
- * \note This identifier corresponds to the ::MOD_PSU_API_IDX_DEVICE API index.
- */
-static const fwk_id_t mod_psu_api_id_device =
-    FWK_ID_API_INIT(FWK_MODULE_IDX_PSU, MOD_PSU_API_IDX_DEVICE);
 
 /*!
  * \}
