d575e6177986ee6bf0d1400b217c096174c1df88
Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
2019-10-16 22:22:34 +0530

    module: add system id information module
    Add System ID Information hardware abstraction module which provides an
    generic interface for modules that generate the System ID information
    and modules that consume this information.
    
    This module expects one register interface driver module (like SID or
    SSC) to provide a generic information about the system. A module
    requesting for system information can bind to this module to get that
    information.
    
    Change-Id: Ifd2ada0b8f8278043dd89d5ef0be0ca7f1f0edad
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>

---
+++ module/system_info/include/mod_system_info.h
@@ -0,0 +1,124 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MOD_SYSTEM_INFO_H
+#define MOD_SYSTEM_INFO_H
+
+#include <stdint.h>
+
+/*!
+ * \addtogroup GroupModules Modules
+ * @{
+ */
+
+/*!
+ * \defgroup GroupModuleSystemInfo System Information Abstraction Module
+ *
+ * \brief HAL Module used to get System Information.
+ *
+ * \details This module provides an abstraction layer to the modules which
+ *      need System Information. This module binds to the register interface
+ *      driver module to obtain a pointer to the system information data. Module
+ *      which requires system information data then can use the api provided
+ *      by this module to get the generic system information.
+ *      @{
+ */
+
+/*!
+ * \brief Generic System Information
+ *
+ * \details This structure holds the generic information about the current
+ *      system. The register interface module should define this structure
+ *      and provide a pointer to this module.
+ */
+struct mod_system_info {
+    /*! Product identification number of the system */
+    uint32_t product_id;
+
+    /*! Configuration number of the system */
+    uint32_t config_id;
+
+    /*! Multi-chip mode tie-off value - enabled or disabled */
+    bool multi_chip_mode;
+
+    /*!
+     * Chip id indicating unique identifier of the chip in a multi socket system
+     */
+    uint8_t chip_id;
+
+    /*! Name of the system */
+    const char *name;
+};
+
+/*!
+ * \brief System Information configuration data
+ */
+struct mod_system_info_config {
+    /*!
+     * Module ID of the register interface driver module which provides a
+     * pointer to system information data. If the product does not support any
+     * driver, this can be set to FWK_ID_NONE.
+     */
+    fwk_id_t system_info_driver_module_id;
+
+    /*! API ID for getting the system information data from the driver module */
+    fwk_id_t system_info_driver_data_api_id;
+};
+
+/*!
+ * \brief API structure to be defined by the driver module.
+ */
+struct mod_system_info_get_driver_data_api {
+    /*!
+     * \brief Get the system information data populated by the driver module.
+     *
+     * \details API to be implemented by the driver module which provides a
+     *      pointer to the system information data.
+     *
+     * \retval NULL if the driver data is not initialized yet.
+     * \return Pointer to the system information driver data.
+     */
+    struct mod_system_info *(*get_driver_data)(void);
+};
+
+/*!
+ * \brief API structure used by the modules requesting for the system
+ *      information data.
+ */
+struct mod_system_info_get_info_api {
+    /*!
+     * \brief Get system information data.
+     *
+     * \details API to be used by the module requesting for the system
+     *      information data.
+     *
+     * \param[out] sys_info Pointer to the system information data.
+     *
+     * \retval FWK_SUCCESS if the sys_info pointer has been successfully set.
+     * \retval FWK_E_SUPPORT if the system information is not supported by the
+     *      product.
+     */
+    int (*get_system_info)(const struct mod_system_info **sys_info);
+};
+
+/*!
+ * \brief Module API indicies.
+ */
+enum mod_system_info_api_idx {
+    MOD_SYSTEM_INFO_GET_API_IDX,
+    MOD_SYSTEM_INFO_API_COUNT
+};
+
+/*!
+ * @}
+ */
+
+/*!
+ * @}
+ */
+
+#endif /* MOD_SYSTEM_INFO_H */
---
+++ module/system_info/src/mod_system_info.c
@@ -0,0 +1,105 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     System Information Hardware Abstraction module.
+ */
+
+#include <fwk_assert.h>
+#include <fwk_id.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+#include <fwk_status.h>
+#include <mod_system_info.h>
+
+/* Pointer to the config data for module use. */
+static const struct mod_system_info_config *config;
+
+/* Pointer to the data provided by the driver module. */
+static struct mod_system_info *system_info;
+
+/* Pointer to the driver function which provides the system info data. */
+static struct mod_system_info_get_driver_data_api *get_driver_data;
+
+static int get_system_info(const struct mod_system_info **sys_info)
+{
+    if (system_info == NULL) {
+        system_info = get_driver_data->get_driver_data();
+        if (system_info == NULL)
+            return FWK_E_SUPPORT;
+    }
+
+    *sys_info = system_info;
+    return FWK_SUCCESS;
+}
+
+/*
+ * API to be used by the modules that need a copy of the system ID information
+ * data.
+ */
+static struct mod_system_info_get_info_api get_system_info_api = {
+    .get_system_info = get_system_info,
+};
+
+/*
+ * Framework handlers
+ */
+static int system_info_init(fwk_id_t module_id, unsigned int element_count,
+        const void *data)
+{
+    fwk_assert(data != NULL);
+
+    /* No elements support */
+    if (element_count > 0)
+        return FWK_E_DATA;
+
+    config = data;
+    return FWK_SUCCESS;
+}
+
+static int system_info_bind(fwk_id_t id, unsigned int round)
+{
+    int status = FWK_SUCCESS;
+
+    if (round == 1) {
+        if (!fwk_id_is_equal(config->system_info_driver_module_id,
+                             FWK_ID_NONE)) {
+
+            /* If module ID is provided, API ID shouldn't be NONE */
+            fwk_assert(!fwk_id_is_equal(config->system_info_driver_data_api_id,
+                                        FWK_ID_NONE));
+
+            status = fwk_module_bind(config->system_info_driver_module_id,
+                                     config->system_info_driver_data_api_id,
+                                     &get_driver_data);
+            if (status != FWK_SUCCESS)
+                return FWK_E_PANIC;
+        }
+    }
+    return status;
+}
+
+static int system_info_process_bind_request(fwk_id_t requester_id,
+        fwk_id_t targer_id, fwk_id_t api_id, const void **api)
+{
+    switch (fwk_id_get_api_idx(api_id)) {
+    case MOD_SYSTEM_INFO_GET_API_IDX:
+        *api = &get_system_info_api;
+        break;
+    default:
+        return FWK_E_PARAM;
+    }
+    return FWK_SUCCESS;
+}
+
+const struct fwk_module module_system_info = {
+    .name = "SYSTEM ID INFORMATION",
+    .type = FWK_MODULE_TYPE_HAL,
+    .init = system_info_init,
+    .bind = system_info_bind,
+    .process_bind_request = system_info_process_bind_request,
+    .api_count = MOD_SYSTEM_INFO_API_COUNT,
+};
