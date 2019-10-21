be2e0cd857c868f69a1fec3da61ec13595427f81
Manoj Kumar <manoj.kumar3@arm.com>
2019-10-21 15:18:42 +0100

    n1sdp: add C2C I2C element in config and C2C handling in PCIe
    This patch adds the I2C element for C2C communication and also
    include PCIe initialization based on C2C module output.
    
    Change-Id: Ie5953da9a4606ee27382a64e88127feb6dda9a50
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

--- product/n1sdp/scp_ramfw/config_n1sdp_i2c.c
+++ product/n1sdp/scp_ramfw/config_n1sdp_i2c.c
@@ -5,25 +5,47 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <stdbool.h>
+#include <fwk_id.h>
+#include <fwk_macros.h>
 #include <fwk_module.h>
 #include <mod_n1sdp_i2c.h>
+#include <n1sdp_scp_irq.h>
 #include <n1sdp_scp_mmap.h>
 #include <config_clock.h>
 
 static const struct fwk_element i2c_element_desc_table[] = {
     [0] = {
-        .name = "I2C0",
+        .name = "SPD-I2C",
         .data = &((struct mod_n1sdp_i2c_device_config) {
-            .reg_base = SCP_I2C0_BASE,
+            .reg_base = DIMM_SPD_I2C_BASE,
             .clock_rate_hz = OSC_FREQ_HZ,
             .bus_speed_hz = MOD_N1SDP_I2C_SPEED_NORMAL,
             .mode = MOD_N1SDP_I2C_MASTER_MODE,
             .ack_en = MOD_N1SDP_I2C_ACK_ENABLE,
             .addr_size = MOD_N1SDP_I2C_ADDRESS_7_BIT,
             .hold_mode = MOD_N1SDP_I2C_HOLD_ON,
+            .c2c_mode = false,
+            .callback_mod_id = FWK_ID_NONE_INIT,
         }),
     },
-    [1] = { 0 }, /* Termination description. */
+    [1] = {
+        .name = "C2C-I2C",
+        .data = &((struct mod_n1sdp_i2c_device_config) {
+            .reg_base = SCP_I2C0_BASE,
+            .clock_rate_hz = (50UL * FWK_MHZ),
+            .bus_speed_hz = MOD_N1SDP_I2C_SPEED_NORMAL,
+            .mode = MOD_N1SDP_I2C_SLAVE_MODE,
+            .ack_en = MOD_N1SDP_I2C_ACK_ENABLE,
+            .addr_size = MOD_N1SDP_I2C_ADDRESS_7_BIT,
+            .hold_mode = MOD_N1SDP_I2C_HOLD_OFF,
+            .slave_addr = 0x14,
+            .c2c_mode = true,
+            .irq = SCP_I2C0_IRQ,
+            .callback_mod_id = FWK_ID_MODULE_INIT(FWK_MODULE_IDX_N1SDP_C2C),
+        }),
+    },
+    [2] = { 0 }, /* Termination description. */
 };
 
 static const struct fwk_element *get_i2c_table(fwk_id_t module_id)
