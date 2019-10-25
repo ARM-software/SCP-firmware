56d6a349c229d7f0989504d6af75dc75c80aba8f
Manoj Kumar <manoj.kumar3@arm.com>
2019-10-25 13:29:52 +0100

    n1sdp_timer_sync: add module for timer synchronization
    N1SDP implements timer synchronization module to synchronize timers
    on master and slave chips in multichip environment.
    
    This patch implements a driver to trigger the synchronization
    mechanism between the chips.
    
    Change-Id: I7a52916ab62a5eed82da9ebfb7a1d0fad1fbd621
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/n1sdp/module/n1sdp_timer_sync/include/mod_n1sdp_timer_sync.h
@@ -0,0 +1,102 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *      N1SDP Timer Synchronization Device Driver.
+ */
+
+#ifndef MOD_N1SDP_TIMER_SYNC_H
+#define MOD_N1SDP_TIMER_SYNC_H
+
+#include <stdint.h>
+#include <fwk_macros.h>
+
+/*!
+ * \addtogroup GroupN1SDPModule N1SDP Product Modules
+ * @{
+ */
+
+/*!
+ * \defgroup GroupN1SDPTimerSync N1SDP Timer Synchronization Driver
+ * @{
+ */
+
+/*!
+ * \brief Module API indices
+ */
+enum mod_n1sdp_timer_sync_api_idx {
+    /*! Timer synchronization API */
+    N1SDP_TIMER_SYNC_API_IDX_TSYNC,
+
+    /*! Number of APIs */
+    N1SDP_TIMER_SYNC_API_COUNT,
+};
+
+/*!
+ * \brief N1SDP Timer Synchronization API
+ */
+struct n1sdp_timer_sync_api {
+   /*!
+    * \brief API to trigger synchronization in master.
+    *
+    * \param id Identifier of the timer sync module.
+    *
+    * \retval FWK_SUCCESS If operation succeeds.
+    * \return One of the possible error return codes.
+    */
+   int (*master_sync)(fwk_id_t id);
+   /*!
+    * \brief API to trigger synchronization in slave.
+    *
+    * \param id Identifier of the timer sync module.
+    *
+    * \retval FWK_SUCCESS If operation succeeds.
+    * \return One of the possible error return codes.
+    */
+   int (*slave_sync)(fwk_id_t id);
+};
+
+/*!
+ * \brief Timer Synchronization Device Configuration
+ */
+struct mod_n1sdp_tsync_config {
+    /*! IRQ number of the timer synchronization module */
+    unsigned int irq;
+
+    /*! Base address of the timer synchronization module */
+    uintptr_t reg;
+
+    /*! CCIX network delay */
+    uint32_t ccix_delay;
+
+    /*! GCNT sync timeout */
+    uint32_t sync_timeout;
+
+    /*! GCNT sync interval */
+    uint32_t sync_interval;
+
+    /*! GCNT offset threshold */
+    uint32_t off_threshold;
+
+    /*! Target counter base address */
+    uint32_t target_cnt_base;
+
+    /*! Offset to access target counter locally */
+    uint32_t local_offset;
+
+    /*! Offset to access target counter remotely */
+    uint64_t remote_offset;
+};
+
+/*!
+ * @}
+ */
+
+/*!
+ * @}
+ */
+
+#endif /* MOD_N1SDP_TIMER_SYNC_H */
