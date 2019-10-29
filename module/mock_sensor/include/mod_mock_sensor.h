26ab29f47e7d99e988619ad61b9b33298c805b2e
Nicola Mazzucato <nicola.mazzucato@arm.com>
2019-10-29 11:31:53 +0000

    mock_sensor: Add module
    This patch adds a mocker for sensors.
    This module implements the sensor driver API and
    defers the get_value requests. This module requires
    the timer facilities in order to setup an alarm.
    This would emulate a sensor that provides a value
    through an ISR and a call to the driver_response API
    of the sensor module.
    
    This module can be used to test sensor call flow
    in platforms that do not provide asynchronous sensor
    drivers.
    
    Change-Id: I37baa428c12d6aa1c3de2f6f534ab5ac158f2352
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ module/mock_sensor/include/mod_mock_sensor.h
@@ -0,0 +1,45 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MOD_MOCK_SENSOR_H
+#define MOD_MOCK_SENSOR_H
+
+#include <stdint.h>
+#include <mod_sensor.h>
+
+/*!
+ * \ingroup GroupModules
+ * \defgroup GroupMockSensor Mock Sensor Driver
+ *
+ * \details The `mock_sensor` module provides a simulated asynchronous driver
+ *      for use alongside the `sensor` HAL interface on systems that do not
+ *      provide a real asynchronous sensor.
+ *
+ *      This mock driver implements the Sensor HAL driver API and always defers
+ *      the 'get_value' request. An example of the execution flow is shown in
+ *      `Deferred Response Architecture`.
+ *
+ * \{
+ */
+
+/*! \brief Element configuration */
+struct mod_mock_sensor_dev_config {
+    /*! Pointer to sensor information */
+    struct mod_sensor_info *info;
+
+    /*! Sensor HAL identifier */
+    fwk_id_t sensor_hal_id;
+
+    /*! Identifier of the alarm assigned to this element */
+    fwk_id_t alarm_id;
+};
+
+/*!
+ * \}
+ */
+
+#endif /* MOD_MOCK_SENSOR_H */
