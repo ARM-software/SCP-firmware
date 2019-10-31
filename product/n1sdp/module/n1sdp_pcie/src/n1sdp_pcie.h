888c412564dbcd5b010586363e389a8c88ccbdbd
Manoj Kumar <manoj.kumar3@arm.com>
2019-10-31 12:28:32 +0000

    n1sdp: increase pcie inbound register size to 48-bits
    This patch increases the PCIe RP inbound region size to 48-bits
    as there are cases where endpoint can generate a 48-bit VA
    which gets translated by SMMU.
    
    Change-Id: Ieca1c59b99f5da50e141237cd79f12ed17fc6ed7
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

--- product/n1sdp/module/n1sdp_pcie/src/n1sdp_pcie.h
+++ product/n1sdp/module/n1sdp_pcie/src/n1sdp_pcie.h
@@ -187,7 +187,7 @@
 
 /* AXI inbound region data */
 #define AXI_IB_REGION_BASE             UINT64_C(0)
-#define AXI_IB_REGION_SIZE_MSB         43
+#define AXI_IB_REGION_SIZE_MSB         48
 
 /*
  * PCIe Descriptor Register definitions
