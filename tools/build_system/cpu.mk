21bb68d869eed04ff40cf349f744225eeb6a225f
Etienne Carriere <etienne.carriere@linaro.org>
2019-11-07 01:48:31 +0100

    build: specific toolchain for cortex-A when building for OP-TEE
    When build SCP-firmware for an OP-TEE product,
    
    When product makefile sets BS_FIRMWARE_CPU=optee, the SCP-firmware
    is built for a Cortex-A target, hence requesting specific toolchain
    and build directives.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- tools/build_system/cpu.mk
+++ tools/build_system/cpu.mk
@@ -10,12 +10,12 @@ BS_CPU_MK := 1
 
 include $(BS_DIR)/defs.mk
 
-BS_ARCH_CPU := $(BS_FIRMWARE_CPU)
-
 # Supported ARMv7-M CPUs
 ARMV7M_CPUS := cortex-m3 cortex-m7
 
 ifneq ($(findstring $(BS_FIRMWARE_CPU),$(ARMV7M_CPUS)),)
+    BS_ARCH_CPU := $(BS_FIRMWARE_CPU)
+
     BS_ARCH_ARCH := armv7-m
     BS_ARCH_ISA := thumb
 
@@ -27,7 +27,13 @@ ifneq ($(findstring $(BS_FIRMWARE_CPU),$(ARMV7M_CPUS)),)
     CFLAGS += -mno-unaligned-access # Disable unaligned access code generation
 else ifeq ($(BS_FIRMWARE_CPU),host)
     BS_ARCH_ARCH := host
+else ifeq ($(BS_FIRMWARE_CPU),optee)
+    BS_ARCH_ARCH := optee
 
+    ifeq ($(BS_ARCH_CPU),cortex-a7)
+    CFLAGS += -mfloat-abi=soft # No hardware floating point support
+    CFLAGS += -mthumb -mthumb-interwork -fno-short-enums -fno-common -mno-unaligned-access
+    endif
 else
     $(erro "$(BS_FIRMWARE_CPU) is not a supported CPU. Aborting...")
 endif
