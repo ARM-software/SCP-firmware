ca08cc31499f08771f5bf7706600480055fe0a95
Aditya Angadi <aditya.angadi@arm.com>
2019-12-04 11:43:15 +0530

    product/rddaniel: add config data for SID driver module
    SID peripheral on this platform provides information about the platform
    ID and config ID. So add the configuration data for SID module. The
    base address of the SID register block is provided for the SID module
    to read out the IDs from the SID registers.
    
    Change-Id: I9235cf6f512c6fc5daab3fb34c2c94a32d4ed2d1
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

---
+++ product/rddaniel/include/scp_mmap.h
@@ -0,0 +1,13 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SCP_MMAP_H
+#define SCP_MMAP_H
+
+#define SCP_SYSTEM_ACCESS_PORT1_BASE           0xA0000000
+
+#endif /* SCP_MMAP_H */
---
+++ product/rddaniel/src/config_sid.c
@@ -0,0 +1,44 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_element.h>
+#include <fwk_module.h>
+#include <mod_sid.h>
+#include <scp_css_mmap.h>
+
+static const struct fwk_element subsystem_table[] = {
+    {
+        .name = "RD-DANIEL",
+        .data = &(struct mod_sid_subsystem_config) {
+            .part_number = 0x78a,
+        }
+    },
+    { 0 },
+};
+
+static const struct fwk_element *get_subsystem_table(fwk_id_t id)
+{
+    return subsystem_table;
+}
+
+const struct fwk_module_config config_sid = {
+    .get_element_table = get_subsystem_table,
+    .data = &(struct mod_sid_config) {
+        .sid_base = SCP_SID_BASE,
+        .pcid_expected = {
+            .PID0 = 0xD2,
+            .PID1 = 0xB0,
+            .PID2 = 0x0B,
+            .PID3 = 0x00,
+            .PID4 = 0x04,
+            .CID0 = 0x0D,
+            .CID1 = 0xF0,
+            .CID2 = 0x05,
+            .CID3 = 0xB1,
+        },
+    },
+};
