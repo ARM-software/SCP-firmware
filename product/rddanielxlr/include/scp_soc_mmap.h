5ed360fceffa2bc5296540a8b783eb7833ab7382
Aditya Angadi <aditya.angadi@arm.com>
2019-12-05 14:50:48 +0530

    product/rddanielxlr: add config data for SCP RAM firmware load module
    The MSCP RAM firmware load module is used to load the SCP RAM firmware
    from the NOR flash to SCP RAM. Provide the base address and size of the
    firmware image to be loaded from and the base address of the location
    to which the firmware is loaded to.
    
    Change-Id: Ia8f4b6d4955ca3415c00d576f40dbb4b35d2444a
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

---
+++ product/rddanielxlr/include/scp_soc_mmap.h
@@ -0,0 +1,15 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SCP_SOC_MMAP_H
+#define SCP_SOC_MMAP_H
+
+#include "scp_mmap.h"
+
+#define SCP_NOR0_FLASH_BASE          (SCP_SOC_EXPANSION1_BASE + 0x07000000)
+
+#endif /* SCP_SOC_MMAP_H */
