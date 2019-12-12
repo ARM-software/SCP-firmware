26a32af3ecb7d92c05e2114423b25f4c203ed193
Chris Kay <chris.kay@arm.com>
2019-12-12 18:27:19 +0000

    fwk: Add variable argument macros
    The macros introduced in this commit form the logic for two core macros:
    
    - FWK_COUNT_ARGS: counts the number of parameters provided to a macro
    - FWK_MAP: calls a macro for every parameter provided
    
    These macros are particularly useful when you need "for every parameter,
    do X" style behaviour at the preprocessor level.
    
    Change-Id: Ibba68279c1e7b590a5df3854e3a66fe00aba2984
    Signed-off-by: Chris Kay <chris.kay@arm.com>

--- framework/include/fwk_macros.h
+++ framework/include/fwk_macros.h
@@ -191,6 +191,163 @@
                             (BUILD_VERSION_PATCH & 0xffff))
 
 /*!
+ * \internal
+ *
+ * \brief Return a comma-delimited sequence of numbers from `7` down to `0`.
+ *
+ * \note This macro is an implementation detail of ::FWK_COUNT_ARGS and is used
+ *      as the input to ::__FWK_COUNT_ARGS_N.
+ */
+#define __FWK_COUNT_ARGS_SEQUENCE 7, 6, 5, 4, 3, 2, 1, 0
+
+/*!
+ * \internal
+ *
+ * \brief Return the value of the *nth* parameter.
+ *
+ * \note This macro is an implementation detail of ::__FWK_COUNT_ARGS_N_EXPAND.
+ *
+ * \details This macro does the actual counting of the parameters. It does so
+ *      through a bit of trickery that involves taking a concatenation of the
+ *      parameters being counted, and a fixed length sequence.
+ *
+ *      Given a call like the following where the parameters are
+ *      `[hello, world]`, the call would look like this (sequence length
+ *      adapted for brevity):
+ *
+ *          __FWK_COUNT_ARGS_N(hello, 3, 2, 1, 0) == 1
+ *
+ *      This works because every time a new parameter is introduced, the
+ *      sequence is shifted one to the right, and therefore the value at `N`
+ *      decremented.
+ *
+ *      You can see this in action by adding another parameter to the example:
+ *
+ *          __FWK_COUNT_ARGS_N(hello, world, 3, 2, 1, 0) == 2
+ *
+ *      The number of parameters this can take, of course, is therefore limited
+ *      to the length of the sequence.
+ */
+#define __FWK_COUNT_ARGS_N(_1, _2, _3, _4, _5, _6, _7, N, ...) N
+
+/*!
+ * \internal
+ *
+ * \brief Call ::__FWK_COUNT_ARGS_N after expanding the parameters.
+ *
+ * \details This macro is part of the implementation of ::FWK_COUNT_ARGS and is
+ *      required to properly expand the parameters provided (otherwise the
+ *      compiler will complain that we have not provided enough parameters).
+ */
+#define __FWK_COUNT_ARGS_N_EXPAND(...) __FWK_COUNT_ARGS_N(__VA_ARGS__)
+
+/*!
+ * \brief Count the number of arguments provided to the macro.
+ *
+ * \details This macro counts the number of arguments passed to it, up to a
+ *      maximum **7**.
+ *
+ * \warning This macro does not work with zero parameters.
+ *
+ * \param[in] ... Parameter list.
+ */
+#define FWK_COUNT_ARGS(...) \
+    __FWK_COUNT_ARGS_N_EXPAND(__VA_ARGS__, __FWK_COUNT_ARGS_SEQUENCE)
+
+/*!
+ * \internal
+ *
+ * \def __FWK_MAP_1
+ *
+ * \brief Map the first parameter to a value.
+ *
+ * \param[in] MACRO Macro to apply.
+ * \param[in] X Value to apply the macro over.
+ *
+ * \def __FWK_MAP_2
+ * \brief Map the second parameter to a value.
+ *
+ * \param[in] MACRO Macro to apply.
+ * \param[in] X Value to apply the macro over.
+ * \param[in] ... Remaining values.
+ *
+ * \def __FWK_MAP_3
+ * \brief Map the third parameter to a value.
+ *
+ * \param[in] MACRO Macro to apply.
+ * \param[in] X Value to apply the macro over.
+ * \param[in] ... Remaining values.
+ *
+ * \def __FWK_MAP_4
+ * \brief Map the fourth parameter to a value.
+ *
+ * \param[in] MACRO Macro to apply.
+ * \param[in] X Value to apply the macro over.
+ * \param[in] ... Remaining values.
+ *
+ * \def __FWK_MAP_5
+ * \brief Map the fifth parameter to a value.
+ *
+ * \param[in] MACRO Macro to apply.
+ * \param[in] X Value to apply the macro over.
+ * \param[in] ... Remaining values.
+ *
+ * \def __FWK_MAP_6
+ * \brief Map the sixth parameter to a value.
+ *
+ * \param[in] MACRO Macro to apply.
+ * \param[in] X Value to apply the macro over.
+ * \param[in] ... Remaining values.
+ *
+ * \def __FWK_MAP_7
+ * \brief Map the seventh parameter to a value.
+ *
+ * \param[in] MACRO Macro to apply.
+ * \param[in] X Value to apply the macro over.
+ * \param[in] ... Remaining values.
+ */
+
+#define __FWK_MAP_1(MACRO, X) MACRO(X)
+#define __FWK_MAP_2(MACRO, X, ...) __FWK_MAP_1(MACRO, __VA_ARGS__) MACRO(X)
+#define __FWK_MAP_3(MACRO, X, ...) __FWK_MAP_2(MACRO, __VA_ARGS__) MACRO(X)
+#define __FWK_MAP_4(MACRO, X, ...) __FWK_MAP_3(MACRO, __VA_ARGS__) MACRO(X)
+#define __FWK_MAP_5(MACRO, X, ...) __FWK_MAP_4(MACRO, __VA_ARGS__) MACRO(X)
+#define __FWK_MAP_6(MACRO, X, ...) __FWK_MAP_5(MACRO, __VA_ARGS__) MACRO(X)
+#define __FWK_MAP_7(MACRO, X, ...) __FWK_MAP_6(MACRO, __VA_ARGS__) MACRO(X)
+
+/*!
+ * \internal
+ *
+ * \brief Get the name of the map macro corresponding to an input of \p N
+ *      parameters.
+ *
+ * \param[in] N Number of parameters.
+ */
+#define __FWK_MAP_MACRO(N) __FWK_MAP_##N
+
+/*!
+ * \internal
+ *
+ * \brief Map a macro with \p N parameters to a macro \p MACRO.
+ *
+ * \param[in] N Number of parameters.
+ * \param[in] MACRO macro to apply.
+ */
+#define __FWK_MAP(N, MACRO, ...) __FWK_MAP_MACRO(N)(MACRO, __VA_ARGS__)
+
+/*!
+ * \brief Map a macro over a set of input parameters.
+ *
+ * \details This macro calls a macro \p MACRO, taking one input parameter, for
+ *      every other parameter passed.
+ *
+ * \param[in] MACRO Macro to call.
+ * \param[in] ... Input parameters.
+ */
+#define FWK_MAP(MACRO, ...) \
+    __FWK_MAP(FWK_COUNT_ARGS(__VA_ARGS__), MACRO, __VA_ARGS__)
+
+/*!
  * @}
  */
 
