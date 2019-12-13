92b2c924f5420460fb8eb0a2914a89232b31ed26
Aditya Angadi <aditya.angadi@arm.com>
2019-12-13 13:55:23 +0530

    product/rddaniel: add build support for scp rom firmware
    Add the linker script and makefile to support the build of the SCP ROM
    firmware.
    
    Change-Id: I6c6a16e81ae83f3b6fb89be0e04ce3d6110525b0
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

---
+++ product/rddaniel/scp_romfw/firmware.mk
@@ -0,0 +1,30 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+BS_FIRMWARE_CPU := cortex-m7
+BS_FIRMWARE_HAS_MULTITHREADING := no
+BS_FIRMWARE_HAS_NOTIFICATION := yes
+BS_FIRMWARE_MODULE_HEADERS_ONLY := \
+    power_domain \
+    timer
+
+BS_FIRMWARE_MODULES := \
+    sid \
+    pcid \
+    pl011 \
+    log \
+    clock \
+    mscp_rom \
+
+BS_FIRMWARE_SOURCES := \
+    config_sid.c \
+    config_log.c \
+    config_pl011.c \
+    config_clock.c \
+    config_scp_rom.c \
+
+include $(BS_DIR)/firmware.mk
