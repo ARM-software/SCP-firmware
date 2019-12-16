4435480e17f18ca8a1efc27ebc0b7c4ea5fd7f0d
Aditya Angadi <aditya.angadi@arm.com>
2019-12-16 10:08:44 +0530

    product/rddaniel: add platform specific definitions for rtx
    Add platform specific definitions for callback functions for cmsis rtx.
    
    Change-Id: Ia2c9521ac814c1af7689164b85d0641d582e5cea
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

---
+++ product/rddaniel/include/clock_soc.h
@@ -0,0 +1,15 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef CLOCK_SOC_H
+#define CLOCK_SOC_H
+
+#include <fwk_macros.h>
+
+#define CLOCK_RATE_REFCLK       (100UL * FWK_MHZ)
+
+#endif /* CLOCK_SOC_H */
