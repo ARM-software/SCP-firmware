d0197f7ba6dbb57cec8bc11ba27f715a4905acbc
Nicola Mazzucato <nicola.mazzucato@arm.com>
2019-12-18 15:38:14 +0000

    juno: Add mock PSU
    This patch adds a mocked PSU to be used when the
    firmware runs on Juno FVP model.
    
    Change-Id: Iaa306499b879c599101990aad326eb805855f249
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

---
+++ product/juno/scp_ramfw/config_mock_psu.c
@@ -0,0 +1,56 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_element.h>
+#include <fwk_module.h>
+#include <mod_mock_psu.h>
+
+static const struct fwk_element element_table[] = {
+    [0] = {
+        .name = "VSYS",
+        .data = &(const struct mod_mock_psu_element_cfg) {
+            .async_alarm_id = FWK_ID_NONE_INIT,
+            .default_enabled = true,
+            .default_voltage = 900,
+        },
+    },
+    [1] = {
+        .name = "VBIG",
+        .data = &(const struct mod_mock_psu_element_cfg) {
+            .async_alarm_id = FWK_ID_NONE_INIT,
+            .default_enabled = true,
+            .default_voltage = 900,
+        },
+    },
+    [2] = {
+        .name = "VLITTLE",
+        .data = &(const struct mod_mock_psu_element_cfg) {
+            .async_alarm_id = FWK_ID_NONE_INIT,
+            .default_enabled = true,
+            .default_voltage = 900,
+        },
+    },
+    [3] = {
+        .name = "VGPU",
+        .data = &(const struct mod_mock_psu_element_cfg) {
+            .async_alarm_id = FWK_ID_NONE_INIT,
+            .default_enabled = true,
+            .default_voltage = 850,
+        },
+    },
+    [4] = { 0 }
+};
+
+static const struct fwk_element *get_element_table(fwk_id_t module_id)
+{
+    return element_table;
+}
+
+struct fwk_module_config config_mock_psu = {
+    .get_element_table = get_element_table,
+    .data = NULL,
+};
