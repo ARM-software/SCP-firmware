2bb8fe4dd7dde8d9001fa7bb83841f12d98f997d
Tarek El-Sherbiny <tarek.el-sherbiny@arm.com>
2020-01-07 10:35:43 +0000

    dbg: Compile in cli fifo and cli time files
    The cli is using fifo and time functions internally.
    This patch compiles the required c files.
    
    Change-Id: I9d36b75b9e5c847c7a4547a61be705f08e0fa301
    Signed-off-by: Tarek El-Sherbiny <tarek.el-sherbiny@arm.com>

---
+++ debugger/src/cli/cli_platform_time.c
@@ -0,0 +1,18 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdint.h>
+#include <cli.h>
+#include <cli_platform.h>
+
+__attribute__((weak)) void cli_platform_get_time(cli_timestamp_t *t)
+{
+}
+
+__attribute__((weak)) void cli_platform_delay_ms(uint32_t ms)
+{
+}
