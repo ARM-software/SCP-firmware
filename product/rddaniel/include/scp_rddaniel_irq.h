8d92660e87743f1be12e202321f202b20bf8eb95
Aditya Angadi <aditya.angadi@arm.com>
2020-01-08 11:24:47 +0530

    product/rddaniel: add configuration data for system power driver
    The system power driver manages the SYSTOP power domain. Provide the
    details such as the base adresses of System PPU's and the SOC IRQ's for
    the same.
    
    Change-Id: I6c9eee1cd26e046e01c8f87ab4bd6c6a70e04688
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

---
+++ product/rddaniel/include/scp_rddaniel_irq.h
@@ -0,0 +1,15 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SCP_RDDANIEL_IRQ_H
+#define SCP_RDDANIEL_IRQ_H
+
+enum scp_rddaniel_interrupt {
+    SOC_WAKEUP0_IRQ            = 16, /* SoC Expansion Wakeup */
+};
+
+#endif /* SCP_RDDANIEL_IRQ_H */
