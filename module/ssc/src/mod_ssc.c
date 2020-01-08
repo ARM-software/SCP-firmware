30dcee9c2141488d52c451e42201fc3b9357c4f8
Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
2020-01-08 13:26:26 +0530

    module: add initial support for ssc registers
    This patch adds a initial module support for Serial Security Control
    interface registers which is available in ARM platforms such as Juno,
    N1SDP and SGI-575. SSC module provides an API using which the
    system_info module can bind to this module and obtain the system
    information data.
    
    Change-Id: I36c750b1ce7d9ab044bffe50715c64cfcb366731
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>

---
+++ module/ssc/src/mod_ssc.c
@@ -0,0 +1,94 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_assert.h>
+#include <fwk_id.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+#include <fwk_mm.h>
+#include <mod_system_info.h>
+#include <mod_ssc.h>
+#include <ssc_reg.h>
+
+static bool initialized;
+static int chip_id, multi_chip_mode, part_number, revision_number;
+static struct mod_system_info sys_info;
+
+static struct mod_system_info *get_driver_data(void)
+{
+    if (!initialized)
+        return NULL;
+
+    return &sys_info;
+}
+
+/*
+ * API for the use of system info module to get the driver data.
+ */
+static struct mod_system_info_get_driver_data_api get_sys_info_driver_data = {
+    .get_driver_data = get_driver_data
+};
+
+/*
+ * Framework Handlers
+ */
+static int ssc_init(fwk_id_t module_id, unsigned int count,
+                          const void *data)
+{
+    const struct mod_ssc_config *config = data;
+    struct ssc_reg *ssc_reg;
+
+    if ((config == NULL) || (config->ssc_base == 0))
+        return FWK_E_DATA;
+
+    ssc_reg = (struct ssc_reg *)config->ssc_base;
+
+    chip_id = (ssc_reg->SSC_CHIPID_ST) & SSC_CHIPID_ST_CHIP_ID_MASK;
+    multi_chip_mode = ((ssc_reg->SSC_CHIPID_ST) &
+                       SSC_CHIPID_ST_MULTI_CHIP_MODE_MASK) >>
+                      SSC_CHIPID_ST_MULTI_CHIP_MODE_POS;
+    part_number = (ssc_reg->PID0 & SSC_PID0_PART_NUMBER_MASK) |
+                  ((ssc_reg->PID1 & SSC_PID1_PART_NUMBER_MASK) <<
+                   SSC_PID1_PART_NUMBER_POS);
+    revision_number = (ssc_reg->PID2 & SSC_PID2_REVISION_NUMBER_MASK) >>
+                      SSC_PID2_REVISION_NUMBER_POS;
+
+    if (config->ssc_debug_cfg_set != 0)
+        ssc_reg->SSC_DBGCFG_SET = config->ssc_debug_cfg_set;
+
+    /* Populate the system info structure */
+    sys_info.product_id = part_number;
+    sys_info.config_id = revision_number;
+    sys_info.chip_id = chip_id;
+    sys_info.multi_chip_mode = multi_chip_mode;
+    sys_info.name = config->product_name;
+
+    initialized = true;
+
+    return FWK_SUCCESS;
+}
+
+static int ssc_process_bind_request(fwk_id_t requester_id, fwk_id_t targer_id,
+        fwk_id_t api_id, const void **api)
+{
+    switch (fwk_id_get_api_idx(api_id)) {
+    case MOD_SSC_SYSTEM_INFO_DRIVER_DATA_API_IDX:
+        *api = &get_sys_info_driver_data;
+        break;
+    default:
+        return FWK_E_PARAM;
+    }
+    return FWK_SUCCESS;
+}
+
+const struct fwk_module module_ssc = {
+    .name = "Serial Security Control",
+    .type = FWK_MODULE_TYPE_DRIVER,
+    .init = ssc_init,
+    .process_bind_request = ssc_process_bind_request,
+    .api_count = MOD_SSC_API_COUNT,
+};
---
+++ module/ssc/src/ssc_reg.h
@@ -0,0 +1,54 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SSC_REG_H
+#define SSC_REG_H
+
+#include <stdint.h>
+#include <fwk_macros.h>
+
+/*!
+ * \brief System Security Control (SSC) register definitions
+ */
+struct ssc_reg {
+           uint8_t  RESERVED1[0x10 - 0x0];
+    FWK_R  uint32_t SSC_DBGCFG_STAT;
+    FWK_W  uint32_t SSC_DBGCFG_SET;
+    FWK_W  uint32_t SSC_DBGCFG_CLR;
+           uint8_t  RESERVED2[0x28 - 0x1C];
+    FWK_RW uint32_t SSC_AUXDBGCFG;
+           uint8_t  RESERVED3[0x30 - 0x2C];
+    FWK_RW uint32_t SSC_GPRETN;
+           uint8_t  RESERVED4[0x40 - 0x34];
+    FWK_R  uint32_t SSC_VERSION;
+           uint8_t  RESERVED5[0x500 - 0x44];
+    FWK_R  uint32_t SSC_CHIPID_ST;
+           uint8_t  RESERVED6[0xFD0 - 0x504];
+    FWK_R  uint32_t PID4;
+           uint8_t  RESERVED7[0xFE0 - 0xFD4];
+    FWK_R  uint32_t PID0;
+    FWK_R  uint32_t PID1;
+    FWK_R  uint32_t PID2;
+    FWK_R  uint32_t PID3;
+    FWK_R  uint32_t COMPID0;
+    FWK_R  uint32_t COMPID1;
+    FWK_R  uint32_t COMPID2;
+    FWK_R  uint32_t COMPID3;
+};
+
+#define SSC_PID0_PART_NUMBER_MASK        UINT32_C(0xFF)
+#define SSC_PID1_PART_NUMBER_MASK        UINT32_C(0x0F)
+#define SSC_PID1_PART_NUMBER_POS         8
+
+#define SSC_PID2_REVISION_NUMBER_MASK    UINT32_C(0xF0)
+#define SSC_PID2_REVISION_NUMBER_POS     4
+
+#define SSC_CHIPID_ST_CHIP_ID_MASK              UINT32_C(0x3F)
+#define SSC_CHIPID_ST_MULTI_CHIP_MODE_MASK      UINT32_C(0x100)
+#define SSC_CHIPID_ST_MULTI_CHIP_MODE_POS       8
+
+#endif /* SSC_REG_H */
