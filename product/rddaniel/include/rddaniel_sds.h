d62347839c205bd4b6475a81440e9be2dad4161c
Aditya Angadi <aditya.angadi@arm.com>
2020-01-08 12:47:27 +0530

    product/rddaniel: add configuration data for sds service module
    SDS module is used to pass runtime data from SCP firmware to the
    firmware on AP core using shared memory. Provide the base address
    and size for the same and also the payloads to be passed.
    
    Change-Id: I09d4ddbf5f4c8c305907e4c02832062f7dc0199c
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

---
+++ product/rddaniel/include/rddaniel_sds.h
@@ -0,0 +1,64 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef RDDANIEL_SDS_H
+#define RDDANIEL_SDS_H
+
+#include <mod_sds.h>
+
+/*
+ * Structure identifiers.
+ */
+enum rddaniel_sds_struct_id {
+    RDDANIEL_SDS_CPU_INFO =             1 | (1 << MOD_SDS_ID_VERSION_MAJOR_POS),
+    RDDANIEL_SDS_FIRMWARE_VERSION =     2 | (1 << MOD_SDS_ID_VERSION_MAJOR_POS),
+    RDDANIEL_SDS_PLATFORM_ID =          3 | (1 << MOD_SDS_ID_VERSION_MAJOR_POS),
+    RDDANIEL_SDS_RESET_SYNDROME =       4 | (1 << MOD_SDS_ID_VERSION_MAJOR_POS),
+    RDDANIEL_SDS_FEATURE_AVAILABILITY = 5 | (1 << MOD_SDS_ID_VERSION_MAJOR_POS),
+    RDDANIEL_SDS_CPU_BOOTCTR =          6 | (1 << MOD_SDS_ID_VERSION_MAJOR_POS),
+    RDDANIEL_SDS_CPU_FLAGS =            7 | (1 << MOD_SDS_ID_VERSION_MAJOR_POS),
+};
+
+/*
+ * Structure sizes.
+ */
+#define RDDANIEL_SDS_CPU_INFO_SIZE                 4
+#define RDDANIEL_SDS_FIRMWARE_VERSION_SIZE         4
+#define RDDANIEL_SDS_PLATFORM_ID_SIZE              8
+#define RDDANIEL_SDS_RESET_SYNDROME_SIZE           4
+#define RDDANIEL_SDS_FEATURE_AVAILABILITY_SIZE     4
+#define RDDANIEL_SDS_CPU_BOOTCTR_SIZE              256
+#define RDDANIEL_SDS_CPU_FLAGS_SIZE                256
+
+/*
+ * Field masks and offsets for the RDDANIEL_SDS_AP_CPU_INFO structure.
+ */
+#define RDDANIEL_SDS_CPU_INFO_PRIMARY_MASK         0xFFFFFFFF
+#define RDDANIEL_SDS_CPU_INFO_PRIMARY_POS          0
+
+/*
+ * Platform information
+ */
+struct rddaniel_sds_platid {
+    /* Subsystem part number */
+    uint32_t platform_identifier;
+    /* Platform type information */
+    uint32_t platform_type_identifier;
+};
+
+/*
+ * Field masks and offsets for the RDDANIEL_SDS_FEATURE_AVAILABILITY structure.
+ */
+#define RDDANIEL_SDS_FEATURE_FIRMWARE_MASK         0x1
+#define RDDANIEL_SDS_FEATURE_DMC_MASK              0x2
+#define RDDANIEL_SDS_FEATURE_MESSAGING_MASK        0x4
+
+#define RDDANIEL_SDS_FEATURE_FIRMWARE_POS          0
+#define RDDANIEL_SDS_FEATURE_DMC_POS               1
+#define RDDANIEL_SDS_FEATURE_MESSAGING_POS         2
+
+#endif /* RDDANIEL_SDS_H */
