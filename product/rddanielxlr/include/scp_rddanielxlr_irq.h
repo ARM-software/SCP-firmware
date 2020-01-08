eb4a93090319cc87eb463667eb48dfc71e4dc584
Aditya Angadi <aditya.angadi@arm.com>
2020-01-08 11:57:37 +0530

    product/rddanielxlr: add configuration data for timer hal
    Provide the configuration data for timer hal which includes the id for
    the timer element and the IRQ number.
    
    Change-Id: I3ccee22cfb78ecb164cd3c73fdb01d82c0aa735a
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

---
+++ product/rddanielxlr/include/scp_rddanielxlr_irq.h
@@ -0,0 +1,15 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SCP_RDDANIELXLR_IRQ_H
+#define SCP_RDDANIELXLR_IRQ_H
+
+enum scp_rddanielxlr_interrupt {
+    TIMREFCLK_IRQ              = 33, /* REFCLK Physical Timer */
+};
+
+#endif /* SCP_RDDANIELXLR_IRQ_H */
