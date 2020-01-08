81db4403adce6695b660a73a958ea94a4107709c
Aditya Angadi <aditya.angadi@arm.com>
2020-01-08 12:33:04 +0530

    product/rddanielxlr: add configuration data for AP context module
    AP context module is used to zero out the shared memory region used by
    SCP and AP on every initializaion or interconnect reset. Provide the
    base address and size of the shared memory region and also the clock id
    of the interconnect to subscribe to.
    
    Change-Id: I6e4654689c6425aa533847d2865d23dcf47c1d11
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

---
+++ product/rddanielxlr/scp_ramfw/config_apcontext.c
@@ -0,0 +1,29 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "clock_soc.h"
+#include "scp_software_mmap.h"
+
+#include <mod_apcontext.h>
+
+#include <fwk_id.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+
+/*
+ * AP Context module configuration
+ */
+static const struct mod_apcontext_config apcontext_data = {
+    .base = SCP_AP_CONTEXT_BASE,
+    .size = SCP_AP_CONTEXT_SIZE,
+    .clock_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_CLOCK,
+        CLOCK_IDX_INTERCONNECT),
+};
+
+struct fwk_module_config config_apcontext = {
+    .data = &apcontext_data,
+};
