1ee0ce88b04ef0a5cd2f82c2c76b7d3e5c2b1902
Aditya Angadi <aditya.angadi@arm.com>
2020-01-08 15:14:51 +0530

    product/rddaniel: add support for MCP firmware in rddaniel
    MCP ROM and RAM firmware, does not provide any specific functionality
    yet. MCP ROM firmware loads the MCP RAM firmware which then executes
    a WFI to enter idle state.
    
    Change-Id: I569b7e06c8bb66f24fb1f16e6ae2f1fb63dcf306
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

---
+++ product/rddaniel/include/mcp_software_mmap.h
@@ -0,0 +1,19 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MCP_RDDANIEL_MMAP_MCP_H
+#define MCP_RDDANIEL_MMAP_MCP_H
+
+#define MCP_ROM_BASE            0x00000000
+#define MCP_RAM0_BASE           0x00800000
+#define MCP_RAM1_BASE           0x20000000
+
+#define MCP_ROM_SIZE            (64 * 1024)
+#define MCP_RAM0_SIZE           (64 * 1024)
+#define MCP_RAM1_SIZE           (64 * 1024)
+
+#endif /* MCP_RDDANIEL_MMAP_MCP_H */
