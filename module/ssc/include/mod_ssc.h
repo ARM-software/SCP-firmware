b9d5d5951a139e4d5cffb62e401cbe9f51a8189d
Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
2020-01-08 13:26:26 +0530

    module: add initial support for ssc registers
    This patch adds a initial module support for Serial Security Control
    interface registers which is available in ARM platforms such as Juno,
    N1SDP and SGI-575. SSC module provides an API using which the
    system_info module can bind to this module and obtain the system
    information data.
    
    Change-Id: I36c750b1ce7d9ab044bffe50715c64cfcb366731
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>

---
+++ module/ssc/include/mod_ssc.h
@@ -0,0 +1,33 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MOD_SSC_H
+#define MOD_SSC_H
+
+/*!
+ * \brief Module configuration.
+ */
+struct mod_ssc_config {
+    /*! Base address of the SSC registers. */
+    uintptr_t ssc_base;
+
+    /*! Debug authentication configuration set register */
+    uint32_t ssc_debug_cfg_set;
+
+    /*! Product name */
+    const char *product_name;
+};
+
+/*!
+ * \brief Module API indicies.
+ */
+enum mod_ssc_api_idx {
+    MOD_SSC_SYSTEM_INFO_DRIVER_DATA_API_IDX,
+    MOD_SSC_API_COUNT
+};
+
+#endif /* MOD_SSC_H */
