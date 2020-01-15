1c60d308c53a6ad86aa1270d619d8649172004e3
Manoj Kumar <manoj.kumar3@arm.com>
2020-01-15 12:09:46 +0000

    n1sdp: add PD level 3 for multichip operation
    This patch fixes commit 51fc98743396e69c3d62fc499c8651c1848567cc where
    slave chip's CPUs, clusters & DBGTOP were assigned under master's SYSTOP
    because power_domain module had a bug in PD level 3 handling.
    
    Since the power_domain bug was fixed, slave chip's power domains are now
    assigned to slave's SYSTOP. Both master SYSTOP (SYSTOP0) and
    slave SYSTOP (SYSTOP1) and assigned to a logical SYSTOP for completion
    of the PD tree. The logical SYSTOP is a dummy entity.
    
    Change-Id: I2d4272e5023a59b6734b484a6017cd878014002d
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>

--- product/n1sdp/scp_ramfw/config_power_domain.h
+++ product/n1sdp/scp_ramfw/config_power_domain.h
@@ -29,32 +29,10 @@ enum pd_single_chip_idx {
 };
 
 /*
- * Power domain indices in multi chip use case (with only master SYSTOP)
- * TBD: Use chip specific SYSTOPs (level 2) and logical SYSTOP (level 3)
- *      once level 3 handling is fixed in PD driver. Then PD tree view
- *      in master SCP will look like:
- *      enum pd_multi_chip_idx {
- *          PD_MULTI_CHIP_IDX_CLUS0CORE0,
- *          PD_MULTI_CHIP_IDX_CLUS0CORE1,
- *          PD_MULTI_CHIP_IDX_CLUS1CORE0,
- *          PD_MULTI_CHIP_IDX_CLUS1CORE1,
- *          PD_MULTI_CHIP_IDX_CLUS2CORE0,
- *          PD_MULTI_CHIP_IDX_CLUS2CORE1,
- *          PD_MULTI_CHIP_IDX_CLUS3CORE0,
- *          PD_MULTI_CHIP_IDX_CLUS3CORE1,
- *          PD_MULTI_CHIP_IDX_CLUSTER0,
- *          PD_MULTI_CHIP_IDX_CLUSTER1,
- *          PD_MULTI_CHIP_IDX_DBGTOP0,
- *          PD_MULTI_CHIP_IDX_CLUSTER2,
- *          PD_MULTI_CHIP_IDX_CLUSTER3,
- *          PD_MULTI_CHIP_IDX_DBGTOP1,
- *          PD_MULTI_CHIP_IDX_SYSTOP0,
- *          PD_MULTI_CHIP_IDX_SYSTOP1,
- *          PD_MULTI_CHIP_IDX_SYSTOP_LOGICAL,
- *          PD_MULTI_CHIP_IDX_COUNT,
- *      };
+ * Power domain indices in multi chip use case
  */
 enum pd_multi_chip_idx {
+    /* PD Level 0 */
     PD_MULTI_CHIP_IDX_CLUS0CORE0,
     PD_MULTI_CHIP_IDX_CLUS0CORE1,
     PD_MULTI_CHIP_IDX_CLUS1CORE0,
@@ -63,12 +41,18 @@ enum pd_multi_chip_idx {
     PD_MULTI_CHIP_IDX_CLUS2CORE1,
     PD_MULTI_CHIP_IDX_CLUS3CORE0,
     PD_MULTI_CHIP_IDX_CLUS3CORE1,
+    /* PD Level 1 */
     PD_MULTI_CHIP_IDX_CLUSTER0,
     PD_MULTI_CHIP_IDX_CLUSTER1,
+    PD_MULTI_CHIP_IDX_DBGTOP0,
     PD_MULTI_CHIP_IDX_CLUSTER2,
     PD_MULTI_CHIP_IDX_CLUSTER3,
-    PD_MULTI_CHIP_IDX_DBGTOP0,
+    PD_MULTI_CHIP_IDX_DBGTOP1,
+    /* PD Level 2 */
     PD_MULTI_CHIP_IDX_SYSTOP0,
+    PD_MULTI_CHIP_IDX_SYSTOP1,
+    /* PD Level 3 */
+    PD_MULTI_CHIP_IDX_SYSTOP_LOGICAL,
     PD_MULTI_CHIP_IDX_COUNT,
 };
 
