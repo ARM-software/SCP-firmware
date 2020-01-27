dd011ec5539b15376891cd2eec38b59cb8c56b8e
Aditya Angadi <aditya.angadi@arm.com>
2020-01-27 09:29:46 +0530

    product/rddanielxlr: add build support for scp ram firmware
    Add the makefile to build the RAM firmware and the corresponding linker
    script.
    
    Change-Id: I458a7303cf041b3d75417a10752b9c61fe8d8770
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

---
+++ product/rddanielxlr/scp_ramfw/firmware.mk
@@ -0,0 +1,64 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+BS_FIRMWARE_CPU := cortex-m7
+BS_FIRMWARE_HAS_MULTITHREADING := yes
+BS_FIRMWARE_HAS_NOTIFICATION := yes
+BS_FIRMWARE_NOTIFICATION_COUNT := 128
+
+BS_FIRMWARE_MODULES := \
+    armv7m_mpu \
+    sid \
+    system_info \
+    pcid \
+    pl011 \
+    log \
+    pik_clock \
+    css_clock \
+    clock \
+    gtimer \
+    timer \
+    cmn_rhodes \
+    ppu_v1 \
+    system_power \
+    mhu2 \
+    smt \
+    scmi \
+    sds \
+    system_pll \
+    apcontext \
+    power_domain \
+    scmi_power_domain \
+    scmi_system_power \
+    rddanielxlr_system
+
+BS_FIRMWARE_SOURCES := \
+    config_system_power.c \
+    config_sid.c \
+    config_system_info.c \
+    rtx_config.c \
+    config_armv7m_mpu.c \
+    config_pl011.c \
+    config_log.c \
+    config_power_domain.c \
+    config_ppu_v1.c \
+    config_mhu2.c \
+    config_smt.c \
+    config_scmi.c \
+    config_sds.c \
+    config_timer.c \
+    config_gtimer.c \
+    config_cmn_rhodes.c \
+    config_scmi_system_power.c \
+    config_scmi_power_domain.c \
+    config_system_pll.c \
+    config_pik_clock.c \
+    config_css_clock.c \
+    config_clock.c \
+    config_apcontext.c
+
+include $(BS_DIR)/firmware.mk
