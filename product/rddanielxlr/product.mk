53a4f6913746f2815b52c43588f42ff495668b78
Aditya Angadi <aditya.angadi@arm.com>
2020-01-27 09:30:43 +0530

    product/rddanielxlr: add the product.mk file for rddanielxlr platform
    Add the product.mk file for RD-Daniel Config-XLR platform and make
    it available to the scp build system as a product.
    
    Change-Id: I3deacb73195ff19fd519c7d314a40da4fb976595
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

---
+++ product/rddanielxlr/product.mk
@@ -0,0 +1,13 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Platforms supported by the rddanielxlr product: RD-Daniel Config-XLR
+#
+
+BS_PRODUCT_NAME := rddanielxlr
+BS_FIRMWARE_LIST := scp_romfw \
+                    scp_ramfw \
+                    mcp_romfw
