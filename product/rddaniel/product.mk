04c3b0e9e13a073e85761750c53f85359c4bc3b2
Aditya Angadi <aditya.angadi@arm.com>
2020-01-27 09:30:43 +0530

    product/rddaniel: add the product.mk file for rd-daniel platform
    Add the product.mk file for RD-Daniel platform and make it availabe to
    the scp build system as a product
    
    Change-Id: I810c483ffe94fb8b951f06c7d85d5f7892840f83
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

---
+++ product/rddaniel/product.mk
@@ -0,0 +1,13 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Platforms supported by the rddaniel product: RDDANIEL.
+#
+
+BS_PRODUCT_NAME := rddaniel
+BS_FIRMWARE_LIST := scp_romfw \
+                    scp_ramfw \
+                    mcp_romfw
