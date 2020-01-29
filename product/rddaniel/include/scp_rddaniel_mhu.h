f9a0f8ff34ee2c285f35acb33bdd86c6498ebcaa
Aditya Angadi <aditya.angadi@arm.com>
2020-01-29 16:35:05 +0530

    product/rddaniel: add configuration data for MHUv2 driver
    Provide the details of the MHUv2 channels used for messaging from the
    application cores to the SCP.
    
    Change-Id: I95a135a772a61568858be8525f9e7b7db684746f
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

---
+++ product/rddaniel/include/scp_rddaniel_mhu.h
@@ -0,0 +1,19 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     MHU module device indexes.
+ */
+
+#ifndef SCP_RDDANIEL_MHU_H
+#define SCP_RDDANIEL_MHU_H
+
+enum scp_rddaniel_mhu_device_idx {
+    SCP_RDDANIEL_MHU_DEVICE_IDX_SCP_AP_S_CLUS0,
+    SCP_RDDANIEL_MHU_DEVICE_IDX_COUNT
+};
+
+#endif /* SCP_RDDANIEL_MHU_H */
