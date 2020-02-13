af0b6a3273407c8d59491cf0b4584bc18cd29c50
Anthony Roberts <anthony.roberts@arm.com>
2020-02-13 16:29:15 +0000

    dbg: Add debugger CLI module
    This module adds support for entering the CLI while running by
    pressing CTRL-E.
    
    It polls the UART in a non-blocking fashion using an alarm, and
    fires an event if the correct key combination is detected, in order
    to avoid running the CLI in the alarm ISR. Handling of further
    events in the queue is blocked until the CLI is exited. Events
    can still be queued, but will not be processed.
    
    Implements support for N1SDP and SGM775 in release mode.
    
    Change-Id: I4a20d71a9539935cba549e03ef4057ce6ddcb538
    Signed-off-by: Anthony Roberts <anthony.roberts@arm.com>

---
+++ module/debugger_cli/include/mod_debugger_cli.h
@@ -0,0 +1,32 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Debugger CLI module.
+ */
+
+#ifndef MOD_DEBUGGER_CLI_H
+#define MOD_DEBUGGER_CLI_H
+
+#include <mod_timer.h>
+#include <fwk_module.h>
+
+/*!
+ * \brief Debugger CLI module configuration.
+ */
+struct mod_debugger_cli_module_config {
+    /*!
+     * Element identifier of the alarm used for polling the UART
+     */
+    fwk_id_t alarm_id;
+
+    /*!
+     * Time period to set for the poll alarm delay (milliseconds)
+     */
+    uint32_t poll_period;
+};
+
+#endif /* MOD_DEBUGGER_CLI_H */
