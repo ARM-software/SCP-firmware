8163e0a158d2fdb42f1e78e4220b63ac12636380
Chris Kay <chris.kay@arm.com>
2020-03-13 15:09:23 +0000

    juno: Remove -Os/-flto for debug mode
    New adjustments to the maximum size of the SCP_BL2 image in Trusted
    Firmware-A means we now have far greater room for expansion.
    
    Change-Id: I9bc4791e5a2347f2d30bdcb3a482b5a3e6801e0a
    Signed-off-by: Chris Kay <chris.kay@arm.com>

--- tools/build_system/rules.mk
+++ tools/build_system/rules.mk
@@ -44,16 +44,7 @@ endif
 # GCC-specific optimization levels for debug and release modes
 #
 
-ifeq ($(PRODUCT)-$(FIRMWARE),juno-scp_ramfw)
-    # Enable link-time optimization
-    CFLAGS_GCC += -flto
-    LDFLAGS_GCC += -Wl,-flto
-
-    DEFAULT_OPT_GCC_DEBUG := s
-else
-    DEFAULT_OPT_GCC_DEBUG := g
-endif
-
+DEFAULT_OPT_GCC_DEBUG := g
 DEFAULT_OPT_GCC_RELEASE := 2
 
 #
