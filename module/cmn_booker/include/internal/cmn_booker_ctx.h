c0a518c23999992abcd81359e0bff47bf7576fb5
Usama Arif <usama.arif@arm.com>
2020-04-21 11:51:39 +0100

    module: add support for CMN Booker interconnect
    Add initial support for CMN Booker interconnect. This include
    configuring SAM tables, programming the nodes and sufficient support for
    single chip operation. When compared to other CMN such as CMN Rhodes and
    CMN600, CMN Booker has support for MTSX node type and enables coherent
    read/writes through HND node.
    
    Change-Id: I3ac4736601d9d79c11ee202e0a35146ba16d1596
    Signed-off-by: Usama Arif <usama.arif@arm.com>

---
+++ module/cmn_booker/include/internal/cmn_booker_ctx.h
@@ -0,0 +1,50 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *      CMN_BOOKER Context structure Interface
+ */
+
+#ifndef INTERNAL_CMN_BOOKER_CTX_H
+#define INTERNAL_CMN_BOOKER_CTX_H
+
+#include <cmn_booker.h>
+
+#include <mod_cmn_booker.h>
+
+#include <stdbool.h>
+#include <stdint.h>
+
+static struct cmn_booker_ctx {
+    const struct mod_cmn_booker_config *config;
+
+    struct cmn_booker_cfgm_reg *root;
+
+    /* Number of HN-F (system cache) nodes in the system */
+    unsigned int hnf_count;
+    uint64_t *hnf_cache_group;
+    uint64_t *sn_nodeid_group;
+
+    /*
+     * External RN-SAMs. The driver keeps a list of tuples (node identifier and
+     * node pointers). The configuration of these nodes is via the SAM API.
+     */
+    unsigned int external_rnsam_count;
+    struct external_rnsam_tuple *external_rnsam_table;
+
+    /*
+     * Internal RN-SAMs. The driver keeps a list of RN-SAM pointers to
+     * configure them once the system has been fully discovered and all
+     * parameters are known
+     */
+    unsigned int internal_rnsam_count;
+    struct cmn_booker_rnsam_reg **internal_rnsam_table;
+
+    bool initialized;
+} *ctx;
+
+
+#endif /* INTERNAL_CMN600_CTX_H */
