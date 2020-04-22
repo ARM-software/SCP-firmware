453314c476d1d4a406043d7c1a2db6825643e7e9
Usama Arif <usama.arif@arm.com>
2020-04-22 16:50:42 +0100

    product/tc0: add configuration data for system PLL driver
    Add the base address of the control registers and the initial rates for
    the PLL hardware in Total Compute.
    
    Change-Id: I48bf5b171b7445ce523c662172fc6feb334b76cf
    Signed-off-by: Usama Arif <usama.arif@arm.com>

---
+++ product/tc0/include/scp_soc_mmap.h
@@ -0,0 +1,23 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SCP_SOC_MMAP_H
+#define SCP_SOC_MMAP_H
+
+#include "scp_mmap.h"
+
+#define SCP_PLL_BASE                 (SCP_SOC_EXPANSION3_BASE + 0x03000000)
+
+#define SCP_PLL_SYSPLL               (SCP_PLL_BASE + 0x00000000)
+#define SCP_PLL_INTERCONNECT         (SCP_PLL_BASE + 0x00000020)
+
+#define SCP_PLL_CPU0                 (SCP_PLL_BASE + 0x00000100)
+#define SCP_PLL_CPU1                 (SCP_PLL_BASE + 0x00000104)
+#define SCP_PLL_CPU2                 (SCP_PLL_BASE + 0x00000108)
+#define SCP_PLL_CPU3                 (SCP_PLL_BASE + 0x0000010C)
+
+#endif /* SCP_SOC_MMAP_H */
