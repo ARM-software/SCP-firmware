8f3d63042e8267a0fabc5f860c030dce367787f3
Usama Arif <usama.arif@arm.com>
2020-04-22 15:59:53 +0100

    product/tc0: add configuration data for log module
    Provide the configuration data for the log module that includes the
    details of the console to use to display the log messages and the type
    of log messages that are allowed to be displayed.
    
    Change-Id: Ie39203c0ab07558770ed58deed52120028887c42
    Signed-off-by: Usama Arif <usama.arif@arm.com>

---
+++ product/tc0/scp_romfw/config_log.c
@@ -0,0 +1,25 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <mod_log.h>
+
+#include <fwk_banner.h>
+#include <fwk_id.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+
+static const struct mod_log_config log_data = {
+    .device_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_PL011, 0),
+    .api_id = FWK_ID_API_INIT(FWK_MODULE_IDX_PL011, 0),
+    .banner = FWK_BANNER_SCP
+              FWK_BANNER_ROM_FIRMWARE
+              BUILD_VERSION_DESCRIBE_STRING "\n",
+};
+
+struct fwk_module_config config_log = {
+    .data = &log_data,
+};
