8d21976034e632d327b1505e7e9c91fba5c8dbc0
Usama Arif <usama.arif@arm.com>
2020-04-22 17:09:44 +0100

    product/tc0: add configuration data for power domain and PPU modules
    Provide the config data such as PPU base addresses and irq map for the
    PPU driver and config data for power domain HAL to construct the power
    domain tree. Only a single core (core 0) needs to be provided for PPU
    config in ROM as AP firmware only uses primary core but all the cores
    need to be provided in PPU RAM firmware which is used when the kernel
    switches ON secondary cores.
    
    Change-Id: I1da7fc3ca0e8bf8ce3132e8a84d564c64ed0de2a
    Signed-off-by: Usama Arif <usama.arif@arm.com>

--- product/tc0/include/scp_css_mmap.h
+++ product/tc0/include/scp_css_mmap.h
@@ -18,4 +18,12 @@
 #define SCP_PIK_SYSTEM_BASE          (SCP_ELEMENT_MANAGEMENT_PERIPHERAL_BASE \
                                        + 0x40000)
 
+#define SCP_PPU_CLUSTER_BASE(n)      (SCP_PIK_CLUSTER_BASE((n)) + 0x1000)
+#define SCP_PPU_CORE_BASE(n, m)      (SCP_PPU_CLUSTER_BASE((n)) + \
+                                       ((m) + 1) * 0x1000)
+
+#define SCP_PPU_SYS0_BASE            (SCP_PIK_SYSTEM_BASE + 0x1000)
+#define SCP_PPU_SYS1_BASE            (SCP_PIK_SYSTEM_BASE + 0x5000)
+
+#define SCP_PPU_BASE(n)              (SCP_PIK_CLUSTER_BASE(n) + 0x2000)
 #endif /* SCP_CSS_MMAP_H */
