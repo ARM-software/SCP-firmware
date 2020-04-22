b6b03ceefd51d0568961fef42f19ad10d5f4c08e
Usama Arif <usama.arif@arm.com>
2020-04-22 15:56:53 +0100

    product/tc0: add configuration data for pl011 UART module
    pl011 controller is used as a console port for debug and log messages.
    Add configuration data of this controller including base address and
    input clock frequency for the pl011 module to use.
    
    Change-Id: Ifa777deaaa6661a3ac706d07de5751fc98d7c017
    Signed-off-by: Usama Arif <usama.arif@arm.com>

---
+++ product/tc0/include/scp_css_mmap.h
@@ -0,0 +1,15 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SCP_CSS_MMAP_H
+#define SCP_CSS_MMAP_H
+
+#include "scp_mmap.h"
+
+#define SCP_UART_BASE                (SCP_PERIPHERAL_BASE + 0x2000)
+
+#endif /* SCP_CSS_MMAP_H */
