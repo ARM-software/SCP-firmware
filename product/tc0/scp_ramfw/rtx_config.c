1c0e2bbcc4e078fbb14991896065423ab76e9853
Usama Arif <usama.arif@arm.com>
2020-04-23 16:43:38 +0100

    product/tc0: add platform specific definitions for RTX
    Add platform specific definitions for callback functions for CMSIS RTX.
    
    Change-Id: If3d158f22eea2f9e6061cfe56811aaf26e068554
    Signed-off-by: Usama Arif <usama.arif@arm.com>

---
+++ product/tc0/scp_ramfw/rtx_config.c
@@ -0,0 +1,38 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "clock_soc.h"
+
+#include <rtx_lib.c>
+#include <rtx_os.h>
+
+#include <fmw_cmsis.h>
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/*
+ * Required by RTX to configure the SysTick timer.
+ */
+uint32_t SystemCoreClock = CLOCK_RATE_REFCLK;
+
+/*
+ * Idle thread
+ */
+__NO_RETURN void osRtxIdleThread(void *argument)
+{
+    while (true)
+        __WFI();
+}
+
+/*
+ * OS error handler
+ */
+uint32_t osRtxErrorNotify(uint32_t code, void *object_id)
+{
+    osRtxIdleThread(object_id);
+}
