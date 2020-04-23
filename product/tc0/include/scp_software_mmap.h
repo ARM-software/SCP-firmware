7513d299f30524b0b9abb9a2bfcefd8b20298b19
Usama Arif <usama.arif@arm.com>
2020-04-23 12:18:15 +0100

    product/tc0: add configuration data for SDS service module
    SDS module is used to pass runtime data from SCP firmware to the
    firmware on AP core using shared memory. Provide the base address
    and size for the same and also the payloads to be passed. CPU info
    and feature availability SDS identifiers are used by both ROM and
    RAM, but bootloader SDS is used by ROM only as it used to copy
    copy SCP RAM firmware from AP secure RAM to SCP RAM.
    
    Change-Id: I6ef9a112b158530bbdf9e1fc311bf002ad444cea
    Signed-off-by: Usama Arif <usama.arif@arm.com>

---
+++ product/tc0/include/scp_software_mmap.h
@@ -0,0 +1,29 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SCP_SOFTWARE_MMAP_H
+#define SCP_SOFTWARE_MMAP_H
+
+
+#include "scp_soc_mmap.h"
+
+#include <fwk_macros.h>
+
+/* SCP trusted and non-trusted RAM base address */
+#define SCP_TRUSTED_RAM_BASE               (SCP_SYSTEM_ACCESS_PORT1_BASE + \
+                                            0x04000000)
+
+/* Secure Shared memory between AP and SCP */
+#define SCP_AP_SHARED_SECURE_BASE          (SCP_TRUSTED_RAM_BASE)
+#define SCP_AP_SHARED_SECURE_SIZE          (4 * FWK_KIB)
+
+
+/* SDS Memory Region */
+#define SCP_SDS_MEM_BASE                   (SCP_AP_SHARED_SECURE_BASE)
+#define SCP_SDS_MEM_SIZE                   (3520)
+
+#endif /* SCP_SOFTWARE_MMAP_H */
