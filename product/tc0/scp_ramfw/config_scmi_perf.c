0550a00707f1d9422cc6a3f08a26c10f83abf8e4
Usama Arif <usama.arif@arm.com>
2020-04-24 15:56:06 +0100

    product/tc0: add configuration files for enabling DVFS
    Provide configuration data for DVFS, PSU and scmi_perf for changing voltage
    and frequency. DVFS uses a mock psu to set the voltage levels.
    
    Change-Id: I0aebb658f2d20225958057ef3f603dcd322209ba
    Signed-off-by: Usama Arif <usama.arif@arm.com>

---
+++ product/tc0/scp_ramfw/config_scmi_perf.c
@@ -0,0 +1,32 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "tc0_scmi.h"
+
+#include <mod_scmi_perf.h>
+
+#include <fwk_module.h>
+
+#include <stdint.h>
+
+static const struct mod_scmi_perf_domain_config domains[] = {
+    [0] = {
+        .permissions = &(const uint32_t[]) {
+            [SCP_SCMI_AGENT_ID_OSPM] = MOD_SCMI_PERF_PERMS_SET_LEVEL  |
+                                       MOD_SCMI_PERF_PERMS_SET_LIMITS,
+            [SCP_SCMI_AGENT_ID_PSCI] = MOD_SCMI_PERF_PERMS_NONE,
+        }
+    },
+};
+
+const struct fwk_module_config config_scmi_perf = {
+    .get_element_table = NULL,
+    .data = &((struct mod_scmi_perf_config) {
+        .domains = &domains,
+        .fast_channels_alarm_id = FWK_ID_NONE_INIT,
+    }),
+};
