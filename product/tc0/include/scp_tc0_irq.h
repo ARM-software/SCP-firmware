95793e6add5e694b2003a5a6fcf956abdf1c3eb6
Usama Arif <usama.arif@arm.com>
2020-04-24 18:45:47 +0100

    product/tc0: add TC0 system module
    The system module sets up the SCMI services and SDS messaging stack.
    
    Change-Id: Ia01cec8583c67dafcc0847ec827d2d55dd8a3499
    Signed-off-by: Usama Arif <usama.arif@arm.com>

--- product/tc0/include/scp_tc0_irq.h
+++ product/tc0/include/scp_tc0_irq.h
@@ -15,6 +15,15 @@ enum scp_tc0_interrupt {
     TIMREFCLK_IRQ              = 33, /* REFCLK Physical Timer */
     MHU_AP_NONSEC_IRQ          = 82, /* MHU non-secure irq bewteen SCP and AP */
     MHU_AP_SEC_IRQ             = 83, /* MHU secure irq bewteen SCP and AP */
+    PPU_CORES0_IRQ             = 50, /* Consolidated PPU Interrupt for cores
+                                        1-32, 129-160 */
+    PPU_CORES1_IRQ             = 51, /* Consolidated PPU Interrupt for cores
+                                        33-64, 161-192 */
+    PPU_CORES2_IRQ             = 52, /* Consolidated PPU Interrupt for cores
+                                        65-96, 193-224 */
+    PPU_CORES3_IRQ             = 53, /* Consolidated PPU Interrupt for cores
+                                        97-128, 225-256 */
+    PPU_CLUSTERS_IRQ           = 54, /* Consolidated clusters PPU */
 };
 
 #endif /* SCP_TC0_IRQ_H */
