79b25b694f12a9a7a8634c89a41dae843111e82c
Jim Quigley <jim.quigley@arm.com>
2020-04-27 12:47:24 +0100

    SCMIv2: Add P2A SCMI Service for notifications
    This patch adds a P2A service channel for notifications.
    
    Change-Id: I5fc661dab47f02b8ce7ef15e868d579bb6333013
    Signed-off-by: Jim Quigley <jim.quigley@arm.com>

--- product/juno/scp_ramfw/config_smt.c
+++ product/juno/scp_ramfw/config_smt.c
@@ -21,7 +21,7 @@
 #include <stdint.h>
 
 static const struct fwk_element element_table[] = {
-    [JUNO_SCMI_SERVICE_IDX_PSCI] = {
+    [JUNO_SCMI_SERVICE_IDX_PSCI_A2P] = {
         .name = "",
         .data = &(struct mod_smt_channel_config) {
             .type = MOD_SMT_CHANNEL_TYPE_SLAVE,
@@ -38,7 +38,7 @@ static const struct fwk_element element_table[] = {
                 POWER_DOMAIN_IDX_SYSTOP),
         }
     },
-    [JUNO_SCMI_SERVICE_IDX_OSPM_0] = {
+    [JUNO_SCMI_SERVICE_IDX_OSPM_A2P_0] = {
         .name = "",
         .data = &(struct mod_smt_channel_config) {
             .type = MOD_SMT_CHANNEL_TYPE_SLAVE,
@@ -55,7 +55,7 @@ static const struct fwk_element element_table[] = {
                 POWER_DOMAIN_IDX_SYSTOP),
         }
     },
-    [JUNO_SCMI_SERVICE_IDX_OSPM_1] = {
+    [JUNO_SCMI_SERVICE_IDX_OSPM_A2P_1] = {
         .name = "",
         .data = &(struct mod_smt_channel_config) {
             .type = MOD_SMT_CHANNEL_TYPE_SLAVE,
@@ -72,6 +72,25 @@ static const struct fwk_element element_table[] = {
                 POWER_DOMAIN_IDX_SYSTOP),
         }
     },
+#ifdef BUILD_HAS_SCMI_NOTIFICATIONS
+    [JUNO_SCMI_SERVICE_IDX_OSPM_P2A] = {
+        .name = "",
+        .data = &(struct mod_smt_channel_config) {
+            .type = MOD_SMT_CHANNEL_TYPE_MASTER,
+            .policies = MOD_SMT_POLICY_INIT_MAILBOX,
+            .mailbox_address = (uintptr_t)SCMI_PAYLOAD_HIGH_P2A_BASE,
+            .mailbox_size = SCMI_PAYLOAD_SIZE,
+            .driver_id = FWK_ID_SUB_ELEMENT_INIT(
+                FWK_MODULE_IDX_MHU,
+                JUNO_MHU_DEVICE_IDX_NS_H,
+                1),
+            .driver_api_id = FWK_ID_API_INIT(FWK_MODULE_IDX_MHU, 0),
+            .pd_source_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_POWER_DOMAIN,
+                POWER_DOMAIN_IDX_SYSTOP),
+        }
+    },
+#endif
     [JUNO_SCMI_SERVICE_IDX_COUNT] = { 0 },
 };
 
