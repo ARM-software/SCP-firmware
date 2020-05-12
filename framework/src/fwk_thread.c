d61d58bac02f2434b8d1ca6cf8fd4dbad5e14736
Jim Quigley <jim.quigley@arm.com>
2020-05-12 15:29:55 +0100

    fwk/thread: Single-threaded mode for blocking events
    This patch implements fwk_thread_put_event_and_wait() for
    platforms which do not support multithreading.
    
    Change-Id: Id3e3dd5054b7df378610b2d511de4e08282ac3a4
    Signed-off-by: Jim Quigley <jim.quigley@arm.com>

--- framework/src/fwk_thread.c
+++ framework/src/fwk_thread.c
@@ -34,6 +34,12 @@ static struct __fwk_thread_ctx ctx;
 static const char err_msg_line[] = "[FWK] Error %d in %s @%d";
 static const char err_msg_func[] = "[FWK] Error %d in %s";
 
+/* States for put_event_and_wait */
+enum wait_states {
+    WAITING_FOR_EVENT = 0,
+    WAITING_FOR_RESPONSE  = 1,
+};
+
 /*
  * Static functions
  */
@@ -74,6 +80,7 @@ static int put_event(struct fwk_event *event)
 {
     struct fwk_event *allocated_event;
     unsigned int interrupt;
+    bool is_wakeup_event = false;
 
     if (event->is_delayed_response) {
         allocated_event = __fwk_thread_search_delayed_response(
@@ -91,6 +98,11 @@ static int put_event(struct fwk_event *event)
             allocated_event->params,
             event->params,
             sizeof(allocated_event->params));
+
+        /* Is this the event put_event_and_wait is waiting for ? */
+        if (ctx.waiting_event_processing_completion &&
+            (ctx.cookie == event->cookie))
+            is_wakeup_event = true;
     } else {
         allocated_event = duplicate_event(event);
         if (allocated_event == NULL)
@@ -99,12 +111,22 @@ static int put_event(struct fwk_event *event)
 
     allocated_event->cookie = event->cookie = ctx.event_cookie_counter++;
 
+    if (is_wakeup_event)
+       ctx.cookie = event->cookie;
+
     if (fwk_interrupt_get_current(&interrupt) != FWK_SUCCESS)
         fwk_list_push_tail(&ctx.event_queue, &allocated_event->slist_node);
     else
         fwk_list_push_tail(&ctx.isr_event_queue, &allocated_event->slist_node);
 
     return FWK_SUCCESS;
+}
+
+static void free_event(struct fwk_event *event)
+{
+    fwk_interrupt_global_disable();
+    fwk_list_push_tail(&ctx.free_event_queue, &event->slist_node);
+    fwk_interrupt_global_enable();
 }
 
 static void process_next_event(void)
@@ -154,16 +176,17 @@ static void process_next_event(void)
         }
     } else {
         status = process_event(event, &async_response_event);
-        if (status != FWK_SUCCESS)
-            FWK_LOG_CRIT(err_msg_line, status, __func__, __LINE__);
+        if ((status != FWK_SUCCESS) && (status != FWK_PENDING)) {
+            FWK_LOG_CRIT(
+                "[FWK] Process event (%s: %s -> %s) (%d)\n",
+                FWK_ID_STR(event->id),
+                FWK_ID_STR(event->source_id),
+                FWK_ID_STR(event->target_id), status);
+        }
     }
 
     ctx.current_event = NULL;
-
-    fwk_interrupt_global_disable();
-    fwk_list_push_tail(&ctx.free_event_queue, &event->slist_node);
-    fwk_interrupt_global_enable();
-
+    free_event(event);
     return;
 }
 
@@ -289,6 +312,172 @@ int fwk_thread_put_event(struct fwk_event *event)
 
     return put_event(event);
 
+error:
+    FWK_LOG_CRIT(err_msg_func, status, __func__);
+    return status;
+}
+
+int fwk_thread_put_event_and_wait(struct fwk_event *event,
+    struct fwk_event *resp_event)
+{
+    const struct fwk_module *module;
+    int (*process_event)(const struct fwk_event *event,
+                         struct fwk_event *resp_event);
+    struct fwk_event response_event;
+    struct fwk_event *next_event;
+    struct fwk_event *allocated_event;
+    unsigned int interrupt;
+    int status = FWK_E_PARAM;
+    enum wait_states wait_state = WAITING_FOR_EVENT;
+
+    if (!ctx.initialized) {
+        status = FWK_E_INIT;
+        goto error;
+    }
+
+    if ((event == NULL) || (resp_event == NULL))
+        goto error;
+
+    if (!fwk_module_is_valid_event_id(event->id))
+        goto error;
+
+    if (fwk_interrupt_get_current(&interrupt) == FWK_SUCCESS) {
+        status = FWK_E_STATE;
+        goto error;
+    }
+
+    if (ctx.current_event != NULL)
+        event->source_id = ctx.current_event->target_id;
+    else if (!fwk_module_is_valid_entity_id(event->source_id)) {
+        FWK_LOG_ERR(
+            "[FWK] deprecated put_event_and_wait (%s: %s -> %s)\n",
+            FWK_ID_STR(event->id),
+            FWK_ID_STR(event->source_id),
+            FWK_ID_STR(event->target_id));
+        goto error;
+    }
+
+    /* No support for nested put_event_and_wait calls */
+    if (ctx.waiting_event_processing_completion) {
+        status = FWK_E_BUSY;
+        goto error;
+    }
+    ctx.waiting_event_processing_completion = true;
+    ctx.previous_event = ctx.current_event;
+
+    FWK_LOG_TRACE(
+        "[FWK] deprecated put_event_and_wait (%s: %s -> %s)\n",
+        FWK_ID_STR(event->id),
+        FWK_ID_STR(event->source_id),
+        FWK_ID_STR(event->target_id));
+
+    event->is_response = false;
+    event->is_delayed_response = false;
+    event->response_requested = true;
+    event->is_notification = false;
+
+    status = put_event(event);
+    if (status != FWK_SUCCESS)
+        goto exit;
+
+    ctx.cookie = event->cookie;
+
+    for (;;) {
+
+        if (fwk_list_is_empty(&ctx.event_queue)) {
+            if (!fwk_list_is_empty(&ctx.isr_event_queue))
+                process_isr();
+            continue;
+        }
+
+        ctx.current_event = next_event = FWK_LIST_GET(fwk_list_head(
+            &ctx.event_queue), struct fwk_event, slist_node);
+
+        if (next_event->cookie != ctx.cookie) {
+            /*
+            * Process any events waiting on the event_queue until
+            * we get to the event from the waiting call.
+            */
+            process_next_event();
+            if (!fwk_list_is_empty(&ctx.isr_event_queue))
+                process_isr();
+            continue;
+        }
+
+        /* This is either the original event or the response event */
+        next_event = FWK_LIST_GET(
+            fwk_list_pop_head(&ctx.event_queue), struct fwk_event, slist_node);
+
+        if (wait_state == WAITING_FOR_EVENT) {
+            module = __fwk_module_get_ctx(next_event->target_id)->desc;
+            process_event = module->process_event;
+
+            response_event = *next_event;
+            response_event.source_id = next_event->target_id;
+            response_event.target_id = next_event->source_id;
+            response_event.is_delayed_response = false;
+
+            /* Execute the event handler */
+            status = process_event(next_event, &response_event);
+            if (status != FWK_SUCCESS)
+                goto exit;
+
+            /*
+             * The response event goes onto the queue now
+             * and we update the cookie to wait for the
+             * response.
+             */
+            response_event.is_response = true;
+            response_event.response_requested = false;
+            if (!response_event.is_delayed_response) {
+                status = put_event(&response_event);
+                if (status != FWK_SUCCESS)
+                    goto exit;
+                ctx.cookie = response_event.cookie;
+            } else {
+                allocated_event = duplicate_event(&response_event);
+                if (allocated_event != NULL) {
+                    fwk_list_push_head(
+                        __fwk_thread_get_delayed_response_list(
+                            response_event.source_id),
+                            &allocated_event->slist_node);
+                } else {
+                    status = FWK_E_NOMEM;
+                    goto exit;
+                }
+                ctx.cookie = allocated_event->cookie;
+            }
+
+            wait_state = WAITING_FOR_RESPONSE;
+            free_event(next_event);
+
+            /*
+             * Check for any interrupt events that might have been
+             * queued while the event was being executed.
+             */
+            while (!fwk_list_is_empty(&ctx.isr_event_queue))
+                process_isr();
+            continue;
+        }
+
+        if (wait_state == WAITING_FOR_RESPONSE) {
+            /*
+             * The response event has been received, return to
+             * the caller.
+             */
+            memcpy(resp_event->params, next_event->params,
+                sizeof(resp_event->params));
+            free_event(next_event);
+            status = FWK_SUCCESS;
+            goto exit;
+        }
+    }
+
+exit:
+    ctx.current_event = ctx.previous_event;
+    ctx.waiting_event_processing_completion = false;
+    if (status == FWK_SUCCESS)
+        return status;
 error:
     FWK_LOG_CRIT(err_msg_func, status, __func__);
     return status;
