13f2ca9dfdf7553abc05b6497cb89583629030b2
Lukasz Luba <lukasz.luba@arm.com>
2020-05-13 17:49:51 +0100

    product/juno: add config of statistics
    Add configuration of the statistics memory region for Juno.
    
    Change-Id: I0c3143297243b6a38c2c23ebc0e7fd278db110fa
    Signed-off-by: Lukasz Luba <lukasz.luba@arm.com>

---
+++ product/juno/scp_ramfw/config_stats.c
@@ -0,0 +1,24 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019-2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include "software_mmap.h"
+#include "juno_alarm_idx.h"
+
+#include <mod_stats.h>
+
+#include <fwk_module.h>
+
+static const struct mod_stats_config_info stats_config_info = {
+    .ap_stats_addr = SCMI_PERF_STATS_BASE - EXTERNAL_DEV_BASE,
+    .scp_stats_addr = SCMI_PERF_STATS_BASE,
+    .stats_region_size = SCMI_PERF_STATS_SIZE,
+    .alarm_id = FWK_ID_SUB_ELEMENT_INIT(FWK_MODULE_IDX_TIMER, 0,
+        JUNO_STATISTICS_ALARM_IDX),
+};
+
+struct fwk_module_config config_statistics = {
+    .data = &stats_config_info,
+};
