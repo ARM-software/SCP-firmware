9a0200919155d725caed569fe4988390c3fa52b9
Chris Kay <chris.kay@arm.com>
2020-06-01 18:16:34 +0100

    isys_rom: Introduce `isys_rom` module
    A lot of the bootloading functionality in InfraSYS-based systems is
    shared, as in MSYS-based systems. This change introduces the `isys_rom`
    module, analogous to the `msys_rom` for InfraSYS systems.
    
    Change-Id: Ib55aa68acb9dd7140808cfb51d0c2341eed3a1ba
    Signed-off-by: Chris Kay <chris.kay@arm.com>

---
+++ module/isys_rom/src/mod_isys_rom.c
@@ -0,0 +1,89 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <mod_bootloader.h>
+
+#include <fwk_event.h>
+#include <fwk_id.h>
+#include <fwk_log.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+#include <fwk_status.h>
+#include <fwk_thread.h>
+
+struct mod_isys_rom_ctx {
+    const struct mod_bootloader_api *bootloader_api;
+} ctx;
+
+enum mod_isys_rom_event {
+    MOD_ISYS_ROM_EVENT_RUN,
+    MOD_ISYS_ROM_EVENT_COUNT,
+};
+
+static int mod_isys_init(
+    fwk_id_t id,
+    unsigned int element_count,
+    const void *data)
+{
+    return FWK_SUCCESS;
+}
+
+static int mod_isys_rom_bind(fwk_id_t id, unsigned int round)
+{
+    int status = FWK_SUCCESS;
+
+    if (round > 0)
+        goto exit;
+
+    status = fwk_module_bind(
+        FWK_ID_MODULE(FWK_MODULE_IDX_BOOTLOADER),
+        FWK_ID_API(FWK_MODULE_IDX_BOOTLOADER, 0),
+        &ctx.bootloader_api);
+    if (status != FWK_SUCCESS)
+        status = FWK_E_PANIC;
+
+exit:
+    return status;
+}
+
+static int mod_isys_rom_start(fwk_id_t id)
+{
+    struct fwk_event event = {
+        .source_id = FWK_ID_MODULE(FWK_MODULE_IDX_ISYS_ROM),
+        .target_id = FWK_ID_MODULE(FWK_MODULE_IDX_ISYS_ROM),
+        .id = FWK_ID_EVENT(FWK_MODULE_IDX_ISYS_ROM, MOD_ISYS_ROM_EVENT_RUN),
+    };
+
+    return fwk_thread_put_event(&event);
+}
+
+static int mod_isys_rom_process_event(
+    const struct fwk_event *event,
+    struct fwk_event *resp)
+{
+    int status = ctx.bootloader_api->load_image();
+
+    FWK_LOG_ERR(
+        "[SYSTEM] Failed to load RAM firmware image: %s",
+        fwk_status_str(status));
+
+    return FWK_SUCCESS;
+}
+
+const struct fwk_module module_isys_rom = {
+    .name = "isys-rom",
+    .type = FWK_MODULE_TYPE_SERVICE,
+
+    .init = mod_isys_init,
+    .bind = mod_isys_rom_bind,
+    .start = mod_isys_rom_start,
+
+    .process_event = mod_isys_rom_process_event,
+    .event_count = MOD_ISYS_ROM_EVENT_COUNT,
+};
+
+const struct fwk_module_config config_isys_rom = { 0 };
