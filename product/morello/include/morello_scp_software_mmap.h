d0770a7cca41a7e4bfbe227bcc3d991039cdf656
Anurag Koul <anurag.koul@arm.com>
2020-06-03 00:07:29 +0100

    morello: add memory map headers for scp
    - Add SCP's peripheral memory map.
    - Add SCP software defined memory map shared b/w AP and SCP cores.
    - Add SCP's internal/external system memory map (ROM, SRAM, Flash, etc.).
    
    Change-Id: I2875e979bd95c7dc11e5f36e38e4337470f024e3
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>

---
+++ product/morello/include/morello_scp_software_mmap.h
@@ -0,0 +1,66 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Software defined memory map shared between SCP and AP cores.
+ */
+
+#ifndef MORELLO_SCP_SOFTWARE_MMAP_H
+#define MORELLO_SCP_SOFTWARE_MMAP_H
+
+#include "morello_scp_mmap.h"
+
+#include <fwk_macros.h>
+
+/*
+ * Trusted shared SRAM region
+ */
+#define SCP_AP_SHARED_SECURE_BASE (SCP_TRUSTED_RAM_BASE)
+#define SCP_AP_SHARED_SECURE_SIZE (4 * FWK_KIB)
+
+/*
+ * Non-trusted shared SRAM region
+ */
+#define SCP_AP_SHARED_NONSECURE_BASE (SCP_NONTRUSTED_RAM_BASE)
+#define SCP_AP_SHARED_NONSECURE_SIZE (4 * FWK_KIB)
+
+/*
+ * SDS Memory Region
+ */
+#define SCP_SDS_MEM_BASE (SCP_AP_SHARED_SECURE_BASE)
+#define SCP_SDS_MEM_SIZE (3520)
+#define SCP_SDS_SECURE_BASE (SCP_AP_SHARED_SECURE_BASE)
+#define SCP_SDS_SECURE_SIZE (3520)
+
+/*
+ * AP Context Area
+ */
+#define SCP_AP_CONTEXT_SIZE (64)
+#define SCP_AP_CONTEXT_BASE \
+    (SCP_AP_SHARED_SECURE_BASE + SCP_AP_SHARED_SECURE_SIZE - \
+     SCP_AP_CONTEXT_SIZE)
+
+/*
+ * SCMI Secure Payload Areas
+ */
+#define SCP_SCMI_PAYLOAD_SIZE (128)
+#define SCP_SCMI_PAYLOAD_S_A2P_BASE (SCP_SDS_SECURE_BASE + SCP_SDS_SECURE_SIZE)
+#define SCP_SCMI_PAYLOAD_S_P2A_BASE \
+    (SCP_SCMI_PAYLOAD_S_A2P_BASE + SCP_SCMI_PAYLOAD_SIZE)
+
+/*
+ * SCMI Non-Secure Payload Areas
+ */
+#define SCP_SCMI_PAYLOAD_NS_A2P_BASE (SCP_AP_SHARED_NONSECURE_BASE)
+#define SCP_SCMI_PAYLOAD_NS_P2A_BASE \
+    (SCP_SCMI_PAYLOAD_NS_A2P_BASE + SCP_SCMI_PAYLOAD_SIZE)
+
+/* SDS non secure region */
+#define SCP_SDS_NONSECURE_BASE \
+    (SCP_SCMI_PAYLOAD_NS_P2A_BASE + SCP_SCMI_PAYLOAD_SIZE)
+#define SCP_SDS_NONSECURE_SIZE (1024)
+
+#endif /* MORELLO_SCP_SOFTWARE_MMAP_H */
---
+++ product/morello/include/morello_scp_system_mmap.h
@@ -0,0 +1,32 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MORELLO_SCP_SYSTEM_MMAP_H
+#define MORELLO_SCP_SYSTEM_MMAP_H
+
+/*
+ * External QSPI flash memory - mapped address
+ */
+#define SCP_QSPI_FLASH_BASE_ADDR 0x30000000
+#define SCP_QSPI_FLASH_BASE_ADDR_ALT 0x00840000
+#define SCP_QSPI_FLASH_SIZE 0x02000000
+
+/*
+ * Internal SCP's ROM/RAM base address
+ */
+#define SCP_ROM_BASE 0x00000000
+#define SCP_RAM0_BASE 0x00800000
+#define SCP_RAM1_BASE 0x20000000
+
+/*
+ * Internal SCP's ROM/RAM sizes
+ */
+#define SCP_ROM_SIZE (128 * 1024)
+#define SCP_RAM0_SIZE (512 * 1024)
+#define SCP_RAM1_SIZE (256 * 1024)
+
+#endif /* MORELLO_SCP_SYSTEM_MMAP_H*/
