103ddde46b14c214ae76140c671c7a509072f518
Jim Quigley <jim.quigley@arm.com>
2020-06-02 12:38:51 +0100

    SCMI: Performance Protocol policy handler
    This patch adds the SCMI performance protocol policy handler.
    
    Change-Id: I10eb79b2f3e70f9e76d7ae30a6af40f85c255c3e
    Signed-off-by: Jim Quigley <jim.quigley@arm.com>

--- module/scmi_perf/include/mod_scmi_perf.h
+++ module/scmi_perf/include/mod_scmi_perf.h
@@ -121,6 +121,97 @@ enum scmi_perf_api_idx {
 };
 
 /*!
+ * \defgroup GroupScmiPerformancePolicyHandlers Policy Handlers
+ *
+ * \brief SCMI Performance Policy Handlers.
+ *
+ * \details The SCMI policy handlers are weak definitions to allow a platform
+ *      to implement a policy appropriate to that platform. The SCMI
+ *      performance policy functions may be overridden in the
+ * `product/<platform>/src` directory.
+ *
+ * \note The `frequency`/`range` values may be changed by the policy handlers.
+ * \note See `product/juno/src/juno_scmi_clock.c` for an example policy
+ *      handler.
+ *
+ * \{
+ */
+
+/*!
+ * \brief Policy handler policies.
+ *
+ * \details These values are returned to the message handler by the policy
+ *      handlers to determine whether the message handler should continue
+ *      processing the message, or whether the request has been rejected.
+ */
+enum mod_scmi_perf_policy_status {
+    /*! Do not execute the message handler */
+    MOD_SCMI_PERF_SKIP_MESSAGE_HANDLER,
+
+    /*! Execute the message handler */
+    MOD_SCMI_PERF_EXECUTE_MESSAGE_HANDLER,
+};
+
+/*!
+ *
+ * \brief SCMI Performance Set Level command policy.
+ *
+ * \details This function determines whether the SCMI message handler should
+ *      allow or reject a given SCMI Performance Set Level command.
+ *
+ *      The SCMI policy handler is executed before the message handler is
+ *      called. The SCMI protocol message handler will only continue if the
+ *      policy handler both returns ::FWK_SUCCESS and sets the policy status to
+ *      ::MOD_SCMI_PERF_EXECUTE_MESSAGE_HANDLER.
+ *
+ * \param[out] policy_status Whether the command should be accepted or not.
+ * \param[in, out] frequency Frequency requested to be set.
+ * \param[in] agent_id Identifier of the agent requesting the service.
+ * \param[in] domain_id Identifier of the performance domain.
+ *
+ * \retval ::FWK_SUCCESS The operation succeeded.
+ *
+ * \return Status code representing the result of the operation.
+ */
+int scmi_perf_level_set_policy(
+    enum mod_scmi_perf_policy_status *policy_status,
+    uint64_t *frequency,
+    unsigned int agent_id,
+    fwk_id_t domain_id);
+
+/*!
+ * \brief SCMI Performance Set Limits command policy.
+ *
+ * \details This function determines whether the SCMI message handler should
+ *      allow or reject a given SCMI Performance Set Limits command.
+ *
+ *      The SCMI policy handler is executed before the message handler is
+ *      called. The SCMI protocol message handler will only continue if the
+ *      policy handler both returns ::FWK_SUCCESS and sets the policy status to
+ *      ::MOD_SCMI_PERF_EXECUTE_MESSAGE_HANDLER.
+ *
+ * \param[out] policy_status Whether the command should be accepted or not.
+ * \param[in, out] range_min Minimum frequency range.
+ * \param[in, out] range_max Maximum frequency range.
+ * \param[in] agent_id Identifier of the agent requesting the service.
+ * \param[in] domain_id Identifier of the performance domain.
+ *
+ * \retval ::FWK_SUCCESS The operation succeeded.
+ *
+ * \return Status code representing the result of the operation.
+ */
+int scmi_perf_limits_set_policy(
+    enum mod_scmi_perf_policy_status *policy_status,
+    uint64_t *range_min,
+    uint64_t *range_max,
+    unsigned int agent_id,
+    fwk_id_t domain_id);
+
+/*!
+ * @}
+ */
+
+/*!
  * @}
  */
 
