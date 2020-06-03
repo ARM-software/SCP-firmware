a97c77f011be887266b7c9e8839993af344789f4
Anurag Koul <anurag.koul@arm.com>
2020-06-03 15:18:10 +0100

    morello: add pik register definitions headers
    Add PIK register definition files for:
        - Power Control Registers
        - System PIK
        - Debug Top Power Control Logic
        - SCP PIK
        - GPU PIK
        - DPU PIK
    
    Change-Id: I258e5ee2d6f4b924e9378fecec888cbed0532825
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>
    Co-authored-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/morello/include/morello_pik_gpu.h
@@ -0,0 +1,46 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MORELLO_PIK_GPU_H
+#define MORELLO_PIK_GPU_H
+
+#include <fwk_macros.h>
+
+#include <stdint.h>
+
+/*!
+ * \brief GPU PIK register definitions
+ */
+struct pik_gpu_reg {
+    uint8_t RESERVED0[0x810];
+    FWK_RW uint32_t GPUCLK_CTRL;
+    FWK_RW uint32_t GPUCLK_DIV1;
+    FWK_RW uint32_t GPUCLK_DIV2;
+    uint8_t RESERVED1[0x820 - 0x81C];
+    FWK_RW uint32_t ACLKGPU_CTRL;
+    uint8_t RESERVED2[0xA00 - 0x824];
+    FWK_R uint32_t CLKFORCE_STATUS;
+    FWK_W uint32_t CLKFORCE_SET;
+    FWK_W uint32_t CLKFORCE_CLR;
+    uint8_t RESERVED3[0xFBC - 0xA0C];
+    FWK_R uint32_t CAP;
+    FWK_R uint32_t PCL_CONFIG;
+    uint8_t RESERVED4[0xFD0 - 0xFC4];
+    FWK_R uint32_t PID4;
+    FWK_R uint32_t PID5;
+    FWK_R uint32_t PID6;
+    FWK_R uint32_t PID7;
+    FWK_R uint32_t PID0;
+    FWK_R uint32_t PID1;
+    FWK_R uint32_t PID2;
+    FWK_R uint32_t PID3;
+    FWK_R uint32_t ID0;
+    FWK_R uint32_t ID1;
+    FWK_R uint32_t ID2;
+    FWK_R uint32_t ID3;
+};
+#endif /* MORELLO_PIK_GPU_H */
---
+++ product/morello/include/morello_scp_pik.h
@@ -0,0 +1,28 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MORELLO_SCP_PIK_H
+#define MORELLO_SCP_PIK_H
+
+#include "morello_pik_cpu.h"
+#include "morello_pik_debug.h"
+#include "morello_pik_dpu.h"
+#include "morello_pik_gpu.h"
+#include "morello_pik_scp.h"
+#include "morello_pik_system.h"
+#include "morello_scc_reg.h"
+#include "morello_scp_mmap.h"
+
+#define PIK_CLUSTER(IDX) ((struct pik_cpu_reg *)SCP_PIK_CLUSTER_BASE(IDX))
+#define PIK_SCP ((struct pik_scp_reg *)SCP_PIK_SCP_BASE)
+#define PIK_SYSTEM ((struct pik_system_reg *)SCP_PIK_SYSTEM_BASE)
+#define PIK_DEBUG ((struct pik_debug_reg *)SCP_PIK_DEBUG_BASE)
+#define PIK_GPU ((struct pik_gpu_reg *)SCP_PIK_GPU_BASE)
+#define PIK_DPU ((struct pik_dpu_reg *)SCP_PIK_DPU_BASE)
+#define SCC ((struct scc_reg *)SCP_SCC_BASE)
+
+#endif /* MORELLO_SCP_PIK_H */
