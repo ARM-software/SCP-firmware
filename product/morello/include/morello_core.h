e107df567a180ea00d1c88690f131a619a9e9831
Anurag Koul <anurag.koul@arm.com>
2020-06-03 17:46:52 +0100

    morello: add morello core support functions
    Add Morello core support functions for determining core
    count, cluster count, multi-chip mode, etc.
    
    Change-Id: I9943f566bf07af403d8e03991fc1d7fa51c96853
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>

---
+++ product/morello/include/morello_core.h
@@ -0,0 +1,23 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MORELLO_CORE_H
+#define MORELLO_CORE_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#define CORES_PER_CLUSTER 2
+#define NUMBER_OF_CLUSTERS 2
+
+unsigned int morello_core_get_core_per_cluster_count(unsigned int cluster);
+unsigned int morello_core_get_core_count(void);
+unsigned int morello_core_get_cluster_count(void);
+bool morello_is_multichip_enabled(void);
+uint8_t morello_get_chipid(void);
+
+#endif /* MORELLO_CORE_H */
---
+++ product/morello/src/morello_core.c
@@ -0,0 +1,43 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "morello_core.h"
+#include "morello_scc_reg.h"
+#include "morello_scp_pik.h"
+
+#include <fwk_assert.h>
+
+unsigned int morello_core_get_core_per_cluster_count(unsigned int cluster)
+{
+    assert(cluster < morello_core_get_cluster_count());
+
+    return CORES_PER_CLUSTER;
+}
+
+unsigned int morello_core_get_core_count(void)
+{
+    return (NUMBER_OF_CLUSTERS * CORES_PER_CLUSTER);
+}
+
+unsigned int morello_core_get_cluster_count(void)
+{
+    return NUMBER_OF_CLUSTERS;
+}
+
+bool morello_is_multichip_enabled(void)
+{
+    return (
+        (SCC->PLATFORM_CTRL & SCC_PLATFORM_CTRL_MULTI_CHIP_MODE_MASK) >>
+        SCC_PLATFORM_CTRL_MULTI_CHIP_MODE_POS);
+}
+
+uint8_t morello_get_chipid(void)
+{
+    return ((uint8_t)(
+        (SCC->PLATFORM_CTRL & SCC_PLATFORM_CTRL_CHIPID_MASK) >>
+        SCC_PLATFORM_CTRL_CHIPID_POS));
+}
