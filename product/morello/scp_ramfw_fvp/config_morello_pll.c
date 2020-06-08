91838dcccc0f986570180fbcb39a82e1055376f5
Anurag Koul <anurag.koul@arm.com>
2020-06-08 02:49:15 +0100

    morello/scp_ramfw_fvp: add clocks' config data
    Add config files for all system clocks/PLLs.
    
    Change-Id: If116e6101d4488229a7c2a91f4ee845ee43034b3
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>

---
+++ product/morello/scp_ramfw_fvp/config_morello_pll.c
@@ -0,0 +1,104 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "config_clock.h"
+#include "morello_scp_mmap.h"
+#include "morello_system_clock.h"
+
+#include <mod_morello_pll.h>
+
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_macros.h>
+#include <fwk_module.h>
+
+static struct morello_pll_custom_freq_param_entry freq_table[] = {
+    {
+        .freq_value_mhz = 1333,
+        .fbdiv = 160,
+        .refdiv = 3,
+        .postdiv = 2,
+    },
+};
+
+static const struct fwk_element morello_pll_element_table[] = {
+    [CLOCK_PLL_IDX_CPU0] =
+        {
+            .name = "CPU_PLL_0",
+            .data = &((struct mod_morello_pll_dev_config){
+                .control_reg0 = (void *)SCP_PLL_CPU0_CTRL,
+                .control_reg1 = (void *)SCP_PLL_CPU0_STAT,
+                .initial_rate = MORELLO_PLL_RATE_CPU_PLL0,
+                .ref_rate = CLOCK_RATE_REFCLK,
+            }),
+        },
+    [CLOCK_PLL_IDX_CPU1] =
+        {
+            .name = "CPU_PLL_1",
+            .data = &((struct mod_morello_pll_dev_config){
+                .control_reg0 = (void *)SCP_PLL_CPU1_CTRL,
+                .control_reg1 = (void *)SCP_PLL_CPU1_STAT,
+                .initial_rate = MORELLO_PLL_RATE_CPU_PLL1,
+                .ref_rate = CLOCK_RATE_REFCLK,
+            }),
+        },
+    [CLOCK_PLL_IDX_CLUS] =
+        {
+            .name = "CLUSTER_PLL",
+            .data = &((struct mod_morello_pll_dev_config){
+                .control_reg0 = (void *)SCP_PLL_CLUS_CTRL,
+                .control_reg1 = (void *)SCP_PLL_CLUS_STAT,
+                .initial_rate = MORELLO_PLL_RATE_CLUSTER_PLL,
+                .ref_rate = CLOCK_RATE_REFCLK,
+            }),
+        },
+    [CLOCK_PLL_IDX_INTERCONNECT] =
+        {
+            .name = "INT_PLL",
+            .data = &((struct mod_morello_pll_dev_config){
+                .control_reg0 = (void *)SCP_PLL_INTERCONNECT_CTRL,
+                .control_reg1 = (void *)SCP_PLL_INTERCONNECT_STAT,
+                .initial_rate = MORELLO_PLL_RATE_INTERCONNECT_PLL,
+                .ref_rate = CLOCK_RATE_REFCLK,
+            }),
+        },
+    [CLOCK_PLL_IDX_SYS] =
+        {
+            .name = "SYS_PLL",
+            .data = &((struct mod_morello_pll_dev_config){
+                .control_reg0 = (void *)SCP_PLL_SYSPLL_CTRL,
+                .control_reg1 = (void *)SCP_PLL_SYSPLL_STAT,
+                .initial_rate = MORELLO_PLL_RATE_SYSTEM_PLL,
+                .ref_rate = CLOCK_RATE_REFCLK,
+            }),
+        },
+    [CLOCK_PLL_IDX_DMC] =
+        {
+            .name = "DMC_PLL",
+            .data = &((struct mod_morello_pll_dev_config){
+                .control_reg0 = (void *)SCP_PLL_DMC_CTRL,
+                .control_reg1 = (void *)SCP_PLL_DMC_STAT,
+                .initial_rate = MORELLO_PLL_RATE_DMC_PLL,
+                .ref_rate = CLOCK_RATE_REFCLK,
+            }),
+        },
+    [CLOCK_PLL_IDX_COUNT] = { 0 }, /* Termination description. */
+};
+
+static const struct fwk_element *morello_pll_get_element_table(
+    fwk_id_t module_id)
+{
+    return morello_pll_element_table;
+}
+
+const struct fwk_module_config config_morello_pll = {
+    .elements = FWK_MODULE_DYNAMIC_ELEMENTS(morello_pll_get_element_table),
+    .data = &((struct morello_pll_module_config){
+        .custom_freq_table = freq_table,
+        .custom_freq_table_size = FWK_ARRAY_SIZE(freq_table),
+    }),
+};
