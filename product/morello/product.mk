61a34b9a3f971a1659de2a786cc1d05264837ae0
Anurag Koul <anurag.koul@arm.com>
2020-06-08 16:28:50 +0100

    morello/scp_ramfw_fvp: enable scp ramfw image build
    Change-Id: I9943ab41335083a930cbab7e7ed9fdcd62cac73f
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>
    Co-authored-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/morello/product.mk
@@ -0,0 +1,9 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+BS_PRODUCT_NAME := morello
+BS_FIRMWARE_LIST := scp_ramfw_fvp
