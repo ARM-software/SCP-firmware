2d1672a85173332212e81914d4816c23963f9fe8
Anurag Koul <anurag.koul@arm.com>
2020-06-09 16:53:23 +0100

    morello/module: add morello_mcp_system module
    Add a new module, "morello_mcp_system", which is the main
    system module for MCP, and runs after all the other modules
    have run.
    
    Change-Id: I8182d12eeaa765049ea3054f43ea56b053432ddd
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>
    Co-authored-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/morello/module/morello_mcp_system/include/mod_morello_mcp_system.h
@@ -0,0 +1,42 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     MORELLO MCP System Support
+ */
+
+#ifndef MOD_MORELLO_MCP_SYSTEM_H
+#define MOD_MORELLO_MCP_SYSTEM_H
+
+/*!
+ * \addtogroup GroupMORELLOModule MORELLO Product Modules
+ * @{
+ */
+
+/*!
+ * \defgroup GroupMORELLOMCPSystem MORELLO MCP System Support
+ * @{
+ */
+
+/*!
+ * \brief Events used by MCP system module.
+ */
+enum mcp_system_event {
+    /*! MCP system run event */
+    MOD_MCP_SYSTEM_EVENT_RUN,
+    /*! MCP system event count */
+    MOD_MCP_SYSTEM_EVENT_COUNT,
+};
+
+/*!
+ * @}
+ */
+
+/*!
+ * @}
+ */
+
+#endif /* MOD_MORELLO_MCP_SYSTEM_H */
