b8495d675cdf7e3ca933cf24ae562e6910b4ed9d
Anurag Koul <anurag.koul@arm.com>
2020-06-09 13:29:02 +0100

    morello: add pik register definitions for mcp
    Change-Id: I36b84a067abf874bcde95d3383dc0b51c1768ad4
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>

---
+++ product/morello/include/morello_mcp_pik.h
@@ -0,0 +1,16 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MORELLO_MCP_PIK_H
+#define MORELLO_MCP_PIK_H
+
+#include "morello_mcp_mmap.h"
+#include "morello_pik_mcp.h"
+
+#define PIK_MCP ((struct pik_mcp_reg *)MCP_PIK_BASE)
+
+#endif /* MORELLO_MCP_PIK_H */
