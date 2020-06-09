6692e56d78e38aad58af382a3faa5f2d142d7e31
Anurag Koul <anurag.koul@arm.com>
2020-06-09 15:21:56 +0100

    morello: add morello_mhu module
    This patch adds a module, morello_mhu, which is based on core
    mhu module but is modified to work for Morello platform until
    SCMI agent support is added.
    
    Change-Id: I653f515d513011c10e1131b30ee0ba33fd1ef3ff
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>
    Co-authored-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/morello/include/morello_mcp_mhu.h
@@ -0,0 +1,16 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MORELLO_MCP_MHU_H
+#define MORELLO_MCP_MHU_H
+
+enum mcp_morello_mhu_device_idx {
+    MORELLO_MHU_DEVICE_IDX_S_SCP,
+    MORELLO_MHU_DEVICE_IDX_COUNT,
+};
+
+#endif /* MORELLO_MCP_MHU_H */
