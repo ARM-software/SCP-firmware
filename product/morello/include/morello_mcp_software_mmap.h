4531d244bc40aab5fd0e9282d823c909d4c0edf3
Anurag Koul <anurag.koul@arm.com>
2020-06-09 01:05:27 +0100

    morello: add memory map headers for mcp
    - Add MCP's peripheral memory map.
    - Add MCP software mappings of memory shared b/w SCP and MCP.
    - Add MCP's internal/external system memory map (ROM, SRAM, Flash, etc.).
    
    Change-Id: I12a83a6b94d8635b10d078e969b780176e5b4cea
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>

---
+++ product/morello/include/morello_mcp_software_mmap.h
@@ -0,0 +1,29 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Software defined memory map shared between SCP and MCP cores.
+ */
+
+#ifndef MORELLO_MCP_SOFTWARE_MMAP_H
+#define MORELLO_MCP_SOFTWARE_MMAP_H
+
+#include "morello_mcp_mmap.h"
+
+/* SCMI payload size */
+#define MCP_SCMI_PAYLOAD_SIZE (128)
+
+/* SCMI non-secure payload areas */
+#define SCMI_PAYLOAD_SCP_TO_MCP_NS (MCP_SCP_SHARED_NONSECURE_RAM)
+#define SCMI_PAYLOAD_MCP_TO_SCP_NS \
+    (MCP_SCP_SHARED_NONSECURE_RAM + MCP_SCMI_PAYLOAD_SIZE)
+
+/* SCMI secure payload areas */
+#define SCMI_PAYLOAD_SCP_TO_MCP_S (MCP_SCP_SHARED_SECURE_RAM)
+#define SCMI_PAYLOAD_MCP_TO_SCP_S \
+    (MCP_SCP_SHARED_SECURE_RAM + MCP_SCMI_PAYLOAD_SIZE)
+
+#endif /* MORELLO_MCP_SOFTWARE_MMAP_H */
---
+++ product/morello/include/morello_mcp_system_mmap.h
@@ -0,0 +1,41 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MORELLO_MCP_SYSTEM_MMAP_H
+#define MORELLO_MCP_SYSTEM_MMAP_H
+
+/*
+ * External QSPI flash memory - mapped address
+ */
+#define MCP_QSPI_FLASH_BASE_ADDR 0x30000000
+#define MCP_QSPI_FLASH_BASE_ADDR_ALT 0x00800000
+#define MCP_QSPI_FLASH_SIZE 0x02000000
+
+/*
+ * Internal MCP's ROM/RAM base address
+ */
+#define MCP_ROM_BASE 0x00000000
+#define MCP_RAM0_BASE 0x00800000
+#define MCP_RAM1_BASE 0x20000000
+
+/*
+ * Internal MCP's ROM/RAM sizes
+ */
+#define MCP_ROM_SIZE (128 * 1024)
+#define MCP_RAM0_SIZE (512 * 1024)
+#define MCP_RAM1_SIZE (256 * 1024)
+
+/*
+ * MCP_BACKUP_DDR3 memory area where the MCP ROM
+ * bypass firmware should be placed. The MCP would
+ * be programmed to boot at this address by programming
+ * it in the MCP_BOOT_ADR SCC register.
+ */
+#define MCP_BACKUP_DDR3_BASE 0x16000000
+#define MCP_BACKUP_DDR3_SIZE (32 * 1024 * 1024)
+
+#endif /* MORELLO_MCP_SYSTEM_MMAP_H */
