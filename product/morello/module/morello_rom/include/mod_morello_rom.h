138c088e5ecc68a6e0740c3a976f146c16200cae
Anurag Koul <anurag.koul@arm.com>
2020-06-09 18:48:43 +0100

    morello/module: add morello_rom module
    This module performs first level of firmware capsule
    image parsing - fetches the RAM firmware, copies it to
    the appropriate memory locations, and passes on the
    control to it.
    
    Change-Id: I8c7828a46ac11f77ec207743bd03bf8e2287b777
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>
    Co-authored-by: Manoj Kumar <manoj.kumar3@arm.com>

---
+++ product/morello/module/morello_rom/include/mod_morello_rom.h
@@ -0,0 +1,44 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MOD_MORELLO_ROM_H
+#define MOD_MORELLO_ROM_H
+
+#include <mod_fip.h>
+
+#include <stdint.h>
+
+/*!
+ * \addtogroup GroupMORELLOModule MORELLO Product Modules
+ * @{
+ */
+
+/*!
+ * \defgroup GroupMORELLORom MORELLO SCP ROM Support
+ * @{
+ */
+
+/*!
+ * \brief Module configuration data.
+ */
+struct morello_rom_config {
+    /*! Base address of the RAM to which SCP BL2 will be copied to */
+    const uintptr_t ramfw_base;
+
+    /*! Type of RAM Firmware to load */
+    enum mod_fip_toc_entry_type image_type;
+};
+
+/*!
+ * @}
+ */
+
+/*!
+ * @}
+ */
+
+#endif /* MOD_MORELLO_ROM_H */
