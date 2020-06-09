fd7c83561a7d76c7681d5d017fb23aa3664c028c
Patrik Berglund <patrik.berglund@arm.com>
2020-06-09 18:04:53 +0100

    n1sdp: change to the AA640001 layout generated by fiptool
    Remove the need for a specific N1SDP flash image packaging tool and
    switch to the fiptool from TF-A.
    
    Change-Id: I00243a850b7f06702f1efb38d30e2e0705cf7414
    Signed-off-by: Patrik Berglund <patrik.berglund@arm.com>

--- product/n1sdp/module/n1sdp_flash/include/mod_n1sdp_flash.h
+++ product/n1sdp/module/n1sdp_flash/include/mod_n1sdp_flash.h
@@ -34,102 +34,22 @@
  */
 
 /*!
- * \brief CRC-16-CCITT reversed polynomial used for CRC calculation.
+ * \defgroup GroupModuleN1SDPFlashEntries Flash partition identifiers.
+ * \{
  */
-#define CRC16_POLYNOMIAL UINT16_C(0x8408)
+struct mod_n1sdp_flash_entry_id;
+
+/*! MCP BL2 Binary */
+extern const struct mod_n1sdp_flash_entry_id mod_n1sdp_flash_entry_mcp_bl2;
+/*! SCP BL2 Binary */
+extern const struct mod_n1sdp_flash_entry_id mod_n1sdp_flash_entry_scp_bl2;
+/*! EL3 Runtime - BL31 Binary */
+extern const struct mod_n1sdp_flash_entry_id mod_n1sdp_flash_entry_tf_bl31;
 
 /*!
- * \brief Type of flash entry.
+ * \}
  */
-enum mod_n1sdp_flash_data_type {
-    /*! SCP bypass ROM */
-    MOD_N1SDP_FLASH_DATA_TYPE_ROM_BYPASS = 1,
 
-    /*! N1SDP Firmware Image Package (NFIP) */
-    MOD_N1SDP_FLASH_DATA_TYPE_NFIP,
-
-    /*! NVM configuration space */
-    MOD_N1SDP_FLASH_DATA_TYPE_NVM_CFG,
-
-    /*! NVM log space */
-    MOD_N1SDP_FLASH_DATA_TYPE_NVM_LOG,
-
-    /*! Number of flash entry types */
-    MOD_N1SDP_FLASH_DATA_TYPE_COUNT,
-};
-
-/*!
- * \brief Type of N1SDP FIP entry.
- */
-enum mod_n1sdp_fip_data_type {
-    /*! SCP BL2 Binary */
-    MOD_N1SDP_FIP_TYPE_SCP_BL2,
-
-    /*! MCP BL2 Binary */
-    MOD_N1SDP_FIP_TYPE_MCP_BL2,
-
-    /*! EL3 Runtime - BL31 Binary */
-    MOD_N1SDP_FIP_TYPE_TF_BL31,
-
-    /*! Secure Payload BL32 Binary */
-    MOD_N1SDP_FIP_TYPE_TF_BL32,
-
-    /*! Non-secure BL33 (UEFI) Binary */
-    MOD_N1SDP_FIP_TYPE_NS_BL33,
-
-    /*! Number of N1SDP FIP entry types */
-    MOD_N1SDP_FIP_TYPE_COUNT,
-};
-
-/*!
- * \brief Attribute flags used for flash & NFIP entries.
- */
-enum mod_n1sdp_flash_data_flag {
-    MOD_N1SDP_FLASH_DATA_FLAG_ACTIVE         = (1 << 0),
-    MOD_N1SDP_FLASH_DATA_FLAG_ENABLED        = (1 << 1),
-    MOD_N1SDP_FLASH_DATA_FLAG_CRC16_ENABLED  = (1 << 2),
-    MOD_N1SDP_FLASH_DATA_FLAG_SECURE         = (1 << 3),
-};
-
-/*!
- * \brief Descriptor for a flash table entry.
- */
-struct mod_n1sdp_flash_descriptor {
-    /*! The type of the flash entry */
-    enum mod_n1sdp_flash_data_type type;
-
-    /*!
-     * The absolute address of the start of the data described by this
-     * entry.
-     */
-    uintptr_t address;
-
-    /*! The size of the data described by this entry */
-    size_t size;
-
-    /*! Attribute flags of the data described by this entry */
-    enum mod_n1sdp_flash_data_flag flags;
-};
-
-/*!
- * \brief Descriptor for a NFIP table entry.
- */
-struct mod_n1sdp_fip_descriptor {
-    /*! The type of the NFIP table entry */
-    enum mod_n1sdp_fip_data_type type;
-
-    /*!
-     * The absolute address of the start of the data described by this
-     * entry.
-     */
-    uintptr_t address;
-
-    /*! The size of the data described by this entry */
-    size_t size;
-
-    /*! Attribute flags of the data described by this entry */
-    enum mod_n1sdp_flash_data_flag flags;
-};
 
 /*!
  * \brief N1SDP Flash Module configuration
@@ -140,9 +60,18 @@ struct mod_n1sdp_flash_config {
 
     /*! Alternate base address of the memory-mapped flash memory. */
     uintptr_t flash_base_address_alt;
+};
 
-    /*! Offset of the Flash Table of Contents (TOC) from flash base address. */
-    uintptr_t toc_offset;
+/*!
+ * \brief A memory-mapped flash entry
+ */
+struct mod_n1sdp_flash_entry {
+    /*! Start of entry data */
+    void * p;
+    /*! Size */
+    size_t size;
+    /*! Flags */
+    uint64_t flags;
 };
 
 /*!
@@ -150,57 +79,20 @@ struct mod_n1sdp_flash_config {
  */
 struct mod_n1sdp_flash_api {
     /*!
-     * \brief Get the total number of flash descriptors.
+     * \brief Retrieve a flash entry.
      *
-     * \param id Module identifier.
-     * \param[out] count The descriptor count.
-     *
-     * \retval FWK_SUCCESS Operation succeeded.
-     * \retval FWK_E_PARAM The \p count parameter was invalid.
-     * \return One of the other specific error codes described by framework.
-     */
-    int (*get_flash_descriptor_count)(fwk_id_t id, unsigned int *count);
+     * \param id Flash entry identifiery.
+     * \param[out] entry Updated if requested entry if found.
 
-    /*!
-     * \brief Get a pointer to the table of descriptors that describe the flash
-     *     memory.
-     *
-     * \param id Module identifier.
-     * \param[out] table The descriptor table.
-     *
-     * \retval FWK_SUCCESS Operation succeeded.
-     * \retval FWK_E_PARAM The \p table parameter was invalid.
-     * \return One of the other specific error codes described by framework.
+     * \retval FWK_SUCCESS Flash entry found and \p entry have been updated.
+     * \retval FWK_E_INIT Flash could not be parsed (during init).
+     * \retval FWK_E_RANGE Not entry matching \p id could be located.
+     * \retval FWK_E_DATA Flash TOC corrupted or otherwise not useable on this
+     *         platform.
      */
-    int (*get_flash_descriptor_table)(
-        fwk_id_t id,
-        struct mod_n1sdp_flash_descriptor **table);
-    /*!
-     * \brief Get the total number of NFIP descriptors.
-     *
-     * \param id Module identifier.
-     * \param[out] count The descriptor count.
-     *
-     * \retval FWK_SUCCESS Operation succeeded.
-     * \retval FWK_E_PARAM The \p count parameter was invalid.
-     * \return One of the other specific error codes described by framework.
-     */
-    int (*get_n1sdp_fip_descriptor_count)(fwk_id_t id, unsigned int *count);
-
-    /*!
-     * \brief Get a pointer to the table of descriptors that describe the FIP
-     *     memory.
-     *
-     * \param id Module identifier.
-     * \param[out] table The descriptor table.
-     *
-     * \retval FWK_SUCCESS Operation succeeded.
-     * \retval FWK_E_PARAM The \p table parameter was invalid.
-     * \return One of the other specific error codes described by framework.
-     */
-    int (*get_n1sdp_fip_descriptor_table)(
-        fwk_id_t id,
-        struct mod_n1sdp_fip_descriptor **table);
+    int (*get_entry)(
+        const struct mod_n1sdp_flash_entry_id * id,
+        struct mod_n1sdp_flash_entry *entry);
 };
 
 /*!
--- product/n1sdp/module/n1sdp_flash/src/mod_n1sdp_flash.c
+++ product/n1sdp/module/n1sdp_flash/src/mod_n1sdp_flash.c
@@ -9,10 +9,9 @@
  *     package residing in on-board QSPI flash memory.
  */
 
-#include <internal/n1sdp_flash_layout.h>
-#include <internal/uuid.h>
-
 #include <mod_n1sdp_flash.h>
+
+#include "fip.h"
 
 #include <fwk_assert.h>
 #include <fwk_id.h>
@@ -22,249 +21,113 @@
 
 #include <string.h>
 
+struct mod_n1sdp_flash_entry_id {
+    struct fip_uuid uuid;
+};
+
+#define FIP_ENTRY_MCP_BL2 \
+    { .u = { 0x54, 0x46, 0x42, 0x22, 0xa4, 0xcf, 0x4b, 0xf8, \
+             0xb1, 0xb6, 0xce, 0xe7, 0xda, 0xde, 0x53, 0x9e } }
+
+const struct mod_n1sdp_flash_entry_id mod_n1sdp_flash_entry_mcp_bl2 = {
+    .uuid = FIP_ENTRY_MCP_BL2
+};
+
+const struct mod_n1sdp_flash_entry_id mod_n1sdp_flash_entry_scp_bl2 = {
+    .uuid = FIPL_ENTRY_SCP_BL2
+};
+
+const struct mod_n1sdp_flash_entry_id mod_n1sdp_flash_entry_tf_bl31 = {
+    .uuid = FIP_ENTRY_TF_BL31
+};
+
 /*
  * Module context
  */
-struct mod_n1sdp_flash_ctx {
-    /* Flash descriptor count */
-    unsigned int flash_desc_count;
+static struct  {
+    struct fip_toc* toc;
+} ctx;
 
-    /* NFIP descriptor count */
-    unsigned int n1sdp_fip_desc_count;
 
-    /* Flash descriptor table */
-    struct mod_n1sdp_flash_descriptor *flash_desc_table;
+static int uuid_cmp(
+    const struct fip_uuid * const a,
+    const struct fip_uuid * const b)
+{
+    return memcmp(a, b, sizeof(*a)) == 0;
+}
 
-    /* FIP descriptor table */
-    struct mod_n1sdp_fip_descriptor *n1sdp_fip_desc_table;
+static int uuid_is_null(const struct fip_uuid * const a)
+{
+    static const struct fip_uuid uuid_null = FIP_ENTRY_NULL;
+    return uuid_cmp(a, &uuid_null);
+}
+
+
+static int get_entry(
+    const struct mod_n1sdp_flash_entry_id * const id,
+    struct mod_n1sdp_flash_entry * const entry)
+{
+    if (!ctx.toc)
+        return FWK_E_INIT;
+
+    const struct fip_entry* e = ctx.toc->entry;
+    while (!uuid_cmp(&e->uuid, &id->uuid)) {
+        if (uuid_is_null(&e->uuid))
+            return FWK_E_RANGE;
+        e++;
+    }
+
+    if (e->size > SIZE_MAX)
+        return FWK_E_DATA;
+    if (e->offset_address > UINTPTR_MAX)
+        return false;
+    uintptr_t address;
+    if (__builtin_add_overflow((uintptr_t)ctx.toc,
+                               (uintptr_t)e->offset_address,
+                               &address)) {
+        return FWK_E_DATA;
+    }
+
+    entry->p = (void*)address;
+    entry->size = e->size;
+    entry->flags = e->flags;
+    return FWK_SUCCESS;
+}
+
+static bool validate_fip_toc(const struct fip_toc * const toc)
+{
+    return toc->signature == FIP_TOC_SIGNATURE;
+}
+
+static const struct mod_n1sdp_flash_api n1sdp_flash_api = {
+    .get_entry = get_entry,
 };
-
-static struct mod_n1sdp_flash_ctx n1sdp_flash_ctx;
-
-/*
- * Module API Implementation
- */
-static int get_flash_descriptor_count(fwk_id_t id, unsigned int *count)
-{
-    if (count == NULL)
-        return FWK_E_PARAM;
-
-    *count = n1sdp_flash_ctx.flash_desc_count;
-
-    return FWK_SUCCESS;
-}
-
-static int get_flash_descriptor_table(
-    fwk_id_t id,
-    struct mod_n1sdp_flash_descriptor **table)
-{
-    if (table == NULL)
-        return FWK_E_PARAM;
-
-    *table = n1sdp_flash_ctx.flash_desc_table;
-
-    return FWK_SUCCESS;
-}
-
-static int get_n1sdp_fip_descriptor_count(fwk_id_t id, unsigned int *count)
-{
-    if (count == NULL)
-        return FWK_E_PARAM;
-
-    *count = n1sdp_flash_ctx.n1sdp_fip_desc_count;
-
-    return FWK_SUCCESS;
-}
-
-static int get_n1sdp_fip_descriptor_table(
-    fwk_id_t id,
-    struct mod_n1sdp_fip_descriptor **table)
-{
-    if (table == NULL)
-        return FWK_E_PARAM;
-
-    *table = n1sdp_flash_ctx.n1sdp_fip_desc_table;
-
-    return FWK_SUCCESS;
-}
-
-static struct mod_n1sdp_flash_api module_api = {
-    .get_flash_descriptor_count = get_flash_descriptor_count,
-    .get_flash_descriptor_table = get_flash_descriptor_table,
-    .get_n1sdp_fip_descriptor_count = get_n1sdp_fip_descriptor_count,
-    .get_n1sdp_fip_descriptor_table = get_n1sdp_fip_descriptor_table,
-};
-
-/*
- * Utility functions
- */
-static uint16_t crc16(const void *data, uint32_t size)
-{
-    uint16_t crc = 0;
-    uint16_t poly = CRC16_POLYNOMIAL;
-    const uint8_t *byte = data;
-    uint32_t i = 0, j = 0;
-
-    fwk_assert(data != NULL);
-    fwk_assert(size != 0);
-
-    for (i = 0; i < size; ++i) {
-        crc = crc ^ byte[i];
-        for (j = 0; j < 8; j++) {
-            if (crc & 0x0001)
-                crc = (crc >> 1) ^ poly;
-            else
-                crc = crc >> 1;
-        }
-    }
-
-    return crc;
-}
-
-/*
- * Flash & FIP parsing functions
- */
-static int n1sdp_fip_parse(uintptr_t n1sdp_fip_base)
-{
-    const struct n1sdp_fip_memory_toc *n1sdp_fip_toc = NULL;
-    const struct n1sdp_fip_toc_entry *toc_entry = NULL;
-    struct mod_n1sdp_fip_descriptor *n1sdp_fip_desc = NULL;
-    struct uuid_t scp_bl2_uuid = UUID_SCP_FIRMWARE_SCP_BL2;
-    struct uuid_t mcp_bl2_uuid = UUID_MCP_FIRMWARE_MCP_BL2;
-    struct uuid_t bl31_uuid = UUID_EL3_RUNTIME_FIRMWARE_BL31;
-    struct uuid_t bl32_uuid = UUID_SECURE_PAYLOAD_BL32;
-    struct uuid_t bl33_uuid = UUID_NON_TRUSTED_FIRMWARE_BL33;
-
-    n1sdp_fip_toc = (const struct n1sdp_fip_memory_toc *)n1sdp_fip_base;
-    if (n1sdp_fip_toc == NULL)
-        return FWK_E_PANIC;
-
-    if (crc16(n1sdp_fip_toc, sizeof(struct n1sdp_fip_memory_toc)) != 0)
-        return FWK_E_DATA;
-
-    if (n1sdp_fip_toc->signature != NFIP_TOC_SIGNATURE)
-        return FWK_E_DATA;
-
-    if (n1sdp_fip_toc->entry_count == 0)
-        return FWK_E_DATA;
-
-    n1sdp_flash_ctx.n1sdp_fip_desc_count = n1sdp_fip_toc->entry_count;
-
-    n1sdp_flash_ctx.n1sdp_fip_desc_table = fwk_mm_calloc(
-        n1sdp_flash_ctx.n1sdp_fip_desc_count,
-        sizeof(struct mod_n1sdp_fip_descriptor));
-
-    for (unsigned int i = 0; i < n1sdp_flash_ctx.n1sdp_fip_desc_count; i++) {
-        toc_entry = &n1sdp_fip_toc->entry[i];
-        /* Check if CRC has to be calculated */
-        if (toc_entry->flags & MOD_N1SDP_FLASH_DATA_FLAG_CRC16_ENABLED) {
-            if (crc16(toc_entry, sizeof(*toc_entry)) != 0)
-                return FWK_E_DATA;
-        }
-
-        n1sdp_fip_desc = &n1sdp_flash_ctx.n1sdp_fip_desc_table[i];
-        n1sdp_fip_desc->address = (n1sdp_fip_base + toc_entry->offset);
-        n1sdp_fip_desc->size = toc_entry->size;
-        n1sdp_fip_desc->flags = toc_entry->flags;
-
-        if (!memcmp(&toc_entry->uuid,
-                    &scp_bl2_uuid,
-                    sizeof(struct uuid_t))) {
-            n1sdp_fip_desc->type = MOD_N1SDP_FIP_TYPE_SCP_BL2;
-        } else if (!memcmp(&toc_entry->uuid,
-                    &mcp_bl2_uuid, sizeof(struct uuid_t))) {
-            n1sdp_fip_desc->type = MOD_N1SDP_FIP_TYPE_MCP_BL2;
-        } else if (!memcmp(&toc_entry->uuid,
-                    &bl31_uuid, sizeof(struct uuid_t))) {
-            n1sdp_fip_desc->type = MOD_N1SDP_FIP_TYPE_TF_BL31;
-        } else if (!memcmp(&toc_entry->uuid,
-                    &bl32_uuid, sizeof(struct uuid_t))) {
-            n1sdp_fip_desc->type = MOD_N1SDP_FIP_TYPE_TF_BL32;
-        } else if (!memcmp(&toc_entry->uuid,
-                    &bl33_uuid, sizeof(struct uuid_t))) {
-            n1sdp_fip_desc->type = MOD_N1SDP_FIP_TYPE_NS_BL33;
-        } else {
-            return FWK_E_DATA;
-        }
-    }
-
-    return FWK_SUCCESS;
-}
-
-static int n1sdp_flash_parse(uintptr_t address, uintptr_t offset)
-{
-    const struct n1sdp_flash_memory_toc *toc = NULL;
-    const struct n1sdp_flash_toc_entry *toc_entry = NULL;
-    struct mod_n1sdp_flash_descriptor *flash_desc = NULL;
-    int status;
-
-    toc = (void *)(address + offset);
-
-    if (crc16(toc, sizeof(struct n1sdp_flash_memory_toc)) != 0)
-        return FWK_E_DATA;
-
-    if (toc->signature != FLASH_TOC_SIGNATURE)
-        return FWK_E_DATA;
-
-    if (toc->entry_count == 0)
-        return FWK_E_DATA;
-
-    n1sdp_flash_ctx.flash_desc_count = toc->entry_count;
-
-    n1sdp_flash_ctx.flash_desc_table = fwk_mm_calloc(
-        n1sdp_flash_ctx.flash_desc_count,
-        sizeof(struct mod_n1sdp_flash_descriptor));
-
-    for (unsigned int i = 0; i < n1sdp_flash_ctx.flash_desc_count; ++i) {
-        toc_entry = &toc->entry[i];
-        /* Check if CRC has to be calculated */
-        if (toc_entry->flags & MOD_N1SDP_FLASH_DATA_FLAG_CRC16_ENABLED) {
-            if (crc16(toc_entry, sizeof(*toc_entry)) != 0)
-                return FWK_E_DATA;
-        }
-
-        flash_desc = &n1sdp_flash_ctx.flash_desc_table[i];
-        flash_desc->address = (address + toc_entry->offset);
-        flash_desc->size = toc_entry->size;
-        flash_desc->flags = toc_entry->flags;
-        flash_desc->type = toc_entry->type;
-
-        /* If flash entry type is N1SDP FIP (NFIP) then parse it */
-        if (toc->entry[i].type == MOD_N1SDP_FLASH_DATA_TYPE_NFIP) {
-            status = n1sdp_fip_parse(flash_desc->address);
-            if (status != FWK_SUCCESS)
-                return FWK_E_DATA;
-        }
-    }
-    return FWK_SUCCESS;
-}
-
-/*
- * Framework handlers
- */
 
 static int n1sdp_flash_init(
     fwk_id_t module_id,
     unsigned int element_count,
     const void *data)
 {
-    int status;
-    const struct mod_n1sdp_flash_config *config = NULL;
-
-    config = (const struct mod_n1sdp_flash_config *)data;
+    const struct mod_n1sdp_flash_config *config = data;
     if ((config == NULL) || (element_count > 0))
         return FWK_E_PANIC;
 
-    /* Parse & validate the flash base address */
-    status = n1sdp_flash_parse(config->flash_base_address, config->toc_offset);
-
-    /* No valid records found. Parse & validate alternate base address */
-    if (status == FWK_E_DATA) {
-        return n1sdp_flash_parse(config->flash_base_address_alt,
-                                  config->toc_offset);
+    struct fip_toc* toc = (void*)config->flash_base_address;
+    if (!validate_fip_toc(toc)) {
+        toc = (void*)config->flash_base_address_alt;
+        if (!validate_fip_toc(toc)) {
+            /*
+             * If we return a failure here the initializing of ROM images might
+             * stall without any log messages.
+             * Failure here are not critical as API function will return
+             * FWK_E_INIT if this failure is triggered.
+             */
+            return FWK_SUCCESS;
+        }
     }
 
-    return status;
+    ctx.toc = toc;
+    return FWK_SUCCESS;
 }
 
 static int n1sdp_flash_process_bind_request(
@@ -273,7 +136,7 @@ static int n1sdp_flash_process_bind_request(
     fwk_id_t api_id,
     const void **api)
 {
-    *api = &module_api;
+    *api = &n1sdp_flash_api;
     return FWK_SUCCESS;
 }
 
