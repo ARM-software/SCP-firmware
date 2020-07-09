0fb270a523836fcde0294fc4c601bb1062504c14
Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
2020-07-09 13:27:24 +0530

    fwk/assert: print location of assertion
    fwk_assert which is called from several framework and module functions,
    enters trap loop silently if condition passed to it fails, without
    providing any information about the failure in the debug builds. Such
    cases will be hard to debug in case of unavailability of a debugger or
    textual trace output. Print the file name and line number to know the
    location of assertion before entering infinite loop.
    
    Change-Id: I9ff067178373c8c38ecfd25d9efc322b3d393ed3
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>

--- framework/src/assert.c
+++ framework/src/assert.c
@@ -1,16 +1,21 @@
 /*
  * Arm SCP/MCP Software
- * Copyright (c) 2020-2021, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2020-2024, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <assert.h>
 #include <fwk_attributes.h>
+#include <fwk_io.h>
+
+#include <assert.h>
 
 #if defined(__ARMCC_VERSION)
 void __aeabi_assert(const char *expr, const char *file, int line)
 {
+    (void)fwk_io_printf(
+        fwk_io_stdout, "Assertion failed at %s:%d\n", file, line);
+
     while (1) {
         continue;
     }
@@ -22,6 +27,13 @@ FWK_WEAK void __assert_func(
     const char *function,
     const char *assertion)
 {
+    (void)fwk_io_printf(
+        fwk_io_stdout,
+        "Assertion failed in %s at %s:%d\n",
+        function,
+        file,
+        line);
+
     while (1) {
         continue;
     }
