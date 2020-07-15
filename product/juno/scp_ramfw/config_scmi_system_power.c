647ea86f6e5f6921d56f53e47673e26a1bc3db81
Tarek El-Sherbiny <tarek.el-sherbiny@arm.com>
2020-07-15 11:54:22 +0100

    scmi_system_power: Add timeout for agents graceful response
    The platform will wait for a configurable timeout to allow all agents
    to respond to a graceful system power request.
    If all agents did not respond within this period the platform will
    fulfill the request forcibly.
    
    Change-Id: I43803615cbad908072d1b6b9e43ccf9f13798faf
    Signed-off-by: Tarek El-Sherbiny <tarek.el-sherbiny@arm.com>

--- product/juno/scp_ramfw/config_scmi_system_power.c
+++ product/juno/scp_ramfw/config_scmi_system_power.c
@@ -5,14 +5,22 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <juno_alarm_idx.h>
+
 #include <mod_scmi_system_power.h>
 #include <mod_system_power.h>
 
 #include <fwk_module.h>
 
 struct fwk_module_config config_scmi_system_power = {
-    .data = &(struct mod_scmi_system_power_config) {
-        .system_view = MOD_SCMI_SYSTEM_VIEW_FULL,
-        .system_suspend_state = MOD_SYSTEM_POWER_POWER_STATE_SLEEP0,
-    },
+    .data =
+        &(struct mod_scmi_system_power_config){
+            .system_view = MOD_SCMI_SYSTEM_VIEW_FULL,
+            .system_suspend_state = MOD_SYSTEM_POWER_POWER_STATE_SLEEP0,
+            .alarm_id = FWK_ID_SUB_ELEMENT_INIT(
+                FWK_MODULE_IDX_TIMER,
+                JUNO_ALARM_ELEMENT_IDX,
+                JUNO_SYSTEM_POWER_ALARM_IDX),
+            .graceful_timeout = 1000, /* ms */
+        },
 };
