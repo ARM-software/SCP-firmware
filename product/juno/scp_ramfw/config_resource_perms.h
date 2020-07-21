c5c09d704a02152d5ac61f2a21f6eafe61b86cd9
Jim Quigley <jim.quigley@arm.com>
2020-07-21 12:14:19 +0100

    juno: Specify the devices for the Juno platform.
    This patch adds the domain devices, the devices and the device list to the
    Juno configuration.
    
    Change-Id: I9fb5ed6ebd525d9880b9f92e795aa75753a4a931
    Signed-off-by: Jim Quigley <jim.quigley@arm.com>

---
+++ product/juno/scp_ramfw/config_resource_perms.h
@@ -0,0 +1,18 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019-2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef CONFIG_RESOURCE_PERMS_H
+#define CONFIG_RESOURCE_PERMS_H
+
+enum juno_res_perms_devices {
+    JUNO_RES_PERMS_DEVICES_CPU = 0,
+    JUNO_RES_PERMS_DEVICES_GPU,
+    JUNO_RES_PERMS_DEVICES_IO,
+    JUNO_RES_PERMS_DEVICES_COUNT
+};
+
+#endif /* CONFIG_RESOURCE_PERMS_H */
