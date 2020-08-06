1af5a8163f12e6d1c35fd4a0e6be5ea0900805ee
Chris Kay <chris.kay@arm.com>
2020-08-06 16:57:04 +0100

    misc: Make logging device elements static
    This commit modifies any `pl011`-like configuration file where the
    elements are not generated and converts them to static elements. This is
    necessary to make the identifiers available at an earlier stage, which
    is required to use them as standard input/output devices further up in
    the patch set.
    
    Change-Id: I4e4aeafa8427b12bba751122f2fb39d20e9fa14a
    Signed-off-by: Chris Kay <chris.kay@arm.com>

--- product/juno/scp_romfw/config_pl011.c
+++ product/juno/scp_romfw/config_pl011.c
@@ -14,35 +14,28 @@
 #include <fwk_macros.h>
 #include <fwk_module.h>
 
-static const struct fwk_element pl011_element_desc_table[] = {
-    [0] = {
-        .name = "board-uart1",
-        .data =
-            &(struct mod_pl011_device_config){
-                .reg_base = FPGA_UART1_BASE,
-                .baud_rate_bps = 115200,
-                .clock_rate_hz = 24 * FWK_MHZ,
-
-                /*
-                 * The UART is usable as soon as the SYSTOP power domain is on.
-                 * The SYSTOP power domain is enforced to be ON at the
-                 * initialization of the Juno PPU module thus it is ON when the
-                 * PL011 is started and there is no need to wait for a clock to
-                 * be locked.
-                 */
-                .clock_id = FWK_ID_NONE_INIT,
-                .pd_id = FWK_ID_NONE_INIT,
-            },
-    },
-
-    [1] = { 0 },
-};
-
-static const struct fwk_element *get_pl011_table(fwk_id_t module_id)
-{
-    return pl011_element_desc_table;
-}
-
 struct fwk_module_config config_pl011 = {
-    .elements = FWK_MODULE_DYNAMIC_ELEMENTS(get_pl011_table),
+    .elements = FWK_MODULE_STATIC_ELEMENTS({
+        [0] = {
+            .name = "board-uart1",
+            .data =
+                &(struct mod_pl011_device_config){
+                    .reg_base = FPGA_UART1_BASE,
+                    .baud_rate_bps = 115200,
+                    .clock_rate_hz = 24 * FWK_MHZ,
+
+                    /*
+                     * The UART is usable as soon as the SYSTOP power domain is
+                     * on. The SYSTOP power domain is enforced to be ON at the
+                     * initialization of the Juno PPU module thus it is ON when
+                     * the PL011 is started and there is no need to wait for a
+                     * clock to be locked.
+                     */
+                    .clock_id = FWK_ID_NONE_INIT,
+                    .pd_id = FWK_ID_NONE_INIT,
+                },
+        },
+
+        [1] = { 0 },
+    }),
 };
