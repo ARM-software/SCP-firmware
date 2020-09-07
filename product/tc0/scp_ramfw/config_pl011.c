7f989eed6b98f0c08b0329d5d4bf7e53ee1700fb
Usama Arif <usama.arif@arm.com>
2020-09-07 17:20:00 +0100

    tc0: use UART0 from RoS AP memory map
    The SCP UART is no longer available after
    platform architecture changes. The #define
    base addresses are also changed to UINT32_T
    to support large additions.
    
    Change-Id: I71b2d74cb18bd3ef48fdf5f6dae5dbee4844ad46
    Signed-off-by: Usama Arif <usama.arif@arm.com>

--- product/tc0/scp_ramfw/config_pl011.c
+++ product/tc0/scp_ramfw/config_pl011.c
@@ -17,20 +17,19 @@
 
 const struct fwk_module_config config_pl011 = {
     .elements = FWK_MODULE_STATIC_ELEMENTS({
-        [0] =
-            {
-                .name = "uart",
-                .data =
-                    &(struct mod_pl011_element_cfg){
-                        .reg_base = SCP_UART_BASE,
-                        .baud_rate_bps = 115200,
-                        .clock_rate_hz = 24 * FWK_MHZ,
-                        .clock_id = FWK_ID_NONE_INIT,
-                        .pd_id = FWK_ID_ELEMENT_INIT(
-                            FWK_MODULE_IDX_POWER_DOMAIN,
-                            PD_STATIC_DEV_IDX_SYSTOP),
-                    },
-            },
+        [0] = {
+            .name = "uart",
+            .data =
+                &(struct mod_pl011_element_cfg){
+                    .reg_base = SCP_UART_BOARD_BASE,
+                    .baud_rate_bps = 115200,
+                    .clock_rate_hz = 24 * FWK_MHZ,
+                    .clock_id = FWK_ID_NONE_INIT,
+                    .pd_id = FWK_ID_ELEMENT_INIT(
+                        FWK_MODULE_IDX_POWER_DOMAIN,
+                        PD_STATIC_DEV_IDX_SYSTOP),
+                },
+        },
 
         [1] = { 0 },
     }),
