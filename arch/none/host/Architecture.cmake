f717c37e7d45fc5f4812c387cd919ca0045240ea
Chris Kay <chris.kay@arm.com>
2020-09-16 19:20:48 +0100

    cmake: Add build support for the `host` architecture
    This commit adds the cmake build suppprt fpr  the `host` architecture.
    
    Change-Id: I1112d16889c0baf1c827d9d78d9e703e0230d616
    Signed-off-by: Chris Kay <chris.kay@arm.com>
    Signed-off-by: Joel Goddard <joel.goddard@arm.com>
    Signed-off-by: Girish Pathak <girish.pathak@arm.com>

---
+++ arch/none/host/Architecture.cmake
@@ -0,0 +1,9 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+set(SCP_ARCHITECTURE "none")
+set(SCP_ARCHITECTURE_TARGET "arch-none")
