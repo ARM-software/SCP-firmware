54ab66229415c1384b207f83051a713754a09bb3
Chris Kay <chris.kay@arm.com>
2020-09-16 17:53:58 +0100

    cmake: Add CMSIS-RTOS2 target
    This commit adds the CMSIS-RTOS2 header-only interface library target,
    `cmsis::rtos2`.
    
    Change-Id: Id91daa243d95d59846b97abc8f3aa3b4ef7b703c
    Signed-off-by: Chris Kay <chris.kay@arm.com>
    Signed-off-by: Joel Goddard <joel.goddard@arm.com>
    Signed-off-by: Girish Pathak <girish.pathak@arm.com>

---
+++ contrib/cmsis/include/RTE_Components.h
@@ -0,0 +1,13 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2017-2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define CMSIS_device_header <fmw_cmsis.h>
+
+#endif /* RTE_COMPONENTS_H */
