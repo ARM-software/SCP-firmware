824ae82331d2545ab597d3ac00ac98edae239d71
Usama Arif <usama.arif@arm.com>
2020-09-16 19:10:36 +0100

    product/tc0: Add pd_id for pl011 module
    This is needed due to recently introduced changes in
    mod_pl011 that perform safety check to see if the device
    is powered on or off. The pl011 config is split
    into ROM and RAM firmware as only RAM firmware has
    power domain.
    
    Change-Id: I877b8b3963e5a605acd8af0d771b9b61909fafaf
    Signed-off-by: Usama Arif <usama.arif@arm.com>

---
+++ product/tc0/scp_ramfw/config_pl011.c
@@ -0,0 +1,36 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "config_power_domain.h"
+#include "scp_css_mmap.h"
+
+#include <mod_pl011.h>
+
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_macros.h>
+#include <fwk_module.h>
+
+const struct fwk_module_config config_pl011 = {
+    .elements = FWK_MODULE_STATIC_ELEMENTS({
+        [0] = {
+            .name = "uart",
+            .data =
+                &(struct mod_pl011_element_cfg){
+                    .reg_base = SCP_UART_BASE,
+                    .baud_rate_bps = 115200,
+                    .clock_rate_hz = 24 * FWK_MHZ,
+                    .clock_id = FWK_ID_NONE_INIT,
+                    .pd_id = FWK_ID_ELEMENT_INIT(
+                        FWK_MODULE_IDX_POWER_DOMAIN,
+                        PD_STATIC_DEV_IDX_SYSTOP),
+                },
+        },
+
+        [1] = { 0 },
+    }),
+};
