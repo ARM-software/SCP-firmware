e04c5a763de667fc55a06c989b1b4c1bcf5ce2fa
Luca Vizzarro <Luca.Vizzarro@arm.com>
2020-09-20 23:07:12 +0100

    module: Add Mock Clock module
    The mock clock module allows to smoothly test the firmware while
    using an emulated system, e.g. FVP. Some clock device drivers
    require hardware features, such as I2C, in order to function
    correctly. Under an emulated environment, these features cannot
    be always reproduced, so the mock clock module allows to replace
    these clock drivers for a smooth operation.
    
    Change-Id: I022c56cdf0cadb062742f83d413fa9bdf4adc5aa
    Signed-off-by: Luca Vizzarro <Luca.Vizzarro@arm.com>

---
+++ module/mock_clock/include/mod_mock_clock.h
@@ -0,0 +1,102 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MOD_MOCK_CLOCK_H
+#define MOD_MOCK_CLOCK_H
+
+#include <fwk_id.h>
+#include <fwk_module_idx.h>
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/*!
+ * \ingroup GroupModules
+ * \defgroup GroupMockClock Mock Clock Driver
+ *
+ * \details The `mock_clock` module provides a mock clock driver for use
+ *      alongside the `clock` interface on systems that do not provide a real
+ *      clock driver.
+ *
+ * \warning When using this module to mock a pre-existing driver
+ *      for any reason, this driver must be forced to bind to the
+ *      ::MOD_MOCK_CLOCK_API_TYPE_RESPONSE_DRIVER API through its module
+ *      configuration. This will avoid any conflict between the pre-existing
+ *      driver and the mocked one.
+ * \{
+ */
+
+/*!
+ * \brief Rate lookup table entry.
+ */
+struct mod_mock_clock_rate {
+    /*! Rate of the clock in Hertz. */
+    uint32_t rate;
+
+    /*! The clock divider used to attain the rate. */
+    unsigned int divider;
+};
+
+/*!
+ * \brief Element configuration.
+ */
+struct mod_mock_clock_element_cfg {
+    /*! Pointer to the clock's rate lookup table. */
+    const struct mod_mock_clock_rate *rate_table;
+
+    /*! The number of rates in the rate lookup table. */
+    unsigned int rate_count;
+};
+
+/*!
+ * \brief API indices.
+ *
+ * \warning The mock clock implements the clock driver API only. The response
+ *      driver API only acts as a pointer to be bound to.
+ */
+enum mod_mock_clock_api_type {
+    /*!
+     * \brief Clock driver.
+     *
+     * \note This API implements the mod_clock::mod_clock_driver_api interface.
+     *
+     * \warning Binding to this API must occur through an element of this
+     *      module.
+     */
+    MOD_MOCK_CLOCK_API_TYPE_DRIVER,
+
+    /*!
+     * \brief Clock driver response.
+     *
+     * \note This API implements the mod_clock::mod_clock_driver_response_api
+     * interface.
+     *
+     * \warning Binding to this API must occur through an element of this
+     *      module.
+     */
+    MOD_MOCK_CLOCK_API_TYPE_RESPONSE_DRIVER,
+
+    /*!
+     * \brief Number of defined APIs.
+     */
+    MOD_MOCK_CLOCK_API_COUNT,
+};
+
+/*!
+ * \brief Driver API identifier.
+ *
+ * \note This identifier corresponds to the ::MOD_MOCK_CLOCK_API_TYPE_DRIVER API
+ *      index.
+ */
+static const fwk_id_t mod_mock_clock_api_id_driver =
+    FWK_ID_API_INIT(FWK_MODULE_IDX_MOCK_CLOCK, MOD_MOCK_CLOCK_API_TYPE_DRIVER);
+
+/*!
+ * \}
+ */
+
+#endif /* MOD_MOCK_CLOCK_H */
