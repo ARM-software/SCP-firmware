7c41801089e5c8b4fd09244fdba484c11e1c2c0b
Chris Kay <chris.kay@arm.com>
2020-09-21 13:16:50 +0100

    cmake: Add Markdownlint integration
    This commit introduces the following targets:
    
    - `format-markdown`: automatically formats any Markdown files
    - `check-markdown`: checks the formatting on Markdown files
    - `lint-markdown`: lints Markdown files
    
    These are executed implicitly as part of the `format`, `check` and
    `lint` targets.
    
    Markdownlint can be found at: https://github.com/markdownlint/markdownlint
    
    Change-Id: I15740f2a7f949dc98780558270ea223b64c52c2f
    Signed-off-by: Chris Kay <chris.kay@arm.com>
    Signed-off-by: Joel Goddard <joel.goddard@arm.com>
    Signed-off-by: Girish Pathak <girish.pathak@arm.com>

---
+++ .mdlrc-style.rb
@@ -0,0 +1,9 @@
+all
+
+rule "MD003", :style => :atx
+rule "MD004", :style => :dash
+rule "MD013", :code_blocks => false
+rule "MD024", :allow_different_nesting => true
+rule "MD029", :style => :ordered
+rule "MD035", :style => "---"
+rule "MD046", :style => :fenced
