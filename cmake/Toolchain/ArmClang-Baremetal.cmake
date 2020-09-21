d989b88a1c01368d6270a111047098924c134d96
Chris Kay <chris.kay@arm.com>
2020-09-21 12:13:27 +0100

    cmake: Add multi-toolchain support
    This commit adds support for selecting a toolchain file. Toolchain files
    may be specified directly through the standard `CMAKE_TOOLCHAIN_FILE`
    variable, or automatically through the `SCP_TOOLCHAIN` variable, whose
    valid entries are on a per-firmware basis.
    
    A firmware may choose a default toolchain to build with through the
    `SCP_TOOLCHAIN_INIT` variable, and valid toolchains are given by the
    existence of `Toolchain-${SCP_TOOLCHAIN}.cmake` files in the firmware
    source directory.
    
    Change-Id: Ia0c172be0241b32b07d507480be0ff9d6813bedb
    Signed-off-by: Chris Kay <chris.kay@arm.com>
    Signed-off-by: Joel Goddard <joel.goddard@arm.com>
    Signed-off-by: Girish Pathak <girish.pathak@arm.com>

---
+++ cmake/Toolchain/ArmClang-Baremetal.cmake
@@ -0,0 +1,19 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+include("${CMAKE_CURRENT_LIST_DIR}/ArmClang-Base.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/Generic-Baremetal.cmake")
+
+foreach(language IN ITEMS ASM C CXX)
+    string(APPEND CMAKE_${language}_FLAGS_INIT "-ffunction-sections ")
+    string(APPEND CMAKE_${language}_FLAGS_INIT "-fdata-sections ")
+    string(APPEND CMAKE_${language}_FLAGS_INIT "-fshort-enums ")
+
+    string(APPEND CMAKE_${language}_FLAGS_DEBUG_INIT "-Og ")
+endforeach()
+
+string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT "--remove ")
