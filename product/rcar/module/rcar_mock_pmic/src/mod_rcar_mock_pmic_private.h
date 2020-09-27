586791b9e01b135998fb698c0e828e6348c94a98
Nicolas Royer <nroyer@baylibre.com>
2020-09-27 17:38:46 +0200

    rcar/module: add rcar mock_pmic module and config data
    Change-Id: I5c9e5c41af22725d70bf57fd84b83138b07d61f7
    Signed-off-by: Tsutomu Muroya <tsutomu.muroya.jy@bp.renesas.com>
    Signed-off-by: Nicolas Royer <nroyer@baylibre.com>

---
+++ product/rcar/module/rcar_mock_pmic/src/mod_rcar_mock_pmic_private.h
@@ -0,0 +1,19 @@
+/*
+ * Renesas SCP/MCP Software
+ * Copyright (c) 2020, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MOD_RCAR_MOCK_PMIC_PRIVATE_H
+#define MOD_RCAR_MOCK_PMIC_PRIVATE_H
+
+#include <mod_rcar_mock_pmic_driver_api_private.h>
+#include <mod_rcar_mock_pmic_module_private.h>
+
+#include <mod_rcar_mock_pmic.h>
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#endif /* MOD_RCAR_MOCK_PMIC_PRIVATE_H */
