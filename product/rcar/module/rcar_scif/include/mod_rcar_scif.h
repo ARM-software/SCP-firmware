8145d3398e3c929d1749d4363c72c581e3d649d7
Nicolas Royer <nroyer@baylibre.com>
2020-09-27 17:36:43 +0200

    rcar/module: add rcar scif module and config data
    Change-Id: I1e4b7df885634f599f652e88ea06b42a9a1d7ae1
    Signed-off-by: Tsutomu Muroya <tsutomu.muroya.jy@bp.renesas.com>
    Signed-off-by: Nicolas Royer <nroyer@baylibre.com>

---
+++ product/rcar/module/rcar_scif/include/mod_rcar_scif.h
@@ -0,0 +1,58 @@
+/*
+ * Renesas SCP/MCP Software
+ * Copyright (c) 2020, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MOD_RCAR_SCIF_H
+#define MOD_RCAR_SCIF_H
+
+#include <fwk_id.h>
+
+#include <stdint.h>
+
+/*!
+ * \addtogroup GroupRCARModule RCAR Product Modules
+ * \{
+ */
+
+/*!
+ * \defgroup GroupRCARSCIF SCIF Driver
+ *
+ * \brief Device driver module for the Primecell® SCIF UART.
+ *
+ * \{
+ */
+
+/*!
+ * \brief SCIF element configuration data.
+ */
+struct mod_rcar_scif_element_cfg {
+    /*!
+     * \brief Base address of the device registers.
+     */
+    uintptr_t reg_base;
+
+    /*!
+     * \brief Baud rate in bits per second.
+     */
+    unsigned int baud_rate_bps;
+
+    /*!
+     * \brief Reference clock in Hertz.
+     */
+    uint64_t clock_rate_hz;
+};
+
+/*!
+ * \}
+ */
+
+/*!
+ * \}
+ */
+
+int mod_rcar_scif_resume();
+
+#endif /* MOD_RCAR_SCIF_H */
