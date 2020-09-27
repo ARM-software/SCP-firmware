482732a38c153d98d4fd2360fefcae99e745e2f1
Nicolas Royer <nroyer@baylibre.com>
2020-09-27 17:38:23 +0200

    rcar/module: add rcar mfismh module and config data
    Change-Id: Id87daed4990c2af79f608f74438d6062915d74f1
    Signed-off-by: Tsutomu Muroya <tsutomu.muroya.jy@bp.renesas.com>
    Signed-off-by: Nicolas Royer <nroyer@baylibre.com>

---
+++ product/rcar/scp_ramfw/config_rcar_mfismh.c
@@ -0,0 +1,41 @@
+/*
+ * Renesas SCP/MCP Software
+ * Copyright (c) 2020, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <rcar_irq.h>
+#include <rcar_mfismh.h>
+#include <rcar_mmap.h>
+
+#include <mod_rcar_mfismh.h>
+
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_module.h>
+
+static const struct fwk_element mfismh_element_table[] = {
+    [RCAR_MFISMH_DEVICE_IDX_S] = { .name = "MFISMH_S",
+                                   .sub_element_count = 1,
+                                   .data =
+                                       &((struct mod_rcar_mfismh_device_config){
+                                           .irq = MFIS_AREICR2_IRQ,
+                                       }) },
+    [RCAR_MFISMH_DEVICE_IDX_NS_L] = { .name = "MSIFMH_NS_L",
+                                      .sub_element_count = 1,
+                                      .data = &((
+                                          struct mod_rcar_mfismh_device_config){
+                                          .irq = MFIS_AREICR1_IRQ,
+                                      }) },
+    [RCAR_MFISMH_DEVICE_IDX_COUNT] = {},
+};
+
+static const struct fwk_element *mfismh_get_element_table(fwk_id_t module_id)
+{
+    return mfismh_element_table;
+}
+
+struct fwk_module_config config_rcar_mfismh = {
+    .elements = FWK_MODULE_DYNAMIC_ELEMENTS(mfismh_get_element_table),
+};
