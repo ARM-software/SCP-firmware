515da73e309744a5b51b3d6396afbca00ab6dc5f
Nicolas Royer <nroyer@baylibre.com>
2020-09-27 17:37:49 +0200

    rcar/module: add rcar dvfs module and config data
    Signed-off-by: Tsutomu Muroya <tsutomu.muroya.jy@bp.renesas.com>
    Signed-off-by: Nicolas Royer <nroyer@baylibre.com>

---
+++ product/rcar/scp_ramfw/config_rcar_dvfs.h
@@ -0,0 +1,17 @@
+/*
+ * Renesas SCP/MCP Software
+ * Copyright (c) 2020, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef CONFIG_RCAR_DVFS_H
+#define CONFIG_RCAR_DVFS_H
+
+enum dvfs_element_idx {
+    DVFS_ELEMENT_IDX_LITTLE,
+    DVFS_ELEMENT_IDX_BIG,
+    DVFS_ELEMENT_IDX_COUNT
+};
+
+#endif /* CONFIG_RCAR_DVFS_H */
