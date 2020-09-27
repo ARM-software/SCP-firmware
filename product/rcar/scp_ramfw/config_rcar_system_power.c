305ed8d985dfd15d69febc8ab07538f0961b8f85
Nicolas Royer <nroyer@baylibre.com>
2020-09-27 17:48:36 +0200

    rcar/module: add rcar system_power module and config data
    Signed-off-by: Tsutomu Muroya <tsutomu.muroya.jy@bp.renesas.com>
    Signed-off-by: Nicolas Royer <nroyer@baylibre.com>

---
+++ product/rcar/scp_ramfw/config_rcar_system_power.c
@@ -0,0 +1,23 @@
+/*
+ * Renesas SCP/MCP Software
+ * Copyright (c) 2020, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_id.h>
+#include <fwk_interrupt.h>
+#include <fwk_macros.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+#include <mod_system_power.h>
+#include <mod_rcar_system.h>
+
+const struct fwk_module_config config_rcar_system_power = {
+    .data = &((struct mod_system_power_config) {
+        .soc_wakeup_irq = FWK_INTERRUPT_NONE,
+        .driver_id = FWK_ID_MODULE_INIT(FWK_MODULE_IDX_RCAR_SYSTEM),
+        .driver_api_id = FWK_ID_API_INIT(FWK_MODULE_IDX_RCAR_SYSTEM,
+             MOD_RCAR_SYSTEM_API_IDX_SYSTEM_POWER_DRIVER)
+    })
+};
