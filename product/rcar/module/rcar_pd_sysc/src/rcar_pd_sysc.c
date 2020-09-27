b732ee24b80fc0dc09f424eeafd4573a44173a2c
Nicolas Royer <nroyer@baylibre.com>
2020-09-27 17:40:31 +0200

    rcar/module: add rcar pd_sysc module and config data
    Change-Id: I69b575fbeaeb33fc1b76618d3d516c26e6c9a793
    Signed-off-by: Tsutomu Muroya <tsutomu.muroya.jy@bp.renesas.com>
    Signed-off-by: Nicolas Royer <nroyer@baylibre.com>

---
+++ product/rcar/module/rcar_pd_sysc/src/rcar_pd_sysc.c
@@ -0,0 +1,104 @@
+/*
+ * Renesas SCP/MCP Software
+ * Copyright (c) 2020, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <mmio.h>
+#include <rcar_common.h>
+#include <rcar_pd_sysc.h>
+
+#include <fwk_status.h>
+
+#include <stddef.h>
+
+/*****************************************************************************
+ * LOCAL FUNCTIONS
+ *****************************************************************************/
+static int rcar_sysc_pwr_on_off(struct rcar_sysc_pd_ctx *pd_ctx, bool on)
+{
+    unsigned int sr_bit, reg_offs;
+    int k;
+
+    if (on) {
+        sr_bit = SYSCSR_PONENB;
+        reg_offs = PWRONCR_OFFS;
+    } else {
+        sr_bit = SYSCSR_POFFENB;
+        reg_offs = PWROFFCR_OFFS;
+    }
+
+    /* Wait until SYSC is ready to accept a power request */
+    for (k = 0; k < SYSCSR_RETRIES; k++) {
+        if (mmio_read_32(SYSC_BASE_ADDR + SYSCSR) & BIT_SHIFT(sr_bit))
+            break;
+        udelay(SYSCSR_DELAY_US);
+    }
+
+    if (k == SYSCSR_RETRIES) {
+        return FWK_E_TIMEOUT;
+    }
+
+    /* Submit power shutoff or power resume request */
+    udelay(SYSCSR_DELAY_US);
+
+    mmio_write_32(
+        (SYSC_BASE_ADDR + pd_ctx->config->chan_offs + reg_offs),
+        BIT_SHIFT(pd_ctx->config->chan_bit));
+    return 0;
+}
+
+int rcar_sysc_power(struct rcar_sysc_pd_ctx *pd_ctx, bool on)
+{
+    unsigned int isr_mask = BIT_SHIFT(pd_ctx->config->isr_bit);
+    unsigned int chan_mask = BIT_SHIFT(pd_ctx->config->chan_bit);
+    unsigned int status;
+    int ret = 0;
+    int k;
+    uint32_t syscier, syscimr;
+
+    syscimr = mmio_read_32(SYSC_BASE_ADDR + SYSCIMR);
+    syscier = mmio_read_32(SYSC_BASE_ADDR + SYSCIER);
+    mmio_write_32((SYSC_BASE_ADDR + SYSCIMR), (syscimr | isr_mask));
+    mmio_write_32((SYSC_BASE_ADDR + SYSCIER), (syscier | isr_mask));
+    mmio_write_32((SYSC_BASE_ADDR + SYSCISCR), isr_mask);
+
+    /* Submit power shutoff or resume request until it was accepted */
+    for (k = 0; k < PWRER_RETRIES; k++) {
+        ret = rcar_sysc_pwr_on_off(pd_ctx, on);
+        if (ret)
+            return ret;
+
+        status = mmio_read_32(
+            SYSC_BASE_ADDR + pd_ctx->config->chan_offs + PWRER_OFFS);
+        if (!(status & chan_mask)) {
+            break;
+        }
+
+        udelay(PWRER_DELAY_US);
+    }
+
+    if (k == PWRER_RETRIES)
+        return FWK_E_BUSY;
+
+    /* Wait until the power shutoff or resume request has completed * */
+    for (k = 0; k < SYSCISR_RETRIES; k++) {
+        if (mmio_read_32(SYSC_BASE_ADDR + SYSCISR) & isr_mask)
+            break;
+        udelay(SYSCISR_DELAY_US);
+    }
+
+    if (k == SYSCISR_RETRIES) {
+        ret = FWK_E_BUSY;
+    }
+
+    mmio_write_32((SYSC_BASE_ADDR + SYSCISCR), isr_mask);
+
+    return ret;
+}
+
+int rcar_sysc_power_get(struct rcar_sysc_pd_ctx *pd_ctx, unsigned int *statee)
+{
+    return FWK_SUCCESS;
+}
---
+++ product/rcar/scp_ramfw/config_rcar_pd_sysc.c
@@ -0,0 +1,129 @@
+/*
+ * Renesas SCP/MCP Software
+ * Copyright (c) 2020, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <config_rcar_pd_sysc.h>
+
+#include <mod_rcar_pd_sysc.h>
+
+#include <fwk_element.h>
+#include <fwk_module.h>
+
+static struct fwk_element rcar_pd_sysc_element_table[] = {
+    [RCAR_PD_SYSC_ELEMENT_IDX_A3IR] =
+        {
+            .name = "a3ir",
+            .data = &((struct mod_rcar_pd_sysc_config){
+                .pd_type = MOD_PD_TYPE_DEVICE,
+                .chan_offs = 0x180,
+                .chan_bit = 0,
+                .isr_bit = R8A7795_PD_A3IR,
+            }),
+        },
+    [RCAR_PD_SYSC_ELEMENT_IDX_3DGE] =
+        {
+            .name = "3dg-e",
+            .data = &((struct mod_rcar_pd_sysc_config){
+                .pd_type = MOD_PD_TYPE_DEVICE,
+                .chan_offs = 0x100,
+                .chan_bit = 4,
+                .isr_bit = R8A7795_PD_3DG_E,
+            }),
+        },
+    [RCAR_PD_SYSC_ELEMENT_IDX_3DGD] =
+        {
+            .name = "3dg-d",
+            .data = &((struct mod_rcar_pd_sysc_config){
+                .pd_type = MOD_PD_TYPE_DEVICE,
+                .chan_offs = 0x100,
+                .chan_bit = 3,
+                .isr_bit = R8A7795_PD_3DG_D,
+            }),
+        },
+    [RCAR_PD_SYSC_ELEMENT_IDX_3DGC] =
+        {
+            .name = "3dg-c",
+            .data = &((struct mod_rcar_pd_sysc_config){
+                .pd_type = MOD_PD_TYPE_DEVICE,
+                .chan_offs = 0x100,
+                .chan_bit = 2,
+                .isr_bit = R8A7795_PD_3DG_C,
+            }),
+        },
+    [RCAR_PD_SYSC_ELEMENT_IDX_3DGB] =
+        {
+            .name = "3dg-b",
+            .data = &((struct mod_rcar_pd_sysc_config){
+                .pd_type = MOD_PD_TYPE_DEVICE,
+                .chan_offs = 0x100,
+                .chan_bit = 1,
+                .isr_bit = R8A7795_PD_3DG_B,
+            }),
+        },
+    [RCAR_PD_SYSC_ELEMENT_IDX_3DGA] =
+        {
+            .name = "3dg-a",
+            .data = &((struct mod_rcar_pd_sysc_config){
+                .pd_type = MOD_PD_TYPE_DEVICE,
+                .chan_offs = 0x100,
+                .chan_bit = 0,
+                .isr_bit = R8A7795_PD_3DG_A,
+            }),
+        },
+    [RCAR_PD_SYSC_ELEMENT_IDX_A2VC1] =
+        {
+            .name = "a2vc1",
+            .data = &((struct mod_rcar_pd_sysc_config){
+                .pd_type = MOD_PD_TYPE_DEVICE,
+                .chan_offs = 0x3c0,
+                .chan_bit = 1,
+                .isr_bit = R8A7795_PD_A2VC1,
+            }),
+        },
+    [RCAR_PD_SYSC_ELEMENT_IDX_A3VC] =
+        {
+            .name = "a3vc",
+            .data = &((struct mod_rcar_pd_sysc_config){
+                .pd_type = MOD_PD_TYPE_DEVICE,
+                .chan_offs = 0x380,
+                .chan_bit = 0,
+                .isr_bit = R8A7795_PD_A3VC,
+            }),
+        },
+    [RCAR_PD_SYSC_ELEMENT_IDX_CR7] =
+        {
+            .name = "cr7",
+            .data = &((struct mod_rcar_pd_sysc_config){
+                .pd_type = MOD_PD_TYPE_DEVICE,
+                .chan_offs = 0x240,
+                .chan_bit = 0,
+                .isr_bit = R8A7795_PD_CR7,
+            }),
+        },
+    [RCAR_PD_SYSC_ELEMENT_IDX_A3VP] =
+        {
+            .name = "a3vp",
+            .data = &((struct mod_rcar_pd_sysc_config){
+                .pd_type = MOD_PD_TYPE_DEVICE,
+                .chan_offs = 0x340,
+                .chan_bit = 0,
+                .isr_bit = R8A7795_PD_A3VP,
+            }),
+        },
+    [RCAR_PD_SYSC_ELEMENT_IDX_COUNT] = { 0 }, /* Termination entry */
+};
+
+static const struct fwk_element *rcar_pd_sysc_get_element_table(fwk_id_t mod)
+{
+    return rcar_pd_sysc_element_table;
+}
+
+/*
+ * Power module configuration data
+ */
+const struct fwk_module_config config_rcar_pd_sysc = {
+    .elements = FWK_MODULE_DYNAMIC_ELEMENTS(rcar_pd_sysc_get_element_table),
+};
