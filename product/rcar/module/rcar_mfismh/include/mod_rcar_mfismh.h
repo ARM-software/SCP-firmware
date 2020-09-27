f092662656b9f1fc7b5c5039e8e49ea4e0390d72
Nicolas Royer <nroyer@baylibre.com>
2020-09-27 17:38:23 +0200

    rcar/module: add rcar mfismh module and config data
    Signed-off-by: Tsutomu Muroya <tsutomu.muroya.jy@bp.renesas.com>
    Signed-off-by: Nicolas Royer <nroyer@baylibre.com>

---
+++ product/rcar/module/rcar_mfismh/include/mod_rcar_mfismh.h
@@ -0,0 +1,51 @@
+/*
+ * Renesas SCP/MCP Software
+ * Copyright (c) 2020, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MOD_RCAR_MFISMH_H
+#define MOD_RCAR_MFISMH_H
+
+#include <stdint.h>
+#include <fwk_macros.h>
+
+/*!
+ * \addtogroup GroupRCARModule RCAR Product Modules
+ * @{
+ */
+
+/*!
+ * \defgroup GroupRCARMHU Message Handling Unit (MHU) Driver
+ * @{
+ */
+
+/*!
+ * \brief MFISMB device
+ *
+ * \details Abstract representation of a bidirectional
+ *      MFISMB device that consists
+ *      of a single receive interrupt line and a pair of register sets, one for
+ *      each direction of communication.
+ */
+struct mod_rcar_mfismh_device_config {
+    /*! IRQ number of the receive interrupt line */
+    unsigned int irq;
+
+    /*! Base address of the registers of the incoming MFISMHU */
+    uintptr_t in;
+
+    /*! Base address of the registers of the outgoing MFISMHU */
+    uintptr_t out;
+};
+
+/*!
+ * @}
+ */
+
+/*!
+ * @}
+ */
+
+#endif /* MOD_RCAR_MFISMH_H */
