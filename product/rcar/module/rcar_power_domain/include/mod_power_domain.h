e88b03fd71f44a24bdf0cc72838f276404de7b0b
Nicolas Royer <nroyer@baylibre.com>
2020-09-27 17:41:19 +0200

    rcar/module: add rcar power_domain module and config data
    Change-Id: I44796fe0ea4c66e90d77c6cd93843914a7978f3b
    Signed-off-by: Tsutomu Muroya <tsutomu.muroya.jy@bp.renesas.com>
    Signed-off-by: Nicolas Royer <nroyer@baylibre.com>

---
+++ product/rcar/module/rcar_power_domain/include/mod_power_domain.h
@@ -0,0 +1,16 @@
+/*
+ * Renesas SCP/MCP Software
+ * Copyright (c) 2020, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MOD_POWER_DOMAIN_H
+#define MOD_POWER_DOMAIN_H
+
+#define FWK_MODULE_IDX_POWER_DOMAIN FWK_MODULE_IDX_RCAR_POWER_DOMAIN
+#define fwk_module_id_power_domain fwk_module_id_rcar_power_domain
+
+#include "mod_rcar_power_domain.h"
+
+#endif /* MOD_POWER_DOMAIN_H */
