f8f27a9cfae895aaf5c86834499a015fb0bab714
Nicolas Royer <nroyer@baylibre.com>
2020-09-27 17:48:36 +0200

    rcar/module: add rcar system_power module and config data
    Change-Id: I4267a31991d36fd7b63b13a7cdb16ab28dda1c2d
    Signed-off-by: Tsutomu Muroya <tsutomu.muroya.jy@bp.renesas.com>
    Signed-off-by: Nicolas Royer <nroyer@baylibre.com>

---
+++ product/rcar/module/rcar_system_power/include/mod_system_power.h
@@ -0,0 +1,15 @@
+/*
+ * Renesas SCP/MCP Software
+ * Copyright (c) 2020, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MOD_SYSTEM_POWER_H
+#define MOD_SYSTEM_POWER_H
+
+#define FWK_MODULE_IDX_SYSTEM_POWER FWK_MODULE_IDX_RCAR_SYSTEM_POWER
+
+#include "mod_rcar_system_power.h"
+
+#endif /* MOD_SYSTEM_POWER_H */
