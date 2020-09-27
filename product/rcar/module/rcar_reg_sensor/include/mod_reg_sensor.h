c96ddcce819116effc4f61d0bddc65aa56c99f6e
Nicolas Royer <nroyer@baylibre.com>
2020-09-27 17:46:43 +0200

    rcar/module: add rcar reg_sensor module
    Change-Id: I59ba3eb358fcfc9b76e0b7daa7621abfaeec5028
    Signed-off-by: Tsutomu Muroya <tsutomu.muroya.jy@bp.renesas.com>
    Signed-off-by: Chikara Asou <chikara.aso.uj@bp.renesas.com>
    Signed-off-by: Nicolas Royer <nroyer@baylibre.com>

---
+++ product/rcar/module/rcar_reg_sensor/include/mod_reg_sensor.h
@@ -0,0 +1,19 @@
+/*
+ * Renesas SCP/MCP Software
+ * Copyright (c) 2020, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MOD_REG_SENSOR_H
+#define MOD_REG_SENSOR_H
+
+#include <mod_sensor.h>
+
+#include <stdint.h>
+
+#define FWK_MODULE_IDX_REG_SENSOR FWK_MODULE_IDX_RCAR_REG_SENSOR
+
+#include "mod_rcar_reg_sensor.h"
+
+#endif /* MOD_REG_SENSOR_H */
