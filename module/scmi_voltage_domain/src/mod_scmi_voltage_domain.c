30046756e05082f561d97b684ecf403dc5fa7c14
jimqui01 <54316584+jimqui01@users.noreply.github.com>
2020-10-07 15:54:57 +0100

    Update mod_scmi_voltage_domain.c
    Remove unused variables.

--- module/scmi_voltage_domain/src/mod_scmi_voltage_domain.c
+++ module/scmi_voltage_domain/src/mod_scmi_voltage_domain.c
@@ -660,8 +660,6 @@ static int scmi_voltd_init(fwk_id_t module_id, unsigned int element_count,
 
 static int scmi_voltd_bind(fwk_id_t id, unsigned int round)
 {
-    const unsigned int agent_count = scmi_voltd_ctx.config->agent_count;
-    unsigned int agent_idx = 0;
     int status = 0;
 
     if (round == 1)
