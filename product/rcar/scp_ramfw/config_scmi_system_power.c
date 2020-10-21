820265d45ed5bddeddb0edc4cfce743e3c70f8c3
Tsutomu Muroya <tsutomu.muroya.jy@bp.renesas.com>
2020-10-21 11:00:16 +0900

    rcar/module: add system power support
    Signed-off-by: Tsutomu Muroya <tsutomu.muroya.jy@bp.renesas.com>
    Signed-off-by: Nicolas Royer <nroyer@baylibre.com>

---
+++ product/rcar/scp_ramfw/config_scmi_system_power.c
@@ -0,0 +1,11 @@
+/*
+ * Renesas SCP/MCP Software
+ * Copyright (c) 2020, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_module.h>
+
+struct fwk_module_config config_scmi_system_power = {
+};
