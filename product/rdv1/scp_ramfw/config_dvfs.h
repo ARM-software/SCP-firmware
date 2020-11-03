7b7d4e68a370df8214d9d0afb3a830aaf9e36bdf
Pranav Madhu <pranav.madhu@arm.com>
2020-11-03 13:24:14 +0530

    product/rdv1: Add support for DVFS and SCMI Perf
    Enable Dynamic Voltage/Frequency Support (DVFS) and SCMI perf support
    for RD-V1 platform. This is required to support ACPI CPPC mechanism
    for the platform.
    
    Change-Id: I1170172f74e0ead84017f98f9a26355d2b8cc095
    Signed-off-by: Pranav Madhu <pranav.madhu@arm.com>

---
+++ product/rdv1/scp_ramfw/config_dvfs.h
@@ -0,0 +1,31 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef CONFIG_DVFS_H
+#define CONFIG_DVFS_H
+
+enum dvfs_element_idx {
+    DVFS_ELEMENT_IDX_CPU0,
+    DVFS_ELEMENT_IDX_CPU1,
+    DVFS_ELEMENT_IDX_CPU2,
+    DVFS_ELEMENT_IDX_CPU3,
+    DVFS_ELEMENT_IDX_CPU4,
+    DVFS_ELEMENT_IDX_CPU5,
+    DVFS_ELEMENT_IDX_CPU6,
+    DVFS_ELEMENT_IDX_CPU7,
+    DVFS_ELEMENT_IDX_CPU8,
+    DVFS_ELEMENT_IDX_CPU9,
+    DVFS_ELEMENT_IDX_CPU10,
+    DVFS_ELEMENT_IDX_CPU11,
+    DVFS_ELEMENT_IDX_CPU12,
+    DVFS_ELEMENT_IDX_CPU13,
+    DVFS_ELEMENT_IDX_CPU14,
+    DVFS_ELEMENT_IDX_CPU15,
+    DVFS_ELEMENT_IDX_COUNT
+};
+
+#endif /* CONFIG_DVFS_H */
