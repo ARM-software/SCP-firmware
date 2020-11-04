afe99b69339e849732aeccdffdee06aefb2d289c
Aditya Angadi <aditya.angadi@arm.com>
2020-11-04 13:08:15 +0530

    product/rdn2: add build support for scp rom firmware
    Add the linker script and makefile to support the build of the SCP ROM
    firmware.
    
    Change-Id: Ic2f19b7d6afacc231206c19a58c590d54571e1dc
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

--- product/rdn2/include/scp_software_mmap.h
+++ product/rdn2/include/scp_software_mmap.h
@@ -12,6 +12,10 @@
 
 #include <fwk_macros.h>
 
+/* SCP ROM and RAM firmware size loaded on main memory */
+#define SCP_BOOT_ROM_SIZE (64 * 1024)
+#define SCP_DTC_RAM_SIZE  (256 * 1024)
+
 /* SCP RAM firmware base and size on the flash */
 #define SCP_RAMFW_IMAGE_FLASH_BASE (SCP_NOR0_FLASH_BASE + 0x03D80000)
 #define SCP_RAMFW_IMAGE_FLASH_SIZE (256 * FWK_KIB)
