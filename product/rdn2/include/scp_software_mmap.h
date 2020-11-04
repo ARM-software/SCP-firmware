78d2eb756d8bd1a8403dcda2b6f327258d526fc7
Aditya Angadi <aditya.angadi@arm.com>
2020-11-04 12:57:38 +0530

    product/rdn2: add config data for bootloader module
    The bootloader module is used to load the SCP RAM firmware from the NOR
    flash to SCP RAM. Provide the base address and size of the firmware
    image to be loaded from and the base address of the location to which
    the firmware is loaded to.
    
    Change-Id: Ia7be81442ef67225f7a85182a96a3feabcae3492
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

---
+++ product/rdn2/include/scp_software_mmap.h
@@ -0,0 +1,19 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SCP_SOFTWARE_MMAP_H
+#define SCP_SOFTWARE_MMAP_H
+
+#include "scp_soc_mmap.h"
+
+#include <fwk_macros.h>
+
+/* SCP RAM firmware base and size on the flash */
+#define SCP_RAMFW_IMAGE_FLASH_BASE (SCP_NOR0_FLASH_BASE + 0x03D80000)
+#define SCP_RAMFW_IMAGE_FLASH_SIZE (256 * FWK_KIB)
+
+#endif /* SCP_SOFTWARE_MMAP_H */
