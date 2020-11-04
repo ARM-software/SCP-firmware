a8d27a3a00866f6b877bb2a00e5d1517fb0035f6
Aditya Angadi <aditya.angadi@arm.com>
2020-11-04 11:52:39 +0530

    product/rdn2: add config data for pl011 UART module
    PL011 controller is used as a console port for debug and log messages.
    Add configuration data of this controller including base address and
    input clock frequency for the PL011 module to use.
    
    Change-Id: I125920a32114c81d213430c37c1550b526e0a32c
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

---
+++ product/rdn2/src/config_pl011.c
@@ -0,0 +1,37 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "scp_css_mmap.h"
+
+#include <mod_pl011.h>
+
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_macros.h>
+#include <fwk_module.h>
+
+static const struct fwk_element config_pl011_elements[] = {
+    [0] = {
+        .name = "uart",
+        .data =
+            &(struct mod_pl011_element_cfg){
+                .reg_base = SCP_UART_BASE,
+                .baud_rate_bps = 115200,
+                .clock_rate_hz = 24 * FWK_MHZ,
+                .clock_id = FWK_ID_NONE_INIT,
+#ifdef BUILD_HAS_MOD_POWER_DOMAIN
+                .pd_id = FWK_ID_NONE_INIT,
+#endif
+            },
+    },
+
+    [1] = { 0 },
+};
+
+const struct fwk_module_config config_pl011 = {
+    .elements = FWK_MODULE_STATIC_ELEMENTS_PTR(config_pl011_elements),
+};
