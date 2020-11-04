bf64c4eb0c77bc0c5ab06ad0da0527c7942aa07a
Aditya Angadi <aditya.angadi@arm.com>
2020-11-04 11:45:12 +0530

    product/rdn2: add config data for SID driver module
    SID peripheral on this platform provides information about the platform
    ID and config ID. So add the configuration data for SID module. The
    base address of the SID register block is provided for the SID module
    to read out the IDs from the SID registers.
    
    Change-Id: Ie6fa5a4c1c4694818bfad82d19406ab7710f2f1d
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

---
+++ product/rdn2/include/scp_css_mmap.h
@@ -0,0 +1,15 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SCP_CSS_MMAP_H
+#define SCP_CSS_MMAP_H
+
+#include "scp_mmap.h"
+
+#define SCP_SID_BASE (SCP_SYSTEM_ACCESS_PORT1_BASE + 0x2A4A0000)
+
+#endif /* SCP_CSS_MMAP_H */
