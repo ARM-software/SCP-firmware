b9bcd876be068813085a475fd24502233df0f23c
Girish Pathak <girish.pathak@arm.com>
2020-11-04 10:58:45 +0000

    cmake: mod_debug
    This change adds CMake build support for module/debug
    
    Change-Id: I76ed2b494a7915a88a80248220952c1a84fbc3fb
    Signed-off-by: Girish Pathak <girish.pathak@arm.com>

---
+++ module/debug/Module.cmake
@@ -0,0 +1,13 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+set(SCP_MODULE "debug")
+set(SCP_MODULE_TARGET "module-debug")
+
+if(SCP_ENABLE_DEBUG_UNIT)
+    list(APPEND SCP_MODULES "debug")
+endif()
