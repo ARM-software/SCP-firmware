f36b501c041b9771ec472ae580e32d3454905550
Aditya Angadi <aditya.angadi@arm.com>
2020-11-05 14:41:34 +0530

    product/rdn2: add the product.mk file for rdn2 platform
    Add the product.mk file for RD-Evans platform and make it available to
    the scp build system as a product.
    
    Change-Id: Ic11252bff3cc1755abe36cdf21d989b1074c6c29
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

---
+++ product/rdn2/product.mk
@@ -0,0 +1,13 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Platforms supported by the RD-N2 product: rdn2
+#
+
+BS_PRODUCT_NAME := rdn2
+BS_FIRMWARE_LIST := scp_romfw \
+                    scp_ramfw \
+                    mcp_romfw
