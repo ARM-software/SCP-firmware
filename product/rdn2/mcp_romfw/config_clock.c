0424ff4703aa8775bde15b7f4b42af9f3f0fc2fb
Aditya Angadi <aditya.angadi@arm.com>
2020-11-05 14:28:53 +0530

    product/rdn2: add support for MCP firmware in rdn2
    MCP ROM and RAM firmware, does not provide any specific functionality
    yet. MCP ROM firmware loads the MCP RAM firmware which then executes
    a WFI to enter idle state.
    
    Change-Id: I6301601d9b3b60b5dc4e612c35ae5c765202b851
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

---
+++ product/rdn2/mcp_romfw/config_clock.c
@@ -0,0 +1,10 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_module.h>
+
+const struct fwk_module_config config_clock = { 0 };
---
+++ product/rdn2/mcp_romfw/fmw_memory.h
@@ -0,0 +1,30 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2020, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     ROM firmware memory layout for the linker script.
+ */
+
+#ifndef FMW_MEMORY_H
+#define FMW_MEMORY_H
+
+#include "mcp_software_mmap.h"
+
+#define FMW_MEM_MODE ARCH_MEM_MODE_DUAL_REGION_RELOCATION
+
+/*
+ * ROM memory
+ */
+#define FMW_MEM0_SIZE MCP_ROM_SIZE
+#define FMW_MEM0_BASE MCP_ROM_BASE
+
+/*
+ * RAM memory
+ */
+#define FMW_MEM1_SIZE MCP_RAM1_SIZE
+#define FMW_MEM1_BASE MCP_RAM1_BASE
+
+#endif /* FMW_MEMORY_H */
