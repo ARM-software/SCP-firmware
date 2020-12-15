ce73dcad69ee590e11f633a7a64edcf405a51c74
Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
2020-12-15 16:48:47 +0530

    product/rdv1mc: add macro to describe cpu pll elements
    The CPU pll config elements have similar initialization data with the
    only exception being that the CPU clock pll index is unique for each
    element. So create a macro to describe the CPU pll elements and use it
    to instantiate the CPU pll config elements.
    
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
    Change-Id: I92dd2b75b73a12ec87c4de2b13e4d6285ac71676

--- product/rdv1mc/scp_ramfw/config_system_pll.c
+++ product/rdv1mc/scp_ramfw/config_system_pll.c
@@ -16,55 +16,25 @@
 #include <fwk_macros.h>
 #include <fwk_module.h>
 
+#define CLOCK_PLL_IDX_CPU(n) \
+    [CLOCK_PLL_IDX_CPU##n] = { \
+        .name = "CPU_PLL_" #n, \
+        .data = &((struct mod_system_pll_dev_config){ \
+            .control_reg = (void *)SCP_PLL_CPU##n, \
+            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1], \
+            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(n), \
+            .initial_rate = 2600 * FWK_MHZ, \
+            .min_rate = MOD_SYSTEM_PLL_MIN_RATE, \
+            .max_rate = MOD_SYSTEM_PLL_MAX_RATE, \
+            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL, \
+        }), \
+    }
+
 static const struct fwk_element system_pll_element_table[] = {
-    [CLOCK_PLL_IDX_CPU0] = {
-        .name = "CPU_PLL_0",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU0,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(0),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
-    [CLOCK_PLL_IDX_CPU1] = {
-        .name = "CPU_PLL_1",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU1,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(1),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
-    [CLOCK_PLL_IDX_CPU2] = {
-        .name = "CPU_PLL_2",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU2,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(2),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
-    [CLOCK_PLL_IDX_CPU3] = {
-        .name = "CPU_PLL_3",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU3,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(3),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
+    CLOCK_PLL_IDX_CPU(0),
+    CLOCK_PLL_IDX_CPU(1),
+    CLOCK_PLL_IDX_CPU(2),
+    CLOCK_PLL_IDX_CPU(3),
     [CLOCK_PLL_IDX_SYS] = {
         .name = "SYS_PLL",
         .data = &((struct mod_system_pll_dev_config) {
