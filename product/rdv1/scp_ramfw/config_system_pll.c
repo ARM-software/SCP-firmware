7c00d7b2bfe67211ebf100c6c82ce367c1c4fa7d
Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
2020-12-15 16:37:50 +0530

    product/rdv1: add macro to describe cpu pll elements
    The CPU pll config elements have similar initialization data with the
    only exception being that the CPU clock pll index is unique for each
    element. So create a macro to describe the CPU pll elements and use it
    to instantiate the CPU pll config elements.
    
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
    Change-Id: Iedae551b1457c475419a927ab8a3a7f9758a9cdc

--- product/rdv1/scp_ramfw/config_system_pll.c
+++ product/rdv1/scp_ramfw/config_system_pll.c
@@ -16,199 +16,37 @@
 #include <fwk_macros.h>
 #include <fwk_module.h>
 
+#define CLOCK_PLL_IDX_CPU(n) \
+    [CLOCK_PLL_IDX_CPU##n] = { \
+        .name = "CPU_PLL_" #n, \
+        .data = &((struct mod_system_pll_dev_config){ \
+            .control_reg = (void *)SCP_PLL_CPU##n, \
+            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1], \
+            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(n), \
+            .initial_rate = 2600 * FWK_MHZ, \
+            .min_rate = MOD_SYSTEM_PLL_MIN_RATE, \
+            .max_rate = MOD_SYSTEM_PLL_MAX_RATE, \
+            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL, \
+        }), \
+    }
+
 static const struct fwk_element system_pll_element_table[] = {
-    [CLOCK_PLL_IDX_CPU0] = {
-        .name = "CPU_PLL_0",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU0,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(0),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
-    [CLOCK_PLL_IDX_CPU1] = {
-        .name = "CPU_PLL_1",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU1,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(1),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
-    [CLOCK_PLL_IDX_CPU2] = {
-        .name = "CPU_PLL_2",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU2,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(2),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
-    [CLOCK_PLL_IDX_CPU3] = {
-        .name = "CPU_PLL_3",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU3,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(3),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
-    [CLOCK_PLL_IDX_CPU4] = {
-        .name = "CPU_PLL_4",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU4,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(4),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
-    [CLOCK_PLL_IDX_CPU5] = {
-        .name = "CPU_PLL_5",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU5,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(5),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
-    [CLOCK_PLL_IDX_CPU6] = {
-        .name = "CPU_PLL_6",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU6,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(6),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
-    [CLOCK_PLL_IDX_CPU7] = {
-        .name = "CPU_PLL_7",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU7,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(7),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
-    [CLOCK_PLL_IDX_CPU8] = {
-        .name = "CPU_PLL_8",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU8,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(8),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
-    [CLOCK_PLL_IDX_CPU9] = {
-        .name = "CPU_PLL_9",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU9,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(9),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
-    [CLOCK_PLL_IDX_CPU10] = {
-        .name = "CPU_PLL_10",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU10,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(10),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
-    [CLOCK_PLL_IDX_CPU11] = {
-        .name = "CPU_PLL_11",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU11,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(11),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
-    [CLOCK_PLL_IDX_CPU12] = {
-        .name = "CPU_PLL_12",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU12,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(12),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
-    [CLOCK_PLL_IDX_CPU13] = {
-        .name = "CPU_PLL_13",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU13,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(13),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
-    [CLOCK_PLL_IDX_CPU14] = {
-        .name = "CPU_PLL_14",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU14,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(14),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
-    [CLOCK_PLL_IDX_CPU15] = {
-        .name = "CPU_PLL_15",
-        .data = &((struct mod_system_pll_dev_config) {
-            .control_reg = (void *)SCP_PLL_CPU15,
-            .status_reg = (void *)&SCP_PIK_PTR->PLL_STATUS[1],
-            .lock_flag_mask = PLL_STATUS_CPUPLLLOCK(15),
-            .initial_rate = 2600 * FWK_MHZ,
-            .min_rate = MOD_SYSTEM_PLL_MIN_RATE,
-            .max_rate = MOD_SYSTEM_PLL_MAX_RATE,
-            .min_step = MOD_SYSTEM_PLL_MIN_INTERVAL,
-        }),
-    },
+    CLOCK_PLL_IDX_CPU(0),
+    CLOCK_PLL_IDX_CPU(1),
+    CLOCK_PLL_IDX_CPU(2),
+    CLOCK_PLL_IDX_CPU(3),
+    CLOCK_PLL_IDX_CPU(4),
+    CLOCK_PLL_IDX_CPU(5),
+    CLOCK_PLL_IDX_CPU(6),
+    CLOCK_PLL_IDX_CPU(7),
+    CLOCK_PLL_IDX_CPU(8),
+    CLOCK_PLL_IDX_CPU(9),
+    CLOCK_PLL_IDX_CPU(10),
+    CLOCK_PLL_IDX_CPU(11),
+    CLOCK_PLL_IDX_CPU(12),
+    CLOCK_PLL_IDX_CPU(13),
+    CLOCK_PLL_IDX_CPU(14),
+    CLOCK_PLL_IDX_CPU(15),
     [CLOCK_PLL_IDX_SYS] = {
         .name = "SYS_PLL",
         .data = &((struct mod_system_pll_dev_config) {
