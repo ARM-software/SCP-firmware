08aee69482e475220183a85b846f1fb9ead3076c
Girish Pathak <girish.pathak@arm.com>
2021-01-04 13:36:30 +0000

    cmake: product/rdv1mc
    This change adds CMake build system support to rdv1mc
    Platform.
    
    Please note that firmware targets do not use libRTX_CM3.a
    or RTX_CM3.lib, instead in CMake build, the library
    is built using sources.
    
    Change-Id: I21f69edf26e3d2ba2163db1fb34af2dd901a5497
    Signed-off-by: Girish Pathak <girish.pathak@arm.com>

---
+++ product/rdv1mc/scp_romfw/Firmware.cmake
@@ -0,0 +1,38 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+#
+# Configure the build system.
+#
+
+set(SCP_FIRMWARE "rdv1mc-bl1")
+
+set(SCP_FIRMWARE_TARGET "rdv1mc-bl1")
+
+set(SCP_TOOLCHAIN_INIT "GNU")
+
+set(SCP_GENERATE_FLAT_BINARY_INIT TRUE)
+
+set(SCP_ENABLE_MULTITHREADING_INIT FALSE)
+
+set(SCP_ENABLE_NOTIFICATIONS_INIT TRUE)
+
+set(SCP_ENABLE_IPO_INIT FALSE)
+
+set(SCP_ARCHITECTURE "armv7-m")
+
+# The order of the modules in the following list is the order in which the
+# modules are initialized, bound, started during the pre-runtime phase.
+# any change in the order will cause firmware initialization errors.
+
+list(APPEND SCP_MODULES "sid")
+list(APPEND SCP_MODULES "system-info")
+list(APPEND SCP_MODULES "pcid")
+list(APPEND SCP_MODULES "pl011")
+list(APPEND SCP_MODULES "clock")
+list(APPEND SCP_MODULES "bootloader")
+list(APPEND SCP_MODULES "isys-rom")
