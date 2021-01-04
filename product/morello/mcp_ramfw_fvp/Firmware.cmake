fa9d0bf1826834b26f68eb108deeb845273a3442
Girish Pathak <girish.pathak@arm.com>
2021-01-04 14:21:47 +0000

    cmake: product/morello
    This change adds CMake build support for morello platform
    
    Please note that firmware targets do not use libRTX_CM3.a
    or RTX_CM3.lib, instead in CMake build, the library
    is built using sources.
    
    Change-Id: Ic150e116af9e13474a128a61268859fdb50e1c18
    Signed-off-by: Girish Pathak <girish.pathak@arm.com>

---
+++ product/morello/mcp_ramfw_fvp/Firmware.cmake
@@ -0,0 +1,40 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+set(SCP_FIRMWARE "morello-fvp-mcp-bl2")
+set(SCP_FIRMWARE_TARGET "morello-fvp-mcp-bl2")
+
+set(SCP_TOOLCHAIN_INIT "GNU")
+
+set(SCP_FIRMWARE_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}")
+
+set(SCP_GENERATE_FLAT_BINARY_INIT TRUE)
+
+set(SCP_ARCHITECTURE "armv7-m")
+
+set(SCP_ENABLE_MULTITHREADING_INIT TRUE)
+
+set(SCP_ENABLE_NOTIFICATIONS_INIT TRUE)
+
+list(PREPEND SCP_MODULE_PATHS
+     "${CMAKE_CURRENT_LIST_DIR}/../module/morello_mcp_system")
+list(PREPEND SCP_MODULE_PATHS "${CMAKE_CURRENT_LIST_DIR}/../module/morello_smt")
+list(PREPEND SCP_MODULE_PATHS "${CMAKE_CURRENT_LIST_DIR}/../module/morello_mhu")
+list(PREPEND SCP_MODULE_PATHS "${CMAKE_CURRENT_LIST_DIR}/../module/scmi_agent")
+
+# The order of the modules in the following list is the order in which the
+# modules are initialized, bound, started during the pre-runtime phase.
+# any change in the order will cause firmware initialization errors.
+
+list(APPEND SCP_MODULES "armv7m-mpu")
+list(APPEND SCP_MODULES "pl011")
+list(APPEND SCP_MODULES "pik-clock")
+list(APPEND SCP_MODULES "clock")
+list(APPEND SCP_MODULES "morello-smt")
+list(APPEND SCP_MODULES "morello-mhu")
+list(APPEND SCP_MODULES "scmi-agent")
+list(APPEND SCP_MODULES "morello-mcp-system")
