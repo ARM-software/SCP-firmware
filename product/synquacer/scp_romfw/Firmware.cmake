9f5986718148b14c4de64f05ac9a4ff501e859d3
Girish Pathak <girish.pathak@arm.com>
2021-01-07 14:43:02 +0000

    cmake: product/synquacer
    This change adds CMake build support for product/synquacer
    
    Please note that firmware targets do not use libRTX_CM3.a
    or RTX_CM3.lib, instead in CMake build, the library
    is built using sources.
    
    Change-Id: I886dd947c8d84d4ffb4d0a0f4c84defd4224d48b
    Signed-off-by: Girish Pathak <girish.pathak@arm.com>

---
+++ product/synquacer/scp_romfw/Firmware.cmake
@@ -0,0 +1,46 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+#
+# Configure the build system.
+#
+
+set(SCP_FIRMWARE "synquacer-bl1")
+
+set(SCP_FIRMWARE_TARGET "synquacer-bl1")
+
+set(SCP_TOOLCHAIN_INIT "GNU")
+
+set(SCP_GENERATE_FLAT_BINARY_INIT TRUE)
+
+set(SCP_ENABLE_MULTITHREADING_INIT FALSE)
+
+set(SCP_ENABLE_NOTIFICATIONS_INIT TRUE)
+
+set(SCP_ENABLE_IPO_INIT FALSE)
+
+set(SCP_ARCHITECTURE "armv7-m")
+
+list(PREPEND SCP_MODULE_PATHS
+     "${CMAKE_CURRENT_LIST_DIR}/../module/synquacer_pik_clock")
+
+list(PREPEND SCP_MODULE_PATHS
+     "${CMAKE_CURRENT_LIST_DIR}/../module/ppu_v0_synquacer")
+
+list(PREPEND SCP_MODULE_PATHS
+     "${CMAKE_CURRENT_LIST_DIR}/../module/synquacer_rom")
+
+# The order of the modules in the following list is the order in which the
+# modules are initialized, bound, started during the pre-runtime phase.
+# any change in the order will cause firmware initialization errors.
+
+list(APPEND SCP_MODULES "armv7m-mpu")
+list(APPEND SCP_MODULES "synquacer-rom")
+list(APPEND SCP_MODULES "clock")
+list(APPEND SCP_MODULES "synquacer-pik-clock")
+list(APPEND SCP_MODULES "gtimer")
+list(APPEND SCP_MODULES "timer")
