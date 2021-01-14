d3c9884ab25b412468c5873973d4817ddf6791cc
Girish Pathak <girish.pathak@arm.com>
2021-01-14 16:21:27 +0000

    cmake: Enable CMake build using exiting tools
    This change enables CMake based build using existing tools
    
    Change-Id: I33b9501ff98b8b21ef7759a440b9051f85b4f148
    Signed-off-by: Girish Pathak <girish.pathak@arm.com

---
+++ Makefile.cmake
@@ -0,0 +1,242 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+#
+# Directories
+#
+export TOP_DIR := $(shell pwd)
+export ARCH_DIR := $(TOP_DIR)/arch
+export FWK_DIR := $(TOP_DIR)/framework
+export TOOLS_DIR := $(TOP_DIR)/tools
+export BS_DIR := $(TOOLS_DIR)/build_system
+export PRODUCTS_DIR := $(TOP_DIR)/product
+
+#
+# Tools
+#
+export RM := rm -rf
+export MD := mkdir -p
+export CP := cp -r
+export DOC := doxygen
+export CMAKE := cmake
+
+#
+# Default options
+#
+DEFAULT_MODE := release
+DEFAULT_BUILD_PATH := $(TOP_DIR)/build
+DEFAULT_VERBOSE := n
+DEFAULT_DEBUGGER := n
+DEFAULT_TOOLCHAIN := GNU
+DEFAULT_BUILD_SYSTEM := Ninja
+export CMSIS_DIR := $(TOP_DIR)/contrib/cmsis/git/CMSIS/Core
+export OS_DIR := $(TOP_DIR)/contrib/cmsis/git/CMSIS/RTOS2
+DEFAULT_LOG_LEVEL_debug := INFO
+DEFAULT_LOG_LEVEL_release := WARN
+
+DEFAULT_CMAKE_TOOL_LOG_LEVEL := NOTICE
+
+#
+# Top-level configuration
+#
+.DEFAULT_GOAL = all
+# MAKECMDGOALS isn't set if there's no explicit goal in the
+# command line, so set the default.
+MAKECMDGOALS ?= $(.DEFAULT_GOAL)
+
+# Mode
+MODE ?= $(DEFAULT_MODE)
+ifneq ($(filter-out debug release, $(MODE)),)
+    $(error "Invalid MODE parameter. Aborting...")
+endif
+
+# Build System
+BUILD_SYSTEM ?= $(DEFAULT_BUILD_SYSTEM)
+ifneq ($(filter-out Make Ninja, $(BUILD_SYSTEM)),)
+    $(error "Invalid BUILD_SYSTEM parameter. Aborting...")
+endif
+
+# Toolchain
+TOOLCHAIN ?= $(DEFAULT_TOOLCHAIN)
+ifneq ($(filter-out GNU ArmClang, $(TOOLCHAIN)),)
+    $(error "Invalid TOOLCHAIN parameter. Aborting...")
+endif
+
+# Log level
+LOG_LEVEL ?= $(DEFAULT_LOG_LEVEL_${MODE})
+CMAKE_SCP_LOG_LEVEL_OPTION=-DSCP_LOG_LEVEL=$(LOG_LEVEL)
+
+# Build directory
+BUILD_PATH ?= $(DEFAULT_BUILD_PATH)
+ifeq ($(BUILD_PATH),)
+    $(error "Invalid BUILD_PATH parameter. Aborting...")
+endif
+export BUILD_DIR = $(abspath $(BUILD_PATH))
+
+# Verbose mode: y/n
+export V ?= $(DEFAULT_VERBOSE)
+ifneq ($(V),n)
+    CMAKE_TOOL_LOG_LEVEL := --log-level=VERBOSE
+    CMAKE_BUILD_VERBOSE_OPTION := -v
+else
+    CMAKE_TOOL_LOG_LEVEL := --log-level=$(DEFAULT_CMAKE_TOOL_LOG_LEVEL)
+    MAKEFLAGS += --silent
+    MAKEFLAGS += --no-print-directory
+endif
+
+# Include debugger library: y/n
+DEBUGGER ?= $(DEFAULT_DEBUGGER)
+ifeq ($(DEBUGGER),y)
+    CMAKE_DEBUGGER_OPTION := -DSCP_ENABLE_DEBUG_UNIT=ON
+endif
+
+#
+# Products
+#
+PRODUCTS := $(shell ls $(PRODUCTS_DIR) 2>/dev/null)
+
+PRODUCT_INDEPENDENT_GOALS := clean help
+
+ifneq ($(filter-out $(PRODUCT_INDEPENDENT_GOALS), $(MAKECMDGOALS)),)
+    ifeq ($(PRODUCT),)
+        $(error "You must define PRODUCT. Aborting...")
+    endif
+
+    PRODUCT_DIR := $(PRODUCTS_DIR)/$(PRODUCT)
+
+    ifeq ($(wildcard $(PRODUCT_DIR)/product.mk),)
+        $(error "Missing product.mk in $(PRODUCT_DIR)")
+    endif
+
+    include $(PRODUCT_DIR)/product.mk
+
+    ifeq ($(BS_FIRMWARE_LIST),)
+        $(error "You must define BS_FIRMWARE_LIST in product.mk. Aborting...")
+    endif
+
+    FIRMWARE_TARGETS := $(addprefix firmware-, $(BS_FIRMWARE_LIST))
+
+PRODUCT_BUILD_PATH := $(BUILD_PATH)/$(BS_PRODUCT_NAME)/$(TOOLCHAIN)/$(MODE)
+
+define msg_start
+================================================================
+Arm SCP/MCP Software build System
+Platform    : $(BS_PRODUCT_NAME)
+Mode        : $(MODE)
+Firmware(s) : $(BS_FIRMWARE_LIST)
+================================================================
+endef
+
+    $(info $(msg_start))
+endif
+
+ifeq ($(MODE),debug)
+	CMAKE_BUILD_TYPE=-DCMAKE_BUILD_TYPE=Debug
+endif
+
+CMAKE_COMMAND_OPTION := $(CMAKE_BUILD_TYPE)
+CMAKE_COMMAND_OPTION += -DSCP_TOOLCHAIN="$(TOOLCHAIN)"
+
+ifeq ($(BUILD_SYSTEM), Ninja)
+    CMAKE_COMMAND_OPTION += -G $(BUILD_SYSTEM)
+endif
+
+CMAKE_COMMAND_OPTION += $(CMAKE_TOOL_LOG_LEVEL)
+CMAKE_COMMAND_OPTION += $(CMAKE_SCP_LOG_LEVEL_OPTION)
+CMAKE_COMMAND_OPTION += $(CMAKE_DEBUGGER_OPTION)
+
+#
+# Rules
+#
+.PHONY: help
+
+help:
+	@echo "Arm SCP/MCP Software build system"
+	@echo ""
+	@echo "Usage: make [PRODUCT=<name>] [OPTIONS] [TARGET]"
+	@echo ""
+	@echo "--------------------------------------------------------------------"
+	@echo "| Available Targets                                                |"
+	@echo "--------------------------------------------------------------------"
+	@echo "    all             Build all firmware defined by PRODUCT=<product>"
+	@echo "    clean           Remove all built products"
+	@echo "    help            Show this documentation"
+	@echo ""
+	@echo "--------------------------------------------------------------------"
+	@echo "| Product Selection                                                |"
+	@echo "--------------------------------------------------------------------"
+	@echo "    PRODUCT"
+	@echo "        Available products: $(PRODUCTS)"
+	@echo "        Select the target product to build. This is a required"
+	@echo "        parameter."
+	@echo ""
+	@echo "--------------------------------------------------------------------"
+	@echo "| Options                                                          |"
+	@echo "--------------------------------------------------------------------"
+	@echo "    BUILD_PATH"
+	@echo "        Value: <Full, user provided path>"
+	@echo "        Default: '$(DEFAULT_BUILD_PATH)'"
+	@echo "        Set the base directory used during the build process."
+	@echo ""
+	@echo "    MODE"
+	@echo "        Value: <debug | release>"
+	@echo "        Default: '$(DEFAULT_MODE)'"
+	@echo "        Choose between release and debug mode."
+	@echo ""
+	@echo "    O"
+	@echo "        Value: <Compiler-specific optimization level>"
+	@echo "        Default: <Compiler and MODE specific>"
+	@echo "        Set the desired level of optimization the compiler will use."
+	@echo ""
+	@echo "    V"
+	@echo "        Value: <y|n>"
+	@echo "        Default: $(DEFAULT_VERBOSE)"
+	@echo "        Enable or disable verbose mode for the build system."
+	@echo ""
+	@echo "    DEBUGGER"
+	@echo "        Value: <y|n>"
+	@echo "        Default: $(DEFAULT_DEBUGGER)"
+	@echo "        Include the debugger library."
+	@echo ""
+	@echo "    LOG_LEVEL"
+	@echo "        Value: <TRACE|INFO|WARN|ERROR|CRIT>"
+	@echo "        Default: $(LOG_LEVEL)"
+	@echo "        Filter log messages less important than this level."
+	@echo ""
+	@echo "    BUILD_SYSTEM"
+	@echo "        Value: <Make|Ninja>"
+	@echo "        Default: $(DEFAULT_BUILD_SYSTEM)"
+	@echo "        Specify CMake to generate GNU Make or Ninja build system."
+	@echo ""
+	@echo "    TOOLCHAIN"
+	@echo "        Value: <GNU|ArmClang>"
+	@echo "        Default: $(DEFAULT_TOOLCHAIN)"
+	@echo "        Specify Toolchain to build the firmware."
+	@echo ""
+
+
+.SECONDEXPANSION:
+
+.PHONY: all
+all: $(FIRMWARE_TARGETS)
+
+firmware-%: $(PRODUCT_BUILD_PATH)/$$@/CMakeCache.txt
+	$(CMAKE) --build $(<D)/ $(CMAKE_BUILD_VERBOSE_OPTION)
+
+.PRECIOUS: $(PRODUCT_BUILD_PATH)/firmware-%/CMakeCache.txt
+
+$(PRODUCT_BUILD_PATH)/firmware-%/CMakeCache.txt:  $(PRODUCT_DIR)/%/Firmware.cmake
+	$(RM) $(@D)
+	$(CMAKE) -B $(@D) -DSCP_FIRMWARE_SOURCE_DIR:PATH=$(PRODUCT_DIR)/$* $(CMAKE_COMMAND_OPTION)
+
+.PHONY: clean
+clean:
+	$(RM) $(BUILD_DIR)
+
+.PHONY: test
+test:
+	$(MAKE) -C $(FWK_DIR)/test all
---
+++ tools/ci_cmake.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+import check_copyright
+import check_doc
+import check_spacing
+import check_tabs
+import docker
+import os
+import signal
+import subprocess
+import sys
+
+from docker.errors import DockerException
+
+
+def banner(text):
+    columns = 80
+    title = " {} ".format(text)
+    print("\n\n{}".format(title.center(columns, "*")))
+
+
+def dockerize(client):
+    (image, _) = client.images.build(path="docker", target="dev", rm=True)
+
+    env = {"ARMLMD_LICENSE_FILE": os.environ["ARMLMD_LICENSE_FILE"]}
+
+    volumes = {
+        os.getcwd(): {
+            "bind": "/scp-firmware",
+            "mode": "rw",
+        }
+    }
+
+    container = client.containers.run(
+        image.id, __file__, environment=env, volumes=volumes, detach=True,
+        tty=True
+    )
+
+    sigint_handler = signal.getsignal(signal.SIGINT)
+    signal.signal(signal.SIGINT, lambda signal, _: container.kill(signal))
+
+    logs = container.attach(stdout=True, stderr=True, stream=True, logs=True)
+    for log in logs:
+        sys.stdout.buffer.write(log)
+
+    signal.signal(signal.SIGINT, sigint_handler)
+
+
+def main():
+    results = []
+
+    try:
+        client = docker.from_env()
+
+        banner("Spawning container")
+
+        try:
+            return dockerize(client)
+        except Exception as ex:
+            print(ex)
+
+            return 1
+    except DockerException:
+        pass
+
+    banner("Code validation")
+
+    result = check_copyright.main()
+    results.append(("Check copyright", result))
+
+    result = check_spacing.main()
+    results.append(("Check spacing", result))
+
+    result = check_tabs.main()
+    results.append(("Check tabs", result))
+
+    result = check_doc.main()
+    results.append(("Check doc", result))
+
+    banner("Build and run framework tests")
+
+    result = subprocess.call("CC=gcc make clean test", shell=True)
+    results.append(("Framework tests", result))
+
+    banner("Test building host product")
+
+    products = ['host', 'juno', 'rdv1', 'sgm775']
+
+    build_types = ['debug', 'release']
+    toolchains = ['GNU', 'ArmClang']
+
+    for product in products:
+        banner("Test building {} product".format(product))
+        for toolchain in toolchains:
+            if product == 'rcar' and toolchain == 'ArmClang':
+                continue
+            for build_type in build_types:
+
+                cmd = 'make -f Makefile.cmake '
+                cmd += 'PRODUCT={} TOOLCHAIN={} MODE={}'.format(product,
+                                                                toolchain,
+                                                                build_type)
+                result = subprocess.call(cmd, shell=True)
+                results.append(("Product {} build ({})".format(product,
+                                                               toolchain),
+                                result))
+
+    banner('Tests summary')
+
+    total_success = 0
+    for result in results:
+        if result[1] == 0:
+            total_success += 1
+            verbose_result = "Success"
+        else:
+            verbose_result = "Failed"
+        print("{}: {}".format(result[0], verbose_result))
+
+    assert total_success <= len(results)
+
+    print(
+        "{} / {} passed ({}% pass rate)".format(
+                                                total_success, len(results),
+                                                int(total_success *
+                                                    100 / len(results))
+        )
+    )
+
+    if total_success < len(results):
+        return 1
+    else:
+        return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
