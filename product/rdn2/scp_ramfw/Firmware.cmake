a9623de1ef7e3e16533315fade450db255f9642a
Girish Pathak <girish.pathak@arm.com>
2021-01-18 12:07:15 +0000

    cmake: product/rdn2
    This change adds CMake build support for rdn2 platform.
    
    Please note that firmware targets do not use libRTX_CM3.a
    or RTX_CM3.lib, instead in CMake build, the library
    is built using sources.
    
    Change-Id: I65fc3d014fe0482b623da96833d69b7e7f42f96f
    Signed-off-by: Girish Pathak <girish.pathak@arm.com>

---
+++ product/rdn2/scp_ramfw/Firmware.cmake
@@ -0,0 +1,62 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+#
+# Configure the build system.
+#
+
+set(SCP_FIRMWARE "rdn2-bl2")
+
+set(SCP_FIRMWARE_TARGET "rdn2-bl2")
+
+set(SCP_TOOLCHAIN_INIT "GNU")
+
+set(SCP_GENERATE_FLAT_BINARY_INIT TRUE)
+
+set(SCP_ENABLE_MULTITHREADING_INIT TRUE)
+
+set(SCP_ENABLE_NOTIFICATIONS_INIT TRUE)
+
+set(SCP_ENABLE_IPO_INIT FALSE)
+
+set(SCP_ARCHITECTURE "armv7-m")
+
+list(PREPEND SCP_MODULE_PATHS
+     "${CMAKE_CURRENT_LIST_DIR}/../module/platform_system")
+list(PREPEND SCP_MODULE_PATHS
+     "${CMAKE_SOURCE_DIR}/module/cmn700")
+list(PREPEND SCP_MODULE_PATHS
+     "${CMAKE_SOURCE_DIR}/module/apremap")
+
+# The order of the modules in the following list is the order in which the
+# modules are initialized, bound, started during the pre-runtime phase.
+# any change in the order will cause firmware initialization errors.
+
+list(APPEND SCP_MODULES "armv7m-mpu")
+list(APPEND SCP_MODULES "apremap")
+list(APPEND SCP_MODULES "sid")
+list(APPEND SCP_MODULES "system-info")
+list(APPEND SCP_MODULES "pcid")
+list(APPEND SCP_MODULES "pl011")
+list(APPEND SCP_MODULES "pik-clock")
+list(APPEND SCP_MODULES "css-clock")
+list(APPEND SCP_MODULES "clock")
+list(APPEND SCP_MODULES "gtimer")
+list(APPEND SCP_MODULES "timer")
+list(APPEND SCP_MODULES "cmn700")
+list(APPEND SCP_MODULES "apcontext")
+list(APPEND SCP_MODULES "ppu-v1")
+list(APPEND SCP_MODULES "system-power")
+list(APPEND SCP_MODULES "mhu2")
+list(APPEND SCP_MODULES "smt")
+list(APPEND SCP_MODULES "scmi")
+list(APPEND SCP_MODULES "sds")
+list(APPEND SCP_MODULES "system-pll")
+list(APPEND SCP_MODULES "power-domain")
+list(APPEND SCP_MODULES "scmi-power-domain")
+list(APPEND SCP_MODULES "scmi-system-power")
+list(APPEND SCP_MODULES "platform-system")
