aa1448c09b68ea18338eb882609f26eca2267d74
Shriram K <shriram.k@arm.com>
2021-03-01 16:35:01 +0530

    product/rdv1: add mcp_platform module for rdv1
    Add a initial implementation of the MCP platform module that indicates
    the completion of the MCP RAM boot stage. This module is expected to get
    additional functionality in subsequent additions to the MCP RAM
    firmware.
    
    Signed-off-by: Shriram K <shriram.k@arm.com>
    Change-Id: I8a60bb49a216e98c3bceb6510702adee7268459d

---
+++ product/rdv1/module/mcp_platform/src/mod_mcp_platform.c
@@ -0,0 +1,42 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_id.h>
+#include <fwk_log.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+#include <fwk_status.h>
+
+#define MOD_NAME "[MCP_PLATFORM] "
+
+static int mod_mcp_platform_init(
+    fwk_id_t module_id,
+    unsigned int element_count,
+    const void *unused)
+{
+    /* No elements support */
+    if (element_count > 0) {
+        return FWK_E_DATA;
+    }
+
+    return FWK_SUCCESS;
+}
+
+static int mod_mcp_platform_start(fwk_id_t id)
+{
+    FWK_LOG_INFO(MOD_NAME "MCP RAM firmware initialized\n");
+    return FWK_SUCCESS;
+}
+
+const struct fwk_module module_mcp_platform = {
+    .name = "mcp_platform",
+    .type = FWK_MODULE_TYPE_SERVICE,
+    .init = mod_mcp_platform_init,
+    .start = mod_mcp_platform_start,
+};
+
+const struct fwk_module_config config_mcp_platform = { 0 };
