5c80e77745d0343786acafbb6f6c9c9e358ed12c
Shriram K <shriram.k@arm.com>
2021-03-01 16:25:40 +0530

    product/rdv1: add config data for pl011 UART module in mcp ramfw
    PL011 controller is used as a console port for debug and log messages.
    Add configuration data of this controller including base address and
    input clock frequency for the PL011 module to use.
    
    Signed-off-by: Shriram K <shriram.k@arm.com>
    Change-Id: I6f738e758623cdf0495f344b10dec698881a77ca

--- product/rdv1/include/mcp_mmap.h
+++ product/rdv1/include/mcp_mmap.h
@@ -12,5 +12,7 @@
 #define MCP_ITC_RAM_BASE        0x00800000
 #define MCP_SOC_EXPANSION1_BASE 0x01000000
 #define MCP_DTC_RAM_BASE        0x20000000
+#define MCP_UART1_BASE          0x4C002000
+#define MCP_UART2_BASE          0x4C003000
 
 #endif /* MCP_MMAP_H */
