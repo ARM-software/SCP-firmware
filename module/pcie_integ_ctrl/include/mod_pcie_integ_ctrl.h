fead18bcaa8b63d9081fe7556e7578b493de4e03
Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
2021-03-16 15:44:49 +0530

    module: add pcie integration control register module
    Reference design platforms that support multiple PCIe controllers use a
    PCIe integration controller to route the ECAM and MMIO accesses to a
    PCIe controller. So add a module that supports the configuration of the
    PCIe integration controller.
    
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
    Signed-off-by: Zakaria Zahi <zakaria.zahi@arm.com>
    Change-Id: Ia4b49807bcedc46a7852a13c2bf3fb8ff57344d9

---
+++ module/pcie_integ_ctrl/include/mod_pcie_integ_ctrl.h
@@ -0,0 +1,94 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     PCIe Integration Control register intialization module for Neoverse
+ *     reference design platforms.
+ */
+
+#ifndef MOD_PCIE_INTEG_CTRL_H
+#define MOD_PCIE_INTEG_CTRL_H
+
+#include <fwk_id.h>
+
+#include <stddef.h>
+#include <stdint.h>
+
+/*!
+ * \addtogroup GroupModules Modules
+ * \{
+ */
+
+/*!
+ * \ingroup GroupModules Modules
+ * \defgroup GroupModulePCIeIntegCtrl PCIe integ ctrl
+ *
+ * \brief PCIe integration control module.
+ *
+ * \details This module implements PCIe integration control register
+ *    initialization.
+ * \{
+ */
+
+/*!
+ * \brief ECAM1, 32-bit and 64-bit MMIO space start and end address
+ */
+struct pcie_ecam_mmio_mmap {
+    /*! Valid flag to indicate if the mapping is valid to program */
+    bool valid;
+
+    /*! ECAM1 start address */
+    uint64_t ecam1_start_addr;
+
+    /*! ECAM1 end address */
+    uint64_t ecam1_end_addr;
+
+    /*! 32-bit MMIO space start address */
+    uint64_t mmiol_start_addr;
+
+    /*! 32-bit MMIO space end address */
+    uint64_t mmiol_end_addr;
+
+    /*! 64-bit MMIO space start address */
+    uint64_t mmioh_start_addr;
+
+    /*! 64-bit MMIO space end address */
+    uint64_t mmioh_end_addr;
+};
+
+/*!
+ * \brief PCIe Integration control registers configuration data which contains
+ * mapping of x4_0, x4_1, x8 and x16.
+ */
+struct mod_pcie_integ_ctrl_config {
+    /*! Register base of IO Macro instance */
+    uintptr_t reg_base;
+
+    /*! ECAM and MMIO memory map for x4_0 */
+    struct pcie_ecam_mmio_mmap x4_0_ecam_mmio_mmap;
+
+    /*! ECAM and MMIO memory map for x4_1 */
+    struct pcie_ecam_mmio_mmap x4_1_ecam_mmio_mmap;
+
+    /*! ECAM and MMIO memory map for x8 */
+    struct pcie_ecam_mmio_mmap x8_ecam_mmio_mmap;
+
+    /*! ECAM and MMIO memory map for x16 */
+    struct pcie_ecam_mmio_mmap x16_ecam_mmio_mmap;
+
+    /*! Identifier of the clock this module depends on */
+    fwk_id_t clock_id;
+};
+
+/*!
+ * \}
+ */
+
+/*!
+ * \}
+ */
+
+#endif /* MOD_PCIE_INTEG_CTRL_H */
---
+++ module/pcie_integ_ctrl/src/pcie_integ_ctrl_reg.h
@@ -0,0 +1,61 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Register definitions for PCIe integration control module.
+ */
+
+#ifndef PCIE_INTEG_CTRL_REG
+#define PCIE_INTEG_CTRL_REG
+
+#include <mod_pcid.h>
+
+#include <fwk_macros.h>
+
+#include <stdint.h>
+
+struct pcie_ctrl_reg_set {
+    FWK_RW uint32_t ECAM1_START_ADDR;
+    FWK_RW uint32_t ECAM1_END_ADDR;
+    FWK_RW uint32_t MMIOL_START_ADDR;
+    FWK_RW uint32_t MMIOL_END_ADDR;
+    FWK_RW uint32_t MMIOH_START_ADDR;
+    FWK_RW uint32_t MMIOH_END_ADDR;
+    FWK_RW uint32_t MMIOH2L_TR_START_ADDR;
+    FWK_RW uint32_t MMIOH2L_TR_END_ADDR;
+    FWK_RW uint32_t CFG_START_ADDR;
+    FWK_RW uint32_t CFG_END_ADDR;
+    FWK_RW uint32_t ECAM2_START_ADDR;
+    FWK_RW uint32_t ECAM2_END_ADDR;
+    FWK_RW uint32_t ECAM2_ADDR_CTRL;
+};
+
+struct pcie_ctrl_reg {
+    struct pcie_ctrl_reg_set pcie_ctrl_x4_0;
+    uint8_t RESERVED0[0x100 - 0x34];
+    struct pcie_ctrl_reg_set pcie_ctrl_x4_1;
+    uint8_t RESERVED1[0x200 - 0x134];
+    struct pcie_ctrl_reg_set pcie_ctrl_x8;
+    uint8_t RESERVED2[0x300 - 0x234];
+    struct pcie_ctrl_reg_set pcie_ctrl_x16;
+    uint8_t RESERVED3[0x400 - 0x334];
+    uint32_t NCI_PMU_CONS_INT_STATUS;
+    uint8_t RESERVED4[0xFD0 - 0x404];
+    struct mod_pcid_registers pcid;
+};
+
+#define PCIE_INTEG_CTRL_REG_ADDR_POS 1
+#define PCIE_INTEG_CTRL_REG_EN       1
+
+#define PCIE_INTEG_CTRL_REG_START_ADDR(addr) \
+    (((addr) >> 20) << PCIE_INTEG_CTRL_REG_ADDR_POS)
+
+#define PCIE_INTEG_CTRL_REG_START_ADDR_EN(addr) \
+    (PCIE_INTEG_CTRL_REG_START_ADDR(addr) | PCIE_INTEG_CTRL_REG_EN)
+
+#define PCIE_INTEG_CTRL_REG_END_ADDR(addr) ((addr) >> 20)
+
+#endif /* PCIE_INTEG_CTRL_REG */
