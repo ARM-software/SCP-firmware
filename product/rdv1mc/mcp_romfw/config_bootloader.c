6e37db287fdc49c6f7ba7d16ca1c2d98a65e85c5
Shriram K <shriram.k@arm.com>
2021-03-22 10:26:39 +0530

    product/rdv1mc: add config data for mcp romfw bootloader module
    The bootloader module is used to load the MCP RAM firmware from the NOR
    flash to MCP RAM. Provide the base address and size of the firmware
    image to be loaded from and the base address of the location to which
    the firmware is loaded to. In addition to this, some of the macros are
    renamed to correspondingly match with the macro names used in the SCP
    firmware of RD-V1-MC platform.
    
    Signed-off-by: Shriram K <shriram.k@arm.com>
    Change-Id: Id76b36ad558f971686b7811cd4215f0ef5b5573d

--- product/rdv1mc/mcp_romfw/config_bootloader.c
+++ product/rdv1mc/mcp_romfw/config_bootloader.c
@@ -5,6 +5,19 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include "mcp_software_mmap.h"
+
+#include <mod_bootloader.h>
+
 #include <fwk_module.h>
 
-struct fwk_module_config config_bootloader = {};
+static const struct mod_bootloader_config bootloader_module_config = {
+    .source_base = MCP_RAMFW_IMAGE_FLASH_BASE,
+    .source_size = MCP_RAMFW_IMAGE_FLASH_SIZE,
+    .destination_base = MCP_ITC_RAM_BASE,
+    .destination_size = MCP_RAMFW_IMAGE_FLASH_SIZE,
+};
+
+struct fwk_module_config config_bootloader = {
+    .data = &bootloader_module_config,
+};
