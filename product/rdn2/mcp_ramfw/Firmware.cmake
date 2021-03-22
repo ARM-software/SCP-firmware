3a15040c55e19ebfa8fa533556b5a9b8f1c99d7f
Shriram K <shriram.k@arm.com>
2021-03-22 08:47:31 +0530

    product/rdn2: add build support for mcp ram firmware
    Add cmake and make build support for RD-N2 MCP RAM firmware.
    
    Change-Id: I6997cfe9c56cb3703f66bc6fa25be42d232bfb01
    Signed-off-by: Shriram K <shriram.k@arm.com>

---
+++ product/rdn2/mcp_ramfw/Firmware.cmake
@@ -0,0 +1,44 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+#
+# Configure the build system.
+#
+
+set(SCP_FIRMWARE "rdn2-mcp-bl2")
+
+set(SCP_ENABLE_NEWLIB_NANO FALSE)
+
+set(SCP_FIRMWARE_TARGET "rdn2-mcp-bl2")
+
+set(SCP_TOOLCHAIN_INIT "GNU")
+
+set(SCP_GENERATE_FLAT_BINARY_INIT TRUE)
+
+set(SCP_ENABLE_MULTITHREADING_INIT TRUE)
+
+set(SCP_ENABLE_NOTIFICATIONS_INIT TRUE)
+
+set(SCP_ENABLE_IPO_INIT FALSE)
+
+set(SCP_ARCHITECTURE "armv7-m")
+
+list(PREPEND SCP_MODULE_PATHS
+    "${CMAKE_CURRENT_LIST_DIR}/../module/mcp_platform")
+
+
+# The order of the modules in the following list is the order in which the
+# modules are initialized, bound, started during the pre-runtime phase.
+# any change in the order will cause firmware initialization errors.
+
+list(APPEND SCP_MODULES "armv7m-mpu")
+list(APPEND SCP_MODULES "pl011")
+list(APPEND SCP_MODULES "clock")
+list(APPEND SCP_MODULES "pcid")
+list(APPEND SCP_MODULES "timer")
+list(APPEND SCP_MODULES "gtimer")
+list(APPEND SCP_MODULES "mcp-platform")
