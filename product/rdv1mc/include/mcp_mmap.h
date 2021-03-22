966082cad5a0fffc287b3af47b83a09b9f3a2f4a
Shriram K <shriram.k@arm.com>
2021-03-22 10:26:39 +0530

    product/rdv1mc: add config data for mcp romfw bootloader module
    The bootloader module is used to load the MCP RAM firmware from the NOR
    flash to MCP RAM. Provide the base address and size of the firmware
    image to be loaded from and the base address of the location to which
    the firmware is loaded to. In addition to this, some of the macros are
    renamed to correspondingly match with the macro names used in the SCP
    firmware of RD-V1-MC platform.
    
    Signed-off-by: Shriram K <shriram.k@arm.com>
    Change-Id: Id76b36ad558f971686b7811cd4215f0ef5b5573d

---
+++ product/rdv1mc/include/mcp_mmap.h
@@ -0,0 +1,16 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MCP_MMAP_H
+#define MCP_MMAP_H
+
+#define MCP_BOOT_ROM_BASE       0x00000000
+#define MCP_ITC_RAM_BASE        0x00800000
+#define MCP_SOC_EXPANSION1_BASE 0x01000000
+#define MCP_DTC_RAM_BASE        0x20000000
+
+#endif /* MCP_MMAP_H */
--- product/rdv1mc/include/mcp_software_mmap.h
+++ product/rdv1mc/include/mcp_software_mmap.h
@@ -5,15 +5,21 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#ifndef MCP_PLATFORM_MMAP_MCP_H
-#define MCP_PLATFORM_MMAP_MCP_H
+#ifndef MCP_SOFTWARE_MMAP_MCP_H
+#define MCP_SOFTWARE_MMAP_MCP_H
 
-#define MCP_ROM_BASE  0x00000000
-#define MCP_RAM0_BASE 0x00800000
-#define MCP_RAM1_BASE 0x20000000
+#include "mcp_mmap.h"
 
-#define MCP_ROM_SIZE  (64 * 1024)
-#define MCP_RAM0_SIZE (64 * 1024)
-#define MCP_RAM1_SIZE (64 * 1024)
+#include <fwk_macros.h>
 
-#endif /* MCP_PLATFORM_MMAP_MCP_H */
+/* MCP ROM and RAM firmware size loaded on main memory */
+#define MCP_BOOT_ROM_SIZE (64 * 1024)
+#define MCP_DTC_RAM_SIZE  (256 * 1024)
+#define MCP_ITC_RAM_SIZE  (256 * 1024)
+
+/* MCP RAM firmware base and size on the flash */
+#define MCP_NOR0_FLASH_BASE        (MCP_SOC_EXPANSION1_BASE + 0x07000000)
+#define MCP_RAMFW_IMAGE_FLASH_BASE (MCP_NOR0_FLASH_BASE + 0x03F80000)
+#define MCP_RAMFW_IMAGE_FLASH_SIZE (256 * FWK_KIB)
+
+#endif /* MCP_SOFTWARE_MMAP_MCP_H */
