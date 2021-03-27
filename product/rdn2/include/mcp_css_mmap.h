67189f2c4d65188ff1517c19f100f688c6dca067
Shriram K <shriram.k@arm.com>
2021-03-27 11:40:09 +0530

    product/rdn2: add generic timer config data for mcp ramfw
    The configuration data for generic timer driver includes the base
    addresses of timer register, counter register and control register along
    with the initial frequency and the id of clock device on which the timer
    depends.
    
    Change-Id: I895aed77c1db98346268262f2577fee84b6e20ef
    Signed-off-by: Shriram K <shriram.k@arm.com>

---
+++ product/rdn2/include/mcp_css_mmap.h
@@ -0,0 +1,16 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MCP_CSS_MMAP_H
+#define MCP_CSS_MMAP_H
+
+#include "mcp_mmap.h"
+
+#define MCP_REFCLK_CNTCTL_BASE   (MCP_PERIPHERAL_BASE + 0x0000)
+#define MCP_REFCLK_CNTBASE0_BASE (MCP_PERIPHERAL_BASE + 0x1000)
+
+#endif /* MCP_CSS_MMAP_H */
