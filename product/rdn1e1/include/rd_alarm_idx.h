03d1d2a5a28b7d3a777019e95ad22e1fbe8d70ce
Shriram K <shriram.k@arm.com>
2021-03-29 13:21:43 +0530

    product/rdn1e1: add timer HAL config data for mcp ramfw
    Provide the configuration data for timer HAL which includes
    the id for the timer element and the IRQ number.
    
    Signed-off-by: Shriram K <shriram.k@arm.com>
    Change-Id: I4e7747ce4637893de2fd9724c5bef79766e1fb62

---
+++ product/rdn1e1/include/rd_alarm_idx.h
@@ -0,0 +1,11 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/* Alarm indices */
+enum rd_alarm_idx {
+    RD_ALARM_IDX_COUNT,
+};
