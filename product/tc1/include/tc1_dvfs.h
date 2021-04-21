084f5323343af7feadd11a5ac7f9f8b5f7624d1a
Usama Arif <usama.arif@arm.com>
2021-04-21 17:04:17 +0100

    product/tc1: add configuration files for enabling DVFS
    Provide configuration data for DVFS, PSU and scmi_perf for changing voltage
    and frequency. DVFS uses a mock psu to set the voltage levels.
    
    Signed-off-by: Usama Arif <usama.arif@arm.com>
    Change-Id: I59b05f4c95b29fefeb3c2a83c54806270ace2fc1

---
+++ product/tc1/include/tc1_dvfs.h
@@ -0,0 +1,21 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Definitions for DVFS module configuration.
+ */
+
+#ifndef TC1_DVFS_H
+#define TC1_DVFS_H
+
+enum dvfs_element_idx {
+    DVFS_ELEMENT_IDX_KLEIN,
+    DVFS_ELEMENT_IDX_MAKALU,
+    DVFS_ELEMENT_IDX_MAKALU_ELP,
+    DVFS_ELEMENT_IDX_COUNT
+};
+
+#endif /* TC1_DVFS_H */
---
+++ product/tc1/include/tc1_mock_psu.h
@@ -0,0 +1,21 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Definitions for mock PSU module configuration.
+ */
+
+#ifndef TC1_MOCK_PSU_H
+#define TC1_MOCK_PSU_H
+
+enum mock_psu_id {
+    MOCK_PSU_ELEMENT_IDX_KLEIN,
+    MOCK_PSU_ELEMENT_IDX_MAKALU,
+    MOCK_PSU_ELEMENT_IDX_MAKALU_ELP,
+    MOCK_PSU_ELEMENT_IDX_COUNT,
+};
+
+#endif /* TC1_MOCK_PSU_H */
