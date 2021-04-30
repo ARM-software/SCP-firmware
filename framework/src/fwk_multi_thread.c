31a26fbef41d9f5c188a78c442e3ea58cbc11159
Leandro Belli <leandro.belli@arm.com>
2021-04-30 12:09:38 +0100

    fwk: Fix required MISRA defects rule 11.9
    According to MISRA rule 11.9 literal "0" shall not be
    used as null pointer constant.
    
    Signed-off-by: Leandro Belli <leandro.belli@arm.com>
    Change-Id: I9350246a55d78f8c1888d69523f50b0eb093d15f

--- framework/src/fwk_multi_thread.c
+++ framework/src/fwk_multi_thread.c
@@ -481,7 +481,7 @@ int execute_signal_handler(fwk_id_t target_id, fwk_id_t signal_id)
     int status;
 
     module = fwk_module_get_ctx(target_id)->desc;
-    if (!module->process_signal) {
+    if (module->process_signal == NULL) {
         return FWK_E_PARAM;
     }
     status = module->process_signal(target_id, signal_id);
