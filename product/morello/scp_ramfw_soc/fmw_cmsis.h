73c05fdcd4977a76cac9a558722a619a63532bac
Manoj Kumar <manoj.kumar3@arm.com>
2021-06-21 16:05:52 +0100

    morello/morello_scp_soc: add cmsis RTX support
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>
    Change-Id: Ie96d91bbc946934b16244f65e2a41bcbca96f192

---
+++ product/morello/scp_ramfw_soc/fmw_cmsis.h
@@ -0,0 +1,13 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef FMW_CMSIS_H
+#define FMW_CMSIS_H
+
+#include <fmw_cmsis_scp.h>
+
+#endif /* FMW_CMSIS_H */
