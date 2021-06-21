7dd890be1b8ebc9bfbcd68812010951cba83c28c
Manoj Kumar <manoj.kumar3@arm.com>
2021-06-21 16:03:43 +0100

    morello/scp_ramfw_soc: add pl011 config data
    Add configuration data for PL011 UART module which is
    used as a console for firmware logs.
    
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>
    Change-Id: Iad75b1ca83072b9e04b99ae65a3a88847fbca4d3

---
+++ product/morello/scp_ramfw_soc/config_pl011.c
@@ -0,0 +1,36 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "morello_scp_mmap.h"
+#include "morello_system_clock.h"
+
+#include <mod_pl011.h>
+
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_module.h>
+
+/*
+ * PL011 module
+ */
+struct fwk_module_config config_pl011 = {
+    .elements = FWK_MODULE_STATIC_ELEMENTS({
+        [0] = {
+            .name = "SCP-UART",
+            .data =
+                &(struct mod_pl011_element_cfg){
+                    .reg_base = SCP_UART_BASE,
+                    .baud_rate_bps = 115200,
+                    .clock_rate_hz = CLOCK_RATE_REFCLK,
+                    .clock_id = FWK_ID_NONE_INIT,
+                    .pd_id = FWK_ID_NONE_INIT,
+                },
+        },
+
+        [1] = { 0 },
+    }),
+};
