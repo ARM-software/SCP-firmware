5bdffaa87ea0be28e07519a4c502cc56c30bd5ad
Manoj Kumar <manoj.kumar3@arm.com>
2021-06-21 16:03:43 +0100

    morello/scp_ramfw_soc: add power management config
    Define and add configurations for system power domains,
    PPUs, etc.
    
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>
    Change-Id: I9f93d80275ad58922411bb845ded5adeaf49625f

---
+++ product/morello/scp_ramfw_soc/config_ppu_v1.c
@@ -0,0 +1,213 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "config_power_domain.h"
+#include "morello_core.h"
+#include "morello_scc_reg.h"
+#include "morello_scp_mmap.h"
+#include "morello_scp_pik.h"
+
+#include <mod_cmn_skeena.h>
+#include <mod_power_domain.h>
+#include <mod_ppu_v1.h>
+
+#include <fwk_assert.h>
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_interrupt.h>
+#include <fwk_macros.h>
+#include <fwk_mm.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Maximum PPU core name size including the null terminator */
+#define PPU_CORE_NAME_SIZE 16
+
+/* Maximum PPU cluster name size including the null terminator */
+#define PPU_CLUS_NAME_SIZE 8
+
+/* Identifiers for the static table */
+enum ppu_v1_static_element_idx {
+    PPU_V1_ELEMENT_IDX_SYS0,
+    PPU_V1_ELEMENT_IDX_SYS1,
+    PPU_V1_ELEMENT_IDX_GPUTOP0,
+    PPU_V1_ELEMENT_IDX_DPUTOP0,
+    PPU_V1_ELEMENT_IDX_COUNT
+};
+
+/* Lookup table for translating cluster indicies into CMN600 node IDs */
+static const unsigned int cluster_idx_to_node_id[2] = { 192, 140 };
+
+/* Module configuration data */
+static struct mod_ppu_v1_config ppu_v1_config_data = {
+    .pd_notification_id = FWK_ID_NOTIFICATION_INIT(
+        FWK_MODULE_IDX_POWER_DOMAIN,
+        MOD_PD_NOTIFICATION_IDX_POWER_STATE_TRANSITION),
+};
+
+static struct fwk_element
+                ppu_v1_static_element_table[PPU_V1_ELEMENT_IDX_COUNT] = {
+    [PPU_V1_ELEMENT_IDX_SYS0] = {
+        .name = "SYS0",
+        .data = &((struct mod_ppu_v1_pd_config) {
+            .pd_type = MOD_PD_TYPE_SYSTEM,
+            .ppu.reg_base = SCP_PPU_SYS0_BASE,
+            .observer_id = FWK_ID_NONE_INIT,
+            .default_power_on = true,
+        }),
+    },
+    [PPU_V1_ELEMENT_IDX_SYS1] = {
+        .name = "SYS1",
+        .data = &((struct mod_ppu_v1_pd_config) {
+            .pd_type = MOD_PD_TYPE_SYSTEM,
+            .ppu.reg_base = SCP_PPU_SYS1_BASE,
+            .observer_id = FWK_ID_NONE_INIT,
+            .default_power_on = true,
+        }),
+    },
+    [PPU_V1_ELEMENT_IDX_GPUTOP0] = {
+        .name = "GPUTOP0",
+        .data = &((struct mod_ppu_v1_pd_config) {
+            .pd_type = MOD_PD_TYPE_DEVICE,
+            .ppu.reg_base = SCP_PPU_GPU_BASE,
+            .observer_id = FWK_ID_NONE_INIT,
+            .default_power_on = true,
+        }),
+    },
+    [PPU_V1_ELEMENT_IDX_DPUTOP0] = {
+        .name = "DPUTOP0",
+        .data = &((struct mod_ppu_v1_pd_config) {
+            .pd_type = MOD_PD_TYPE_DEVICE,
+            .ppu.reg_base = SCP_PPU_DPU_BASE,
+            .observer_id = FWK_ID_NONE_INIT,
+            .default_power_on = true,
+        }),
+    },
+};
+
+static const struct fwk_element *ppu_v1_get_element_table(fwk_id_t module_id)
+{
+    struct fwk_element *element_table, *element;
+    struct mod_ppu_v1_pd_config *pd_config_table, *pd_config;
+    unsigned int core_idx;
+    unsigned int cluster_idx;
+    unsigned int core_count;
+    unsigned int cluster_count;
+    unsigned int core_element_count = 0;
+
+    core_count = morello_core_get_core_count();
+    cluster_count = morello_core_get_cluster_count();
+
+    fwk_assert(cluster_count == FWK_ARRAY_SIZE(cluster_idx_to_node_id));
+
+    /*
+     * Allocate element descriptors based on:
+     *   Number of cores
+     *   + Number of cluster descriptors
+     *   + Number of system power domain descriptors
+     *   + 1 terminator descriptor
+     */
+    element_table = fwk_mm_calloc(
+        core_count + cluster_count +
+            FWK_ARRAY_SIZE(ppu_v1_static_element_table) + 1,
+        sizeof(struct fwk_element));
+    if (element_table == NULL) {
+        return NULL;
+    }
+
+    pd_config_table = fwk_mm_calloc(
+        core_count + cluster_count, sizeof(struct mod_ppu_v1_pd_config));
+    if (pd_config_table == NULL) {
+        return NULL;
+    }
+
+    for (cluster_idx = 0; cluster_idx < cluster_count; cluster_idx++) {
+        for (core_idx = 0;
+             core_idx < morello_core_get_core_per_cluster_count(cluster_idx);
+             core_idx++) {
+            element = &element_table[core_element_count];
+            pd_config = &pd_config_table[core_element_count];
+
+            element->name = fwk_mm_alloc(PPU_CORE_NAME_SIZE, 1);
+            if (element->name == NULL) {
+                return NULL;
+            }
+
+            (void)snprintf(
+                (char *)element->name,
+                PPU_CORE_NAME_SIZE,
+                "CLUS%uCORE%u",
+                (uint8_t)cluster_idx,
+                (uint8_t)core_idx);
+
+            element->data = pd_config;
+
+            pd_config->pd_type = MOD_PD_TYPE_CORE;
+            pd_config->ppu.reg_base = SCP_PPU_CORE_BASE(cluster_idx, core_idx);
+            pd_config->ppu.irq = FWK_INTERRUPT_NONE;
+            pd_config->cluster_id = FWK_ID_ELEMENT(
+                FWK_MODULE_IDX_PPU_V1, (core_count + cluster_idx));
+            pd_config->observer_id = FWK_ID_NONE;
+            core_element_count++;
+        }
+
+        element = &element_table[core_count + cluster_idx];
+        pd_config = &pd_config_table[core_count + cluster_idx];
+
+        element->name = fwk_mm_alloc(PPU_CLUS_NAME_SIZE, 1);
+        if (element->name == NULL) {
+            return NULL;
+        }
+
+        (void)snprintf(
+            (char *)element->name,
+            PPU_CLUS_NAME_SIZE,
+            "CLUS%u",
+            (uint8_t)cluster_idx);
+
+        element->data = pd_config;
+
+        pd_config->pd_type = MOD_PD_TYPE_CLUSTER;
+        pd_config->ppu.reg_base = SCP_PPU_CLUSTER_BASE(cluster_idx);
+        pd_config->ppu.irq = FWK_INTERRUPT_NONE;
+        pd_config->observer_id = fwk_module_id_cmn_skeena;
+        pd_config->observer_api = FWK_ID_API(
+            FWK_MODULE_IDX_CMN_SKEENA, MOD_CMN_SKEENA_API_IDX_PPU_OBSERVER);
+        pd_config->post_ppu_on_param =
+            (void *)&cluster_idx_to_node_id[cluster_idx];
+        pd_config->opmode = (SCC->BOOT_GPR1 & SCC_BOOTGPR1_L3_CACHE_EN_MASK) ?
+            PPU_V1_OPMODE_04 :
+            PPU_V1_OPMODE_00;
+    }
+
+    (void)memcpy(
+        &element_table[core_count + cluster_count],
+        ppu_v1_static_element_table,
+        sizeof(ppu_v1_static_element_table));
+
+    /*
+     * Configure pd_source_id with the SYSTOP identifier from the power domain
+     * module which is dynamically defined based on the number of cores.
+     */
+    ppu_v1_config_data.pd_source_id = fwk_id_build_element_id(
+        fwk_module_id_power_domain, PD_SINGLE_CHIP_IDX_SYSTOP0);
+
+    return element_table;
+}
+
+/*
+ * Power module configuration data
+ */
+const struct fwk_module_config config_ppu_v1 = {
+    .elements = FWK_MODULE_DYNAMIC_ELEMENTS(ppu_v1_get_element_table),
+    .data = &ppu_v1_config_data,
+};
