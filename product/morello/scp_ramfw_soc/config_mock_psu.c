5c2a6b43720169771a247fa13527ee0c133199b9
Manoj Kumar <manoj.kumar3@arm.com>
2021-06-21 16:03:43 +0100

    morello/scp_ramfw_soc: add support for dvfs/scmi_perf
    Enable DVFS (frequency scaling only). Voltage scaling is
    not supported at the moment and will be enabled at a later
    stage.
    
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>
    Change-Id: I1fa437ccd8de4c88f23fc8e29156fc2e831505ec

---
+++ product/morello/scp_ramfw_soc/config_mock_psu.c
@@ -0,0 +1,67 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <config_mock_psu.h>
+
+#include <mod_mock_psu.h>
+
+#include <fwk_element.h>
+#include <fwk_module.h>
+
+static const struct fwk_element
+    element_table[CONFIG_MOCK_PSU_ELEMENT_IDX_COUNT + 1] = {
+            [CONFIG_MOCK_PSU_ELEMENT_IDX_CLUS0] = {
+                .name = "CLUSTER_0_CPUS",
+                .data = &(const struct mod_mock_psu_element_cfg) {
+                    .async_alarm_id = FWK_ID_NONE_INIT,
+                    .async_alarm_api_id = FWK_ID_NONE_INIT,
+
+                    .async_response_id = FWK_ID_NONE_INIT,
+                    .async_response_api_id = FWK_ID_NONE_INIT,
+
+                    .default_enabled = true,
+                    .default_voltage = 800,
+                },
+            },
+            [CONFIG_MOCK_PSU_ELEMENT_IDX_CLUS1] = {
+                .name = "CLUSTER_1_CPUS",
+                .data = &(const struct mod_mock_psu_element_cfg) {
+                    .async_alarm_id = FWK_ID_NONE_INIT,
+                    .async_alarm_api_id = FWK_ID_NONE_INIT,
+
+                    .async_response_id = FWK_ID_NONE_INIT,
+                    .async_response_api_id = FWK_ID_NONE_INIT,
+
+                    .default_enabled = true,
+                    .default_voltage = 800,
+                },
+            },
+            [CONFIG_MOCK_PSU_ELEMENT_IDX_GPU] = {
+                .name = "GPU",
+                .data = &(const struct mod_mock_psu_element_cfg) {
+                    .async_alarm_id = FWK_ID_NONE_INIT,
+                    .async_alarm_api_id = FWK_ID_NONE_INIT,
+
+                    .async_response_id = FWK_ID_NONE_INIT,
+                    .async_response_api_id = FWK_ID_NONE_INIT,
+
+                    .default_enabled = true,
+                    .default_voltage = 1000,
+                },
+            },
+            { 0 }
+};
+
+static const struct fwk_element *get_element_table(fwk_id_t module_id)
+{
+    return element_table;
+}
+
+struct fwk_module_config config_mock_psu = {
+    .elements = FWK_MODULE_DYNAMIC_ELEMENTS(get_element_table),
+    .data = NULL,
+};
