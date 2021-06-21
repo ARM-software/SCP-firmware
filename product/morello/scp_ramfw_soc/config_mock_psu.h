97f95af1f95ee14ecbece3fd9439608fe0ff61a0
Manoj Kumar <manoj.kumar3@arm.com>
2021-06-21 16:03:43 +0100

    morello/scp_ramfw_soc: add support for dvfs/scmi_perf
    Enable DVFS (frequency scaling only). Voltage scaling is
    not supported at the moment and will be enabled at a later
    stage.
    
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>
    Change-Id: I1fa437ccd8de4c88f23fc8e29156fc2e831505ec

---
+++ product/morello/scp_ramfw_soc/config_mock_psu.h
@@ -0,0 +1,18 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef CONFIG_MOCK_PSU_H
+#define CONFIG_MOCK_PSU_H
+
+enum config_mock_psu_element_idx {
+    CONFIG_MOCK_PSU_ELEMENT_IDX_CLUS0,
+    CONFIG_MOCK_PSU_ELEMENT_IDX_CLUS1,
+    CONFIG_MOCK_PSU_ELEMENT_IDX_GPU,
+    CONFIG_MOCK_PSU_ELEMENT_IDX_COUNT,
+};
+
+#endif /* CONFIG_MOCK_PSU_H */
---
+++ product/morello/scp_ramfw_soc/config_psu.h
@@ -0,0 +1,18 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef CONFIG_PSU_H
+#define CONFIG_PSU_H
+
+enum config_psu_element_idx {
+    CONFIG_PSU_ELEMENT_IDX_CLUS0,
+    CONFIG_PSU_ELEMENT_IDX_CLUS1,
+    CONFIG_PSU_ELEMENT_IDX_GPU,
+    CONFIG_PSU_ELEMENT_IDX_COUNT,
+};
+
+#endif /* CONFIG_PSU_H */
