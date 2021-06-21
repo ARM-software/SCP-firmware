3a79435adac8a1a4bad54127a8fbfa28352d47ed
Manoj Kumar <manoj.kumar3@arm.com>
2021-06-21 16:03:43 +0100

    morello/scp_ramfw_soc: add ssc and system_info config data
    Add configurations for ssc and system_info modules.
    
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>
    Change-Id: I98a73ca81c9d26bd05499215fc949866e3c54e77

---
+++ product/morello/scp_ramfw_soc/config_system_info.c
@@ -0,0 +1,24 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <mod_ssc.h>
+#include <mod_system_info.h>
+
+#include <fwk_id.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+
+#include <stddef.h>
+
+const struct fwk_module_config config_system_info = {
+    .data = &((struct mod_system_info_config){
+        .system_info_driver_module_id = FWK_ID_MODULE_INIT(FWK_MODULE_IDX_SSC),
+        .system_info_driver_data_api_id = FWK_ID_API_INIT(
+            FWK_MODULE_IDX_SSC,
+            MOD_SSC_SYSTEM_INFO_DRIVER_DATA_API_IDX),
+    }),
+};
