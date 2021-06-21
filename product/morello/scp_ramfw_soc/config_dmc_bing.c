9b937f869f99678e76f94e677e5da7c19bc557d3
Manoj Kumar <manoj.kumar3@arm.com>
2021-06-21 16:03:43 +0100

    morello/scp_ramfw_soc: add config data for dmc_bing module
    Add configuration data for Morello's dmc_bing module.
    
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>
    Change-Id: Id3d05f6f5c524cca1404e9b344251546c2b07071

---
+++ product/morello/scp_ramfw_soc/config_dmc_bing.c
@@ -0,0 +1,58 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "config_clock.h"
+#include "morello_scp_mmap.h"
+
+#include <mod_dmc_bing.h>
+
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+
+#include <stdbool.h>
+
+/* Table of DMC_BING elements descriptions. */
+static struct fwk_element dmc_bing_element_table[3] = {
+    [0] =
+        {
+            .name = "DMC_BING-0",
+            .data = &((struct mod_dmc_bing_element_config){
+                .dmc_bing_base = SCP_DMC0,
+                .ddr_phy_base = SCP_DDR_PHY0,
+                .clock_id = FWK_ID_ELEMENT_INIT(
+                    FWK_MODULE_IDX_CLOCK,
+                    CLOCK_IDX_INTERCONNECT),
+            }),
+        },
+    [1] =
+        {
+            .name = "DMC_BING-1",
+            .data = &((struct mod_dmc_bing_element_config){
+                .dmc_bing_base = SCP_DMC1,
+                .ddr_phy_base = SCP_DDR_PHY1,
+                .clock_id = FWK_ID_ELEMENT_INIT(
+                    FWK_MODULE_IDX_CLOCK,
+                    CLOCK_IDX_INTERCONNECT),
+            }),
+        },
+    [2] = { 0 }, /* Termination description. */
+};
+
+static const struct fwk_element *dmc_bing_get_element_table(fwk_id_t module_id)
+{
+    return dmc_bing_element_table;
+}
+
+/* Configuration of the DMC_BING module. */
+const struct fwk_module_config config_dmc_bing = {
+    .elements = FWK_MODULE_DYNAMIC_ELEMENTS(dmc_bing_get_element_table),
+    .data = &((struct mod_dmc_bing_module_config){
+        .ddr_speed = DDR_CLOCK_MHZ,
+    }),
+};
