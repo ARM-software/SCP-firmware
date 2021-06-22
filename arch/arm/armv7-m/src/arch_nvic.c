ARM-software/SCP-firmware

commit 77e9fe7fb3b6f0e6b3af569041707bb62b4e1dad

armv7-m/arch_nvic:: Fix typecasting
The type of "irq" needs to be aligned with IRQn_Type.

This patch fixes that.

Change-Id: I8fb5a4d9d891e78f01d6088aaad27d9c69daa9ef
Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

arch/arm/armv7-m/src/arch_nvic.c
@@ -227,9 +227,8 @@ int arch_nvic_init(const struct fwk_arch_interrupt_driver **driver)
     uint32_t ictr_intlinesnum;
     uint32_t align_entries;
     uint32_t align_word;
-
     uint32_t *vector;
-    unsigned int irq;
+    char irq;
 
     if (driver == NULL)
         return FWK_E_PARAM;
@@ -277,7 +276,7 @@ int arch_nvic_init(const struct fwk_arch_interrupt_driver **driver)
     SCB->VTOR = (uint32_t)vector;
 
     /* Initialize IRQs */
-    for (irq = 0; irq < irq_count; irq++) {
+    for (irq = 0; irq < (char)irq_count; irq++) {
         /* Ensure IRQs are disabled during boot sequence */
         NVIC_DisableIRQ((IRQn_Type)irq);
         NVIC_ClearPendingIRQ((IRQn_Type)irq);

commit 82239e2074f87175e247a3f03db8fea47d6d2f2a

armv7-m: Fix IRQn type conversion issues
On platforms with more than 128 interrupts, IRQ numbers 128 and above
cause negative indexing into the newly-created vector table, due in part
to a newly-introduced `char` cast as well as truncation to `IRQn_Type`
caused by our usage of `-fshort-enums`, causing difficult-to-diagnose
crashes later in the program.

This commit changes IRQs so that they are represented using `uint32_t`,
which is guaranteed to hold all Armv7-M IRQs. We cannot use `IRQn_Type`
to represent arbitrary IRQ integers, as they may be truncated if short
enums are enabled and the IRQ has not been included in the type.

This bug was originally introduced in commit `77e9fe7f`.

Change-Id: I398cc922521d78a2498862b75d85162e7f034d09
Signed-off-by: Chris Kay <chris.kay@arm.com>

arch/arm/armv7-m/src/arch_nvic.c
@@ -9,6 +9,7 @@
  */
 
 #include <fwk_arch.h>
+#include <fwk_assert.h>
 #include <fwk_interrupt.h>
 #include <fwk_macros.h>
 #include <fwk_mm.h>
@@ -19,13 +20,26 @@
 
 #include <fmw_cmsis.h>
 
+#include <limits.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
 
-static unsigned int isr_count;
-static unsigned int irq_count;
+/* We use short enums, so avoid truncation of larger unsigned IRQ numbers */
+#define IRQN_TYPE_MAX \
+    ((uint64_t)(((UINT64_C(1) << (sizeof(IRQn_Type) * CHAR_BIT)) - 1U) / 2U))
+
+static uint32_t isr_count;
+static uint32_t irq_count;
+
+static_assert(
+    UINT32_MAX >= IRQN_TYPE_MAX,
+    "`uint32_t` cannot hold all possible IRQ numbers");
+
+static_assert(
+    UINT_MAX >= IRQN_TYPE_MAX,
+    "`unsigned int` cannot hold all possible IRQ numbers");
 
 /*
  * For interrupts with parameters, their entry in the vector table points to a
@@ -238,7 +252,7 @@ int arch_nvic_init(const struct fwk_arch_interrupt_driver **driver)
     uint32_t align_entries;
     uint32_t align_word;
     uint32_t *vector;
-    char irq;
+    uint32_t irq;
 
     if (driver == NULL) {
         return FWK_E_PARAM;
@@ -248,6 +262,8 @@ int arch_nvic_init(const struct fwk_arch_interrupt_driver **driver)
     ictr_intlinesnum = SCnSCB->ICTR & SCnSCB_ICTR_INTLINESNUM_Msk;
     irq_count = (ictr_intlinesnum + 1) * 32;
     isr_count = NVIC_USER_IRQ_OFFSET + irq_count;
+
+    fwk_assert(irq_count <= IRQN_TYPE_MAX);
 
     /*
      * Allocate and initialize a table for the callback functions and their
@@ -287,7 +303,7 @@ int arch_nvic_init(const struct fwk_arch_interrupt_driver **driver)
     SCB->VTOR = (uint32_t)vector;
 
     /* Initialize IRQs */
-    for (irq = 0; irq < (char)irq_count; irq++) {
+    for (irq = 0; irq < irq_count; irq++) {
         /* Ensure IRQs are disabled during boot sequence */
         NVIC_DisableIRQ((IRQn_Type)irq);
         NVIC_ClearPendingIRQ((IRQn_Type)irq);