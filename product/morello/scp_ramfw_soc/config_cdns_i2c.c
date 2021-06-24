bc8a45d3e2254170fff157d355bb41e5f5fb589d
Manoj Kumar <manoj.kumar3@arm.com>
2021-06-24 11:20:01 +0100

    morello/scp_ramfw_soc: include cadence i2c configuration
    Add configuration data required by cdns_i2c, a SCP core
    module. This module is required to communicate to attached
    DIMM in order to query the DRAM configuration/information.
    
    Signed-off-by: Manoj Kumar <manoj.kumar3@arm.com>
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>
    Change-Id: I9dae87e9db6b70bd403150d7259129d48489aa64

---
+++ product/morello/scp_ramfw_soc/config_cdns_i2c.c
@@ -0,0 +1,62 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "config_clock.h"
+#include "morello_scp_mmap.h"
+
+#include <mod_cdns_i2c.h>
+
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_macros.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+
+#include <fmw_cmsis.h>
+
+#include <stdbool.h>
+
+static const struct fwk_element i2c_element_desc_table[3] = {
+    [0] = {
+        .name = "SPD-I2C",
+        .data = &((struct mod_cdns_i2c_device_config) {
+            .reg_base = DIMM_SPD_I2C_BASE,
+            .clock_rate_hz = OSC_FREQ_HZ,
+            .bus_speed_hz = MOD_CDNS_I2C_SPEED_NORMAL,
+            .mode = MOD_CDNS_I2C_MASTER_MODE,
+            .ack_en = MOD_CDNS_I2C_ACK_ENABLE,
+            .addr_size = MOD_CDNS_I2C_ADDRESS_7_BIT,
+            .hold_mode = MOD_CDNS_I2C_HOLD_ON,
+            .callback_mod_id = FWK_ID_NONE_INIT,
+        }),
+    },
+    [1] = {
+        .name = "C2C-I2C",
+        .data = &((struct mod_cdns_i2c_device_config) {
+            .reg_base = SCP_I2C0_BASE,
+            .clock_rate_hz = (50UL * FWK_MHZ),
+            .bus_speed_hz = MOD_CDNS_I2C_SPEED_NORMAL,
+            .mode = MOD_CDNS_I2C_SLAVE_MODE,
+            .ack_en = MOD_CDNS_I2C_ACK_ENABLE,
+            .addr_size = MOD_CDNS_I2C_ADDRESS_7_BIT,
+            .hold_mode = MOD_CDNS_I2C_HOLD_OFF,
+            .slave_addr = 0x14,
+            .irq = SCP_I2C0_IRQ,
+            .callback_mod_id = FWK_ID_NONE_INIT,
+        }),
+    },
+    [2] = { 0 }, /* Termination description. */
+};
+
+static const struct fwk_element *get_i2c_table(fwk_id_t module_id)
+{
+    return i2c_element_desc_table;
+}
+
+const struct fwk_module_config config_cdns_i2c = {
+    .elements = FWK_MODULE_DYNAMIC_ELEMENTS(get_i2c_table),
+};
