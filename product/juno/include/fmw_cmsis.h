89f320e93d68f662db1e22c6f907be34c2ee4b31
Leandro Belli <leandro.belli@arm.com>
2021-06-30 14:57:56 +0100

    Juno: Fix IRQ number overflow
    This change ensures that `sizeof(IRQn_type) >= 2`, so that
    interrupt number variable do not undergo truncation causing boot
    failures. A problem which has been observed on certain platforms.
    
    Signed-off-by: Leandro Belli <leandro.belli@arm.com>
    Change-Id: I832117fde37bd61e75518f198ca602dfc2b72801

--- product/juno/include/fmw_cmsis.h
+++ product/juno/include/fmw_cmsis.h
@@ -158,6 +158,8 @@ typedef enum IRQn {
     SCP_EXT_INTR29_IRQ = 125, /* SCP Customer Extension */
     SCP_EXT_INTR30_IRQ = 126, /* SCP Customer Extension */
     SCP_EXT_INTR31_IRQ = 127, /* SCP Customer Extension */
+
+    IRQn_MAX = INT16_MAX,
 } IRQn_Type;
 
 #include <core_cm3.h>
