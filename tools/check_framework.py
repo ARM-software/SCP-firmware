032049d165229b17319cfa805c7d149801754c1c
Leandro Belli <leandro.belli@arm.com>
2021-07-09 13:41:56 +0100

    check_framework: create and separate framework check file
    This patch creates and separates frameworks checks into a
    different file to improve readability and scalability.
    Framework checks were done inside ci script. Moving it
    to a different script allows to have the same interface
    as the rest of the checks.
    
    Signed-off-by: Leandro Belli <leandro.belli@arm.com>
    Change-Id: I89723bc15b5d7f34df8e3e71237c16467a28d863

---
+++ tools/check_framework.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+Build and check framework
+This script runs "CC=gcc make clean test" and performs all frameworks tests.
+"""
+
+import sys
+import subprocess
+
+
+def banner(text):
+    columns = 80
+    title = " {} ".format(text)
+    print("\n\n{}".format(title.center(columns, "*")))
+
+
+def main():
+    banner("Build and run framework tests")
+
+    result = subprocess.Popen(
+        "CC=gcc make clean test",
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+
+    (stdout, stderr) = result.communicate()
+
+    print(stdout.decode())
+
+    if stderr:
+        print(stderr.decode())
+        return 1
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
