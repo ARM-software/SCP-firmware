9924b5e14b4f7b6ef0d7c808c116ffd65910c4c1
Ahmed Gadallah <ahmed.gadallah@arm.com>
2021-07-14 21:07:29 +0100

    product/tc0: Add config for Voltage Domain
    This patch consists of the required Voltage domain
    configurations for scmi layer, Voltage Domain interface,
    Voltage domain mock driver, makefiles and cmake configurations
    for tc0.
    
    Signed-off-by: Ahmed Gadallah <ahmed.gadallah@arm.com>
    Change-Id: I67682827dc57130c706f9c81d1c28c3e2c640663

---
+++ product/tc0/scp_ramfw/config_mock_voltage_domain.h
@@ -0,0 +1,17 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef CONFIG_MOCK_VOLTAGE_DOMAIN_H
+#define CONFIG_MOCK_VOLTAGE_DOMAIN_H
+
+enum config_mock_voltage_domain_element_idx {
+    CONFIG_MOCK_VOLTAGE_DOMAIN_ELEMENT_IDX_USB,
+
+    CONFIG_MOCK_VOLTAGE_DOMAIN_ELEMENT_IDX_COUNT,
+};
+
+#endif /* CONFIG_MOCK_VOLTAGE_DOMAIN_H */
---
+++ product/tc0/scp_ramfw/config_voltage_domain.c
@@ -0,0 +1,44 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <config_mock_voltage_domain.h>
+
+#include <mod_mock_voltage_domain.h>
+#include <mod_voltage_domain.h>
+
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+
+#include <stdint.h>
+
+static const struct fwk_element voltage_domain_element_table[] = {
+    [0] = {
+        .name = "USB",
+        .data = &((const struct mod_voltd_dev_config){
+        .driver_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_MOCK_VOLTAGE_DOMAIN,
+                CONFIG_MOCK_VOLTAGE_DOMAIN_ELEMENT_IDX_USB),
+        .api_id = FWK_ID_API_INIT(
+                FWK_MODULE_IDX_MOCK_VOLTAGE_DOMAIN,
+                MOD_MOCK_VOLTAGE_DOMAIN_API_IDX_VOLTD),
+        }),
+    },
+
+    [1] = { 0 }, /* Termination description */
+};
+
+static const struct fwk_element *voltage_domain_get_element_table(
+    fwk_id_t module_id)
+{
+    return voltage_domain_element_table;
+}
+
+const struct fwk_module_config config_voltage_domain = {
+    .elements = FWK_MODULE_DYNAMIC_ELEMENTS(voltage_domain_get_element_table),
+};
