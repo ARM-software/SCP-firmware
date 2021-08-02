733615227881d1cceb1a5ecb410c3be902ab7be8
Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
2021-08-02 22:45:33 +0530

    product/rdn2: update PCIe IO_MACRO_ELEMENT_CONFIG macro
    Modify IO_MACRO_ELEMENT_CONFIG macro to accept PCIe Integration Control
    register as separate parameter. This avoids forced mapping of ECAM and
    MMIOL/H regions to a specific IO Macro.
    
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
    Change-Id: I5e44578c6d438f2f636953ec13f70380134187f6

--- product/rdn2/scp_ramfw/config_pcie_integ_ctrl.c
+++ product/rdn2/scp_ramfw/config_pcie_integ_ctrl.c
@@ -20,11 +20,12 @@
 #define RD_N2_PCIE_INTEG_END_ADDR(index, base, size) \
     (base + (size * (index + 1ULL)) - 1ULL)
 
-#define IO_MACRO_ELEMENT_CONFIG(idx, ecam_size, mmiol_size, mmioh_size) \
+#define IO_MACRO_ELEMENT_CONFIG( \
+    idx, pcie_integ_ctrl_reg_base, ecam_size, mmiol_size, mmioh_size) \
     { \
         .name = "IO Macro " #idx, \
         .data = &((struct mod_pcie_integ_ctrl_config) { \
-            .reg_base = PCIE_INTEG_CTRL_REG_BASE(idx), \
+            .reg_base = pcie_integ_ctrl_reg_base, \
             .x16_ecam_mmio_mmap = { \
                 .valid = true, \
                 .allow_ns_access = true, \
@@ -49,12 +50,14 @@
 static const struct fwk_element pcie_integ_ctrl_element_table[] = {
     IO_MACRO_ELEMENT_CONFIG(
         0,
+        PCIE_INTEG_CTRL_REG_BASE(0),
         AP_PCIE_ECAM_SIZE_PER_RC,
         AP_PCIE_MMIOL_SIZE_PER_RC,
         AP_PCIE_MMIOH_SIZE_PER_RC),
 
     IO_MACRO_ELEMENT_CONFIG(
         1,
+        PCIE_INTEG_CTRL_REG_BASE(1),
         AP_PCIE_ECAM_SIZE_PER_RC,
         AP_PCIE_MMIOL_SIZE_PER_RC,
         AP_PCIE_MMIOH_SIZE_PER_RC),
@@ -62,12 +65,14 @@ static const struct fwk_element pcie_integ_ctrl_element_table[] = {
 #if (PLATFORM_VARIANT == 0)
     IO_MACRO_ELEMENT_CONFIG(
         2,
+        PCIE_INTEG_CTRL_REG_BASE(2),
         AP_PCIE_ECAM_SIZE_PER_RC,
         AP_PCIE_MMIOL_SIZE_PER_RC,
         AP_PCIE_MMIOH_SIZE_PER_RC),
 
     IO_MACRO_ELEMENT_CONFIG(
         3,
+        PCIE_INTEG_CTRL_REG_BASE(3),
         AP_PCIE_ECAM_SIZE_PER_RC,
         AP_PCIE_MMIOL_SIZE_PER_RC,
         AP_PCIE_MMIOH_SIZE_PER_RC),
