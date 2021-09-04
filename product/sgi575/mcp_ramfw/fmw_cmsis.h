ce9965671df5b5a26ba575b1bd5c869a7a4135d2
Shriram K <shriram.k@arm.com>
2021-09-04 21:08:01 +0530

    product/sgi575: add CMSIS configuration for mcp ramfw
    Add fmw_cmsis.h header file for mcp ramfw. This file includes the
    CMSIS configuration from sgi575/include/fmw_cmsis_mcp.h file.
    
    Signed-off-by: Shriram K <shriram.k@arm.com>
    Change-Id: I997c9f68f3e5b79f36f0b57f835cda8094fdce41

---
+++ product/sgi575/mcp_ramfw/fmw_cmsis.h
@@ -0,0 +1,15 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef FMW_CMSIS_H
+#define FMW_CMSIS_H
+
+#include <fmw_cmsis_mcp.h>
+
+extern uint32_t SystemCoreClock;
+
+#endif /* FMW_CMSIS_H */
