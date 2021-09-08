dfedfa73efb5753120238e6810bcd96c7cd6bfdc
Leandro Belli <leandro.belli@arm.com>
2021-09-08 16:45:45 +0100

    product/sgm775: Fix Cppcheck issues
    Fix Cppcheck errors and warnings for SGM775 platform.
    
    Signed-off-by: Leandro Belli <leandro.belli@arm.com>
    Change-Id: I725865e6cdd7b5f0ad7e48868c9334acb03e85fe

--- product/sgm775/scp_ramfw/config_resource_perms.c
+++ product/sgm775/scp_ramfw/config_resource_perms.c
@@ -48,48 +48,52 @@ static struct mod_res_agent_protocol_permissions agent_protocol_permissions[] =
  */
 static struct mod_res_agent_msg_permissions agent_msg_permissions[] = {
     [AGENT_IDX(SCMI_AGENT_ID_OSPM)] = {
+        .messages = {
         /* Example, Base, disable unused msg 12 */
-        .messages[MOD_RES_PERMS_SCMI_BASE_MESSAGE_IDX] = 0x0,
+        [MOD_RES_PERMS_SCMI_BASE_MESSAGE_IDX] = 0x0,
         /* Power Domain */
-        .messages[MOD_RES_PERMS_SCMI_POWER_DOMAIN_MESSAGE_IDX] = 0x0,
+        [MOD_RES_PERMS_SCMI_POWER_DOMAIN_MESSAGE_IDX] = 0x0,
         /* System Power Domain */
-        .messages[MOD_RES_PERMS_SCMI_SYS_POWER_MESSAGE_IDX] = 0x0,
+        [MOD_RES_PERMS_SCMI_SYS_POWER_MESSAGE_IDX] = 0x0,
         /* Performance */
-        .messages[MOD_RES_PERMS_SCMI_PERF_MESSAGE_IDX] = 0x0,
+        [MOD_RES_PERMS_SCMI_PERF_MESSAGE_IDX] = 0x0,
         /* Clock */
-        .messages[MOD_RES_PERMS_SCMI_CLOCK_MESSAGE_IDX] = 0x0,
+        [MOD_RES_PERMS_SCMI_CLOCK_MESSAGE_IDX] = 0x0,
         /* Sensors */
-        .messages[MOD_RES_PERMS_SCMI_SENSOR_MESSAGE_IDX] = 0x0,
+        [MOD_RES_PERMS_SCMI_SENSOR_MESSAGE_IDX] = 0x0,
         /* Reset Domains */
-        .messages[MOD_RES_PERMS_SCMI_RESET_DOMAIN_MESSAGE_IDX] = 0x0,
+        [MOD_RES_PERMS_SCMI_RESET_DOMAIN_MESSAGE_IDX] = 0x0,
+        },
     },
     [AGENT_IDX(SCMI_AGENT_ID_PSCI)] = {
-        .messages[0] = 0x0, /* Base */
-        .messages[1] = 0x0, /* Power Domain */
-        .messages[2] = 0x0, /* System Power Domain */
-        .messages[3] =
-            ((1 << (MOD_SCMI_PERF_DOMAIN_ATTRIBUTES -
-                  MOD_SCMI_PERF_DOMAIN_ATTRIBUTES)) |
-             /* DESCRIBE_LEVELS is required for some reason ... */
-             (0 << (MOD_SCMI_PERF_DESCRIBE_LEVELS -
-                  MOD_SCMI_PERF_DOMAIN_ATTRIBUTES)) |
-             (1 << (MOD_SCMI_PERF_LIMITS_SET -
-                  MOD_SCMI_PERF_DOMAIN_ATTRIBUTES)) |
-             (1 << (MOD_SCMI_PERF_LIMITS_GET -
-                  MOD_SCMI_PERF_DOMAIN_ATTRIBUTES)) |
-             (1 << (MOD_SCMI_PERF_LEVEL_SET -
-                  MOD_SCMI_PERF_DOMAIN_ATTRIBUTES)) |
-             (1 << (MOD_SCMI_PERF_LEVEL_GET -
-                  MOD_SCMI_PERF_DOMAIN_ATTRIBUTES)) |
-             (1 << (MOD_SCMI_PERF_NOTIFY_LIMITS -
-                  MOD_SCMI_PERF_DOMAIN_ATTRIBUTES)) |
-             (1 << (MOD_SCMI_PERF_NOTIFY_LEVEL -
-                  MOD_SCMI_PERF_DOMAIN_ATTRIBUTES)) |
-             (1 << (MOD_SCMI_PERF_DESCRIBE_FAST_CHANNEL -
-                  MOD_SCMI_PERF_DOMAIN_ATTRIBUTES))),
-        .messages[4] = 0x0, /* Clock */
-        .messages[5] = 0x0, /* Sensors */
+        .messages = {
+            [0] = 0x0, /* Base */
+            [1] = 0x0, /* Power Domain */
+            [2] = 0x0, /* System Power Domain */
+            [3] =
+                ((1 << (MOD_SCMI_PERF_DOMAIN_ATTRIBUTES -
+                    MOD_SCMI_PERF_DOMAIN_ATTRIBUTES)) |
+                /* DESCRIBE_LEVELS is required for some reason ... */
+                (0 << (MOD_SCMI_PERF_DESCRIBE_LEVELS -
+                    MOD_SCMI_PERF_DOMAIN_ATTRIBUTES)) |
+                (1 << (MOD_SCMI_PERF_LIMITS_SET -
+                    MOD_SCMI_PERF_DOMAIN_ATTRIBUTES)) |
+                (1 << (MOD_SCMI_PERF_LIMITS_GET -
+                    MOD_SCMI_PERF_DOMAIN_ATTRIBUTES)) |
+                (1 << (MOD_SCMI_PERF_LEVEL_SET -
+                    MOD_SCMI_PERF_DOMAIN_ATTRIBUTES)) |
+                (1 << (MOD_SCMI_PERF_LEVEL_GET -
+                    MOD_SCMI_PERF_DOMAIN_ATTRIBUTES)) |
+                (1 << (MOD_SCMI_PERF_NOTIFY_LIMITS -
+                    MOD_SCMI_PERF_DOMAIN_ATTRIBUTES)) |
+                (1 << (MOD_SCMI_PERF_NOTIFY_LEVEL -
+                    MOD_SCMI_PERF_DOMAIN_ATTRIBUTES)) |
+                (1 << (MOD_SCMI_PERF_DESCRIBE_FAST_CHANNEL -
+                    MOD_SCMI_PERF_DOMAIN_ATTRIBUTES))),
+            [4] = 0x0, /* Clock */
+            [5] = 0x0, /* Sensors */
         },
+    },
 };
 
 /*
