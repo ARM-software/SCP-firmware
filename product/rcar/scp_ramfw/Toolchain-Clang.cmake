acc625050172bb4cb4c0f99d331b51e7ed655067
Leandro Belli <leandro.belli@arm.com>
2021-10-22 16:39:03 +0100

    cmake/llvm/product: add LLVM support for all armv7-m platforms
    This patch adds LLVM support for all armv7-m platforms.
    This includes Juno, Morello, n1sdp, rcar, rdn1e1, rdn2, rdv1,
    rdv1mc, sgi575, sgm775, sgm776, Synquacer, TC0 and TC1.
    
    Signed-off-by: Leandro Belli <leandro.belli@arm.com>
    Change-Id: Ib325b05eb60c7a3b8dde781885393ab225bf6c28

---
+++ product/rcar/scp_ramfw/Toolchain-Clang.cmake
@@ -0,0 +1,20 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+include_guard()
+
+set(CMAKE_SYSTEM_PROCESSOR "aarch64")
+
+set(SCP_AARCH64_PROCESSOR_TARGET "cortex-a57.cortex-a53")
+set(CMAKE_TOOLCHAIN_PREFIX "aarch64-none-elf-")
+
+set(CMAKE_ASM_COMPILER_TARGET "aarch64-none-elf")
+set(CMAKE_C_COMPILER_TARGET "aarch64-none-elf")
+set(CMAKE_CXX_COMPILER_TARGET "aarch64-none-elf")
+
+include(
+    "${CMAKE_CURRENT_LIST_DIR}/../../../cmake/Toolchain/Clang-Baremetal.cmake")
