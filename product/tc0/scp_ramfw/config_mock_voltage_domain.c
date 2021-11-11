7500d2572493085115d068096a886efd9a7b7eeb
Ahmed Gadallah <ahmed.gadallah@arm.com>
2021-11-11 16:35:10 +0000

    product/tc0: Fix Voltage Domain configuration
    The default voltage value was configured outside the configured
    valid values range. This value is changed in this patch from 5000000
    to 500000.
    
    Additionally the element name in the configuration is changed from
    "USB" to "DUMMY_VOLTD" and the corresponding identifiers are changed
    from "*_USB" to "*_DUMMY". This is to avoid confusion and to emphasize
    that this is an example configuration
    
    Signed-off-by: Ahmed Gadallah <ahmed.gadallah@arm.com>
    Change-Id: I5269ec371d538984388e880341bf93b24e691315

--- product/tc0/scp_ramfw/config_mock_voltage_domain.c
+++ product/tc0/scp_ramfw/config_mock_voltage_domain.c
@@ -15,15 +15,15 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-static const int32_t usb_discrete_voltage_values[] = {
+static const int32_t dummy_discrete_voltage_values[] = {
     -500000,
     800000,
     10000,
 };
 
 static const struct fwk_element element_table[] = {
-    [CONFIG_MOCK_VOLTAGE_DOMAIN_ELEMENT_IDX_USB] = {
-        .name = "USB",
+    [CONFIG_MOCK_VOLTAGE_DOMAIN_ELEMENT_IDX_DUMMY] = {
+        .name = "DUMMY_VOLTD",
         .data = &(const struct mod_mock_voltage_domain_element_cfg) {
             .async_alarm_id = FWK_ID_NONE_INIT,
             .async_alarm_api_id = FWK_ID_NONE_INIT,
@@ -32,11 +32,11 @@ static const struct fwk_element element_table[] = {
             .async_response_api_id = FWK_ID_NONE_INIT,
 
             .default_mode_id = MOD_VOLTD_MODE_ID_OFF,
-            .default_voltage = 5000000,
+            .default_voltage = 500000,
 
             .level_type = MOD_VOLTD_VOLTAGE_LEVEL_CONTINUOUS,
-            .level_count = FWK_ARRAY_SIZE(usb_discrete_voltage_values),
-            .voltage_levels = usb_discrete_voltage_values,
+            .level_count = FWK_ARRAY_SIZE(dummy_discrete_voltage_values),
+            .voltage_levels = dummy_discrete_voltage_values,
         },
     },
     { 0 }
