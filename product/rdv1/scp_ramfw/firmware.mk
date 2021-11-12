7ceb223602010e2d7a8cf53e0a5bd36b02a30067
Girish Pathak <girish.pathak@arm.com>
2021-11-12 16:35:54 +0000

    product/rdv1: Remove multi-threading use
    This change is a precursor to the subsequent changes where
    multi-threading support will be removed from the SCP firmware.
    
    The term multi-threaded mode is misleading since it implies multiple
    threads of execution are concurrently supported
    (like in an OS), but that is not true. Also multi-threading add call
    overhead and could make things slower, and creates a larger memory
    footprint. Simply all of above is not needed at present, hence we
    expect multi-thread mode to be removed in near future from the firmware
    
    Change-Id: If15c3657858e0104ea606afa292b69cee58b86f3
    Signed-off-by: Girish Pathak <girish.pathak@arm.com>

--- product/rdv1/scp_ramfw/firmware.mk
+++ product/rdv1/scp_ramfw/firmware.mk
@@ -6,7 +6,6 @@
 #
 
 BS_FIRMWARE_CPU := cortex-m7
-BS_FIRMWARE_HAS_MULTITHREADING := yes
 BS_FIRMWARE_HAS_NOTIFICATION := yes
 BS_FIRMWARE_USE_NEWLIB_NANO_SPECS := no
 BS_FIRMWARE_HAS_FAST_CHANNELS := yes
@@ -44,7 +43,6 @@ BS_FIRMWARE_SOURCES := \
     config_system_power.c \
     config_sid.c \
     config_system_info.c \
-    rtx_config.c \
     config_armv7m_mpu.c \
     config_pl011.c \
     config_power_domain.c \
