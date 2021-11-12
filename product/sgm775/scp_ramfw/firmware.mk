8bde47dbcfb6e519db0a3632e2ccdfb9bb042365
Girish Pathak <girish.pathak@arm.com>
2021-11-12 16:32:58 +0000

    product/sgm775: Remove multi-threading use
    This change is a precursor to the subsequent changes where
    multi-threading support will be removed from the SCP firmware.
    
    The term multi-threaded mode is misleading since it implies multiple
    threads of execution are concurrently supported
    (like in an OS), but that is not true. Also multi-threading add call
    overhead and could make things slower, and creates a larger memory
    footprint. Simply all of above is not needed at present, hence we
    expect multi-thread mode to be removed in near future from the firmware
    
    Change-Id: Idd6f959dcfa53bde07191e72c3f4e7384a145d5c
    Signed-off-by: Girish Pathak <girish.pathak@arm.com>

--- product/sgm775/scp_ramfw/firmware.mk
+++ product/sgm775/scp_ramfw/firmware.mk
@@ -9,7 +9,6 @@
 #
 
 BS_FIRMWARE_CPU := cortex-m3
-BS_FIRMWARE_HAS_MULTITHREADING := no
 BS_FIRMWARE_HAS_NOTIFICATION := yes
 BS_FIRMWARE_HAS_RESOURCE_PERMISSIONS := yes
 BS_FIRMWARE_USE_NEWLIB_NANO_SPECS := yes
@@ -81,10 +80,6 @@ BS_FIRMWARE_SOURCES := \
     config_sid.c \
     config_system_info.c \
     config_scmi_power_domain.c
-
-ifeq ($(BS_FIRMWARE_HAS_MULTITHREADING),yes)
-    BS_FIRMWARE_SOURCES += rtx_config.c
-endif
 
 ifeq ($(BUILD_HAS_DEBUGGER),yes)
     BS_FIRMWARE_MODULES += debugger_cli
