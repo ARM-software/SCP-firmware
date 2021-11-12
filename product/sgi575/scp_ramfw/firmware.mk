6b8cda8556226043d40aaacdb91ec1ac6d2d73b4
Girish Pathak <girish.pathak@arm.com>
2021-11-12 16:30:17 +0000

    product/sgi575: Remove multi-threading use
    This change is a precursor to the subsequent changes where
    multi-threading support will be removed from the SCP firmware.
    
    The term multi-threaded mode is misleading since it implies multiple
    threads of execution are concurrently supported
    (like in an OS), but that is not true. Also multi-threading add call
    overhead and could make things slower, and creates a larger memory
    footprint. Simply all of above is not needed at present, hence we
    expect multi-thread mode to be removed in near future from the firmware
    
    Change-Id: I9b6a7c56cef1e3ba2383a27afbbfcb2579f81adc
    Signed-off-by: Girish Pathak <girish.pathak@arm.com>

--- product/sgi575/scp_ramfw/firmware.mk
+++ product/sgi575/scp_ramfw/firmware.mk
@@ -9,7 +9,6 @@
 #
 
 BS_FIRMWARE_CPU := cortex-m7
-BS_FIRMWARE_HAS_MULTITHREADING := yes
 BS_FIRMWARE_HAS_NOTIFICATION := yes
 BS_FIRMWARE_HAS_RESOURCE_PERMISSIONS := yes
 BS_FIRMWARE_USE_NEWLIB_NANO_SPECS := no
@@ -53,7 +52,6 @@ BS_FIRMWARE_MODULES := \
     resource_perms
 
 BS_FIRMWARE_SOURCES := \
-    rtx_config.c \
     config_armv7m_mpu.c \
     config_pl011.c \
     config_cmn600.c \
