10e79ba4d75db80302b15c110b18c6e2c2deac94
sah01 <sahil@arm.com>
2021-12-02 06:38:18 +0000

    morello: Split platform_info sds struct for fvp and soc
    Different platform_info sds struct definition will be used
    for fvp and soc.
    
    Signed-off-by: sahil <sahil@arm.com>
    Signed-off-by: Anurag Koul <anurag.koul@arm.com>
    Change-Id: I99de0ad839a480287a1329f4ab3a0845c9fd757b

--- product/morello/include/morello_sds.h
+++ product/morello/include/morello_sds.h
@@ -44,7 +44,11 @@ enum morello_sds_region_idx {
 #define MORELLO_SDS_FEATURE_AVAILABILITY_SIZE 4
 #define MORELLO_SDS_CPU_BOOTCTR_SIZE 256
 #define MORELLO_SDS_CPU_FLAGS_SIZE 256
-#define MORELLO_SDS_PLATFORM_INFO_SIZE        22
+#if defined(PLAT_FVP)
+#    define MORELLO_SDS_PLATFORM_INFO_SIZE 8
+#else
+#    define MORELLO_SDS_PLATFORM_INFO_SIZE 22
+#endif
 
 /*
  * Field masks and offsets for the MORELLO_SDS_AP_CPU_INFO structure.
