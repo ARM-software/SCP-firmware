b468bc03e52e3b00b2dd047b38740e03798cb022
Girish Pathak <girish.pathak@arm.com>
2021-12-08 17:27:06 +0000

    cppcheck: cppcheck output in verbose mode
    CMake build executes cppcheck and compiler in parallel
    In case of error, it's not very obvious from the log which
    tool caused the corresponding error. This change introduces
    a python script wrapper around cppcheck to display more
    verbose messages when cppcheck is invoked and highlights
    the cppcheck error.
    
    This commit also updates cppcheck_suppress_list to suppress
    false errors in fwk_log.c for lines 187 and 189.
    
    Change-Id: Id83d5264410b3a2c931661b424281c435f091040
    Signed-off-by: Girish Pathak <girish.pathak@arm.com>

---
+++ tools/cppcheck_wrapper.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+"""
+    A wrapper around cppcheck which will be called from CMake's context.
+
+    CMake doesn't give any mechanism to execute any programs or
+    commands prior or after cppcheck's invocation.
+
+    Usually CMAKE_C_CPPCHECK will contain the path to cppcheck binary
+    on the build system, instead CMAKE_C_CPPCHECK will now contain
+    path to this script which in turn calls cppcheck but this enables
+    to inject any commands prior to and after actual invocation of
+    cppcheck.
+
+    One use case would be to mark cppcheck's invocation as below
+
+    [CPPCHECK] ------>
+    ....
+    ....
+    [CPPCHECK] <------
+
+    This script also prints a warning if the version of the cppcheck
+    does not match the required version.
+
+"""
+
+import sys
+import subprocess
+import re
+
+# Holds required version info.
+required_tool_version = "1.90"
+
+tool_name = "CPPCHECK"
+
+
+# Helper function
+def print_msg(message, output):
+    if output:
+        print("[{}]  {}".format(tool_name, message))
+
+
+if __name__ == '__main__':
+    # CMake create a list of argument which include path of the
+    # tool(e.g. cppcheck) binary and its arguments.
+    tool_path = sys.argv[1]
+    tool_args = ' '.join(sys.argv[2:])
+    cmd = "{} {}".format(tool_path, tool_args)
+
+    # Check if output of the tool (e.g. cppcheck) is to be displayed
+    if re.search("verbose", tool_args):
+        verbose = True
+    else:
+        verbose = False
+
+    # If yes, then remove verbose string from argument list.
+    if verbose:
+        tool_args.replace("verbose", "")
+
+    # Check version
+    r = subprocess.Popen(tool_path + " --version",
+                         shell=True,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+
+    out, err = r.communicate()
+
+    tool_version = out.decode("utf-8", "ignore")
+
+    if out:
+        print_msg("------>", output=verbose)
+        print_msg("cppcheck {}"
+                  " version {} ".format(tool_path, tool_version),
+                  output=verbose)
+
+        if not re.search(required_tool_version, tool_version):
+            print_msg("Warning: recommended"
+                      " {} version is"
+                      " but found {}".format(tool_name,
+                                             required_tool_version,
+                                             tool_version),
+                      output=True)
+    if err:
+        print_msg("Error: failed to find"
+                  " binary at {}".format(tool_path),
+                  output=True)
+        exit(1)
+
+    print_msg(cmd, output=verbose)
+
+    r = subprocess.Popen(cmd,
+                         shell=True,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+
+    out, err = r.communicate()
+
+    if out:
+        print_msg("" + out.decode("utf-8", "ignore"), output=verbose)
+    if err:
+        print_msg("" + err.decode("utf-8", "ignore"), output=True)
+
+    print_msg("<------", output=verbose)
