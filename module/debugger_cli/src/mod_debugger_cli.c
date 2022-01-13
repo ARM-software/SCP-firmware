42b0f500c9ccecdd490d190395a17962d4038105
Brett Warren <brett.warren@arm.com>
2022-01-13 01:10:50 +0000

    mod_debugger: move mod_timer.h include to .c file
    The inclusion of mod_timer.h in dmc500/debugger_cli headers
    causes the work-in-progress unit testing branch to fail
    compilation, mitigating this would enable merge of that branch.
    
    Signed-off-by: Brett Warren <brett.warren@arm.com>
    Change-Id: I1ad418361355f94918bdb1b163e5de15d26a23c0

--- module/debugger_cli/src/mod_debugger_cli.c
+++ module/debugger_cli/src/mod_debugger_cli.c
@@ -1,6 +1,6 @@
 /*
  * Arm SCP/MCP Software
- * Copyright (c) 2020-2021, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2020-2022, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -9,6 +9,7 @@
 #include <cli_platform.h>
 
 #include <mod_debugger_cli.h>
+#include <mod_timer.h>
 
 #include <fwk_assert.h>
 #include <fwk_module.h>
