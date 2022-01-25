27586221d438958f352ca02951597c504fa1d5dd
Mike Leach <mike.leach@linaro.org>
2022-01-25 14:45:00 +0000

    framework: dts: add in framework dt config, bindings and generators
    Add in fixed and cmake generated framework bindings files for device tree.
    
    Signed-off-by: Mike Leach <mike.leach@linaro.org>

---
+++ framework/include/fwk_dt_bindings.h
@@ -0,0 +1,21 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef FWK_DT_BINDINGS_H
+#define FWK_DT_BINDINGS_H
+
+/* 
+ * Auto generated module indexes for Device tree inclusion.
+ * Match indices in main C code.
+ */
+#include "fwk_dt_bindings_gen.h"
+
+/* Additional module bindings for DTS use */
+
+
+
+#endif /* FWK_DT_BINDINGS_H */
