434925da114c46b3153457a4a7da96cd5d6f30fe
Ahmed Gadallah <ahmed.gadallah@arm.com>
2022-02-25 01:14:07 +0000

    Add new plaform rd_fremont
    Adding rd_fremont along with an LCP target consisting of
    armv8m MPU configuration example.
    
    Signed-off-by: Ahmed Gadallah <ahmed.gadallah@arm.com>
    Change-Id: Ibe77e1a62ded74caca6b1bfa04cee51e565127b7

---
+++ product/rd_fremont/lcp_ramfw/Firmware.cmake
@@ -0,0 +1,36 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2022, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+#
+# Configure the build system.
+#
+
+set(SCP_FIRMWARE "rd-fremont-lcp")
+
+set(SCP_FIRMWARE_TARGET "rd-fremont-lcp")
+
+set(SCP_TOOLCHAIN_INIT "GNU")
+
+set(SCP_GENERATE_FLAT_BINARY_INIT TRUE)
+
+set(SCP_ENABLE_NOTIFICATIONS_INIT TRUE)
+
+set(SCP_ENABLE_IPO_INIT FALSE)
+
+set(SCP_ARCHITECTURE "arm-m")
+
+set(SCP_ENABLE_DEBUGGER_INIT FALSE)
+
+list(PREPEND SCP_MODULE_PATHS
+    "${CMAKE_CURRENT_LIST_DIR}/../module/mod_lcp_platform")
+
+# The order of the modules in the following list is the order in which the
+# modules are initialized, bound, started during the pre-runtime phase.
+# any change in the order will cause firmware initialization errors.
+
+list(APPEND SCP_MODULES "armv8m-mpu")
+list(APPEND SCP_MODULES "lcp-platform")
---
+++ product/rd_fremont/lcp_ramfw/config_armv8m_mpu.c
@@ -0,0 +1,86 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2022, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "lcp_mmap.h"
+#include "mod_armv8m_mpu.h"
+
+#include <fwk_macros.h>
+#include <fwk_module.h>
+
+#include <fmw_cmsis.h>
+
+static const uint8_t attributes[] = {
+    /* Device memory, non Gathering, non Re-ordering, non Early Write
+       Acknowledgement */
+    [MPU_ATTR_0] =
+        ARM_MPU_ATTR(ARM_MPU_ATTR_DEVICE, ARM_MPU_ATTR_DEVICE_nGnRnE),
+    /* Normal memory, non Cacheable */
+    [MPU_ATTR_1] =
+        ARM_MPU_ATTR(ARM_MPU_ATTR_NON_CACHEABLE, ARM_MPU_ATTR_NON_CACHEABLE),
+};
+
+static const ARM_MPU_Region_t regions[] = {
+    {
+        /* LCP_CORE_TCM_REGION*/
+        .RBAR = ARM_MPU_RBAR(
+            LCP_CORE_ITCM_REGION_START, /* BASE */
+            ARM_MPU_SH_NON, /* SH */
+            0, /* RO */
+            0, /* NP */
+            0 /* XN */),
+        .RLAR = ARM_MPU_RLAR(LCP_CORE_ITCM_REGION_END, MPU_ATTR_1),
+    },
+    {
+        /* LCP_CORE_DTCM_REGION*/
+        .RBAR = ARM_MPU_RBAR(
+            LCP_CORE_DTCM_REGION_START, /* BASE */
+            ARM_MPU_SH_INNER, /* SH */
+            0, /* RO */
+            0, /* NP */
+            1 /* XN */),
+        .RLAR = ARM_MPU_RLAR(LCP_CORE_DTCM_REGION_END, MPU_ATTR_1),
+    },
+    {
+        /* LCP_CORE_PERIPERAL_REGION */
+        .RBAR = ARM_MPU_RBAR(
+            LCP_CORE_PERIPHERAL_REGION_START, /* BASE */
+            ARM_MPU_SH_NON, /* SH */
+            0, /* RO */
+            0, /* NP */
+            1 /* XN */),
+        .RLAR = ARM_MPU_RLAR(LCP_CORE_PERIPHERAL_REGION_END, MPU_ATTR_0),
+    },
+    {
+        /* LCP_SRAM_REGION*/
+        .RBAR = ARM_MPU_RBAR(
+            LCP_SRAM_REGION_START, /* BASE */
+            ARM_MPU_SH_OUTER, /* SH */
+            0, /* RO */
+            0, /* NP */
+            1 /* XN */),
+        .RLAR = ARM_MPU_RLAR(LCP_SRAM_REGION_END, MPU_ATTR_1),
+    },
+    {
+        /* LCP_DEVICE_REGION */
+        .RBAR = ARM_MPU_RBAR(
+            LCP_DEVICE_REGION_START, /* BASE */
+            ARM_MPU_SH_OUTER, /* SH */
+            0, /* RO */
+            0, /* NP */
+            1 /* XN */),
+        .RLAR = ARM_MPU_RLAR(LCP_DEVICE_REGION_END, MPU_ATTR_0),
+    },
+};
+
+const struct fwk_module_config config_armv8m_mpu = {
+    .data = &((struct mod_armv8m_mpu_config){
+        .region_count = FWK_ARRAY_SIZE(regions),
+        .regions = regions,
+        .attributes_count = FWK_ARRAY_SIZE(attributes),
+        .attributes = attributes,
+    }),
+};
