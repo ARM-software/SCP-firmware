e69991d8e9f5183f80c471c2ec36c36c159d931d
Ahmed Gadallah <ahmed.gadallah@arm.com>
2022-02-25 01:14:07 +0000

    Add new plaform rdfremont
    Adding rdfremont along with an LCP target consisting of
    armv8m MPU configuration example.
    
    Signed-off-by: Ahmed Gadallah <ahmed.gadallah@arm.com>
    Change-Id: Ibe77e1a62ded74caca6b1bfa04cee51e565127b7

---
+++ product/rdfremont/include/fmw_cmsis_lcp.h
@@ -0,0 +1,56 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2022, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef FMW_CMSIS_LCP_H
+#define FMW_CMSIS_LCP_H
+
+#include <stdint.h>
+
+#define __CHECK_DEVICE_DEFINES
+#define __CM55_REV             0x0000U
+#define __FPU_PRESENT          0U
+#define __MPU_PRESENT          1U
+#define __VTOR_PRESENT         1U
+#define __PMU_PRESENT          0U
+#define __DSP_PRESENT          0U
+#define __ICACHE_PRESENT       0U
+#define __DCACHE_PRESENT       0U
+#define __DTCM_PRESENT         0U
+#define __NVIC_PRIO_BITS       3U
+#define __SAUREGION_PRESENT    0U
+#define __Vendor_SysTickConfig 0U
+
+typedef enum IRQn {
+    Reset_IRQn = -15,
+    NonMaskableInt_IRQn = -14,
+    HardFault_IRQn = -13,
+    MemoryManagement_IRQn = -12,
+    BusFault_IRQn = -11,
+    UsageFault_IRQn = -10,
+    SecureFault_IRQn = -9,
+    SVCall_IRQn = -5,
+    DebugMonitor_IRQn = -4,
+    PendSV_IRQn = -2,
+    SysTick_IRQn = -1,
+
+    WDG_RST_RQST_IRQ = 0, /* Watchdog reset request */
+    WDG_INT_IRQ = 1, /* Watchdog interrupt */
+    RESERVED2_IRQ = 2, /* Reserved */
+    TIMER_IRQ = 3, /* Timer */
+    RESERVED4_IRQ = 4, /* Reserved */
+    RESERVED5_IRQ = 5, /* Reserved */
+    RESERVED6_IRQ = 6, /* Reserved */
+    RESERVED7_IRQ = 7, /* Reserved */
+    RESERVED8_IRQ = 8, /* Reserved */
+    RESERVED9_IRQ = 9, /* Reserved */
+
+    IRQn_MAX = INT16_MAX,
+} IRQn_Type;
+
+#include <core_cm55.h>
+
+#endif /* FMW_CMSIS_LCP_H */
---
+++ product/rdfremont/lcp_ramfw/fmw_memory.h
@@ -0,0 +1,30 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2022, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     RAM firmware memory layout for the linker script.
+ */
+
+#ifndef FMW_MEMORY_H
+#define FMW_MEMORY_H
+
+#include "lcp_mmap.h"
+
+#define FMW_MEM_MODE ARCH_MEM_MODE_DUAL_REGION_RELOCATION
+
+/*
+ * RAM instruction memory
+ */
+#define FMW_MEM0_SIZE LCP_ITCM_SIZE
+#define FMW_MEM0_BASE LCP_ITCM_NS_BASE
+
+/*
+ * RAM data memory
+ */
+#define FMW_MEM1_SIZE LCP_DTCM_SIZE
+#define FMW_MEM1_BASE LCP_DTCM_NS_BASE
+
+#endif /* FMW_MEMORY_H */
