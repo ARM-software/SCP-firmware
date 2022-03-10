c6b577dc7a94bc8b4011bdb43f5b201335a93d64
Shriram K <shriram.k@arm.com>
2022-03-10 14:45:43 +0530

    product/rdv1mc: fix gtimer module config in mcp ramfw
    Set the control register in the gtimer config data to GENERIC REFCLK
    CNTControl address. Set skip_cntcontrol_init flag to true since MCP
    cannot configure the CNTCONTROL register which is accessible by secure
    accesses only.
    
    The configuration of this register will be done by the gtimer module in
    scp ramfw.
    
    Signed-off-by: Shriram K <shriram.k@arm.com>
    Change-Id: I9c3a259b3e4fed6c5f93f3a4dc3ce3dbae48f1d8

--- product/rdv1mc/include/mcp_mmap.h
+++ product/rdv1mc/include/mcp_mmap.h
@@ -1,6 +1,6 @@
 /*
  * Arm SCP/MCP Software
- * Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2021-2022, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -8,12 +8,13 @@
 #ifndef MCP_MMAP_H
 #define MCP_MMAP_H
 
-#define MCP_BOOT_ROM_BASE       0x00000000
-#define MCP_ITC_RAM_BASE        0x00800000
-#define MCP_SOC_EXPANSION1_BASE 0x01000000
-#define MCP_DTC_RAM_BASE        0x20000000
-#define MCP_UART1_BASE          0x4C002000
-#define MCP_UART2_BASE          0x4C003000
-#define MCP_PERIPHERAL_BASE     0x4C000000
+#define MCP_BOOT_ROM_BASE            0x00000000
+#define MCP_ITC_RAM_BASE             0x00800000
+#define MCP_SOC_EXPANSION1_BASE      0x01000000
+#define MCP_DTC_RAM_BASE             0x20000000
+#define MCP_UART1_BASE               0x4C002000
+#define MCP_UART2_BASE               0x4C003000
+#define MCP_PERIPHERAL_BASE          0x4C000000
+#define MCP_SYSTEM_ACCESS_PORT1_BASE 0xA0000000
 
 #endif /* MCP_MMAP_H */
