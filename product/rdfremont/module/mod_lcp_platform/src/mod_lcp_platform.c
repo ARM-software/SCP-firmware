11b2da0b1dc5ed05b82d8bb68ebf89afeb1b3413
Ahmed Gadallah <ahmed.gadallah@arm.com>
2022-03-10 20:00:39 +0000

    rdfremont: Add timer configuration
    Add timer and interrupt configuration to validate interrupt
    handling in the LCP.
    
    Signed-off-by: Ahmed Gadallah <ahmed.gadallah@arm.com>
    Change-Id: I01bbf36890914fb17bab4c6eecb8b1d26b830996

--- product/rdfremont/module/mod_lcp_platform/src/mod_lcp_platform.c
+++ product/rdfremont/module/mod_lcp_platform/src/mod_lcp_platform.c
@@ -4,14 +4,35 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
+#include <lcp_device.h>
 
 #include <fwk_id.h>
+#include <fwk_interrupt.h>
 #include <fwk_log.h>
 #include <fwk_module.h>
 #include <fwk_module_idx.h>
 #include <fwk_status.h>
 
 #define MOD_NAME "[LCP_PLATFORM] "
+#define LCP_TIMER_RELOAD 0xFFFFFU
+
+volatile struct {
+    uint32_t counter;
+} lcp_platform_ctx;
+
+void timer_isr()
+{
+    lcp_platform_ctx.counter++;
+    /* clear interrupt flag */
+    LCP_TIMER_REG_S->INTSTATUS = 1U;
+}
+
+void mod_lcp_config_timer()
+{
+    LCP_TIMER_REG_S->CTRL = CTRL_ENABLE_MASK | CTRL_INT_EN_MASK;
+
+    LCP_TIMER_REG_S->RELOAD = LCP_TIMER_RELOAD;
+}
 
 static int mod_lcp_platform_init(
     fwk_id_t module_id,
@@ -28,7 +49,14 @@ static int mod_lcp_platform_init(
 
 static int mod_lcp_platform_start(fwk_id_t id)
 {
+    fwk_interrupt_set_isr(TIMER_IRQ, timer_isr);
+
+    fwk_interrupt_enable(TIMER_IRQ);
+
+    mod_lcp_config_timer();
+
     FWK_LOG_INFO(MOD_NAME "LCP RAM firmware initialized\n");
+
     return FWK_SUCCESS;
 }
 
