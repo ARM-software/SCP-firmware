d50a2692c5ce6f8a0e714e29d5e2ad9567b7a9ad
Pranav Madhu <pranav.madhu@arm.com>
2022-03-22 22:46:13 +0530

    cmake: Add DVFS and SCMI Perf support for RD-N2
    Enable Dynamic Voltage/Frequency support (DVFS) and SCMI perf support
    for RD-N2 platform. This is required to support ACPI CPPC mechanism
    for the platform. Also enable the SCMI fastchannel, as fastchannel is
    the communication medium used by the application processor to request
    the performance level.
    
    Change-Id: I5f3b57dfeb5bcaa867db89d743bb40f77dfe6ea9
    Signed-off-by: Pranav Madhu <pranav.madhu@arm.com>

--- product/rdn2/scp_ramfw/Firmware.cmake
+++ product/rdn2/scp_ramfw/Firmware.cmake
@@ -24,6 +24,8 @@ set(SCP_ARCHITECTURE "arm-m")
 
 set(SCP_ENABLE_NEWLIB_NANO FALSE)
 
+set(SCP_ENABLE_FAST_CHANNELS TRUE)
+
 list(PREPEND SCP_MODULE_PATHS
      "${CMAKE_CURRENT_LIST_DIR}/../module/platform_system")
 list(PREPEND SCP_MODULE_PATHS
@@ -62,3 +64,7 @@ list(APPEND SCP_MODULES "power-domain")
 list(APPEND SCP_MODULES "scmi-power-domain")
 list(APPEND SCP_MODULES "scmi-system-power")
 list(APPEND SCP_MODULES "platform-system")
+list(APPEND SCP_MODULES "psu")
+list(APPEND SCP_MODULES "mock-psu")
+list(APPEND SCP_MODULES "dvfs")
+list(APPEND SCP_MODULES "scmi-perf")
