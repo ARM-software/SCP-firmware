3660f67ff4172f4ed85af8f2f4a3c2b4a233287f
Etienne Carriere <etienne.carriere@linaro.org>
2022-04-15 14:10:18 +0200

    product/optee-stm32mp1: Add stm32mp1 OP-TEE platform product
    Platform stm32mp1 running OP-TEE that embeds SCMI services exposed
    to the non-secure world. The product currently only exposes SCMI base
    protocol.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

---
+++ product/optee-stm32mp1/fw/Toolchain-GNU.cmake
@@ -0,0 +1,22 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2022, Linaro Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+include_guard()
+
+set(CMAKE_SYSTEM_PROCESSOR "optee")
+set(CMAKE_TOOLCHAIN_PREFIX ${CFG_CROSS_COMPILE})
+
+set(CMAKE_ASM_COMPILER_TARGET ${CFG_CROSS_COMPILE})
+set(CMAKE_C_COMPILER_TARGET ${CFG_CROSS_COMPILE})
+set(CMAKE_CXX_COMPILER_TARGET ${CFG_CROSS_COMPILE})
+
+include(
+    "${CMAKE_CURRENT_LIST_DIR}/../../../cmake/Toolchain/GNU-Baremetal.cmake")
+
+foreach(language IN ITEMS ASM C CXX)
+    string(APPEND CMAKE_${language}_FLAGS_INIT ${CFG_CFLAGS_OPTEE})
+endforeach()
---
+++ product/optee-stm32mp1/fw/config_scmi.c
@@ -0,0 +1,55 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2022, Linaro Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_macros.h>
+#include <fwk_log.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+
+#include <internal/scmi.h>
+#include <mod_optee_smt.h>
+#include <mod_scmi.h>
+#include <scmi_agents.h>
+
+static const struct fwk_element service_table[] = {
+    [SCMI_SERVICE_IDX_NS_CHANNEL0] = {
+        .name = "service-0",
+        .data = &((struct mod_scmi_service_config) {
+            .transport_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_OPTEE_SMT, 0),
+            .transport_api_id = FWK_ID_API_INIT(FWK_MODULE_IDX_OPTEE_SMT,
+                                                MOD_OPTEE_SMT_API_IDX_SCMI_TRANSPORT),
+            .scmi_agent_id = SCMI_AGENT_ID_NSEC0,
+            .scmi_p2a_id = FWK_ID_NONE_INIT,
+        }),
+    },
+    [SCMI_SERVICE_IDX_COUNT] = { 0 }
+};
+
+static const struct fwk_element *get_scmi_service_table(fwk_id_t module_id)
+{
+    return service_table;
+}
+
+static const struct mod_scmi_agent agent_table[] = {
+    [SCMI_AGENT_ID_NSEC0] = {
+        .type = SCMI_AGENT_TYPE_OSPM,
+        .name = "OSPM0",
+    },
+};
+
+struct fwk_module_config config_scmi = {
+    .data = &((struct mod_scmi_config) {
+        .protocol_count_max = 9,
+        .agent_count = FWK_ARRAY_SIZE(agent_table) - 1,
+        .agent_table = agent_table,
+        .vendor_identifier = "Linaro",
+        .sub_vendor_identifier = "PMWG",
+    }),
+    .elements = FWK_MODULE_DYNAMIC_ELEMENTS(get_scmi_service_table),
+};
