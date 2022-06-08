72583c4ff9e7b4978675dee3dc7eb84d8779ef3b
Tomás Agustín González Orlando <tomasagustin.gonzalezorlando@arm.com>
2022-06-08 17:35:42 +0100

    rdfremont: Set MHUv3 memory addresses
    Define some possible MHUv3 connections and devices in rdfremont and
    set the addresses of all the Mailbox (MBX) and PostBox (PBX) in the
    LCP.
    These addresses will be used by the MHUv3 driver to perform
    MHUv3 operations like triggering a message transfer.
    
    Change-Id: I106fcf6889877cc9e8d096ab34764d7b8b148e69
    Signed-off-by: Tomás Agustín González Orlando <tomasagustin.gonzalezorlando@arm.com>

---
+++ product/rdfremont/include/lcp_mhu3.h
@@ -0,0 +1,19 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2022, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     MHU module device indexes.
+ */
+
+#ifndef SCP_MHU3_H
+#define SCP_MHU3_H
+
+enum fremont_mhu_devices {
+    MHU3_DEVICE_IDX_SCP_LCP,
+    MHU3_DEVICE_IDX_COUNT,
+};
+
+#endif /* SCP_MHU3_H */
