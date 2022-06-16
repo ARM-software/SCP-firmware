a77045fde78f53bf7b4ff9fb352e2e50832c1f4d
Mike Leach <mike.leach@linaro.org>
2022-06-16 14:58:52 +0100

    module/clock: dts: update module to build from devicetree
    Update build and add config_dt source to configure from devicetree
    when selected by product.
    
    Signed-off-by: Mike Leach <mike.leach@linaro.org>

---
+++ module/clock/src/config_dt_clock.c
@@ -0,0 +1,65 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2019-2021, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <mod_clock.h>
+#include <mod_power_domain.h>
+
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+#include <fwk_devicetree.h>
+#include <fwk_dt_config_common.h>
+#include <fwk_dt_config.h>
+#include <fwk_dt_config_dyn.h>
+
+#define CLK_DEV_COMPAT arm_scp_clk_dev
+
+#define CFG_CLK_DEV_ELEM_DATA(n) \
+    .driver_id = FWK_DT_FWK_ID(CLK_DEV_COMPAT, n, driver_id), \
+    .api_id = FWK_DT_FWK_ID(CLK_DEV_COMPAT, n, api_id), \
+    .pd_source_id = FWK_DT_OPT_FWK_ID(CLK_DEV_COMPAT, n, pd_source_id),
+
+#define CFG_CLK_DEV_ELEM_INIT(n) \
+    [n] = { \
+    .name = FWK_DT_INST_PROP(CLK_DEV_COMPAT, n, name), \
+    .data = &((struct mod_clock_dev_config) { \
+        CFG_CLK_DEV_ELEM_DATA(n) \
+        }), \
+    },
+
+/* build tables if at least one instance in device tree */
+#if DT_HAS_COMPAT_STATUS_OKAY(CLK_DEV_COMPAT)
+
+/* data table */
+static struct fwk_element clock_element_table[] = {
+        // Macro for array elements 
+        DT_FOREACH_OKAY_INST_arm_scp_clk_dev(CFG_CLK_DEV_ELEM_INIT)
+        // last null element
+        [DT_N_INST_arm_scp_clk_dev_NUM_OKAY] = { 0 },
+};
+
+/* access data table from dynamic element callback */
+#ifdef FWK_MODULE_GEN_DYNAMIC_CLOCK
+const struct fwk_element *_static_get_element_table_clock() {
+    return clock_element_table;
+}
+#endif
+
+/* config structure */
+struct fwk_module_config config_dt_clock = {
+    #ifdef FWK_MODULE_GEN_DYNAMIC_CLOCK
+    .elements = FWK_MODULE_DYNAMIC_ELEMENTS(dyn_get_element_table_clock),
+    #else
+    .elements = FWK_MODULE_STATIC_ELEMENTS_PTR(clock_element_table),
+    #endif
+};
+
+#else 
+/* no DT references - set up NULL object */
+struct fwk_module_config config_dt_clock = { 0 };
+#endif
