76e5108287b135b5ae8bb9d141fc31cfa28b7751
Brett Warren <brett.warren@arm.com>
2022-06-20 23:19:32 +0100

    unit_test: expose TEST_ON_TARGET option
    Make TEST_ON_TARGET select the minimal scp_ut
    image and expose it in Makefile.cmake.
    
    Signed-off-by: Brett Warren <brett.warren@arm.com>
    Change-Id: Ica51affb30b83a0cb598b68672375c4d828e32bf

--- product/juno/product.mk
+++ product/juno/product.mk
@@ -1,11 +1,18 @@
 #
 # Arm SCP/MCP Software
-# Copyright (c) 2019-2021, Arm Limited and Contributors. All rights reserved.
+# Copyright (c) 2019-2022, Arm Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
 BS_PRODUCT_NAME := juno
-BS_FIRMWARE_LIST := scp_romfw \
-                    scp_romfw_bypass \
-                    scp_ramfw
+
+ifeq ($(TEST_ON_TARGET),1)
+    BS_FIRMWARE_LIST := scp_romfw \
+                        scp_romfw_bypass \
+                        scp_ut
+else
+    BS_FIRMWARE_LIST := scp_romfw \
+                        scp_romfw_bypass \
+                        scp_ramfw
+endif
