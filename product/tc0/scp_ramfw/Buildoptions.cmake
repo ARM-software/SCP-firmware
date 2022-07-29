0a9e4fed2a7dcc3539f1d119c9b44d744b7170c9
Leandro Belli <leandro.belli@arm.com>
2022-07-29 16:39:23 +0100

    SCMI: add missing rename BUILD_HAS_FAST_CHANNELS with _SCMI_PERF_
    It completes some missing renaming in patch
    d9e35828a4727de8a6e928e76609cba7f07af816
    
    Signed-off-by: Leandro Belli <leandro.belli@arm.com>
    Change-Id: Id8f9a5e8848806ab244b70b1f32545014045ceb5

--- product/tc0/scp_ramfw/Buildoptions.cmake
+++ product/tc0/scp_ramfw/Buildoptions.cmake
@@ -1,7 +1,15 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2022, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
 cmake_dependent_option(
-    SCP_ENABLE_FAST_CHANNELS "Enable the SCMI Fast Channels ?"
-    "${SCP_ENABLE_FAST_CHANNELS_INIT}" "DEFINED SCP_ENABLE_FAST_CHANNELS_INIT"
-    "${SCP_ENABLE_FAST_CHANNELS}")
+    SCP_ENABLE_SCMI_PERF_FAST_CHANNELS "Enable the SCMI-perf Fast Channels ?"
+    "${SCP_ENABLE_SCMI_PERF_FAST_CHANNELS_INIT}"
+    "DEFINED SCP_ENABLE_SCMI_PERF_FAST_CHANNELS_INIT"
+    "${SCP_ENABLE_SCMI_PERF_FAST_CHANNELS}")
 
 cmake_dependent_option(
     SCP_ENABLE_PLUGIN_HANDLER "Enable the Performance Plugin Handler ?"
