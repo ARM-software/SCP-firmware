a0f4e51546e107eece3b7b61ccb86574d74d4dbc
Chuyue Luo <Chuyue.Luo@arm.com>
2022-08-23 10:17:29 +0100

    scmi_perf/test: Add further unit tests
    Add unit tests for the following functions:
    - scmi_perf_domain_attributes_handler
    - scmi_perf_describe_levels_handler
    - scmi_perf_describe_fast_channels
    - find_opp_for_level
    - validate_new_limits
    
    Co-authored-by: Nicola Mazzucato <nicola.mazzucato@arm.com>
    Signed-off-by: Chuyue Luo <Chuyue.Luo@arm.com>
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>
    Change-Id: Ie442634db79ff48b12a66a9335e6de2e51c4beaf

--- module/scmi_perf/test/config_scmi_perf.h
+++ module/scmi_perf/test/config_scmi_perf.h
@@ -1,6 +1,6 @@
 /*
  * Arm SCP/MCP Software
- * Copyright (c) 2022, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2022-2023, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -18,6 +18,8 @@
 #define TEST_MODULE_IDX       0x5
 #define TEST_SCMI_AGENT_IDX_0 0x1
 
+#define TEST_OPP_COUNT 0x5
+
 enum dvfs_element_idx {
     DVFS_ELEMENT_IDX_0,
     DVFS_ELEMENT_IDX_1,
@@ -26,7 +28,16 @@ enum dvfs_element_idx {
 };
 
 static const struct mod_scmi_perf_domain_config domains[] = {
-    [DVFS_ELEMENT_IDX_0] = {},
+    [DVFS_ELEMENT_IDX_0] = {
+        .fast_channels_addr_scp =
+            (uint64_t[]){
+                [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] = 1,
+            },
+        .fast_channels_addr_ap =
+            (uint64_t[]){
+                [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] = 1,
+            },
+    },
     [DVFS_ELEMENT_IDX_1] = {},
     [DVFS_ELEMENT_IDX_2] = {},
 };
@@ -37,4 +48,38 @@ struct fwk_module_config config_scmi_perf = {
         .perf_doms_count = FWK_ARRAY_SIZE(domains),
         .fast_channels_alarm_id = FWK_ID_NONE_INIT,
     }),
+};
+
+static const struct mod_dvfs_domain_config test_dvfs_config = {
+    .latency = 1200,
+    .sustained_idx = 2,
+    .opps =
+        (struct mod_dvfs_opp[]){
+            {
+                .level = 100 * 1000000UL,
+                .frequency = 101 * FWK_KHZ,
+                .voltage = 102,
+            },
+            {
+                .level = 200 * 1000000UL,
+                .frequency = 201 * FWK_KHZ,
+                .voltage = 202,
+            },
+            {
+                .level = 300 * 1000000UL,
+                .frequency = 301 * FWK_KHZ,
+                .voltage = 302,
+            },
+            {
+                .level = 400 * 1000000UL,
+                .frequency = 401 * FWK_KHZ,
+                .voltage = 402,
+            },
+            {
+                .level = 500 * 1000000UL,
+                .frequency = 501 * FWK_KHZ,
+                .voltage = 502,
+            },
+            { 0 },
+        }
 };
