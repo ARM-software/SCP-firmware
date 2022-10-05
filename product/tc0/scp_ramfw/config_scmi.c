6d8c4fd6a714e68982e46ec235092487ce258cbd
Girish Pathak <girish.pathak@arm.com>
2022-10-05 15:15:00 +0100

    product/tc0: Move from module smt to transport
    This change makes use of a more generic transport module instead of
    smt for the tc0 platform. The new transport module has additional
    features such as in-band message transport and fast channel
    interface that are missing in the traditional smt.
    
    Since the transport module provides all the features of the smt
    module, the smt module will be removed entirely from the codebase
    after all other dependent platforms moves to use the transport module.
    
    Change-Id: I4559ac2b8fbdcc05dd6e344c10e45d20408ba6f1
    Signed-off-by: Girish Pathak <girish.pathak@arm.com>

--- product/tc0/scp_ramfw/config_scmi.c
+++ product/tc0/scp_ramfw/config_scmi.c
@@ -1,6 +1,6 @@
 /*
  * Arm SCP/MCP Software
- * Copyright (c) 2020-2021, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2020-2022, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -8,7 +8,7 @@
 #include "tc0_scmi.h"
 
 #include <mod_scmi.h>
-#include <mod_smt.h>
+#include <mod_transport.h>
 
 #include <fwk_element.h>
 #include <fwk_id.h>
@@ -21,14 +21,14 @@ static const struct fwk_element service_table[] = {
         .name = "PSCI",
         .data = &((struct mod_scmi_service_config) {
             .transport_id = FWK_ID_ELEMENT_INIT(
-                FWK_MODULE_IDX_SMT,
+                FWK_MODULE_IDX_TRANSPORT,
                 SCP_TC0_SCMI_SERVICE_IDX_PSCI),
             .transport_api_id = FWK_ID_API_INIT(
-                FWK_MODULE_IDX_SMT,
-                MOD_SMT_API_IDX_SCMI_TRANSPORT),
+                FWK_MODULE_IDX_TRANSPORT,
+                MOD_TRANSPORT_API_IDX_SCMI_TO_TRANSPORT),
             .transport_notification_init_id = FWK_ID_NOTIFICATION_INIT(
-                FWK_MODULE_IDX_SMT,
-                MOD_SMT_NOTIFICATION_IDX_INITIALIZED),
+                FWK_MODULE_IDX_TRANSPORT,
+                MOD_TRANSPORT_NOTIFICATION_IDX_INITIALIZED),
             .scmi_agent_id = SCP_SCMI_AGENT_ID_PSCI,
             .scmi_p2a_id = FWK_ID_NONE_INIT,
         }),
@@ -37,14 +37,14 @@ static const struct fwk_element service_table[] = {
         .name = "OSPM0",
         .data = &((struct mod_scmi_service_config) {
             .transport_id = FWK_ID_ELEMENT_INIT(
-                FWK_MODULE_IDX_SMT,
+                FWK_MODULE_IDX_TRANSPORT,
                 SCP_TC0_SCMI_SERVICE_IDX_OSPM_0),
             .transport_api_id = FWK_ID_API_INIT(
-                FWK_MODULE_IDX_SMT,
-                MOD_SMT_API_IDX_SCMI_TRANSPORT),
+                FWK_MODULE_IDX_TRANSPORT,
+                MOD_TRANSPORT_API_IDX_SCMI_TO_TRANSPORT),
             .transport_notification_init_id = FWK_ID_NOTIFICATION_INIT(
-                FWK_MODULE_IDX_SMT,
-                MOD_SMT_NOTIFICATION_IDX_INITIALIZED),
+                FWK_MODULE_IDX_TRANSPORT,
+                MOD_TRANSPORT_NOTIFICATION_IDX_INITIALIZED),
             .scmi_agent_id = SCP_SCMI_AGENT_ID_OSPM,
         }),
     },
@@ -52,14 +52,14 @@ static const struct fwk_element service_table[] = {
         .name = "OSPM1",
         .data = &((struct mod_scmi_service_config) {
             .transport_id = FWK_ID_ELEMENT_INIT(
-                FWK_MODULE_IDX_SMT,
+                FWK_MODULE_IDX_TRANSPORT,
                 SCP_TC0_SCMI_SERVICE_IDX_OSPM_1),
             .transport_api_id = FWK_ID_API_INIT(
-                FWK_MODULE_IDX_SMT,
-                MOD_SMT_API_IDX_SCMI_TRANSPORT),
+                FWK_MODULE_IDX_TRANSPORT,
+                MOD_TRANSPORT_API_IDX_SCMI_TO_TRANSPORT),
             .transport_notification_init_id = FWK_ID_NOTIFICATION_INIT(
-                FWK_MODULE_IDX_SMT,
-                MOD_SMT_NOTIFICATION_IDX_INITIALIZED),
+                FWK_MODULE_IDX_TRANSPORT,
+                MOD_TRANSPORT_NOTIFICATION_IDX_INITIALIZED),
             .scmi_agent_id = SCP_SCMI_AGENT_ID_OSPM,
             .scmi_p2a_id = FWK_ID_NONE_INIT,
         }),
