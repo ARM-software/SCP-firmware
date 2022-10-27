dbd92d0e2064a77bd0c982233f3a24cd3129357f
Leandro Belli <leandro.belli@arm.com>
2022-10-27 14:37:16 +0100

    fwk/arch: remove all `Makefile` deprecated files
    This patch removes all Makefile related files since they are
    deprecated and non longer maintained.
    
    Signed-off-by: Leandro Belli <leandro.belli@arm.com>
    Change-Id: I69dd25464f455d386112b0a3a92e17e0dc2b09a3

--- arch/none/vendor.mk
+++
@@ -1,8 +0,0 @@
-#
-# Arm SCP/MCP Software
-# Copyright (c) 2018-2021, Arm Limited and Contributors. All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-#
-
-include $(ARCH_DIR)/$(BS_ARCH_VENDOR)/$(BS_ARCH_ARCH)/arch.mk
