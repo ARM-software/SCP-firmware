6f2cbe7466a799d7867e6a42a65e8756fcd0cdbc
Girish Pathak <girish.pathak@arm.com>
2022-11-18 18:41:36 +0000

    module/mhu3: Add unit tests
    This change adds unit test code for module mhu3.
    
    Change-Id: I6dab855dea7df7f2393b553b1accb5cdb9e2cd0c
    Signed-off-by: Girish Pathak <girish.pathak@arm.com>

---
+++ module/mhu3/test/fwk_module_idx.h
@@ -0,0 +1,23 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2022, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef TEST_FWK_MODULE_MODULE_IDX_H
+#define TEST_FWK_MODULE_MODULE_IDX_H
+
+#include <fwk_id.h>
+
+enum fwk_module_idx {
+    FWK_MODULE_IDX_SCMI,
+    FWK_MODULE_IDX_TRANSPORT,
+    FWK_MODULE_IDX_MHU3,
+    FWK_MODULE_IDX_COUNT,
+};
+
+static const fwk_id_t fwk_module_id_scmi =
+    FWK_ID_MODULE_INIT(FWK_MODULE_IDX_SCMI);
+
+#endif /* TEST_FWK_MODULE_MODULE_IDX_H */
