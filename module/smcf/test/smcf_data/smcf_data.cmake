7e077d9a526e3010c2a5ba4ac903019acc7289dd
Tarek El-Sherbiny <tarek.el-sherbiny@arm.com>
2023-01-03 17:25:04 +0000

    smcf unit_test: Create module unit tests
    This patch adds unit tests for both mgi.c and mod_smcf.c.
    Each test is in a separate directory. The tests for mod_smcf.c
    requires mocking the mgi.c functions. The required mocks are
    generated using the mock generator script gm.rb in unit_test
    directory and saved under smcf/test/mocks directory.
    
    Signed-off-by: Tarek El-Sherbiny <tarek.el-sherbiny@arm.com>
    Co-authored-by: Abhishek Rawat <abhishek.rawat@arm.com>
    Change-Id: I33250e35b9c58fb26166084fbbf1ef0df4c4f4c2

---
+++ module/smcf/test/smcf_data/smcf_data.cmake
@@ -0,0 +1,32 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2023, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+set(TEST_SRC smcf_data)
+set(TEST_FILE smcf_data)
+
+if(TEST_ON_TARGET)
+    set(TEST_MODULE smcf)
+    set(MODULE_ROOT ${CMAKE_SOURCE_DIR}/module)
+else()
+    set(TEST_FILE smcf_data)
+    set(UNIT_TEST_TARGET ${TEST_FILE}_unit_test)
+endif()
+
+set(MODULE_SRC ${MODULE_ROOT}/${TEST_MODULE}/src)
+set(MODULE_INC ${MODULE_ROOT}/${TEST_MODULE}/include)
+set(MODULE_UT_SRC ${CMAKE_CURRENT_LIST_DIR})
+set(MODULE_UT_INC ${CMAKE_CURRENT_LIST_DIR})
+set(MODULE_UT_MOCK_SRC ${CMAKE_CURRENT_LIST_DIR}/../mocks)
+
+list(APPEND MOCK_REPLACEMENTS fwk_module)
+list(APPEND MOCK_REPLACEMENTS fwk_id)
+list(APPEND MOCK_REPLACEMENTS fwk_mm)
+
+include(${SCP_ROOT}/unit_test/module_common.cmake)
+
+target_sources(${UNIT_TEST_TARGET}
+        PRIVATE ${MODULE_UT_MOCK_SRC}/Mockmgi.c)
