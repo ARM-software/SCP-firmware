2823b5cf7244baf3f8c579c2b43ebadd394d7636
Ben Horgan <ben.horgan@arm.com>
2023-01-17 14:25:29 +0000

    product/tc2: Add dvfs power values
    Adding the power values for dvfs based on provisional values of
    dynamic-power-coefficient.
    
    Signed-off-by: Ben Horgan <ben.horgan@arm.com>
    Change-Id: I101a70ceebb11cbfb5914b8fec9d17759bbbaeed

--- product/tc2/scp_ramfw/config_dvfs.c
+++ product/tc2/scp_ramfw/config_dvfs.c
@@ -1,6 +1,6 @@
 /*
  * Arm SCP/MCP Software
- * Copyright (c) 2022, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2022-2023, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -18,31 +18,52 @@
 #include <fwk_module.h>
 #include <fwk_module_idx.h>
 
+/*
+ * The power cost figures in this file are built using the dynamic power
+ * consumption formula (P = CfV^2), where C represents the capacitance of one
+ * processing element in the domain (a core or shader core). This power figure
+ * is scaled linearly with the number of processing elements in the performance
+ * domain to give a rough representation of the overall power draw. The
+ * capacitance constants are given in mW/MHz/V^2 and were taken from the Linux
+ * device trees, which provide a dynamic-power-coefficient field in uW/MHz/V^2.
+ * This conversion of units, from uW/MHz/V^2 to mW/MHz/V^2, is done by dividing
+ * by 1000.
+ */
+
+/* dynamic-power-coeffient/1000 */
+#define HAYES_DPC  0.230
+#define HUNTER_DPC 0.495
+
 static struct mod_dvfs_opp operating_points_hayes[6] = {
     {
         .level = 768 * 1000000UL,
         .frequency = 768 * FWK_KHZ,
         .voltage = 550,
+        .power = (uint32_t)(HAYES_DPC * 768 * 0.550 * 0.550),
     },
     {
         .level = 1153 * 1000000UL,
         .frequency = 1153 * FWK_KHZ,
         .voltage = 650,
+        .power = (uint32_t)(HAYES_DPC * 1153 * 0.650 * 0.650),
     },
     {
         .level = 1537 * 1000000UL,
         .frequency = 1537 * FWK_KHZ,
         .voltage = 750,
+        .power = (uint32_t)(HAYES_DPC * 1537 * 0.750 * 0.750),
     },
     {
         .level = 1844 * 1000000UL,
         .frequency = 1844 * FWK_KHZ,
         .voltage = 850,
+        .power = (uint32_t)(HAYES_DPC * 1844 * 0.850 * 0.850),
     },
     {
         .level = 2152 * 1000000UL,
         .frequency = 2152 * FWK_KHZ,
         .voltage = 950,
+        .power = (uint32_t)(HAYES_DPC * 2152 * 0.950 * 0.950),
     },
     { 0 }
 };
@@ -52,26 +73,31 @@ static struct mod_dvfs_opp operating_points_hunter[6] = {
         .level = 946 * 1000000UL,
         .frequency = 946 * FWK_KHZ,
         .voltage = 550,
+        .power = (uint32_t)(HUNTER_DPC * 946 * 0.550 * 0.550),
     },
     {
         .level = 1419 * 1000000UL,
         .frequency = 1419 * FWK_KHZ,
         .voltage = 650,
+        .power = (uint32_t)(HUNTER_DPC * 1419 * 0.650 * 0.650),
     },
     {
         .level = 1893 * 1000000UL,
         .frequency = 1893 * FWK_KHZ,
         .voltage = 750,
+        .power = (uint32_t)(HUNTER_DPC * 1893 * 0.750 * 0.750),
     },
     {
         .level = 2271 * 1000000UL,
         .frequency = 2271 * FWK_KHZ,
         .voltage = 850,
+        .power = (uint32_t)(HUNTER_DPC * 2271 * 0.850 * 0.850),
     },
     {
         .level = 2650 * 1000000UL,
         .frequency = 2650 * FWK_KHZ,
         .voltage = 950,
+        .power = (uint32_t)(HUNTER_DPC * 2650 * 0.950 * 0.950),
     },
     { 0 }
 };
