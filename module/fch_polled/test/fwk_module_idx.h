db575b26848e395222ffed70adcc9b812b008e1f
Girish Pathak <girish.pathak@arm.com>
2023-03-03 16:58:56 +0000

    fch_polled/unit_test: Add unit tests for fch_polled
    This change adds unit tests for new fch_polled
    module.
    
    Change-Id: Ia6ec7e5d009afd6c75cff3894ef4e7a98b4e19f3
    Signed-off-by: Girish Pathak <girish.pathak@arm.com>
    Signed-off-by: Ahmed Gadallah <ahmed.gadallah@arm.com>

---
+++ module/fch_polled/test/fwk_module_idx.h
@@ -0,0 +1,20 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2023, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef TEST_FWK_MODULE_MODULE_IDX_H
+#define TEST_FWK_MODULE_MODULE_IDX_H
+
+#include <fwk_id.h>
+
+enum fwk_module_idx {
+    FWK_MODULE_IDX_TRANSPORT,
+    FWK_MODULE_IDX_TIMER,
+    FWK_MODULE_IDX_FCH_POLLED,
+    FWK_MODULE_IDX_COUNT,
+};
+
+#endif /* TEST_FWK_MODULE_MODULE_IDX_H */
