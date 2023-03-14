c85cca5d159380a0cc6fb75e77be2ffe77f4ae0a
sahil <sahil@arm.com>
2023-03-14 10:45:42 +0530

    n1sdp: Add configs for timer and gtimer in MCP RAM firmware
    This patch adds initial support for timer and gtimer modules.
    Further upcoming patches will make use of these modules.
    
    Signed-off-by: sahil <sahil@arm.com>
    Change-Id: I7a64d3fcfc1e0b4c618f76181af5fc2506f6de19

--- product/n1sdp/mcp_ramfw/Firmware.cmake
+++ product/n1sdp/mcp_ramfw/Firmware.cmake
@@ -1,6 +1,6 @@
 #
 # Arm SCP/MCP Software
-# Copyright (c) 2021-2022, Arm Limited and Contributors. All rights reserved.
+# Copyright (c) 2021-2023, Arm Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -39,6 +39,8 @@ list(APPEND SCP_MODULES "armv7m-mpu")
 list(APPEND SCP_MODULES "pl011")
 list(APPEND SCP_MODULES "pik-clock")
 list(APPEND SCP_MODULES "clock")
+list(APPEND SCP_MODULES "gtimer")
+list(APPEND SCP_MODULES "timer")
 list(APPEND SCP_MODULES "n1sdp-mhu")
 list(APPEND SCP_MODULES "n1sdp-smt")
 list(APPEND SCP_MODULES "scmi-agent")
