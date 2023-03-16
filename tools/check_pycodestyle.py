4313ecb883ca09845936d07fcddd8054621d1918
Mike Leach <mike.leach@linaro.org>
2023-03-16 20:25:57 +0000

    tools: Fix minor issues with tools
    Update cppcheck suppress list to handle a header file introduced in the imported
    device tree headers.
    
    Fix check_pycodestyle which indicates it uses python3 but called python in the
    script, failing on python3 based hosts
    
    Signed-off-by: Mike Leach <mike.leach@linaro.org>

--- tools/check_pycodestyle.py
+++ tools/check_pycodestyle.py
@@ -26,7 +26,7 @@ def main():
     banner("Build and run PyCodeStyle tests")
 
     result = subprocess.Popen(
-        "python -m pycodestyle {}".format(INCLUDE_DIRECTORIES),
+        "python3 -m pycodestyle {}".format(INCLUDE_DIRECTORIES),
         shell=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE)
