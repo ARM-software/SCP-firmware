a1bee2b3f268644f783f4005678d218a441147a4
Mohamed Omar Asaker <mohamed.omarasaker@arm.com>
2023-06-07 18:30:07 +0100

    module/optee/mbx: Add a skeleton of unit test
    The unit test is based on the ut template stated in the user guide.
    
    Signed-off-by: Mohamed Omar Asaker <mohamed.omarasaker@arm.com>
    Change-Id: I19dabe987a7a1b7dea3f98fe75cb3aded27da7a0

---
+++ module/optee/mbx/test/mod_optee_mbx_extra.h
@@ -0,0 +1,21 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2023, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Unity test framework needs the mocked version of the functions which are
+ * called by the function under test, e.g. in this case
+ * mutex_init and mutex_unlock.
+ * Normally mocked version of the function can be generated using
+ * CMock tool(see unit_test/user_guide.md)
+ * by providing the header that includes declarations. However, in a few cases
+ * it would need other header files as well complicating the Mocks generation.
+ * This file demonstrates, how to achieve requires mocks generation without
+ * much dependency.
+ */
+
+#include <kernel/mutex.h>
+
+void mutex_init(struct mutex *m);
+void mutex_unlock(struct mutex *m);
