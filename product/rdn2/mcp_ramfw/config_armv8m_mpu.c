0ff90408ef841250be24eef4aa9849c05b3c1d1c
Ahmed Gadallah <ahmed.gadallah@arm.com>
2023-08-09 15:07:41 +0100

    [TEST]rdn2: add MPU configuration
    Do not merge.
    
    Signed-off-by: Ahmed Gadallah <ahmed.gadallah@arm.com>
    Change-Id: I16f80de4e02578e4a404618e931a37f483838701

--- product/rdn2/mcp_ramfw/config_armv8m_mpu.c
+++ product/rdn2/mcp_ramfw/config_armv8m_mpu.c
@@ -13,41 +13,52 @@
 
 #include <fmw_cmsis.h>
 
-static const uint8_t attributes[2] = {
+static const uint8_t attributes[3] = {
     /* Device memory, non Gathering, non Re-ordering, non Early Write
        Acknowledgement */
-    /*
     [MPU_ATTR_0] =
         ARM_MPU_ATTR(ARM_MPU_ATTR_DEVICE, ARM_MPU_ATTR_DEVICE_nGnRnE),
-    */
+
     /* Normal memory, non Cacheable */
-    /*
     [MPU_ATTR_1] =
         ARM_MPU_ATTR(ARM_MPU_ATTR_NON_CACHEABLE, ARM_MPU_ATTR_NON_CACHEABLE),
-    */
+
+    /* Normal memory, Inner Cacheable */
+    [MPU_ATTR_2] = ARM_MPU_ATTR(
+        ARM_MPU_ATTR_MEMORY_(0, 1, 1, 1),
+        ARM_MPU_ATTR_NON_CACHEABLE),
 };
 
 static const ARM_MPU_Region_t regions[] = {
     {
-        /* 0x0000_0000 - 0xFFFF_FFFF */
-        /*
-        .RBAR = ...
-        .RLAR = ...
-        */
+        /* 0x0000_0000 - 0x007F_FFFF */
+        .RBAR = ARM_MPU_RBAR(
+            MCP_BOOT_ROM_BASE, /* BASE */
+            ARM_MPU_SH_INNER, /* SH */
+            0, /* RO */
+            0, /* NP */
+            1 /* XN */),
+        .RLAR = ARM_MPU_RLAR(MCP_ITC_RAM_BASE - 1, MPU_ATTR_1),
     },
     {
-        /* 0x0080_0000 - 0x00A0_0000 */
-        /*
-        .RBAR = ...
-        .RLAR = ...
-        */
+        /* 0x0080_0000 - 0x009F_FFFF*/
+        .RBAR = ARM_MPU_RBAR(
+            MCP_ITC_RAM_BASE, /* BASE */
+            ARM_MPU_SH_NON, /* SH */
+            1, /* RO */
+            0, /* NP */
+            0 /* XN */),
+        .RLAR = ARM_MPU_RLAR(0x9FFFFF, MPU_ATTR_2),
     },
     {
-        /* 0x2000_0000 - 2020_0000 */
-        /*
-        .RBAR = ...
-        .RLAR = ...
-        */
+        /* 0x2000_0000 - 201F_FFFF*/
+        .RBAR = ARM_MPU_RBAR(
+            MCP_DTC_RAM_BASE, /* BASE */
+            ARM_MPU_SH_NON, /* SH */
+            0, /* RO */
+            0, /* NP */
+            1 /* XN */),
+        .RLAR = ARM_MPU_RLAR(0x201FFFFF, MPU_ATTR_2),
     },
 };
 
--- product/rdn2/scp_ramfw/config_armv8m_mpu.c
+++ product/rdn2/scp_ramfw/config_armv8m_mpu.c
@@ -15,48 +15,72 @@
 
 #include <fmw_cmsis.h>
 
-static const uint8_t attributes[2] = {
+static const uint8_t attributes[3] = {
     /* Device memory, non Gathering, non Re-ordering, non Early Write
        Acknowledgement */
-    /*
     [MPU_ATTR_0] =
         ARM_MPU_ATTR(ARM_MPU_ATTR_DEVICE, ARM_MPU_ATTR_DEVICE_nGnRnE),
-    */
+
     /* Normal memory, non Cacheable */
-    /*
     [MPU_ATTR_1] =
         ARM_MPU_ATTR(ARM_MPU_ATTR_NON_CACHEABLE, ARM_MPU_ATTR_NON_CACHEABLE),
-    */
+
+    /* Normal memory, Inner Cacheable */
+    [MPU_ATTR_2] = ARM_MPU_ATTR(
+        ARM_MPU_ATTR_MEMORY_(0, 1, 1, 1),
+        ARM_MPU_ATTR_NON_CACHEABLE),
 };
 
 static const ARM_MPU_Region_t regions[] = {
     {
-        /* 0x0000_0000 - 0xFFFF_FFFF */
-        /*
-        .RBAR = ...
-        .RLAR = ...
-        */
+        /* 0x0000_0000 - 0x007F_FFFF */
+        .RBAR = ARM_MPU_RBAR(
+            0x00000000, /* BASE */
+            ARM_MPU_SH_INNER, /* SH */
+            0, /* RO */
+            0, /* NP */
+            1 /* XN */),
+        .RLAR = ARM_MPU_RLAR(0x7fffff, MPU_ATTR_1),
+    },
+    {
+        /* 0x0080_0000 - 0x00FF_FFFF*/
+        .RBAR = ARM_MPU_RBAR(
+            SCP_ITC_RAM_BASE, /* BASE */
+            ARM_MPU_SH_NON, /* SH */
+            1, /* RO */
+            0, /* NP */
+            0 /* XN */),
+        .RLAR = ARM_MPU_RLAR(0xffffff, MPU_ATTR_2),
+    },
+    {
+        /* 0x2000_0000 - 0x20FF_FFFF */
+        .RBAR = ARM_MPU_RBAR(
+            SCP_DTC_RAM_BASE, /* BASE */
+            ARM_MPU_SH_NON, /* SH */
+            0, /* RO */
+            0, /* NP */
+            1 /* XN */),
+        .RLAR = ARM_MPU_RLAR(0xffffff, MPU_ATTR_2),
     },
     {
         /* 0xA400_0000 - 0xA400_7FFF*/
-        /*
-        .RBAR = ...
-        .RLAR = ...
-        */
+        .RBAR = ARM_MPU_RBAR(
+            SCP_TRUSTED_RAM_BASE, /* BASE */
+            ARM_MPU_SH_INNER, /* SH */
+            0, /* RO */
+            0, /* NP */
+            1 /* XN */),
+        .RLAR = ARM_MPU_RLAR(0xA4007FFF, MPU_ATTR_2),
     },
     {
-        /* 0xA400_0000 - 0xA400_7FFF*/
-        /*
-        .RBAR = ...
-        .RLAR = ...
-        */
-    },
-    {
-        /* 0xA600_0000 - 0xA600_7FFF */
-        /*
-        .RBAR = ...
-        .RLAR = ...
-        */
+        /* 0xA600_0000 - 0xA600_7FFF*/
+        .RBAR = ARM_MPU_RBAR(
+            SCP_NONTRUSTED_RAM_BASE, /* BASE */
+            ARM_MPU_SH_INNER, /* SH */
+            0, /* RO */
+            0, /* NP */
+            1 /* XN */),
+        .RLAR = ARM_MPU_RLAR(0xA6007FFF, MPU_ATTR_2),
     },
 };
 
