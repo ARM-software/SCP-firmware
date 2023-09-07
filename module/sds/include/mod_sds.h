c8806ab938e21cfa1e7ab731c7f53fb04ce50a2c
abhraw01 <abhishek.rawat@arm.com>
2023-09-07 14:20:41 +0100

    sds: update clock identifier for sds module as optional identifier
    This change updates the sds module's clock identifier type as optional
    identifier(fwk_optional_id_t) to avoid build guards.
    
    Signed-off-by: Abhishek Rawat <abhishek.rawat@arm.com>
    Change-Id: I2b0c3c143995445b27af7a9fd90e18bb2227f48a

--- module/sds/include/mod_sds.h
+++ module/sds/include/mod_sds.h
@@ -134,10 +134,8 @@ struct mod_sds_config {
      */
     unsigned int region_count;
 
-#ifdef BUILD_HAS_MOD_CLOCK
     /*! Identifier of the clock that this module depends on */
-    fwk_id_t clock_id;
-#endif
+    fwk_optional_id_t clock_id;
 
     /*!
      * Platform notification source and notification id (optional)
