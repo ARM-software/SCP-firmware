a39a90ec24ea2f2dab0f371cab4cf5d81ea533d5
khaled ali ahmed <khaled.aliahmed@arm.com>
2023-09-08 11:28:41 +0100

    thermal_mgmt: Add a cold state power configuration
    Add cold_state_power configuration to be used instead of TDP when
    temperature is below `switch_on_temperature`.
    To accommodate the cases where it's possible not to limit or use
    different limits than TDP for the distributable power when
    the system is "cold."
    
    Signed-off-by: khaled ali ahmed <khaled.aliahmed@arm.com>
    Change-Id: I5fe9b148ec2a26bcfc0c2242b596d970a7b26d4b

--- module/thermal_mgmt/test/mod_thermal_mgmt_unit_test.c
+++ module/thermal_mgmt/test/mod_thermal_mgmt_unit_test.c
@@ -1,6 +1,6 @@
 /*
  * Arm SCP/MCP Software
- * Copyright (c) 2022, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2022-2023, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -483,7 +483,8 @@ void test_thermal_mgmt_pi_control_switched_off(void)
     pi_control(element_id);
     TEST_ASSERT_EQUAL(dev_ctx->integral_error, 0);
     TEST_ASSERT_EQUAL(
-        dev_ctx->thermal_allocatable_power, (uint32_t)dev_ctx->config->tdp);
+        dev_ctx->thermal_allocatable_power,
+        (uint32_t)dev_ctx->config->cold_state_power);
 }
 
 void test_thermal_mgmt_pi_control_anti_windup(void)
