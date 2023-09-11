90da07c1ba44d4cb00439cb5d58623ecc2626f98
Shriram K <shriram.k@arm.com>
2023-09-11 15:58:32 +0000

    rdfremont: add system info module config data for scp_ramfw
    System Info module provides API to obtain platform configuration data in
    a platform independent manner. So, add configuration data for system
    info module in scp ramfw. The configuration data specifies the PID
    module as the driver to obtain the platform configuration data.
    
    Signed-off-by: Shriram K <shriram.k@arm.com>
    Change-Id: I5de1b605ec66057709ea407865958504b9410562

---
+++ product/neoverse-rd/rdfremont/scp_ramfw/config_system_info.c
@@ -0,0 +1,27 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2024, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Configuration data for module 'system_info'.
+ */
+
+#include <mod_sid.h>
+#include <mod_system_info.h>
+
+#include <fwk_id.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+
+#include <stddef.h>
+
+const struct fwk_module_config config_system_info = {
+    .data = &((struct mod_system_info_config){
+        .system_info_driver_module_id = FWK_ID_MODULE_INIT(FWK_MODULE_IDX_SID),
+        .system_info_driver_data_api_id = FWK_ID_API_INIT(
+            FWK_MODULE_IDX_SID,
+            MOD_SID_SYSTEM_INFO_DRIVER_DATA_API_IDX),
+    }),
+};
