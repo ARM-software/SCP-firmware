22668eda1df76537e07b30bd79dd46dbfd18b155
Shriram K <shriram.k@arm.com>
2023-09-12 15:37:29 +0000

    rdfremont: add config data for scmi power domain in scpfw
    There is no specific configuration data to be supplied to the scmi
    power domain module. So, leave the config empty.
    
    Signed-off-by: Shriram K <shriram.k@arm.com>
    Change-Id: Ib91a7f9c96e091b1e8f1f45181be99b9a60c1318

---
+++ product/neoverse-rd/rdfremont/scp_ramfw/config_scmi_power_domain.c
@@ -0,0 +1,14 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2024, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Configuration data for module 'scmi_power_domain'.
+ */
+
+#include <fwk_module.h>
+
+/* No elements, no module configuration data */
+struct fwk_module_config config_scmi_power_domain = { 0 };
