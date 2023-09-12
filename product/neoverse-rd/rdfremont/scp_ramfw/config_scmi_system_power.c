ea8b3bddc1d559a7e50bf38056eac39cbf591492
Shriram K <shriram.k@arm.com>
2023-09-12 15:38:58 +0000

    rdfremont: add config data for scmi system power module in scpfw
    Provide the configuration data for scmi system power module in scp
    ramfw to manage system states.
    
    Signed-off-by: Shriram K <shriram.k@arm.com>
    Change-Id: Iddb20ee912a1918e2e61f7c0b15dfe697a9f1f1a

---
+++ product/neoverse-rd/rdfremont/scp_ramfw/config_scmi_system_power.c
@@ -0,0 +1,21 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2024, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Configuration data for module 'scmi_system_power'.
+ */
+
+#include <mod_scmi_system_power.h>
+#include <mod_system_power.h>
+
+#include <fwk_module.h>
+
+const struct fwk_module_config config_scmi_system_power = {
+    .data = &((struct mod_scmi_system_power_config){
+        .system_view = MOD_SCMI_SYSTEM_VIEW_FULL,
+        .system_suspend_state = MOD_SYSTEM_POWER_POWER_STATE_SLEEP0,
+    }),
+};
