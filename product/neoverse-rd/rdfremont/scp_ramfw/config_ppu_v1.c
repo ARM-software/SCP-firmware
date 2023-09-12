cbdd98dc4f69969633b77b6469fc1964c0e40e92
Shriram K <shriram.k@arm.com>
2023-09-12 13:22:33 +0000

    rdfremont: add config data for ppu v1 module in scpfw
    Provide the configuration data of all the available ppu instances to
    the PPU v1 driver. The PPU instances of CPUs, Clusters and the SYSTOP
    are included in the configuration data.
    
    Signed-off-by: Shriram K <shriram.k@arm.com>
    Change-Id: I7200636f06f78e8037d72d4fdb45b998aa7ca032

---
+++ product/neoverse-rd/rdfremont/scp_ramfw/config_ppu_v1.c
@@ -0,0 +1,151 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2024, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Configuration data for module 'ppu_v1'.
+ */
+
+#include "platform_core.h"
+#include "scp_cfgd_power_domain.h"
+#include "scp_css_mmap.h"
+
+#include <mod_power_domain.h>
+#include <mod_ppu_v1.h>
+
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_interrupt.h>
+#include <fwk_macros.h>
+#include <fwk_mm.h>
+#include <fwk_module.h>
+#include <fwk_module_idx.h>
+#include <fwk_string.h>
+
+#include <stdio.h>
+#include <string.h>
+
+/* Number of pre-initialized module configuration data elements */
+#define PPU_STATIC_ELEMENT_COUNT 1
+
+/* Maximum PPU core name size including the null terminator */
+#define PPU_CORE_NAME_SIZE 20
+
+/* Maximum PPU cluster name size including the null terminator */
+#define PPU_CLUS_NAME_SIZE 7
+
+/* Module configuration data */
+static struct mod_ppu_v1_config ppu_v1_config_data = {
+    .pd_notification_id = FWK_ID_NOTIFICATION_INIT(
+        FWK_MODULE_IDX_POWER_DOMAIN,
+        MOD_PD_NOTIFICATION_IDX_POWER_STATE_TRANSITION),
+};
+
+/* List of static PPU elements */
+static struct fwk_element ppu_element_table[PPU_STATIC_ELEMENT_COUNT] = {
+    {
+        .name = "SYS0",
+        .data = &((struct mod_ppu_v1_pd_config){
+            .pd_type = MOD_PD_TYPE_SYSTEM,
+            .ppu.reg_base = SCP_PPU_SYS0_BASE,
+            .default_power_on = true,
+            .observer_id = FWK_ID_NONE_INIT,
+        }),
+    },
+};
+
+static const struct fwk_element *ppu_v1_get_element_table(fwk_id_t module_id)
+{
+    struct fwk_element *element_table, *element;
+    struct mod_ppu_v1_pd_config *pd_config_table, *pd_config;
+    unsigned int core_idx;
+    unsigned int cluster_idx;
+    unsigned int core_count;
+    unsigned int cluster_count;
+    unsigned int core_element_count = 0;
+
+    core_count = platform_get_core_count();
+    cluster_count = platform_get_cluster_count();
+
+    /*
+     * Allocate element descriptors based on:
+     *   Number of cores
+     *   + Number of cluster descriptors
+     *   + Number of system power domain descriptors
+     *   + 1 terminator descriptor
+     */
+    element_table = fwk_mm_calloc(
+        core_count + cluster_count + FWK_ARRAY_SIZE(ppu_element_table) + 1,
+        sizeof(struct fwk_element));
+
+    pd_config_table = fwk_mm_calloc(
+        core_count + cluster_count, sizeof(struct mod_ppu_v1_pd_config));
+
+    for (cluster_idx = 0; cluster_idx < cluster_count; cluster_idx++) {
+        for (core_idx = 0;
+             core_idx < platform_get_core_per_cluster_count(cluster_idx);
+             core_idx++) {
+            element = &element_table[core_element_count];
+            pd_config = &pd_config_table[core_element_count];
+
+            element->name = fwk_mm_alloc(PPU_CORE_NAME_SIZE, 1);
+
+            snprintf(
+                (char *)element->name,
+                PPU_CORE_NAME_SIZE,
+                "CLUS%uCORE%u",
+                cluster_idx,
+                core_idx);
+
+            element->data = pd_config;
+
+            pd_config->pd_type = MOD_PD_TYPE_CORE;
+            pd_config->ppu.reg_base =
+                SCP_CLUSTER_UTILITY_CORE_PPU_BASE(cluster_idx);
+            pd_config->ppu.irq = FWK_INTERRUPT_NONE;
+            pd_config->cluster_id = FWK_ID_ELEMENT(
+                FWK_MODULE_IDX_PPU_V1, (core_count + cluster_idx));
+            pd_config->observer_id = FWK_ID_NONE;
+            core_element_count++;
+        }
+
+        element = &element_table[core_count + cluster_idx];
+        pd_config = &pd_config_table[core_count + cluster_idx];
+
+        element->name = fwk_mm_alloc(PPU_CLUS_NAME_SIZE, 1);
+
+        snprintf(
+            (char *)element->name, PPU_CLUS_NAME_SIZE, "CLUS%u", cluster_idx);
+
+        element->data = pd_config;
+
+        pd_config->pd_type = MOD_PD_TYPE_CLUSTER;
+        pd_config->ppu.irq = FWK_INTERRUPT_NONE;
+        pd_config->observer_id = FWK_ID_NONE;
+        pd_config->observer_api = FWK_ID_NONE;
+        pd_config->ppu.reg_base =
+            SCP_CLUSTER_UTILITY_CLUSTER_PPU_BASE(cluster_idx);
+    }
+
+    fwk_str_memcpy(
+        &element_table[core_count + cluster_count],
+        ppu_element_table,
+        sizeof(ppu_element_table));
+
+    /*
+     * Configure pd_source_id with the SYSTOP identifier from the power domain
+     * module which is dynamically defined based on the number of cores.
+     */
+    ppu_v1_config_data.pd_source_id = fwk_id_build_element_id(
+        fwk_module_id_power_domain,
+        core_count + cluster_count + PD_STATIC_DEV_IDX_SYSTOP);
+
+    return element_table;
+}
+
+const struct fwk_module_config config_ppu_v1 = {
+    .data = &ppu_v1_config_data,
+    .elements = FWK_MODULE_DYNAMIC_ELEMENTS(ppu_v1_get_element_table),
+};
