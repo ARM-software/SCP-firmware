37cdb9f5b5f300dce61ce11b960719596c6c7e8e
Shriram K <shriram.k@arm.com>
2023-09-12 20:28:43 +0000

    rdfremont: add config data for clock module in mcpfw
    The clock module config data is empty for mcp firmware since this module
    is required only to resolve build time dependencies across other modules.
    
    Signed-off-by: Shriram K <shriram.k@arm.com>
    Change-Id: I6bd3f5203cb2b2297546f3c8999180849b17f390

---
+++ product/neoverse-rd/rdfremont/mcp_ramfw/config_clock.c
@@ -0,0 +1,13 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2024, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Configuration data for module 'clock'.
+ */
+
+#include <fwk_module.h>
+
+const struct fwk_module_config config_clock = { 0 };
