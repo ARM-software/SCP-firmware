b0941ef891b38e8a10bdd9b82ebfee996c17af8f
Shriram K <shriram.k@arm.com>
2023-09-14 21:34:11 +0000

    rdfremont: add platform module for mcpfw
    Add an initial implementation of the MCP platform module that indicates
    the completion of the MCP RAM boot stage. This module is expected to get
    additional functionality in subsequent additions to the MCP RAM
    firmware.
    
    Signed-off-by: Shriram K <shriram.k@arm.com>
    Change-Id: I16bafa6199f0d90beb8eba6d927c2f8d87692362

---
+++ product/neoverse-rd/rdfremont/module/mcp_platform/src/mod_mcp_platform.c
@@ -0,0 +1,38 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2024, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_id.h>
+#include <fwk_log.h>
+#include <fwk_module.h>
+#include <fwk_status.h>
+
+#define MOD_NAME "[MCP_PLATFORM] "
+
+/*
+ * Framework handlers.
+ */
+static int mod_mcp_platform_init(
+    fwk_id_t module_id,
+    unsigned int element_count,
+    const void *unused)
+{
+    return FWK_SUCCESS;
+}
+
+static int mod_mcp_platform_start(fwk_id_t id)
+{
+    FWK_LOG_INFO(MOD_NAME "MCP RAM firmware initialized");
+    return FWK_SUCCESS;
+}
+
+const struct fwk_module module_mcp_platform = {
+    .type = FWK_MODULE_TYPE_SERVICE,
+    .init = mod_mcp_platform_init,
+    .start = mod_mcp_platform_start,
+};
+
+const struct fwk_module_config config_mcp_platform = { 0 };
