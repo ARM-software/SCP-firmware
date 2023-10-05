138512f6ec417fe27ce6ab74acc473328df0d345
Md Golam Tanvir Zim <mdgolamtanvir.zim@arm.com>
2023-10-05 11:32:43 +0100

    product/juno: Update fast channel config to use transport fc
    This change enables juno ram firmware to use transport layer based
    fast channel implementation. Necessary changes are added to the
    config files of relevant modules.
    
    Signed-off-by: Md Golam Tanvir Zim <mdgolamtanvir.zim@arm.com>
    Change-Id: I78f7ee37fabef8f5d8162d59da86ae80281623e4

--- product/juno/scp_ramfw/config_scmi_perf.c
+++ product/juno/scp_ramfw/config_scmi_perf.c
@@ -1,6 +1,6 @@
 /*
  * Arm SCP/MCP Software
- * Copyright (c) 2020-2022, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2020-2024, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -26,131 +26,208 @@
 #    include <mod_stats.h>
 #endif
 
-#define FC_LEVEL_SET(PERF_IDX) \
-    (SCMI_FAST_CHANNEL_BASE + MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LEVEL_SET + \
-     (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * PERF_IDX))
+#ifdef BUILD_HAS_MOD_TRANSPORT_FC
+#    define FCH_INIT(FCH_NUM) \
+        { \
+            .transport_id = \
+                FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_TRANSPORT, FCH_NUM), \
+            .transport_api_id = FWK_ID_API_INIT( \
+                FWK_MODULE_IDX_TRANSPORT, \
+                MOD_TRANSPORT_API_IDX_FAST_CHANNELS), \
+        }
 
-#define FC_LIMIT_SET(PERF_IDX) \
-    (SCMI_FAST_CHANNEL_BASE + MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LIMIT_SET + \
-     (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * PERF_IDX))
+static const struct mod_scmi_perf_domain_config
+    domains[DVFS_ELEMENT_IDX_COUNT] = {
+        [DVFS_ELEMENT_IDX_BIG] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+         .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(JUNO_TRANSPORT_SCMI_PERF_FCH_BIG_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(JUNO_TRANSPORT_SCMI_PERF_FCH_BIG_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(JUNO_TRANSPORT_SCMI_PERF_FCH_BIG_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(JUNO_TRANSPORT_SCMI_PERF_FCH_BIG_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#    endif
+#    ifdef BUILD_HAS_STATISTICS
+        .stats_collected = true,
+#    endif
+        },
+        [DVFS_ELEMENT_IDX_LITTLE] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+         .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(JUNO_TRANSPORT_SCMI_PERF_FCH_LITTLE_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(JUNO_TRANSPORT_SCMI_PERF_FCH_LITTLE_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(JUNO_TRANSPORT_SCMI_PERF_FCH_LITTLE_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(JUNO_TRANSPORT_SCMI_PERF_FCH_LITTLE_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#    endif
+#    ifdef BUILD_HAS_STATISTICS
+        .stats_collected = true,
+#    endif
+        },
+        [DVFS_ELEMENT_IDX_GPU] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+         .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(JUNO_TRANSPORT_SCMI_PERF_FCH_GPU_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(JUNO_TRANSPORT_SCMI_PERF_FCH_GPU_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(JUNO_TRANSPORT_SCMI_PERF_FCH_GPU_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(JUNO_TRANSPORT_SCMI_PERF_FCH_GPU_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#        ifdef BUILD_HAS_STATISTICS
+        .stats_collected = true,
+#        endif
+#    endif
+        }
+    };
 
-#define FC_LEVEL_GET(PERF_IDX) \
-    (SCMI_FAST_CHANNEL_BASE + MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LEVEL_GET + \
-     (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * PERF_IDX))
-
-#define FC_LIMIT_GET(PERF_IDX) \
-    (SCMI_FAST_CHANNEL_BASE + MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LIMIT_GET + \
-     (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * PERF_IDX))
-
-#define FC_LEVEL_SET_AP(PERF_IDX) \
-    (SCMI_FAST_CHANNEL_BASE + MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LEVEL_SET + \
-     (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * PERF_IDX) - EXTERNAL_DEV_BASE)
-
-#define FC_LIMIT_SET_AP(PERF_IDX) \
-    (SCMI_FAST_CHANNEL_BASE + MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LIMIT_SET + \
-     (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * PERF_IDX) - EXTERNAL_DEV_BASE)
-
-#define FC_LEVEL_GET_AP(PERF_IDX) \
-    (SCMI_FAST_CHANNEL_BASE + MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LEVEL_GET + \
-     (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * PERF_IDX) - EXTERNAL_DEV_BASE)
-
-#define FC_LIMIT_GET_AP(PERF_IDX) \
-    (SCMI_FAST_CHANNEL_BASE + MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LIMIT_GET + \
-     (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * PERF_IDX) - EXTERNAL_DEV_BASE)
-
-static const struct mod_scmi_perf_domain_config domains[] = {
-    [DVFS_ELEMENT_IDX_LITTLE] = {
-#ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+#else
+#    define FC_LEVEL_SET_ADDR(PERF_IDX) \
+        (SCMI_FAST_CHANNEL_BASE + \
+         MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LEVEL_SET + \
+         (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * PERF_IDX))
+#    define FC_LIMIT_SET_ADDR(PERF_IDX) \
+        (SCMI_FAST_CHANNEL_BASE + \
+         MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LIMIT_SET + \
+         (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * PERF_IDX))
+#    define FC_LEVEL_GET_ADDR(PERF_IDX) \
+        (SCMI_FAST_CHANNEL_BASE + \
+         MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LEVEL_GET + \
+         (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * PERF_IDX))
+#    define FC_LIMIT_GET_ADDR(PERF_IDX) \
+        (SCMI_FAST_CHANNEL_BASE + \
+         MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LIMIT_GET + \
+         (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * PERF_IDX))
+#    define FC_LEVEL_SET_AP_ADDR(PERF_IDX) \
+        (SCMI_FAST_CHANNEL_BASE + \
+         MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LEVEL_SET + \
+         (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * PERF_IDX) - \
+         EXTERNAL_DEV_BASE)
+#    define FC_LIMIT_SET_AP_ADDR(PERF_IDX) \
+        (SCMI_FAST_CHANNEL_BASE + \
+         MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LIMIT_SET + \
+         (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * PERF_IDX) - \
+         EXTERNAL_DEV_BASE)
+#    define FC_LEVEL_GET_AP_ADDR(PERF_IDX) \
+        (SCMI_FAST_CHANNEL_BASE + \
+         MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LEVEL_GET + \
+         (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * PERF_IDX) - \
+         EXTERNAL_DEV_BASE)
+#    define FC_LIMIT_GET_AP_ADDR(PERF_IDX) \
+        (SCMI_FAST_CHANNEL_BASE + \
+         MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LIMIT_GET + \
+         (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * PERF_IDX) - \
+         EXTERNAL_DEV_BASE)
+static const struct mod_scmi_perf_domain_config
+    domains[DVFS_ELEMENT_IDX_COUNT] = {
+        [DVFS_ELEMENT_IDX_LITTLE] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
         .fast_channels_addr_scp = (uint64_t[]) {
             [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
-                FC_LEVEL_SET(DVFS_ELEMENT_IDX_LITTLE),
+                FC_LEVEL_SET_ADDR(DVFS_ELEMENT_IDX_LITTLE),
             [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
-                FC_LIMIT_SET(DVFS_ELEMENT_IDX_LITTLE),
+                FC_LIMIT_SET_ADDR(DVFS_ELEMENT_IDX_LITTLE),
             [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
-                FC_LEVEL_GET(DVFS_ELEMENT_IDX_LITTLE),
+                FC_LEVEL_GET_ADDR(DVFS_ELEMENT_IDX_LITTLE),
             [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
-                FC_LIMIT_GET(DVFS_ELEMENT_IDX_LITTLE),
+                FC_LIMIT_GET_ADDR(DVFS_ELEMENT_IDX_LITTLE),
         },
         .fast_channels_addr_ap = (uint64_t[]) {
             [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
-                FC_LEVEL_SET_AP(DVFS_ELEMENT_IDX_LITTLE),
+                FC_LEVEL_SET_AP_ADDR(DVFS_ELEMENT_IDX_LITTLE),
             [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
-                FC_LIMIT_SET_AP(DVFS_ELEMENT_IDX_LITTLE),
+                FC_LIMIT_SET_AP_ADDR(DVFS_ELEMENT_IDX_LITTLE),
             [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
-                FC_LEVEL_GET_AP(DVFS_ELEMENT_IDX_LITTLE),
+                FC_LEVEL_GET_AP_ADDR(DVFS_ELEMENT_IDX_LITTLE),
             [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
-                FC_LIMIT_GET_AP(DVFS_ELEMENT_IDX_LITTLE),
+                FC_LIMIT_GET_AP_ADDR(DVFS_ELEMENT_IDX_LITTLE),
         },
-#endif
-#ifdef BUILD_HAS_STATISTICS
+#    endif
+#    ifdef BUILD_HAS_STATISTICS
         .stats_collected = true,
-#endif
+#    endif
     },
-    [DVFS_ELEMENT_IDX_BIG] = {
-#ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+        [DVFS_ELEMENT_IDX_BIG] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
         .fast_channels_addr_scp = (uint64_t[]) {
             [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
-                FC_LEVEL_SET(DVFS_ELEMENT_IDX_BIG),
+                FC_LEVEL_SET_ADDR(DVFS_ELEMENT_IDX_BIG),
             [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
-                FC_LIMIT_SET(DVFS_ELEMENT_IDX_BIG),
+                FC_LIMIT_SET_ADDR(DVFS_ELEMENT_IDX_BIG),
             [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
-                FC_LEVEL_GET(DVFS_ELEMENT_IDX_BIG),
+                FC_LEVEL_GET_ADDR(DVFS_ELEMENT_IDX_BIG),
             [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
-                FC_LIMIT_GET(DVFS_ELEMENT_IDX_BIG),
+                FC_LIMIT_GET_ADDR(DVFS_ELEMENT_IDX_BIG),
         },
         .fast_channels_addr_ap = (uint64_t[]) {
             [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
-                FC_LEVEL_SET_AP(DVFS_ELEMENT_IDX_BIG),
+                FC_LEVEL_SET_AP_ADDR(DVFS_ELEMENT_IDX_BIG),
             [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
-                FC_LIMIT_SET_AP(DVFS_ELEMENT_IDX_BIG),
+                FC_LIMIT_SET_AP_ADDR(DVFS_ELEMENT_IDX_BIG),
             [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
-                FC_LEVEL_GET_AP(DVFS_ELEMENT_IDX_BIG),
+                FC_LEVEL_GET_AP_ADDR(DVFS_ELEMENT_IDX_BIG),
             [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
-                FC_LIMIT_GET_AP(DVFS_ELEMENT_IDX_BIG),
+                FC_LIMIT_GET_AP_ADDR(DVFS_ELEMENT_IDX_BIG),
         },
-#endif
-#ifdef BUILD_HAS_STATISTICS
+#    endif
+#    ifdef BUILD_HAS_STATISTICS
         .stats_collected = true,
-#endif
+#    endif
     },
-    [DVFS_ELEMENT_IDX_GPU] = {
-#ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+        [DVFS_ELEMENT_IDX_GPU] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
         .fast_channels_addr_scp = (uint64_t[]) {
             [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
-                FC_LEVEL_SET(DVFS_ELEMENT_IDX_GPU),
+                FC_LEVEL_SET_ADDR(DVFS_ELEMENT_IDX_GPU),
             [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
-                FC_LIMIT_SET(DVFS_ELEMENT_IDX_GPU),
+                FC_LIMIT_SET_ADDR(DVFS_ELEMENT_IDX_GPU),
             [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
-                FC_LEVEL_GET(DVFS_ELEMENT_IDX_GPU),
+                FC_LEVEL_GET_ADDR(DVFS_ELEMENT_IDX_GPU),
             [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
-                FC_LIMIT_GET(DVFS_ELEMENT_IDX_GPU),
+                FC_LIMIT_GET_ADDR(DVFS_ELEMENT_IDX_GPU),
         },
         .fast_channels_addr_ap = (uint64_t[]) {
             [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
-                FC_LEVEL_SET_AP(DVFS_ELEMENT_IDX_GPU),
+                FC_LEVEL_SET_AP_ADDR(DVFS_ELEMENT_IDX_GPU),
             [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
-                FC_LIMIT_SET_AP(DVFS_ELEMENT_IDX_GPU),
+                FC_LIMIT_SET_AP_ADDR(DVFS_ELEMENT_IDX_GPU),
             [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
-                FC_LEVEL_GET_AP(DVFS_ELEMENT_IDX_GPU),
+                FC_LEVEL_GET_AP_ADDR(DVFS_ELEMENT_IDX_GPU),
             [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
-                FC_LIMIT_GET_AP(DVFS_ELEMENT_IDX_GPU),
+                FC_LIMIT_GET_AP_ADDR(DVFS_ELEMENT_IDX_GPU),
         },
-#endif
-#ifdef BUILD_HAS_MOD_STATISTICS
+#    endif
+#    ifdef BUILD_HAS_MOD_STATISTICS
         .stats_collected = true,
-#endif
+#    endif
     },
 };
+#endif /* BUILD_HAS_MOD_TRANSPORT_FC */
 
 struct fwk_module_config config_scmi_perf = {
     .data = &((struct mod_scmi_perf_config){
         .domains = &domains,
         .perf_doms_count = FWK_ARRAY_SIZE(domains),
 #ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+#    ifndef BUILD_HAS_MOD_TRANSPORT_FC
         .fast_channels_alarm_id = FWK_ID_SUB_ELEMENT_INIT(
             FWK_MODULE_IDX_TIMER,
             0,
             JUNO_SCMI_FAST_CHANNEL_IDX),
+#    endif
         .fast_channels_rate_limit = SCMI_PERF_FC_MIN_RATE_LIMIT,
 #else
         .fast_channels_alarm_id = FWK_ID_NONE_INIT,
--- product/juno/scp_ramfw/config_transport.c
+++ product/juno/scp_ramfw/config_transport.c
@@ -1,6 +1,6 @@
 /*
  * Arm SCP/MCP Software
- * Copyright (c) 2019-2022, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2019-2024, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -10,6 +10,7 @@
 #include "juno_scmi.h"
 #include "software_mmap.h"
 
+#include <mod_fch_polled.h>
 #include <mod_transport.h>
 
 #include <fwk_element.h>
@@ -20,11 +21,18 @@
 #include <stddef.h>
 #include <stdint.h>
 
-static const struct fwk_element element_table[] = {
+static const struct fwk_element element_table[
+#ifdef BUILD_HAS_MOD_TRANSPORT_FC
+    JUNO_TRANSPORT_CHANNELS_COUNT + 1] = {
+    [JUNO_TRANSPORT_SCMI_SERVICE_PSCI] = {
+#else
+    JUNO_SCMI_SERVICE_IDX_COUNT + 1] = {
+    [JUNO_SCMI_SERVICE_IDX_PSCI_A2P] = {
+#endif
     /* JUNO_SCMI_SERVICE_IDX_PSCI_A2P */
-    { .name = "",
-      .data =
-          &(struct mod_transport_channel_config){
+        .name = "",
+        .data =
+            &(struct mod_transport_channel_config){
               .channel_type = MOD_TRANSPORT_CHANNEL_TYPE_COMPLETER,
               .policies =
                   (MOD_TRANSPORT_POLICY_INIT_MAILBOX |
@@ -39,11 +47,17 @@ static const struct fwk_element element_table[] = {
               .pd_source_id = FWK_ID_ELEMENT_INIT(
                   FWK_MODULE_IDX_POWER_DOMAIN,
                   POWER_DOMAIN_IDX_SYSTOP),
-          } },
+            }
+        },
+#ifdef BUILD_HAS_MOD_TRANSPORT_FC
+    [JUNO_TRANSPORT_SCMI_SERVICE_OSPM_0] = {
+#else
+    [JUNO_SCMI_SERVICE_IDX_OSPM_A2P_0] = {
+#endif
     /* JUNO_SCMI_SERVICE_IDX_OSPM_A2P_0 */
-    { .name = "",
-      .data =
-          &(struct mod_transport_channel_config){
+        .name = "",
+        .data =
+            &(struct mod_transport_channel_config){
               .channel_type = MOD_TRANSPORT_CHANNEL_TYPE_COMPLETER,
               .policies = MOD_TRANSPORT_POLICY_INIT_MAILBOX,
               .out_band_mailbox_address = (uintptr_t)SCMI_PAYLOAD_LOW_A2P_BASE,
@@ -56,10 +70,16 @@ static const struct fwk_element element_table[] = {
               .pd_source_id = FWK_ID_ELEMENT_INIT(
                   FWK_MODULE_IDX_POWER_DOMAIN,
                   POWER_DOMAIN_IDX_SYSTOP),
-          } },
+            }
+        },
+#ifdef BUILD_HAS_MOD_TRANSPORT_FC
+    [JUNO_TRANSPORT_SCMI_SERVICE_OSPM_1] = {
+#else
+    [JUNO_SCMI_SERVICE_IDX_OSPM_A2P_1] = {
+#endif
     /* JUNO_SCMI_SERVICE_IDX_OSPM_A2P_1 */
-    { .name = "",
-      .data =
+        .name = "",
+        .data =
           &(struct mod_transport_channel_config){
               .channel_type = MOD_TRANSPORT_CHANNEL_TYPE_COMPLETER,
               .policies = MOD_TRANSPORT_POLICY_INIT_MAILBOX,
@@ -73,12 +93,18 @@ static const struct fwk_element element_table[] = {
               .pd_source_id = FWK_ID_ELEMENT_INIT(
                   FWK_MODULE_IDX_POWER_DOMAIN,
                   POWER_DOMAIN_IDX_SYSTOP),
-          } },
+            }
+        },
 #ifdef BUILD_HAS_SCMI_NOTIFICATIONS
+#    ifdef BUILD_HAS_MOD_TRANSPORT_FC
+    [JUNO_TRANSPORT_SCMI_SERVICE_OSPM_P2A] = {
+#    else
+    [JUNO_SCMI_SERVICE_IDX_OSPM_P2A] = {
+#    endif
     /* JUNO_SCMI_SERVICE_IDX_OSPM_P2A */
-    { .name = "",
-      .data =
-          &(struct mod_transport_channel_config){
+        .name = "",
+        .data =
+            &(struct mod_transport_channel_config){
               .channel_type = MOD_TRANSPORT_CHANNEL_TYPE_REQUESTER,
               .policies = MOD_TRANSPORT_POLICY_INIT_MAILBOX,
               .out_band_mailbox_address = (uintptr_t)SCMI_PAYLOAD_HIGH_P2A_BASE,
@@ -91,9 +117,219 @@ static const struct fwk_element element_table[] = {
               .pd_source_id = FWK_ID_ELEMENT_INIT(
                   FWK_MODULE_IDX_POWER_DOMAIN,
                   POWER_DOMAIN_IDX_SYSTOP),
-          } },
+            }
+        },
 #endif
+
+#ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+    [JUNO_TRANSPORT_SCMI_PERF_FCH_BIG_LEVEL_SET] = {
+        .name = "FCH_BIG_LEVEL_SET",
+        .data = &((
+            struct mod_transport_channel_config){
+            .transport_type =
+                MOD_TRANSPORT_CHANNEL_TRANSPORT_TYPE_FAST_CHANNELS,
+            .channel_type =
+                MOD_TRANSPORT_CHANNEL_TYPE_COMPLETER,
+            .driver_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                JUNO_PLAT_FCH_BIG_LEVEL_SET),
+            .driver_api_id = FWK_ID_API_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                MOD_FCH_POLLED_API_IDX_TRANSPORT),
+            }
+        ),
+    },
+    [JUNO_TRANSPORT_SCMI_PERF_FCH_BIG_LIMIT_SET] = {
+        .name = "FCH_BIG_LIMIT_SET",
+        .data = &((
+            struct mod_transport_channel_config){
+            .transport_type =
+                MOD_TRANSPORT_CHANNEL_TRANSPORT_TYPE_FAST_CHANNELS,
+            .channel_type =
+                MOD_TRANSPORT_CHANNEL_TYPE_COMPLETER,
+            .driver_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                JUNO_PLAT_FCH_BIG_LIMIT_SET),
+            .driver_api_id = FWK_ID_API_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                MOD_FCH_POLLED_API_IDX_TRANSPORT),
+            }
+        ),
+    },
+    [JUNO_TRANSPORT_SCMI_PERF_FCH_BIG_LEVEL_GET] = {
+        .name = "FCH_BIG_LEVEL_GET",
+        .data = &((
+            struct mod_transport_channel_config){
+            .transport_type =
+                MOD_TRANSPORT_CHANNEL_TRANSPORT_TYPE_FAST_CHANNELS,
+            .channel_type =
+                MOD_TRANSPORT_CHANNEL_TYPE_COMPLETER,
+            .driver_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                JUNO_PLAT_FCH_BIG_LEVEL_GET),
+            .driver_api_id = FWK_ID_API_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                MOD_FCH_POLLED_API_IDX_TRANSPORT),
+            }
+        ),
+    },
+    [JUNO_TRANSPORT_SCMI_PERF_FCH_BIG_LIMIT_GET] = {
+        .name = "FCH_BIG_LIMIT_GET",
+        .data = &((
+            struct mod_transport_channel_config){
+            .transport_type =
+                MOD_TRANSPORT_CHANNEL_TRANSPORT_TYPE_FAST_CHANNELS,
+            .channel_type =
+                MOD_TRANSPORT_CHANNEL_TYPE_COMPLETER,
+            .driver_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                JUNO_PLAT_FCH_BIG_LIMIT_GET),
+            .driver_api_id = FWK_ID_API_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                MOD_FCH_POLLED_API_IDX_TRANSPORT),
+            }
+        ),
+    },
+    [JUNO_TRANSPORT_SCMI_PERF_FCH_LITTLE_LEVEL_SET] = {
+        .name = "FCH_LITTLE_LEVEL_SET",
+        .data = &((
+            struct mod_transport_channel_config){
+            .transport_type =
+                MOD_TRANSPORT_CHANNEL_TRANSPORT_TYPE_FAST_CHANNELS,
+            .channel_type =
+                MOD_TRANSPORT_CHANNEL_TYPE_COMPLETER,
+            .driver_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                JUNO_PLAT_FCH_LITTLE_LEVEL_SET),
+            .driver_api_id = FWK_ID_API_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                MOD_FCH_POLLED_API_IDX_TRANSPORT),
+            }
+        ),
+    },
+    [JUNO_TRANSPORT_SCMI_PERF_FCH_LITTLE_LIMIT_SET] = {
+        .name = "FCH_LITTLE_LIMIT_SET",
+        .data = &((
+            struct mod_transport_channel_config){
+            .transport_type =
+                MOD_TRANSPORT_CHANNEL_TRANSPORT_TYPE_FAST_CHANNELS,
+            .channel_type =
+                MOD_TRANSPORT_CHANNEL_TYPE_COMPLETER,
+            .driver_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                JUNO_PLAT_FCH_LITTLE_LIMIT_SET),
+            .driver_api_id = FWK_ID_API_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                MOD_FCH_POLLED_API_IDX_TRANSPORT),
+            }
+        ),
+    },
+    [JUNO_TRANSPORT_SCMI_PERF_FCH_LITTLE_LEVEL_GET] = {
+        .name = "FCH_LITTLE_LEVEL_GET",
+        .data = &((
+            struct mod_transport_channel_config){
+            .transport_type =
+                MOD_TRANSPORT_CHANNEL_TRANSPORT_TYPE_FAST_CHANNELS,
+            .channel_type =
+                MOD_TRANSPORT_CHANNEL_TYPE_COMPLETER,
+            .driver_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                JUNO_PLAT_FCH_LITTLE_LEVEL_GET),
+            .driver_api_id = FWK_ID_API_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                MOD_FCH_POLLED_API_IDX_TRANSPORT),
+            }
+        ),
+    },
+    [JUNO_TRANSPORT_SCMI_PERF_FCH_LITTLE_LIMIT_GET] = {
+        .name = "FCH_LITTLE_LIMIT_GET",
+        .data = &((
+            struct mod_transport_channel_config){
+            .transport_type =
+                MOD_TRANSPORT_CHANNEL_TRANSPORT_TYPE_FAST_CHANNELS,
+            .channel_type =
+                MOD_TRANSPORT_CHANNEL_TYPE_COMPLETER,
+            .driver_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                JUNO_PLAT_FCH_LITTLE_LIMIT_GET),
+            .driver_api_id = FWK_ID_API_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                MOD_FCH_POLLED_API_IDX_TRANSPORT),
+            }
+        ),
+    },
+    [JUNO_TRANSPORT_SCMI_PERF_FCH_GPU_LEVEL_SET] = {
+        .name = "FCH_GPU_LEVEL_SET",
+        .data = &((
+            struct mod_transport_channel_config){
+            .transport_type =
+                MOD_TRANSPORT_CHANNEL_TRANSPORT_TYPE_FAST_CHANNELS,
+            .channel_type =
+                MOD_TRANSPORT_CHANNEL_TYPE_COMPLETER,
+            .driver_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                JUNO_PLAT_FCH_GPU_LEVEL_SET),
+            .driver_api_id = FWK_ID_API_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                MOD_FCH_POLLED_API_IDX_TRANSPORT),
+            }
+        ),
+    },
+    [JUNO_TRANSPORT_SCMI_PERF_FCH_GPU_LIMIT_SET] = {
+        .name = "FCH_GPU_LIMIT_SET",
+        .data = &((
+            struct mod_transport_channel_config){
+            .transport_type =
+                MOD_TRANSPORT_CHANNEL_TRANSPORT_TYPE_FAST_CHANNELS,
+            .channel_type =
+                MOD_TRANSPORT_CHANNEL_TYPE_COMPLETER,
+            .driver_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                JUNO_PLAT_FCH_GPU_LIMIT_SET),
+            .driver_api_id = FWK_ID_API_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                MOD_FCH_POLLED_API_IDX_TRANSPORT),
+            }
+        ),
+    },
+    [JUNO_TRANSPORT_SCMI_PERF_FCH_GPU_LEVEL_GET] = {
+        .name = "FCH_GPU_LEVEL_GET",
+        .data = &((
+            struct mod_transport_channel_config){
+            .transport_type =
+                MOD_TRANSPORT_CHANNEL_TRANSPORT_TYPE_FAST_CHANNELS,
+            .channel_type =
+                MOD_TRANSPORT_CHANNEL_TYPE_COMPLETER,
+            .driver_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                JUNO_PLAT_FCH_GPU_LEVEL_GET),
+            .driver_api_id = FWK_ID_API_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                MOD_FCH_POLLED_API_IDX_TRANSPORT),
+            }
+        ),
+    },
+    [JUNO_TRANSPORT_SCMI_PERF_FCH_GPU_LIMIT_GET] = {
+        .name = "FCH_GPU_LIMIT_GET",
+        .data = &((
+            struct mod_transport_channel_config){
+            .transport_type =
+                MOD_TRANSPORT_CHANNEL_TRANSPORT_TYPE_FAST_CHANNELS,
+            .channel_type =
+                MOD_TRANSPORT_CHANNEL_TYPE_COMPLETER,
+            .driver_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                JUNO_PLAT_FCH_GPU_LIMIT_GET),
+            .driver_api_id = FWK_ID_API_INIT(
+                FWK_MODULE_IDX_FCH_POLLED,
+                MOD_FCH_POLLED_API_IDX_TRANSPORT),
+            }
+        ),
+    },
+    [JUNO_TRANSPORT_CHANNELS_COUNT] = { 0 },
+#else
     [JUNO_SCMI_SERVICE_IDX_COUNT] = { 0 },
+#endif
 };
 
 static const struct fwk_element *get_element_table(fwk_id_t module_id)
