131f33fc2a20c52974ee16adcd0019e1e2a7368e
Md Golam Tanvir Zim <mdgolamtanvir.zim@arm.com>
2023-10-06 16:50:25 +0100

    product/rdv1: Update fast channel config to use transport fc
    This change enables rdv1 ram firmware to use transport layer based
    fast channel implementation. Necessary changes are added to the
    config files of relevant modules.
    
    Signed-off-by: Md Golam Tanvir Zim <mdgolamtanvir.zim@arm.com>
    Change-Id: I5818fe2dd36dd9dfaec0ea4df8c39fab9bfdd89d

--- product/neoverse-rd/rdv1/scp_ramfw/config_scmi_perf.c
+++ product/neoverse-rd/rdv1/scp_ramfw/config_scmi_perf.c
@@ -1,10 +1,12 @@
 /*
  * Arm SCP/MCP Software
- * Copyright (c) 2021-2022, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2021-2024, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include "config_dvfs.h"
+#include "platform_scmi.h"
 #include "rd_alarm_idx.h"
 #include "scp_software_mmap.h"
 
@@ -17,43 +19,297 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define FAST_CHANNEL_ADDRESS_SCP(n) \
-    { \
-        [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] = SCP_SCMI_FAST_CHANNEL_BASE + \
-            MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LEVEL_SET + \
-            (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * n), \
-        [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] = SCP_SCMI_FAST_CHANNEL_BASE + \
-            MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LIMIT_SET + \
-            (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * n), \
-        [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] = SCP_SCMI_FAST_CHANNEL_BASE + \
-            MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LEVEL_GET + \
-            (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * n), \
-        [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] = SCP_SCMI_FAST_CHANNEL_BASE + \
-            MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LIMIT_GET + \
-            (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * n) \
-    }
+#ifdef BUILD_HAS_MOD_TRANSPORT_FC
 
-#define FAST_CHANNEL_ADDRESS_AP(n) \
-    { \
-        [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] = SCP_SCMI_FAST_CHANNEL_BASE + \
-            MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LEVEL_SET + \
-            (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * n) - \
-            SCP_SYSTEM_ACCESS_PORT1_BASE, \
-        [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] = SCP_SCMI_FAST_CHANNEL_BASE + \
-            MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LIMIT_SET + \
-            (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * n) - \
-            SCP_SYSTEM_ACCESS_PORT1_BASE, \
-        [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] = SCP_SCMI_FAST_CHANNEL_BASE + \
-            MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LEVEL_GET + \
-            (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * n) - \
-            SCP_SYSTEM_ACCESS_PORT1_BASE, \
-        [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] = SCP_SCMI_FAST_CHANNEL_BASE + \
-            MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LIMIT_GET + \
-            (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * n) - \
-            SCP_SYSTEM_ACCESS_PORT1_BASE \
-    }
+static const struct mod_scmi_perf_domain_config
+    domains[DVFS_ELEMENT_IDX_COUNT] = {
+        [DVFS_ELEMENT_IDX_CPU0] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+        .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU0_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU0_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU0_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU0_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#    endif
+    },
+        [DVFS_ELEMENT_IDX_CPU1] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+        .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU1_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU1_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU1_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU1_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#    endif
+    },
+        [DVFS_ELEMENT_IDX_CPU2] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+        .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU2_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU2_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU2_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU2_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#    endif
+    },
+        [DVFS_ELEMENT_IDX_CPU3] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+        .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU3_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU3_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU3_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU3_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#    endif
+    },
+        [DVFS_ELEMENT_IDX_CPU4] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+        .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU4_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU4_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU4_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU4_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#    endif
+    },
+        [DVFS_ELEMENT_IDX_CPU5] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+        .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU5_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU5_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU5_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU5_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#    endif
+    },
+        [DVFS_ELEMENT_IDX_CPU6] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+        .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU6_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU6_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU6_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU6_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#    endif
+    },
+        [DVFS_ELEMENT_IDX_CPU7] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+        .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU7_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU7_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU7_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU7_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#    endif
+    },
+        [DVFS_ELEMENT_IDX_CPU8] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+        .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU8_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU8_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU8_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU8_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#    endif
+    },
+        [DVFS_ELEMENT_IDX_CPU9] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+        .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU9_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU9_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU9_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU9_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#    endif
+    },
+        [DVFS_ELEMENT_IDX_CPU10] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+        .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU10_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU10_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU10_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU10_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#    endif
+    },
+        [DVFS_ELEMENT_IDX_CPU11] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+        .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU11_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU11_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU11_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU11_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#    endif
+    },
+        [DVFS_ELEMENT_IDX_CPU12] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+        .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU12_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU12_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU12_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU12_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#    endif
+    },
+        [DVFS_ELEMENT_IDX_CPU13] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+        .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU13_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU13_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU13_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU13_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#    endif
+    },
+        [DVFS_ELEMENT_IDX_CPU14] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+        .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU14_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU14_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU14_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU14_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#    endif
+    },
+        [DVFS_ELEMENT_IDX_CPU15] = {
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+        .fch_config = (struct scmi_perf_fch_config[]) {
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU15_LEVEL_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU15_LIMIT_SET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU15_LEVEL_GET),
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] =
+                FCH_INIT(RDV1_TRANSPORT_SCMI_PERF_FCH_CPU15_LIMIT_GET),
+        },
+        .supports_fast_channels = true,
+#    endif
+    },
+};
+#else
+#    define FAST_CHANNEL_ADDRESS_SCP(n) \
+        { \
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] = \
+                SCP_SCMI_FAST_CHANNEL_BASE + \
+                MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LEVEL_SET + \
+                (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * n), \
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] = \
+                SCP_SCMI_FAST_CHANNEL_BASE + \
+                MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LIMIT_SET + \
+                (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * n), \
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] = \
+                SCP_SCMI_FAST_CHANNEL_BASE + \
+                MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LEVEL_GET + \
+                (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * n), \
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] = \
+                SCP_SCMI_FAST_CHANNEL_BASE + \
+                MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LIMIT_GET + \
+                (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * n) \
+        }
 
-#ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+#    define FAST_CHANNEL_ADDRESS_AP(n) \
+        { \
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_SET] = \
+                SCP_SCMI_FAST_CHANNEL_BASE + \
+                MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LEVEL_SET + \
+                (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * n) - \
+                SCP_SYSTEM_ACCESS_PORT1_BASE, \
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_SET] = \
+                SCP_SCMI_FAST_CHANNEL_BASE + \
+                MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LIMIT_SET + \
+                (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * n) - \
+                SCP_SYSTEM_ACCESS_PORT1_BASE, \
+            [MOD_SCMI_PERF_FAST_CHANNEL_LEVEL_GET] = \
+                SCP_SCMI_FAST_CHANNEL_BASE + \
+                MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LEVEL_GET + \
+                (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * n) - \
+                SCP_SYSTEM_ACCESS_PORT1_BASE, \
+            [MOD_SCMI_PERF_FAST_CHANNEL_LIMIT_GET] = \
+                SCP_SCMI_FAST_CHANNEL_BASE + \
+                MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_LIMIT_GET + \
+                (MOD_SCMI_PERF_FAST_CHANNEL_OFFSET_TOTAL * n) - \
+                SCP_SYSTEM_ACCESS_PORT1_BASE \
+        }
+
+#    ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
 static const struct mod_scmi_perf_domain_config domains[] = {
     [0] = { .fast_channels_addr_scp = (uint64_t[])FAST_CHANNEL_ADDRESS_SCP(0),
             .fast_channels_addr_ap = (uint64_t[])FAST_CHANNEL_ADDRESS_AP(0) },
@@ -88,22 +344,25 @@ static const struct mod_scmi_perf_domain_config domains[] = {
     [15] = { .fast_channels_addr_scp = (uint64_t[])FAST_CHANNEL_ADDRESS_SCP(15),
              .fast_channels_addr_ap = (uint64_t[])FAST_CHANNEL_ADDRESS_AP(15) },
 };
-#else
+#    else
 static const struct mod_scmi_perf_domain_config domains[16] = { 0 };
-#endif
+#    endif
+#endif /* BUILD_HAS_MOD_TRANSPORT_FC */
 
 const struct fwk_module_config config_scmi_perf = {
     .data = &((struct mod_scmi_perf_config){
         .domains = &domains,
         .perf_doms_count = FWK_ARRAY_SIZE(domains),
 #ifdef BUILD_HAS_SCMI_PERF_FAST_CHANNELS
+#    ifndef BUILD_HAS_MOD_TRANSPORT_FC
         .fast_channels_alarm_id = FWK_ID_SUB_ELEMENT_INIT(
             FWK_MODULE_IDX_TIMER,
             0,
             RD_SCMI_FAST_CHANNEL_IDX),
+#    endif
         .fast_channels_rate_limit = SCMI_PERF_FC_MIN_RATE_LIMIT,
 #else
         .fast_channels_alarm_id = FWK_ID_NONE_INIT,
-#endif
+#endif /* BUILD_HAS_MOD_TRANSPORT_FC */
     }),
 };
