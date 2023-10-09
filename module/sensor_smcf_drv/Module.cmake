c8516a69dc46889e4f6c4a00583e4789ebf4940b
Abhishek Rawat <abhishek.rawat@arm.com>
2023-10-09 17:51:18 +0100

    sensor_smcf_drv: Add Sensor SMCF data driver
    This change adds Sensor SMCF data driver which fetches the Sensor
    data from SMCF and expose that data to client modules on two levels
    of granularity, a single 64 bit value and multiple samples from the
    requested sensor.
    
    Signed-off-by: Abhishek Rawat <abhishek.rawat@arm.com>
    Change-Id: I5483b9e856930c669d36dcc702d804f4a59bb163

---
+++ module/sensor_smcf_drv/Module.cmake
@@ -0,0 +1,10 @@
+#
+# Arm SCP/MCP Software
+# Copyright (c) 2023, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+set(SCP_MODULE "sensor-smcf-drv")
+
+set(SCP_MODULE_TARGET "module-sensor-smcf-drv")
