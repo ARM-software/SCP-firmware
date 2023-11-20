9b3290d39bf7686ef2fed443746be2f7d5ef2e83
Raghuram Thammiraju <raghuram.thammiraju@arm.com>
2023-11-20 09:32:35 +0000

    rdfremont: move product 'rdfremont' to product group 'neoverse-rd'
    As part of process to consolidate all the reference design platforms
    under a product group, move RD-Fremont platform support into
    'neoverse-rd' product group and fix code style check failures.
    
    Signed-off-by: Raghuram Thammiraju <raghuram.thammiraju@arm.com>
    Change-Id: I7300c6cf74fd370f9a9c4b64c18f140ad3a7a697

--- product/neoverse-rd/rdfremont/lcp_ramfw/Toolchain-Clang.cmake
+++ product/neoverse-rd/rdfremont/lcp_ramfw/Toolchain-Clang.cmake
@@ -1,6 +1,6 @@
 #
 # Arm SCP/MCP Software
-# Copyright (c) 2022, Arm Limited and Contributors. All rights reserved.
+# Copyright (c) 2022-2023, Arm Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -13,5 +13,5 @@ set(CMAKE_ASM_COMPILER_TARGET "arm-arm-none-eabi")
 set(CMAKE_C_COMPILER_TARGET "arm-arm-none-eabi")
 set(CMAKE_CXX_COMPILER_TARGET "arm-arm-none-eabi")
 
-include(
-    "${CMAKE_CURRENT_LIST_DIR}/../../../cmake/Toolchain/Clang-Baremetal.cmake")
+set(CMAKE_TOP_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../..")
+include("${CMAKE_TOP_DIR}/cmake/Toolchain/Clang-Baremetal.cmake")
