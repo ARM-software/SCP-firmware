733cbe303422d00eca3f36c348cae0ff5ec0da5f
Shriram K <shriram.k@arm.com>
2024-01-10 22:04:39 +0530

    rdfremont: add generic timer config data for mcpfw
    The configuration data for generic timer driver includes the base
    addresses of timer register, counter register and control register along
    with the initial frequency and the id of clock device on which the timer
    depends. However, the MCP does not configure the control register.
    
    Signed-off-by: Shriram K <shriram.k@arm.com>
    Change-Id: I89caa73ee78ef33d8feda6973fd7a429fa71ac71

---
+++ product/neoverse-rd/rdfremont/mcp_ramfw/include/mcp_clock.h
@@ -0,0 +1,18 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2024, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     MCP clock definitions.
+ */
+
+#ifndef MCP_CLOCK_H
+#define MCP_CLOCK_H
+
+#include "clock_css.h"
+
+#include <fwk_macros.h>
+
+#endif /* MCP_CLOCK_H */
--- product/neoverse-rd/rdfremont/mcp_ramfw/include/mcp_css_mmap.h
+++ product/neoverse-rd/rdfremont/mcp_ramfw/include/mcp_css_mmap.h
@@ -21,6 +21,18 @@
 #define MCP_DTC_RAM_SIZE (256 * 1024)
 
 /* SCP sub-system peripherals */
+#define MCP_REFCLK_CNTCTL_BASE   (0x4C000000)
+#define MCP_REFCLK_CNTBASE0_BASE (0x4C001000)
 #define MCP_UART_BASE (0x4C002000)
+
+/* MCP addresses translation window base addresses */
+#define MCP_ADDRESS_TRANSLATION_WINDOW1_BASE (0xA0000000)
+
+/* Offsets within MCP's Address Translation Window0 */
+#define MCP_ATW1_REFCLK_CNTCONTROL_OFFSET (0x2A430000)
+
+/* MCP addresses mapped via ATU into address translation windows */
+#define MCP_REFCLK_CNTCONTROL_BASE \
+    (MCP_ADDRESS_TRANSLATION_WINDOW1_BASE + MCP_ATW1_REFCLK_CNTCONTROL_OFFSET)
 
 #endif /* MCP_CSS_MMAP_H */
