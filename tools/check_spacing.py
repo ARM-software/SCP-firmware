6e09a08ee527863bbbdd2d7c51aee67d5183f476
Leandro Belli <leandro.belli@arm.com>
2024-01-12 17:12:44 +0000

    tools/check_spacing: modularize and improve readability
    This patch simplifies the way that `check_spacing.py` script works by
    creating helper functions and reducing the number of nested for
    loops.
    
    Signed-off-by: Leandro Belli <leandro.belli@arm.com>
    Change-Id: I9377bf4fa266dd9f66e18572e2ecff062d3eccc4

--- tools/check_spacing.py
+++ tools/check_spacing.py
@@ -1,60 +1,62 @@
 #!/usr/bin/env python3
 #
 # Arm SCP/MCP Software
-# Copyright (c) 2015-2023, Arm Limited and Contributors. All rights reserved.
+# Copyright (c) 2015-2024, Arm Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
+
 """
     Check for trailing spaces and non-UNIX line endings in the source code.
 """
+
 import argparse
 import os
 import re
-import shutil
-import subprocess
 import sys
-import tempfile
 import fnmatch
 import glob
+from dataclasses import dataclass, asdict
+from utils import banner, get_filtered_files
 
 
 #
 # Directories to exclude
 #
 
-# Exclude all mod_test "mocks" directories
+# Exclude all mod_test 'mocks' directories
 UNIT_TEST_MOCKS = glob.glob('module/**/test/**/mocks', recursive=True)
 
 EXCLUDE_DIRECTORIES = [
     '.git',
     'build',
     'tools',
-    'contrib/cmsis/git',
-    "contrib/run-clang-format/git",
-    "contrib/cmock/git",
+    'contrib',
     'product/rcar/src/CMSIS-FreeRTOS',
     'unit_test/unity_mocks',
 ] + UNIT_TEST_MOCKS
 
 #
-# Exclude patterns (applied to files only)
+# File types to check
 #
-EXCLUDE = [
-    "*.html",
-    "*.xml",
-    "*.css",
-    "*.gif",
-    "*.dat",
-    "*.pyc",
-    "*.jar",
-    "*.md",
-    "*.swp",
-    "*.a",
-    "*.pdf",
-    ".*"
+FILE_TYPES = [
+    '*.c',
+    '*.h',
+    '*.cmake',
+    'CMakeLists.txt',
 ]
 
+#
+# Code file types to check
+#
+CODE_FILES = [
+    '*.c',
+    '*.h',
+]
+
+#
+# Code keywords to check
+#
 KEYWORDS = [
     'for',
     'if',
@@ -62,201 +64,194 @@ KEYWORDS = [
     'while',
 ]
 
-#
-# File types for which spaces after keywords will be corrected
-#
-FILE_TYPES = [
-    '*.c',
-    '*.h',
-]
+
+@dataclass
+class Analysis:
+    trailing_spaces: int = 0
+    trailing_lines: int = 0
+    incorrect_spaces: int = 0
+    modified_files: int = 0
+    non_unix_eol_files: int = 0
+    missing_new_line_files: int = 0
+
+    def has_errors(self) -> bool:
+        return any([value for value in asdict(self).values()])
+
+    def add(self, partial_analysis):
+        for elem, count in asdict(partial_analysis).items():
+            if hasattr(self, elem):
+                setattr(self, elem, getattr(self, elem) + count)
+            else:
+                # Handle the error or ignore
+                print(f'Warning: Attribute {elem} not found.')
+        return self
+
+    def __str__(self) -> str:
+        msg = ''
+        msg += self._str_message('trailing spaces', self.trailing_spaces)
+        msg += self._str_message('trailing lines', self.trailing_lines)
+        msg += self._str_message('abnormal spaces', self.incorrect_spaces)
+        msg += self._str_message('files with non-UNIX or mixed line endings',
+                                 self.non_unix_eol_files)
+        msg += self._str_message('files with missing newlines at EOF',
+                                 self.missing_new_line_files)
+        if self.modified_files:
+            msg += f'- {self.modified_files} files modified.\n'
+        return msg
+
+    def _str_message(self, name, count) -> str:
+        if count == 0:
+            return f'- No {name} found.\n'
+        else:
+            return f'- {count} {name} found.\n'
 
 
-def is_valid_type(filename):
-    for file_type in FILE_TYPES:
-        if fnmatch.fnmatch(filename, file_type):
-            return True
-    return False
+def is_valid_code_type(filename):
+    return any(fnmatch.fnmatch(filename, file_type)
+               for file_type in CODE_FILES)
+
+
+def get_regex_patterns(keywords):
+    regex_patterns = dict.fromkeys(keywords, 0)
+    for keyword in keywords:
+        regex_patterns[keyword] = re.compile(
+            f'(.*\\W)({keyword})(\\s*)(\\(.*)')
+
+    return regex_patterns
+
+
+def check_line(path, line, regex_patterns, analysis, trim, correct):
+    # Note that all newlines are converted to '\n',
+    # so the following will work regardless of
+    # what the underlying file format is using to
+    # represent a line break.
+    if line.endswith(' \n'):
+        print('{}:{} has trailing space'.format(line, path))
+        analysis.trailing_spaces += 1
+        if trim:
+            line = line.rstrip()+'\n'
+    if not is_valid_code_type(os.path.basename(path)):
+        return line
+
+    for keyword, regex_pattern in regex_patterns.items():
+        if line.find(keyword) < 0:
+            continue
+        m = regex_pattern.search(line)
+        if m and m.group(3) != ' ':
+            analysis.incorrect_spaces += 1
+            print(f'Abnormal spacing. \'{keyword}\', {path}:{line} \
+                --> {line.rstrip()}')
+            if correct:
+                line = m.group(1) + m.group(2) + ' ' + \
+                            m.group(4) + '\n'
+
+    return line
+
+
+def write_file(path, analysis, modified_file, trim, correct):
+    #
+    # Trim and/or correct file, depending on the provided arguments
+    #
+    write_file = False
+    if trim and (analysis.trailing_spaces
+                 or analysis.trailing_lines) != 0:
+        print('Trimming {}...'.format(path))
+        write_file = True
+    if correct and analysis.incorrect_spaces != 0:
+        print('Correcting {}...'.format(path))
+        write_file = True
+    if write_file:
+        analysis.modified_files += 1
+        with open(path, 'w') as file:
+            file.write(modified_file)
+
+
+def check_files(file_paths, regex_patterns, trim, correct):
+    analysis = Analysis()
+    for path in file_paths:
+        partial_analysis = Analysis()
+        content = ''
+        try:
+            with open(path, encoding='utf-8') as file:
+                lines = file.readlines()
+                if lines and not lines[-1].endswith('\n'):
+                    partial_analysis.missing_new_line_files += 1
+                    print(f'{path}: is missing a new line at the end of file')
+
+                for line in lines:
+                    processed_line = check_line(path,
+                                                line,
+                                                regex_patterns,
+                                                partial_analysis,
+                                                trim,
+                                                correct)
+                    content += processed_line
+
+        except UnicodeDecodeError:
+            print(f'Invalid file format {path}')
+
+        if content.endswith('\n\n'):
+            print(f'Blank line at the end of file --> {path}')
+            if trim:
+                content = content.rstrip() + '\n'
+            trailing_lines = len(content) - len(content.rstrip() + '\n')
+            partial_analysis.trailing_lines += trailing_lines
+
+        #
+        # If file.newlines has been set it is either a string with
+        # the determined line ending or a tuple with all the line
+        # endings we have encountered
+        #
+        if file.newlines:
+            if isinstance(file.newlines, tuple):
+                print('{} has mixed line endings'.format(path))
+                partial_analysis.non_unix_eol_files += 1
+            elif file.newlines != '\n':
+                print('{} has non-UNIX line endings'.format(path))
+                partial_analysis.non_unix_eol_files += 1
+
+        write_file(path, partial_analysis, content, trim, correct)
+
+        analysis.add(partial_analysis)
+
+    return analysis
+
+
+def run(trim=False, correct=False):
+    print(banner('Checking for incorrect spacing in the code...'))
+
+    if trim:
+        print('Trim mode is enabled.')
+    if correct:
+        print('Correct mode is enabled.')
+
+    regex_patterns = get_regex_patterns(KEYWORDS)
+    files = get_filtered_files(EXCLUDE_DIRECTORIES, FILE_TYPES)
+    analysis = check_files(files, regex_patterns, trim, correct)
+    print(analysis)
+
+    return not analysis.has_errors()
+
+
+def parse_args(argv, prog_name):
+    parser = argparse.ArgumentParser(
+        prog=prog_name,
+        description='Perform checks for incorrect spacing in the code')
+
+    parser.add_argument('-t', '--trim', dest='trim',
+                        required=False, default=False, action='store_true',
+                        help='Remove trailing spaces.')
+
+    parser.add_argument('-c', '--correct', dest='correct',
+                        required=False, default=False, action='store_true',
+                        help='Correct spaces after keywords.')
+
+    return parser.parse_args(argv)
 
 
 def main(argv=[], prog_name=''):
-    parser = argparse.ArgumentParser(prog=prog_name)
-    parser.add_argument('-t', '--trim',
-                        help='Remove trailing spaces.',
-                        action='store_true',
-                        default=False)
-    parser.add_argument('-c', '--correct',
-                        help='Correct spaces after keywords.',
-                        action='store_true',
-                        default=False)
-    args = parser.parse_args(argv)
-
-    print('Checking for incorrect spacing in the code...')
-    if args.trim:
-        print("Trim mode is enabled.")
-    if args.correct:
-        print("Correct mode is enabled.")
-
-    regex_patterns = dict.fromkeys(KEYWORDS, 0)
-
-    trailing_spaces_count = 0
-    trailing_lines_count = 0
-    correct_spaces_count = 0
-    modified_files = 0
-    non_unix_eol_files = 0
-    missing_new_lines_files = 0
-
-    cwd = os.getcwd()
-    print("Executing from {}".format(cwd))
-
-    for i, directory in enumerate(EXCLUDE_DIRECTORIES):
-        EXCLUDE_DIRECTORIES[i] = os.path.abspath(directory)
-        print("\tAdding to the exclude list: {}"
-              .format(EXCLUDE_DIRECTORIES[i]))
-
-    for root, dirs, files in os.walk(cwd, topdown=True):
-        #
-        # Exclude directories based on the EXCLUDE_DIRECTORIES pattern list
-        #
-        dirs[:] = [d for d in dirs
-                   if os.path.join(root, d) not in EXCLUDE_DIRECTORIES]
-
-        #
-        # Exclude files based on the EXCLUDE pattern list
-        #
-        matches = list()
-        for filename in files:
-            for file_pattern in EXCLUDE:
-                if fnmatch.fnmatch(filename, file_pattern):
-                    matches.append(filename)
-                    break
-        for match in matches:
-            files.remove(match)
-
-        for keyword in KEYWORDS:
-            regex_patterns[keyword] = re.compile(
-                '(.*\\W)(%s)(\\s*)(\\(.*)' % keyword)
-        #
-        # Check files
-        #
-        for filename in files:
-            path = os.path.join(root, filename)
-            content = ''
-            trailing_spaces = 0
-            trailing_lines = 0
-            incorrect_spaces = 0
-
-            with open(path, encoding="utf-8") as file:
-                lines = file.readlines()
-                last_line_number = len(lines)-1
-                for line, string in enumerate(lines):
-                    if line == last_line_number and string[-1] != '\n':
-                        print('{} is missing a new line at the end of file'.
-                              format(path))
-                        missing_new_lines_files += 1
-
-                    # Note that all newlines are converted to '\n',
-                    # so the following will work regardless of
-                    # what the underlying file format is using to
-                    # represent a line break.
-                    if string.endswith(' \n'):
-                        print('{}:{} has trailing space'.format(line, path))
-                        trailing_spaces += 1
-                        if args.trim:
-                            string = string.rstrip()+'\n'
-                    if not is_valid_type(os.path.basename(path)):
-                        content += string
-                        continue
-                    for keyword in KEYWORDS:
-                        key_index = string.find(keyword)
-                        if key_index == -1:
-                            continue
-                        m = regex_patterns[keyword].search(string)
-                        if m and m.group(3) != ' ':
-                            incorrect_spaces += 1
-                            print('Abnormal spacing. "{}", {}:{} --> {}'
-                                  .format(keyword, path, line,
-                                          string.rstrip()))
-                            if args.correct:
-                                string = m.group(1) + m.group(2) + ' ' + \
-                                         m.group(4) + '\n'
-                    content += string
-
-                if content.endswith('\n\n'):
-                    print('Blank line at the end of file --> {}'.format(path))
-                    c_len = len(content)
-                    if args.trim:
-                        content = content.rstrip()+'\n'
-                    trailing_lines += c_len - len(content.rstrip() + '\n')
-                #
-                # If file.newlines has been set it is either a string with
-                # the determined line ending or a tuple with all the line
-                # endings we have encountered
-                #
-                if file.newlines:
-                    if isinstance(file.newlines, tuple):
-                        print('{} has mixed line endings'.format(path))
-                        non_unix_eol_files += 1
-                    elif file.newlines != '\n':
-                        print('{} has non-UNIX line endings'.format(path))
-                        non_unix_eol_files += 1
-
-            #
-            # Trim and/or correct file, depending on the provided arguments
-            #
-            write_file = False
-            if args.trim and (trailing_spaces or trailing_lines) != 0:
-                print("Trimming {}...".format(path))
-                write_file = True
-            if args.correct and incorrect_spaces != 0:
-                print("Correcting {}...".format(path))
-                write_file = True
-            if write_file:
-                modified_files += 1
-                with open(path, 'w') as file:
-                    file.write(content)
-
-            trailing_spaces_count += trailing_spaces
-            trailing_lines_count += trailing_lines
-            correct_spaces_count += incorrect_spaces
-
-    if trailing_spaces_count == 0:
-        print("No trailing spaces found")
-    else:
-        print('{} trailing spaces found.'.format(trailing_spaces_count))
-
-    if trailing_lines_count == 0:
-        print("No trailing lines found")
-    else:
-        print('{} trailing lines found.'.format(trailing_lines_count))
-
-    if correct_spaces_count == 0:
-        print("No abnormal spaces found")
-    else:
-        print('Abnormal spaces found on {} lines.'
-              .format(correct_spaces_count))
-
-    if (args.trim or args.correct) and modified_files:
-        print("{} files modified".format(modified_files))
-
-    if non_unix_eol_files == 0:
-        print("No files with non-UNIX or mixed line endings found")
-    else:
-        print("{} files have non-UNIX or mixed line endings"
-              .format(non_unix_eol_files))
-
-    if missing_new_lines_files == 0:
-        print("No files with missing newlines at EOF found")
-    else:
-        print("{} text files are missing newlines at EOF"
-              .format(missing_new_lines_files))
-
-    if (trailing_spaces_count or
-            trailing_lines_count or
-            correct_spaces_count or
-            non_unix_eol_files or
-            missing_new_lines_files):
-        return 1
-    return 0
+    args = parse_args(argv, prog_name)
+    return 0 if run(args.trim, args.correct) else 1
 
 
 if __name__ == '__main__':
