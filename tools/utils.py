df64bd3569a0c34bce56ad56d26f8ac258b35539
Leandro Belli <leandro.belli@arm.com>
2024-01-12 14:04:20 +0000

    tools/utils: Add function to filter files from the project
    A new function is introduced called `get_filtered_files` to
    recursively find and filter files in the current working directory
    and its subdirectories. It allows to simplify the check scripts.
    
    Signed-off-by: Leandro Belli <leandro.belli@arm.com>
    Change-Id: I10707e4f2cbc53b32194514a543e2d244e18356c

--- tools/utils.py
+++ tools/utils.py
@@ -6,6 +6,8 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import subprocess
+import fnmatch
+import os
 from typing import List, Tuple
 
 
@@ -81,3 +83,64 @@ class Results(List[Tuple[str, int]]):
         )
 
         return out_str
+
+
+def get_excluded_directories(exclude_dir_patterns):
+    """
+    Converts a list of directory names or patterns into a list of their
+    absolute paths, intended to be used as an exclusion list.
+
+    Parameters:
+    exclude_dir_patterns (list): A list of directory names or patterns to
+    exclude. These can be relative or absolute paths.
+
+    Returns:
+    list: A list of absolute paths corresponding to the input directory names
+    or patterns.
+    """
+
+    filtered_dirs = list()
+    for dir_pattern in exclude_dir_patterns:
+        filtered_dirs.append(os.path.abspath(dir_pattern))
+        print(f'\tAdding to the exclude list: {filtered_dirs[-1]}')
+
+    return filtered_dirs
+
+
+def get_filtered_files(exclude_dir_patterns, file_types):
+    """
+    Recursively find and filter files in the current working directory and its
+    subdirectories.
+
+    Parameters:
+    exclude_dir_patterns (list): A list of directory paths patterns to exclude
+    from the search.
+    file_types (list): A list of file patterns to include in the result.
+
+    Returns:
+    list: A list of absolute paths to filtered files.
+    """
+
+    filtered_files = list()
+    exclude_directories = get_excluded_directories(exclude_dir_patterns)
+    cwd = os.getcwd()
+    for root, dirs, files in os.walk(cwd, topdown=True):
+        #
+        # Exclude directories based on the EXCLUDE_DIRECTORIES pattern list
+        #
+        dirs[:] = list(filter(lambda d: os.path.join(root, d) not in
+                              exclude_directories, dirs))
+
+        #
+        # List files based on the FILE_TYPES pattern list
+        #
+        files = list(filter(lambda f: any(fnmatch.fnmatch(f, pattern)
+                            for pattern in file_types), files))
+
+        #
+        # Append all filtered files with the path
+        #
+        filtered_files.extend(list(map(lambda f: os.path.join(root, f),
+                                       files)))
+
+    return filtered_files
