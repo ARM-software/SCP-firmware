985f8ea8e8767fead09fa8caea2136566019cf0c
Leandro Belli <leandro.belli@arm.com>
2024-01-18 17:48:16 +0000

    tools/check_copyright: modularize and improve readability
    This patch simplifies the way the check_copyright.py script works
    by creating helper functions and reducing the number of nested
    for loops. It simplifies the process of retrieving file changes
    and adds the possibility to set the commit hash from which to
    compare.
    
    Signed-off-by: Leandro Belli <leandro.belli@arm.com>
    Change-Id: I722ee086b6d615029fd5013de42cd2017c38660e

--- tools/check_copyright.py
+++ tools/check_copyright.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 # Arm SCP/MCP Software
-# Copyright (c) 2015-2023, Arm Limited and Contributors. All rights reserved.
+# Copyright (c) 2015-2024, Arm Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -13,6 +13,8 @@ This checker supports the following comment styles:
     ; Used by .s (ARM toolchain) and .scat (scatter file) files
     # Used by Makefile (including .mk), .py (Python) and dxy (Doxygen) files
 """
+
+import argparse
 import os
 import fnmatch
 import re
@@ -20,21 +22,21 @@ import sys
 import datetime
 import subprocess
 import glob
+from dataclasses import dataclass, asdict
 from itertools import islice
+from utils import banner, get_filtered_files
 
 #
 # Directories to exclude
 #
 
-# Exclude all mod_test "mocks" directories
+# Exclude all mod_test 'mocks' directories
 UNIT_TEST_MOCKS = glob.glob('module/**/test/**/mocks', recursive=True)
 
 EXCLUDE_DIRECTORIES = [
     '.git',
     'build',
-    'contrib/cmsis/git',
-    "contrib/run-clang-format/git",
-    "contrib/cmock/git",
+    'contrib',
     'product/rcar/src/CMSIS-FreeRTOS',
     'unit_test/unity_mocks',
 ] + UNIT_TEST_MOCKS
@@ -62,25 +64,13 @@ FILE_TYPES = [
 # Supported comment styles (Python regex)
 #
 COMMENT_PATTERN = '^(( \\*)|(;)|(\\#))'
-COMPANY_PATTERN = '(Arm|Renesas|Linaro)'
+COMPANY_PATTERN = '(Arm|'\
+                  'Renesas|'\
+                  'Linaro)'
 COMPANY_FULL_NAME_PATTERN = \
-    '(Arm Limited and Contributors|Renesas Electronics Corporation|'\
+    '(Arm Limited and Contributors|'\
+    'Renesas Electronics Corporation|'\
     'Linaro Limited and Contributors)'
-
-#
-# git command using diff-filter to include Added (A), Copied (C), Modified (M),
-# Renamed (R), type changed (T), Unmerged (U), Unknown (X) files
-# Deleted files (D) are not included
-#
-GIT_CMD = \
-    'git diff-tree --name-only --no-commit-id -r --diff-filter=ACMRTUX HEAD'
-
-#
-# git log command using raw provides changed entries along with a field for
-# rename or copy and its score. For example, R100 denotes a file renamed with
-# 100% similarity. It behaves similary to git whatchanged.
-#
-GIT_WHATCHANGED_CMD = 'git log --raw HEAD -1'
 
 #
 # License pattern to match
@@ -106,134 +96,138 @@ LICENSE_PATTERN = \
 HEAD_LINE_COUNT = 10
 
 
-class ErrorYear(Exception):
-    pass
+@dataclass
+class Analysis:
+    error_copyright: int = 0
+    error_year_format: int = 0
+    error_current_year: int = 0
+
+    def has_errors(self):
+        return any([value for value in asdict(self).values()])
+
+    def append(self, partial_analysis):
+        for elem, count in asdict(partial_analysis).items():
+            if hasattr(self, elem):
+                setattr(self, elem, getattr(self, elem) + count)
+            else:
+                # Handle the error or ignore
+                print(f'Warning: Attribute {elem} not found.')
+        return self
+
+    def __str__(self):
+        msg = ''
+        msg += self._str_message('files with invalid copyright',
+                                 self.error_copyright)
+        msg += self._str_message('files with invalid year(s) format',
+                                 self.error_year_format)
+        msg += self._str_message('files with incorrect year ranges',
+                                 self.error_current_year)
+        return msg
+
+    def _str_message(self, name, count) -> str:
+        return f'- {count} {name}.\n'
 
 
-class ErrorCopyright(Exception):
-    pass
+def get_changed_files(commit_range):
+    """
+    Get a list of changed files for the given commit range.
+    """
+    #
+    # git command using diff-filter to include Added (A), Copied (C),
+    # Modified (M), Renamed (R), type changed (T), Unmerged (U), Unknown (X)
+    # files Deleted files (D) are not included
+    #
+    diff_cmd = f'git diff --name-status {commit_range} --diff-filter=ACMRTUX'
+
+    try:
+        result = subprocess.Popen(
+            diff_cmd,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError as e:
+        print('ERROR ' + e.returncode + ': Failed to get changed files')
+        return None
+
+    result_out, _ = result.communicate()
+    result_out = result_out.decode('utf-8').strip().split('\n')
+    result_out = list(map(lambda diff: diff.split('\t'), result_out))
+
+    # Remove files renamed with 100% similarity. R100 denotes this.
+    result_out = list(filter(lambda diff: diff[0] != 'R100', result_out))
+
+    # Get modified file and convert to absolute path
+    current_directory = os.getcwd()
+    return list(map(
+        lambda diff: os.path.abspath(os.path.join(current_directory,
+                                                  diff[-1])), result_out))
 
 
-class ErrorYearNotCurrent(Exception):
-    pass
-
-
-def is_valid_directory(filename):
-    for dir in EXCLUDE_DIRECTORIES:
-        if filename.startswith(dir):
-            return False
-    return True
-
-
-def is_valid_file_type(filename):
-    for file_type in FILE_TYPES:
-        if fnmatch.fnmatch(filename, file_type):
-            return True
-    return False
-
-
-def is_file_renamed_identical(filename, identical_files_list):
-    for identical_file_str in identical_files_list:
-        if filename in identical_file_str:
-            return True
-
-    return False
-
-
-def check_copyright(pattern, filename):
-    with open(filename, encoding="utf-8") as file:
+def check_copyright(filename, pattern, analysis):
+    with open(filename, encoding='utf-8') as file:
         # Read just the first HEAD_LINE_COUNT lines of a file
-        head_lines = islice(file, HEAD_LINE_COUNT)
-        head = ''
-        for line in head_lines:
-            head += line
+        head = ''.join(islice(file, HEAD_LINE_COUNT))
 
         match = pattern.search(head)
         if not match:
-            raise ErrorCopyright
+            print(f'{filename}: Invalid copyright header.')
+            analysis.error_copyright += 1
+            return
 
         years = match.group('years').split('-')
         if len(years) > 1:
             if years[0] > years[1]:
-                raise ErrorYear
+                print(f'{filename}: Invalid year format.')
+                analysis.eror_year_format += 1
+                return
 
         now = datetime.datetime.now()
         final_year = len(years) - 1
         if int(years[final_year]) != now.year:
-            raise ErrorYearNotCurrent
+            print(f'{filename}: Outdated copyright year range.')
+            analysis.error_current_year += 1
+            return
 
 
-def main():
+def run(commit_hash):
+    print(banner('Checking the copyrights in the code...'))
+
+    changed_files = get_changed_files(commit_hash)
+    if len(changed_files) == 0:
+        return False
+
+    filtered_files = get_filtered_files(EXCLUDE_DIRECTORIES, FILE_TYPES)
+    changed_files = list(set(changed_files) & set(filtered_files))
+
     pattern = re.compile(LICENSE_PATTERN, re.MULTILINE)
-    error_year_count = 0
-    error_copyright_count = 0
-    error_incorrect_year_count = 0
+    analysis = Analysis()
 
-    print("Checking the copyrights in the code...")
+    for file in changed_files:
+        partial_analysis = Analysis()
+        check_copyright(file, pattern, partial_analysis)
+        analysis.append(partial_analysis)
 
-    cwd = os.getcwd()
-    print("Executing from {}".format(cwd))
-
-    try:
-        result = subprocess.Popen(
-            GIT_CMD,
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
-    except subprocess.CalledProcessError as e:
-        print("ERROR " + e.returncode + ": Failed to get last changed files")
-        return 1
-
-    try:
-        result_wch = subprocess.Popen(
-            GIT_WHATCHANGED_CMD,
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
-    except subprocess.CalledProcessError as e:
-        print("ERROR " + e.returncode + ": Failed to get last changed files")
-        return 1
-
-    wch_outs, wch_errs = result_wch.communicate()
-    identical_files = [line
-                       for line in wch_outs.decode("utf-8").split('\n')
-                       if " R100" in line]
-
-    for line in result.stdout:
-        filename = line.decode("utf-8").strip('\n')
-
-        if (is_valid_file_type(filename) and
-                is_valid_directory(filename) and
-                not is_file_renamed_identical(filename, identical_files)):
-            try:
-                check_copyright(pattern, filename)
-            except ErrorYear:
-                print("{}: Invalid year format.".format(filename))
-                error_year_count += 1
-
-            except ErrorCopyright:
-                print("{}: Invalid copyright header.".format(filename))
-                error_copyright_count += 1
-
-            except ErrorYearNotCurrent:
-                print("{}: Outdated copyright year range.".
-                      format(filename))
-                error_incorrect_year_count += 1
-
-    if error_year_count != 0 or error_copyright_count != 0 or \
-       error_incorrect_year_count != 0:
-        print("\t{} files with invalid year(s) format."
-              .format(error_year_count))
-        print("\t{} files with invalid copyright."
-              .format(error_copyright_count))
-        print("\t{} files with incorrect year ranges."
-              .format(error_incorrect_year_count))
-
-        return 1
-    else:
-        print("Check copyright - No errors found.")
-        return 0
+    print(analysis)
+    return not analysis.has_errors()
 
 
-if __name__ == "__main__":
-    sys.exit(main())
+def parse_args(argv, prog_name):
+    parser = argparse.ArgumentParser(
+        prog=prog_name,
+        description='Perform code style check the correct license header')
+
+    parser.add_argument('-c', '--commit', dest='commit_hash',
+                        required=False, default='HEAD', type=str,
+                        action='store', help='Specify a commit hash. If not \
+                        specified, defaults to the previous commit.')
+
+    return parser.parse_args(argv)
+
+
+def main(argv=[], prog_name=''):
+    args = parse_args(argv, prog_name)
+    return 0 if run(args.commit_hash) else 1
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:], sys.argv[0]))
