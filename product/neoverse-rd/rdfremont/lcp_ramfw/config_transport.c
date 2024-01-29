370e4e42ebed824d88ffacebc74e1e3279510f5b
Pranav Madhu <pranav.madhu@arm.com>
2024-01-29 09:21:01 +0000

    neoverse-rd/rdfremont: update transport module config data in lcpfw
    Update transport module config data for LCP with AP-LCP fastchannel for
    DVFS and remove the SCP-LCP transport channel as it is not used at this
    stage. Also, set the CMake flag
    SCP_TARGET_EXCLUDE_SCMI_PERF_PROTOCOL_OPS to enable fast channel
    support.
    
    Signed-off-by: Pranav Madhu <pranav.madhu@arm.com>
    Change-Id: If0825f6a1083ac5d9ac782a010262c39c5598722

--- product/neoverse-rd/rdfremont/lcp_ramfw/config_transport.c
+++ product/neoverse-rd/rdfremont/lcp_ramfw/config_transport.c
@@ -7,7 +7,9 @@
 
 #include <lcp_css_mmap.h>
 #include <lcp_mhu3.h>
+#include <lcp_platform_transport.h>
 
+#include <mod_mhu3.h>
 #include <mod_transport.h>
 
 #include <fwk_element.h>
@@ -18,22 +20,24 @@
 #include <stdint.h>
 
 static const struct fwk_element transport_element_table[] = {
-    [0] = {
-        .name = "LCP2SCP_TRANSPORT",
+    [LCP_PLATFORM_TRANSPORT_IDX_DVFS_SET_LVL] = {
+        .name = "AP2LCP_TRANSPORT DVFS SET LEVEL",
         .data = &((
             struct mod_transport_channel_config) {
-#ifdef BUILD_HAS_FAST_CHANNELS
             .transport_type = MOD_TRANSPORT_CHANNEL_TRANSPORT_TYPE_FAST_CHANNELS,
-#endif
-            .channel_type = MOD_TRANSPORT_CHANNEL_TYPE_REQUESTER,
+            .channel_type = MOD_TRANSPORT_CHANNEL_TYPE_COMPLETER,
             .driver_id =
                 FWK_ID_SUB_ELEMENT_INIT(
                     FWK_MODULE_IDX_MHU3,
                     MHU3_DEVICE_IDX_LCP_AP_FCH_DVFS_SET_LVL,
                     0),
+            .driver_api_id =
+                FWK_ID_API_INIT(
+                    FWK_MODULE_IDX_MHU3,
+                    MOD_MHU3_API_IDX_TRANSPORT_DRIVER),
         }),
     },
-    [1] = { 0 },
+    [LCP_PLATFORM_TRANSPORT_IDX_COUNT] = { 0 },
 };
 
 static const struct fwk_element *transport_get_element_table(fwk_id_t module_id)
---
+++ product/neoverse-rd/rdfremont/lcp_ramfw/include/lcp_platform_transport.h
@@ -0,0 +1,20 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2023-2024, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Definitions for Transport module configuration.
+ */
+
+#ifndef PLATFORM_TRANSPORT_H
+#define PLATFORM_TRANSPORT_H
+
+/* Transport indices */
+enum lcp_platform_transport_idx {
+    LCP_PLATFORM_TRANSPORT_IDX_DVFS_SET_LVL,
+    LCP_PLATFORM_TRANSPORT_IDX_COUNT,
+};
+
+#endif /* PLATFORM_TRANSPORT_H */
