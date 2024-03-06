b5e2d89d1281389b75fad5b051578b10d816fcfa
Wen Ping Teh <wenping.teh@arm.com>
2024-03-06 13:21:42 +0000

    optee: Move optee files into a new parent directory
    Directory structure clean-up for better differentiation of Arm and
    non-Arm developed code. Optee files are moved into a common parent
    directory under product/.
    
    unit_test: Remove optee/mbx from unit_test/CMakeLists.txt list of
    modules because it's unit test is moved to product/. optee/mbx unit
    test will be added back under product/ in another new patch.
    
    Signed-off-by: Wen Ping Teh <wenping.teh@arm.com>
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Change-Id: I9440ca518dee25bc73c09b863f32c65f4a858675

--- product/optee/optee-fvp/fw/Toolchain-GNU.cmake
+++ product/optee/optee-fvp/fw/Toolchain-GNU.cmake
@@ -1,6 +1,6 @@
 #
 # Arm SCP/MCP Software
-# Copyright (c) 2022, Linaro Limited and Contributors. All rights reserved.
+# Copyright (c) 2022-2024, Linaro Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -15,7 +15,7 @@ set(CMAKE_C_COMPILER_TARGET ${CFG_CROSS_COMPILE})
 set(CMAKE_CXX_COMPILER_TARGET ${CFG_CROSS_COMPILE})
 
 include(
-    "${CMAKE_CURRENT_LIST_DIR}/../../../cmake/Toolchain/GNU-Baremetal.cmake")
+    "${CMAKE_CURRENT_LIST_DIR}/../../../../cmake/Toolchain/GNU-Baremetal.cmake")
 
 foreach(language IN ITEMS ASM C CXX)
     string(APPEND CMAKE_${language}_FLAGS_INIT ${CFG_CFLAGS_OPTEE})
