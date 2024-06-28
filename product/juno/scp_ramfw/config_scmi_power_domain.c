d5f955445f4fe84811bdd3765d5893dbb86d574e
Abhishek Rawat <abhishek.rawat@arm.com>
2024-06-28 15:13:36 +0100

    juno: Add per agent mapping in SCMI Power protocol
    This change defines the per agent mapping config for OSPM
    and PSCI in SCMI Power protocol. It only introduces the mapping
    for JUNO and not change the behaviour as all ten power domains
    are still exposed to both the agents.
    
    Signed-off-by: Abhishek Rawat <abhishek.rawat@arm.com>
    Change-Id: I2469b7da83742b2c4e5c41da7654a4836bc790d2

--- product/juno/scp_ramfw/config_scmi_power_domain.c
+++ product/juno/scp_ramfw/config_scmi_power_domain.c
@@ -1,11 +1,12 @@
 /*
  * Arm SCP/MCP Software
- * Copyright (c) 2020-2021, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2020-2024, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #include <config_power_domain.h>
+#include <juno_scmi.h>
 
 #include <mod_scmi_power_domain.h>
 
@@ -14,15 +15,53 @@
 #include <fwk_module.h>
 #include <fwk_module_idx.h>
 
-#ifdef BUILD_HAS_MOD_DEBUG
-struct fwk_module_config config_scmi_power_domain = {
-    .data = &((struct mod_scmi_pd_config) {
-        .debug_pd_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_POWER_DOMAIN,
-                                           POWER_DOMAIN_IDX_DBGSYS),
-        .debug_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_DEBUG, 0),
-    }),
+#ifdef BUILD_HAS_AGENT_LOGICAL_DOMAIN
+struct mod_scmi_pd_agent_config agent_table[3] = {
+    [JUNO_SCMI_AGENT_IDX_OSPM] = {
+        .domain_count = 10,
+        .domains = (uint32_t [10]) {
+            [0] = POWER_DOMAIN_IDX_BIG_CPU0,
+            [1] = POWER_DOMAIN_IDX_BIG_CPU1,
+            [2] = POWER_DOMAIN_IDX_LITTLE_CPU0,
+            [3] = POWER_DOMAIN_IDX_LITTLE_CPU1,
+            [4] = POWER_DOMAIN_IDX_LITTLE_CPU2,
+            [5] = POWER_DOMAIN_IDX_LITTLE_CPU3,
+            [6] = POWER_DOMAIN_IDX_BIG_SSTOP,
+            [7] = POWER_DOMAIN_IDX_LITTLE_SSTOP,
+            [8] = POWER_DOMAIN_IDX_DBGSYS,
+            [9] = POWER_DOMAIN_IDX_GPUTOP,
+        },
+    },
+
+    [JUNO_SCMI_AGENT_IDX_PSCI] = {
+        .domain_count = 10,
+        .domains = (uint32_t [10]) {
+            [0] = POWER_DOMAIN_IDX_BIG_CPU0,
+            [1] = POWER_DOMAIN_IDX_BIG_CPU1,
+            [2] = POWER_DOMAIN_IDX_LITTLE_CPU0,
+            [3] = POWER_DOMAIN_IDX_LITTLE_CPU1,
+            [4] = POWER_DOMAIN_IDX_LITTLE_CPU2,
+            [5] = POWER_DOMAIN_IDX_LITTLE_CPU3,
+            [6] = POWER_DOMAIN_IDX_BIG_SSTOP,
+            [7] = POWER_DOMAIN_IDX_LITTLE_SSTOP,
+            [8] = POWER_DOMAIN_IDX_DBGSYS,
+            [9] = POWER_DOMAIN_IDX_GPUTOP,
+        },
+    },
 };
-#else
-/* No elements, no module configuration data */
-struct fwk_module_config config_scmi_power_domain = { 0 };
 #endif
+
+struct fwk_module_config config_scmi_power_domain = {
+    .data =
+        &(struct mod_scmi_pd_config){
+#ifdef BUILD_HAS_MOD_DEBUG
+            .debug_pd_id = FWK_ID_ELEMENT_INIT(
+                FWK_MODULE_IDX_POWER_DOMAIN,
+                POWER_DOMAIN_IDX_DBGSYS),
+            .debug_id = FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_DEBUG, 0),
+#endif
+#ifdef BUILD_HAS_AGENT_LOGICAL_DOMAIN
+            .agent_config_table = agent_table,
+#endif
+        },
+};
