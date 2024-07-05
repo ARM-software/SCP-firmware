d7cd4e06077972fb9923080f0530ee50ada2b4a7
Wen Ping Teh <wenping.teh@arm.com>
2024-07-05 17:02:28 +0100

    ci/filter: Filter cmock and cmsis from coverage report
    Modify filter_coverage_report.py to filter out cmock and cmsis from
    coverage report. A report can have their own filter by appending
    the 'regex_list'. This is done by using get() method which will return
    the default regex if filename is not found.
    
    Update a few variables naming to PEP8 style guide.
    
    Signed-off-by: Wen Ping Teh <wenping.teh@arm.com>

--- tools/filter_coverage_report.py
+++ tools/filter_coverage_report.py
@@ -13,45 +13,39 @@ from argparse import RawDescriptionHelpFormatter
 from collections import namedtuple
 
 """
-Filters out test and mock code from coverage report so that
-only actual SCP code is evaluated
+Filters out cmock and cmsis from coverage report so that only actual SCP
+code is evaluated. A report can have their own filter by appending
+the 'regex_list'.
 """
 
-# Append new report filename here.
-all_filename = [
-    'scp_v2_fwk_test_coverage.info',
-    'scp_v2_unit_test_coverage.info',
-    'scp_v2_product_test_coverage.info',
-]
+default_regex = [re.compile(r"contrib/cmock/"),
+                 re.compile(r"contrib/cmsis/")]
 
-# Append new report regex here.
-regex_list = [
-    [re.compile(r"framework/test/"),
-     ],
-    [re.compile(r"contrib/cmock/"),
-     re.compile(r"module/.*/test"),
-     re.compile(r"unit_test/"),
-     ],
-    [re.compile(r"contrib/cmock/"),
-     re.compile(r"framework/"),
-     re.compile(r"product/.*/test"),
-     re.compile(r"unit_test/"),
-     ],
-]
+# Append specific files filter.
+regex_list = {
+    'scp_v2_fwk_test_coverage.info': [re.compile(r"framework/test/")],
+    'scp_v2_unit_test_coverage.info': [re.compile(r"contrib/cmock/"),
+                                       re.compile(r"module/.*/test"),
+                                       re.compile(r"unit_test/")],
+    'scp_v2_product_test_coverage.info': [re.compile(r"contrib/cmock/"),
+                                          re.compile(r"framework/"),
+                                          re.compile(r"product/.*/test"),
+                                          re.compile(r"unit_test/")],
+}
 
 STARTER = "SF:"
 ENDED = "end_of_record"
 
 
-def get_filtered_info(FileName):
+def get_filtered_info(filename):
     within = False
     filter = False
     dump_lines = []
-    regexes = regex_list[all_filename.index(FileName)]
-    OutFileName = FileName.replace('.info', '_filtered.info')
+    regexes = regex_list.get(filename, default_regex)
+    out_filename = filename.replace('.info', '_filtered.info')
 
-    with open(OutFileName, "w") as fd:
-        with open(FileName, "r") as fd_in:
+    with open(out_filename, "w") as fd:
+        with open(filename, "r") as fd_in:
             for line in fd_in.readlines():
 
                 if STARTER in line:
@@ -70,37 +64,27 @@ def get_filtered_info(FileName):
                     dump_lines = []
                     within = False
                     filter = False
-
-
-def run(FileName):
-    if FileName not in all_filename:
-        print('ERROR: Filename not found. Please use supported filename:')
-        print(*all_filename, sep='\n')
-        return False
-
-    get_filtered_info(FileName)
     return True
 
 
 def parse_args(argv, prog_name):
     parser = argparse.ArgumentParser(
         prog=prog_name,
-        description='Filters out test and mock code from coverage report\n'
-                    'To add new coverage report, please append the report '
-                    'filename and regex in this script.',
+        description='Filters out cmock and cmsis from coverage report\n'
+                    'To filter specific regex for a report, please append '
+                    'the regex list in this script.',
         formatter_class=RawDescriptionHelpFormatter)
 
-    parser.add_argument('-f', '--filename', dest='FileName', required=True,
+    parser.add_argument('-f', '--filename', dest='filename', required=True,
                         type=str, action='store', help=f'Required argument.\
-                        Filename of the coverage report to be filtered. \
-                        Supported filename: {all_filename}')
+                        Filename of the coverage report to be filtered.')
 
     return parser.parse_args(argv)
 
 
 def main(argv=[], prog_name=''):
     args = parse_args(argv, prog_name)
-    return 0 if run(args.FileName) else 1
+    return 0 if get_filtered_info(args.filename) else 1
 
 
 if __name__ == '__main__':
