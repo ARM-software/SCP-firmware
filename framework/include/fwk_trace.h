813d518e23fcc57704bde5db84c2576bc2f58ea6
Mohamed Omar Asaker <mohamed.omarasaker@arm.com>
2024-07-16 15:21:04 +0100

    fwk/trace: Add tracing mechanism
    Similar to fwk Timer adding a fwk trace mechanism.
    Which depends on trace driver that should be provided by product.
    
    The driver should provide functions to
    - Get trace count which get current timestamp or cycle from the start.
    - Report trace entry which dumps the entry data.
    
    Signed-off-by: Mohamed Omar Asaker <mohamed.omarasaker@arm.com>

---
+++ framework/include/fwk_trace.h
@@ -0,0 +1,138 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2024, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef FWK_TRACE_H
+#define FWK_TRACE_H
+
+#include <inttypes.h>
+#include <stdint.h>
+
+/*!
+ * \addtogroup GroupLibFramework
+ * \defgroup GroupTime Trace operations.
+ *
+ * \details This component adds support for various trace operations.
+ *
+ * \{
+ */
+
+/*!
+ * \brief Start trace of an event.
+ *
+ * \param[in] ID Event ID.
+ * \return Status code representing the result of the operation.
+ */
+#define FWK_TRACE_START(ID) fwk_trace_start(ID)
+
+/*!
+ * \brief Finish trace of an event.
+ *
+ * \param[in] ID Event ID.
+ * \return Status code representing the result of the operation.
+ */
+#define FWK_TRACE_FINISH(ID, MSG) \
+    fwk_trace_finish(__FILE__, __func__, __LINE__, ID, MSG)
+
+typedef uint64_t fwk_trace_count_t;
+typedef uint32_t fwk_trace_id_t;
+#define PRItraceid    PRIu32
+#define PRItracecount PRIu64
+
+/*!
+ * \brief Trace driver
+ */
+struct fwk_trace_driver {
+    /*! The number of the trace entries. */
+    unsigned int trace_entry_count;
+
+    /*!
+     * \brief Get current cycle/timestamp count
+     *
+     * \return trace count
+     */
+    fwk_trace_count_t (*get_trace_count)(void);
+
+    /*!
+     * \brief Report trace entry
+     *
+     * \param[in] filename File name where end of trace occured.
+     * \param[in] func Function where end of trace occured.
+     * \param[in] line Line number where end of trace occured.
+     * \param[in] id ID of the event which trace ended.
+     * \param[in] trace_count Count of how many time units passed along the
+     *                        event.
+     * \param[in] msg optional message to add to the trace entry.
+     *
+     */
+    void (*report_trace_entry)(
+        const char *filename,
+        const char *func,
+        const unsigned int line,
+        const fwk_trace_id_t id,
+        const fwk_trace_count_t trace_count,
+        const char *msg);
+};
+
+/*!
+ * \brief Initialize tracing
+ * Note: This function has Constructor attribute which adds it to the list of
+ * function executes before `main`.
+ */
+void fwk_trace_init(void);
+
+/*!
+ * \brief Start trace of an event.
+ *
+ * \param[in] id Event ID.
+ * \return Status code representing the result of the operation.
+ */
+int fwk_trace_start(const fwk_trace_id_t id);
+
+/*!
+ * \brief Finish trace of an event.
+ *
+ * \param[in] filename Name of the file in which the tracing finished.
+ *                     use __FILE__ macro.
+ * \param[in] line Line number in which the tracing finished.
+ *                 use __LINE__ macro.
+ * \param[in] func Name of the function in which the fwk_trace_finish is called.
+ *                 use __func__ macro.
+ * \param[in] id Event ID.
+ * \param[in] msg An optional message.
+ *
+ * \note id must match a started tracing point to report the count spent between
+ *       the tracing points. The rest of the params helps in reporting, and it
+ *       is left on the driver how it uses them.
+ *
+ * \return Status code representing the result of the operation.
+ */
+int fwk_trace_finish(
+    const char *filename,
+    const char *func,
+    const unsigned int line,
+    const fwk_trace_id_t id,
+    const char *msg);
+
+/*!
+ * \brief Register a framework trace driver.
+ *
+ * \details
+ *
+ *      This is a weak function provided by the framework that, by default, does
+ *      not register a driver, and should be overridden by the firmware if you
+ *      wish to provide one.
+ *
+ *
+ * \return Framework trace driver.
+ */
+struct fwk_trace_driver fmw_trace_driver(void);
+
+/*!
+ * \}
+ */
+
+#endif /* FWK_TRACE_H */
---
+++ framework/src/fwk_trace.c
@@ -0,0 +1,104 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2024, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_attributes.h>
+#include <fwk_log.h>
+#include <fwk_mm.h>
+#include <fwk_string.h>
+#include <fwk_trace.h>
+
+#include <string.h>
+
+static struct {
+    struct fwk_trace_driver driver; /* Trace driver */
+    fwk_trace_count_t *start_timestamp;
+    bool *entry_pending;
+} fwk_trace_ctx;
+
+FWK_CONSTRUCTOR void fwk_trace_init(void)
+{
+    struct fwk_trace_driver driver = fmw_trace_driver();
+    (void)fwk_str_memcpy(&fwk_trace_ctx.driver, &driver, sizeof(driver));
+    if (fwk_trace_ctx.driver.trace_entry_count != 0) {
+        fwk_trace_ctx.start_timestamp = fwk_mm_calloc(
+            fwk_trace_ctx.driver.trace_entry_count, sizeof(fwk_trace_count_t));
+        fwk_trace_ctx.entry_pending = fwk_mm_calloc(
+            fwk_trace_ctx.driver.trace_entry_count, sizeof(fwk_trace_count_t));
+    }
+}
+
+static inline fwk_trace_count_t calc_delta(
+    fwk_trace_count_t start,
+    fwk_trace_count_t end)
+{
+    return (end >= start) ? (end - start) :
+                            ((fwk_trace_count_t)(-1) - start + end);
+}
+
+int fwk_trace_start(fwk_trace_id_t id)
+{
+    if (id >= fwk_trace_ctx.driver.trace_entry_count) {
+        FWK_LOG_ERR("id is not valid");
+        return FWK_E_PARAM;
+    }
+    if (fwk_trace_ctx.entry_pending[id] == true) {
+        FWK_LOG_ERR("tracing id 0x%" PRItraceid " has already started", id);
+        return FWK_E_STATE;
+    }
+    if (fwk_trace_ctx.driver.get_trace_count == NULL) {
+        FWK_LOG_ERR("start trace driver is not set!");
+        return FWK_E_DEVICE;
+    }
+    fwk_trace_ctx.entry_pending[id] = true;
+    fwk_trace_ctx.start_timestamp[id] = fwk_trace_ctx.driver.get_trace_count();
+
+    return FWK_SUCCESS;
+}
+
+int fwk_trace_finish(
+    const char *filename,
+    const char *func,
+    const unsigned int line,
+    fwk_trace_id_t id,
+    const char *msg)
+{
+    if (id >= fwk_trace_ctx.driver.trace_entry_count) {
+        FWK_LOG_ERR("id is not valid");
+        return FWK_E_PARAM;
+    }
+    if (fwk_trace_ctx.entry_pending[id] == false) {
+        FWK_LOG_ERR(
+            "%s:%u: tracing id 0x%" PRItraceid " has not been started",
+            func,
+            line,
+            id);
+        return FWK_E_STATE;
+    }
+    fwk_trace_ctx.entry_pending[id] = false;
+    if (fwk_trace_ctx.driver.get_trace_count == NULL ||
+        fwk_trace_ctx.driver.report_trace_entry == NULL) {
+        FWK_LOG_ERR("finish trace driver is not set!");
+        return FWK_E_DEVICE;
+    }
+    fwk_trace_count_t trace_count = calc_delta(
+        fwk_trace_ctx.start_timestamp[id],
+        fwk_trace_ctx.driver.get_trace_count());
+    fwk_trace_ctx.start_timestamp[id] = 0;
+    fwk_trace_ctx.driver.report_trace_entry(
+        filename, func, line, id, trace_count, msg);
+
+    return FWK_SUCCESS;
+}
+
+FWK_WEAK struct fwk_trace_driver fmw_trace_driver(void)
+{
+    return (struct fwk_trace_driver){
+        .trace_entry_count = 0,
+        .get_trace_count = NULL,
+        .report_trace_entry = NULL,
+    };
+}
