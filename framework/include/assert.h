3c4305d916c2c6dd650c030e8740fc1c723cda19
Wei-Chia Su <Wei-Chia.Su@arm.com>
2024-08-07 10:47:01 +0100

    cmake: Terminate Include What You Use (IWYU) support
    This commit removes the dependencies of IWYU including pragmas in source file.
    
    Signed-off-by: Wei-Chia Su <Wei-Chia.Su@arm.com>

--- framework/include/assert.h
+++ framework/include/assert.h
@@ -1,6 +1,6 @@
 /*
  * Arm SCP/MCP Software
- * Copyright (c) 2020-2021, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2020-2024, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -8,7 +8,7 @@
 #ifndef ASSERT_H
 #define ASSERT_H
 
-#include_next <assert.h> /* IWYU pragma: keep */
+#include_next <assert.h>
 
 #ifndef static_assert
 #    define static_assert _Static_assert
